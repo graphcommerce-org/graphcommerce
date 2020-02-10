d7ee07c1358e46fa96caf887b11654cd97cab573
Paul Hachmang <paul@reachdigital.nl>
2020-02-10 21:49:30 +0100

    Codegen: Allow Maybe values to be null

--- generated/graphql.ts
+++ generated/graphql.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 import * as ApolloReactCommon from '@apollo/react-common'
 import * as ApolloReactHooks from '@apollo/react-hooks'
 
-export type Maybe<T> = T
+export type Maybe<T> = T | null
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string
