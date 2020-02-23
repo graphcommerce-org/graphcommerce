9d2ed61f2ab2edee1dfd79f5504f56e5ab08a3c0
Paul Hachmang <paul@reachdigital.nl>
2020-02-23 13:18:53 +0100

    Remove aliases

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-// This file only exists for eslint-plugin-resolver-webpack
-// https://github.com/benmosher/eslint-plugin-import/issues/1286
-const alias = require('./next.config-alias')
-
-module.exports = {
-  resolve: {
-    alias,
-  },
-}
