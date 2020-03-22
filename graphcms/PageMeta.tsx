44c6057f4068e87bf7ef6c4e655a2ebdb5dbfc9f
Paul Hachmang <paul@reachdigital.nl>
2020-03-22 18:42:21 +0100

    fix(seo): simplified head tag

--- graphcms/PageMeta.tsx
+++ graphcms/PageMeta.tsx
@@ -5,35 +5,23 @@ import { getCanonical } from './Link'
 const PageMeta: React.FC<GraphCmsPageProps> = ({ page }) => {
   if (!page) return <></>
 
-  let hrefLang
-  if (isPageNlHasEn(page)) {
-    hrefLang = (
-      <>
-        <link rel='alternate' hrefLang='nl' href={getCanonical(page.url!)} />
-        <link rel='alternate' hrefLang='en' href={getCanonical(page.urlEN!)} />
-        <link rel='alternate' hrefLang='x-default' href={getCanonical(page.url!)} />
-      </>
-    )
-  } else if (isPageEnHasNl(page)) {
-    hrefLang = (
-      <>
-        <link rel='alternate' hrefLang='en' href={getCanonical(page.url!)} />
-        <link rel='alternate' hrefLang='nl' href={getCanonical(page.urlNL!)} />
-        <link rel='alternate' hrefLang='x-default' href={getCanonical(page.urlNL!)} />
-      </>
-    )
-  }
-
   return (
-    <>
-      <Head>
-        <title>{page.metaTitle}</title>
-        <meta name='description' content={page.metaDescription || ''} />
-        <meta name='robots' content={page.metaRobots} />
-        <link rel='canonical' href={getCanonical(page.url!)} />
-        {hrefLang}
-      </Head>
-    </>
+    <Head>
+      <title>{page.metaTitle}</title>
+      <meta name='description' content={page.metaDescription || ''} />
+      <meta name='robots' content={page.metaRobots} />
+      <link rel='canonical' href={getCanonical(page.url!)} />
+      {isPageNlHasEn(page) && [
+        <link rel='alternate' hrefLang='nl' href={getCanonical(page.url!)} />,
+        <link rel='alternate' hrefLang='en' href={getCanonical(page.urlEN!)} />,
+        <link rel='alternate' hrefLang='x-default' href={getCanonical(page.url!)} />,
+      ]}
+      {isPageEnHasNl(page) && [
+        <link rel='alternate' hrefLang='en' href={getCanonical(page.url!)} />,
+        <link rel='alternate' hrefLang='nl' href={getCanonical(page.urlNL!)} />,
+        <link rel='alternate' hrefLang='x-default' href={getCanonical(page.urlNL!)} />,
+      ]}
+    </Head>
   )
 }
 
