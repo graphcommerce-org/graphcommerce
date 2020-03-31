73211bb21f91f20defcf2ee1833a9df54c1ce197
Paul Hachmang <paul@reachdigital.nl>
2020-03-31 13:39:10 +0200

    refactor(rowcompanyslider): removed Slide component

--- components/RowCompanySlider/index.tsx
+++ components/RowCompanySlider/index.tsx
@@ -6,16 +6,12 @@ import { ImageTypes } from '../PictureResponsive'
 import { ScrollSnapSlider } from '../ScrollSnapSlider'
 import Container from '../Container'
 
-const Slide = styled(FilestackPicture)({
-  // width: '225px',
-})
-
 const RowCompanySlider: React.FC<GQLRowCompanySliderFragment> = ({ companies }) => {
   return (
     <Container>
       <ScrollSnapSlider>
         {companies.map(company => (
-          <Slide
+          <FilestackPicture
             key={company.id}
             width={Math.round((company.logo.width! / company.logo.height!) * 100)}
             height={100}
