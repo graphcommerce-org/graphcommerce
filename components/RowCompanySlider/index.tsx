1a20bfc5d9604738462fe5fcdb2b3c2122280af7
Paul Hachmang <paul@reachdigital.nl>
2020-03-31 13:14:28 +0200

    fix(rowcompanyslider): using container for center align

--- components/RowCompanySlider/index.tsx
+++ components/RowCompanySlider/index.tsx
@@ -4,11 +4,7 @@ import { GQLRowCompanySliderFragment } from '../../generated/graphql'
 import { FilestackPicture } from '../FilestackPicture'
 import { ImageTypes } from '../PictureResponsive'
 import { ScrollSnapSlider } from '../ScrollSnapSlider'
-
-// Using https://cssinjs.org/styled-jss?v=v2.2.3#default-styled-function
-const Container = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-}))
+import Container from '../Container'
 
 const Slide = styled(FilestackPicture)({
   // width: '225px',
@@ -18,33 +14,6 @@ const RowCompanySlider: React.FC<GQLRowCompanySliderFragment> = ({ companies }) 
   return (
     <Container>
       <ScrollSnapSlider>
-        {companies.map(company => (
-          <Slide
-            key={company.id}
-            width={Math.round((company.logo.width! / company.logo.height!) * 100)}
-            height={100}
-            src={company.logo.url}
-            type={(company.logo.mimeType as ImageTypes) ?? 'image/png'}
-          />
-        ))}
-        {companies.map(company => (
-          <Slide
-            key={company.id}
-            width={Math.round((company.logo.width! / company.logo.height!) * 100)}
-            height={100}
-            src={company.logo.url}
-            type={(company.logo.mimeType as ImageTypes) ?? 'image/png'}
-          />
-        ))}
-        {companies.map(company => (
-          <Slide
-            key={company.id}
-            width={Math.round((company.logo.width! / company.logo.height!) * 100)}
-            height={100}
-            src={company.logo.url}
-            type={(company.logo.mimeType as ImageTypes) ?? 'image/png'}
-          />
-        ))}
         {companies.map(company => (
           <Slide
             key={company.id}
