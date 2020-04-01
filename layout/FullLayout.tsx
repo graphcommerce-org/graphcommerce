7290ffbd5c2d7c58f045ed6756626cf1ea0b117c
Paul Hachmang <paul@reachdigital.nl>
2020-04-01 16:12:05 +0200

    perf: remove roboto from header, since its not used

--- layout/FullLayout.tsx
+++ layout/FullLayout.tsx
@@ -100,10 +100,6 @@ const FullLayout: GraphCmsPage['layout'] = ({ children, ...props }) => {
       <Head>
         <meta name='theme-color' content={theme.palette.primary.main} />
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-        />
       </Head>
       <CssBaseline />
       <PageMeta {...props} />
