3d7c2ff8354a71b38dc34f9d29a812e48960d71a
Paul Hachmang <paul@reachdigital.nl>
2020-04-12 00:19:50 +0200

    perf: removed react-docgen-typescript-loader to speed up storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,13 +13,8 @@ module.exports = {
       test: /\.(ts|tsx)$/,
       use: [
         {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [require.resolve('babel-preset-react-app')],
-          },
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
+          loader: 'babel-loader',
+          options: { presets: ['next/babel'] },
         },
       ],
     })
