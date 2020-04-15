04ac28c4b9b96a3018707a97696ea9ecf90554a1
Carlo Carels <carlo@reachdigital.nl>
2020-04-15 22:03:32 +0200

    fix(bloglist): add a maxWidth to the container

--- components/BlogList/index.tsx
+++ components/BlogList/index.tsx
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 const BlogList: React.FC<GQLGetBlogListQuery> = ({ blogPosts }) => {
   const classes = useStyles()
   return (
-    <Container className={classes.blogList}>
+    <Container maxWidth='xl' className={classes.blogList}>
       {blogPosts.map((blogPost) => (
         <BlogListItem key={blogPost.id} {...blogPost} />
       ))}
