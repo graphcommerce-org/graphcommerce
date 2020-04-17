e49d107ead06e20b2d05c0e8c8b4ff1b12c3ef22
Paul Hachmang <paul@reachdigital.nl>
2020-04-17 10:30:44 +0200

    refactor: removed img[loading] from next-env.d.ts already in typescript

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,9 +4,3 @@
 /// <reference path="./generated/graphql.d.ts" />
 
 import 'react'
-
-declare module 'react' {
-  interface ImgHTMLAttributes<T> extends HTMLAttributes<T> {
-    loading?: 'lazy' | 'eager' | 'auto'
-  }
-}
