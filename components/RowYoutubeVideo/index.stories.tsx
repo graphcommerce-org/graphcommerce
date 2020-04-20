d9275a79a651ca02ceb1f33ed4d5fe16b8e52dd5
Frank Harland <frank@h-o.nl>
2020-04-20 17:19:15 +0200

    chore(storybook): add missing stories

--- components/RowYoutubeVideo/index.stories.tsx
+++ components/RowYoutubeVideo/index.stories.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { number, withKnobs } from '@storybook/addon-knobs'
 import initApolloClient from '../../lib/apollo'
-import RowColumnTwo from '.'
-import { useGetAllRowColumTwoQuery } from '../../generated/apollo'
+import RowYoutubeVideo from '.'
+import { useGetAllRowYoutubeVideosQuery } from '../../generated/apollo'
 
 export default {
-  title: 'row|RowColumnTwo',
-  component: RowColumnTwo,
+  title: 'row|RowYoutubeVideo',
+  component: RowYoutubeVideo,
   decorators: [withKnobs],
 }
 
 export const GraphCMS = () => {
-  const { data } = useGetAllRowColumTwoQuery({
+  const { data } = useGetAllRowYoutubeVideosQuery({
     client: initApolloClient(),
     variables: { skip: number('GraphCMS Entry #', 0) },
   })
   if (!data) return null
-  return data.rowColumnTwos.map((props) => <RowColumnTwo {...props} key={props.id} />)
+  return data.rowYoutubeVideos.map((props) => <RowYoutubeVideo {...props} key={props.videoId} />)
 }
