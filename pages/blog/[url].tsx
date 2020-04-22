3fa243d50755af5ab1d6967ec15a39c28be6799c
Paul Hachmang <paul@reachdigital.nl>
2020-04-22 20:36:02 +0200

    fix(blog-view): extend page query and use containers for rendering

--- pages/blog/[url].tsx
+++ pages/blog/[url].tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import { GetStaticProps } from 'next'
-import { Container, makeStyles } from '@material-ui/core'
+import { Container } from '@material-ui/core'
 import LayoutFull, { PageWithLayoutFull, PageLayoutProps } from '../../components/PageLayout'
 import extractParams, { StaticPageParams } from '../../lib/staticParams'
 import getStaticPathsFactory from '../../lib/getStaticPaths'
@@ -9,50 +9,23 @@ import ContentRenderer from '../../components/ContentRenderer'
 import AuthorCard from '../../components/AuthorCard'
 import ContactFormLoader from '../../components/ContactForm'
 
-const useStyles = makeStyles({
-  article: {
-    margin: '0 auto',
-    maxWidth: 800,
-  },
-})
-
 const BlogView: PageWithLayoutFull = ({ page }) => {
-  const classes = useStyles()
   const image = 'https://media.graphcms.com/resize=fit:max,w:100/L4GgPJCbS5OHefBPtaTp'
 
   return (
-    <Container maxWidth='lg'>
-      {/* TODO get data below from parent component */}
-      <AuthorCard name='Erwin Otten' date='19 juni 2015' imageUrl={image} />
-      <Typography variant='h1'>{page.title}</Typography>
-      <div className={classes.article}>
+    <>
+      <Container maxWidth='lg'>
+        {page.author && <AuthorCard author={page.author} date={page.releaseDate} />}
+        <Typography variant='h1'>{page.title}</Typography>
+      </Container>
+      <Container maxWidth='md'>
         <ContentRenderer content={page.content} />
-      </div>
-      <ContactFormLoader />
-    </Container>
+      </Container>
+      <Container maxWidth='lg'>
+        <ContactFormLoader />
+      </Container>
+    </>
   )
-
-  // return (
-  //   <div>
-  //     <JsonLd<BlogPosting>
-  //       item={{
-  //         '@context': 'https://schema.org',
-  //         '@type': 'BlogPosting',
-  //         headline: page.metaTitle!,
-  //         image: page.blogPost.image?.url,
-  //         datePublished: page.blogPost.publicPublishedAt,
-  //       }}
-  //     />
-  //     <h1>
-  //       <Link metaRobots={page.metaRobots} href={page.url!}>
-  //         {page.breadcrumbTitle}
-  //       </Link>
-  //     </h1>
-
-  //     {/* eslint-disable react/no-danger */}
-  //     <div dangerouslySetInnerHTML={{ __html: page.blogPost?.content! }} />
-  //   </div>
-  // )
 }
 
 BlogView.layout = LayoutFull
