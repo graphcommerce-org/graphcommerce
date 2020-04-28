3fdd138e680a0df32d7db46e1e9fad1d7ab64f2b
Carlo Carels <carlo@reachdigital.nl>
2020-04-28 14:44:51 +0200

    refactor(blogviewstyles): change the media queries to built-in Material-ui functions

--- components/BlogView/useBlogViewStyles.tsx
+++ components/BlogView/useBlogViewStyles.tsx
@@ -12,10 +12,24 @@ const useBlogViewStyles = makeStyles((theme: Theme) => ({
   featured: {
     display: 'flex',
     marginBottom: vpCalc(20, 50),
+
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+    },
+    [theme.breakpoints.up('lg')]: {
+      alignItems: 'center',
+    },
   },
   assetWrapper: {
     flexBasis: '46%',
     textAlign: 'right',
+    [theme.breakpoints.down('md')]: {
+      flexBasis: '100%',
+      marginBottom: '40px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      padding: '0 30px',
+    },
   },
   asset: {
     width: '100%',
@@ -24,33 +38,24 @@ const useBlogViewStyles = makeStyles((theme: Theme) => ({
   pageTitle: {
     flex: '1 0 50%',
     marginTop: '-0.2em',
+
+    [theme.breakpoints.up('lg')]: {
+      padding: '0 0 0 30px',
+    },
   },
   article: {
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '800px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: '800px',
+    },
     '& img': {
       maxWidth: '100%',
       height: 'auto',
     },
     '& p': {
       margin: '40px 0',
-    },
-  },
-  '@media only screen and (max-width: 1023px)': {
-    featured: {
-      flexFlow: 'column wrap',
-    },
-    assetWrapper: {
-      flexBasis: '100%',
-    },
-  },
-  '@media only screen and (min-width: 1024px)': {
-    featured: {
-      alignItems: 'center',
-    },
-    pageTitle: {
-      padding: '0 0 0 30px',
-    },
-    assetWrapper: {
-      padding: '0 30px',
     },
   },
 }))
