79122ec69631138ff7c0fc7948dd2756cc272836
Paul Hachmang <paul@reachdigital.nl>
2020-04-29 12:42:10 +0200

    feat(richtext): added condensed mode to RichText

--- components/RichText/useRichTextStyles.tsx
+++ components/RichText/useRichTextStyles.tsx
@@ -1,12 +1,24 @@
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, Theme } from '@material-ui/core'
 import { vpCalc } from '../Theme'
 
-const useRichTextStyles = makeStyles({
-  body1: { marginBottom: '1em' },
-  h1: { marginTop: vpCalc(9, 0), marginBottom: vpCalc(21, 50), fontWeight: 400 },
-  h2: { marginTop: vpCalc(22, 40), marginBottom: vpCalc(20, 40) },
-  h3: { marginTop: vpCalc(22, 30), marginBottom: vpCalc(22, 30) },
+export type RichTextStylesProps = {
+  condensed: boolean
+}
 
+const useRichTextStyles = makeStyles<Theme, RichTextStylesProps>({
+  body1: ({ condensed }) => ({
+    ...(!condensed && { marginBottom: '1em' }),
+  }),
+  h1: ({ condensed }) => ({
+    fontWeight: 400,
+    ...(!condensed && { marginTop: vpCalc(9, 0), marginBottom: vpCalc(21, 50) }),
+  }),
+  h2: ({ condensed }) => ({
+    ...(!condensed && { marginTop: vpCalc(22, 40), marginBottom: vpCalc(20, 40) }),
+  }),
+  h3: ({ condensed }) => ({
+    ...(!condensed && { marginTop: vpCalc(22, 30), marginBottom: vpCalc(22, 30) }),
+  }),
   asset: { width: '100%', height: 'auto' },
 })
 
