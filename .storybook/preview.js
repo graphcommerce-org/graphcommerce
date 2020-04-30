e232de304d7bb1e31cf4b688756fc7214bf0a9ee
Paul Hachmang <paul@reachdigital.nl>
2020-04-30 17:20:20 +0200

    chore: storybook used wrong import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import { addDecorator } from '@storybook/react'
 import { withMuiTheme } from '@harelpls/storybook-addon-materialui'
-import { theme } from '../components/Theme'
+import { defaultTheme } from '../components/Theme'
 
-addDecorator(withMuiTheme({ Default: theme }))
+addDecorator(withMuiTheme({ Default: defaultTheme }))
