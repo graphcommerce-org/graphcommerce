37159ed1d78cd98bab1ff8ca0e64f5438cb2b753
Paul Hachmang <paul@reachdigital.nl>
2020-05-10 16:32:10 +0200

    refactor: move to static imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { GetStaticProps } from 'next'
-import LayoutFull, { PageWithLayoutFull, PageLayoutProps } from '../components/PageLayout'
+import LayoutFull, {
+  PageWithLayoutFull,
+  PageLayoutProps,
+  getStaticProps as get,
+} from '../components/PageLayout'
 import ContentRenderer from '../components/ContentRenderer'
 import RowHeroHome from '../components/RowHero/RowHeroHome'
 import { StaticPageVariables } from '../lib/staticParams'
@@ -24,6 +28,5 @@ export default Home
 
 export const getStaticProps: GetStaticProps<PageLayoutProps> = async () => {
   const params: StaticPageVariables = { url: '/', locale: 'nl' }
-  const { getStaticProps: get } = await import('../components/PageLayout')
   return { props: await get(params) }
 }
