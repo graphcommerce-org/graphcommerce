2d9fcba220ea4ea2d22677f0406f35a93749baef
Paul Hachmang <paul@reachdigital.nl>
2020-05-25 12:37:42 +0200

    fix: solve ts errors AspectRatioContainer

--- components/AspectRatioContainer/index.tsx
+++ components/AspectRatioContainer/index.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
+import { UseStyles } from 'components/Theme'
 
-const AspectRatioContainer: React.FC = (props) => {
-  const { width, height, children } = props
-
-  const useStyles = makeStyles({
-    root: {
-      position: `relative`,
-      paddingTop: `calc(100% / ${width} * ${height})`,
-      '& img, & video': {
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        width: `100%`,
-        height: `auto`,
-      },
+const useStyles = makeStyles({
+  root: ({ width, height }: Props) => ({
+    position: `relative`,
+    paddingTop: `calc(100% / ${width} * ${height})`,
+    '& img, & video': {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: `100%`,
+      height: `auto`,
     },
-  })
-  const classes = useStyles()
+  }),
+})
 
+type Props = { width: number; height: number }
+export type AspectRatioContainerProps = Props & UseStyles<typeof useStyles>
+
+const AspectRatioContainer: React.FC<AspectRatioContainerProps> = ({ children, ...styleProps }) => {
+  const classes = useStyles(styleProps)
   return <div className={classes.root}>{children}</div>
 }
 
