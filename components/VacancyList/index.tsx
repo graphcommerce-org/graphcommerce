6f715c71a6c2007731812de072219b58ed387c5a
Carlo Carels <carlo@reachdigital.nl>
2020-05-27 16:24:10 +0200

    style(vacanciespage): add styling to the vacanylist

--- components/VacancyList/index.tsx
+++ components/VacancyList/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import { makeStyles, Container, Theme } from '@material-ui/core'
+import { vpCalc } from 'components/Theme'
 import VacancyListItem from './VacancyListItem'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
-    vacancyList: {
+    root: {
+      paddingTop: vpCalc(104, 250),
+      paddingBottom: vpCalc(48, 160),
+      background: theme.palette.primary.dark,
+      position: 'relative',
+      overflow: 'hidden',
+      top: `calc(${vpCalc(73, 155)} * -1)`,
+    },
+    list: {
       display: 'flex',
       flexFlow: 'row wrap',
+      justifyContent: 'space-between',
     },
     item: {
-      flex: '0 0 50%',
+      flex: '1 0 100%',
+      marginBottom: vpCalc(24, 75),
+    },
+    [theme.breakpoints.up(1024)]: {
+      item: {
+        flex: `0 0 calc(50% - ${vpCalc(24, 75)} /2)`,
+        '&:nth-child(2n)': {
+          marginLeft: vpCalc(24, 75),
+        },
+      },
     },
   }),
   { name: 'VacancyList' },
@@ -19,12 +38,16 @@ const VacancyList: React.FC<GQLGetVacancyListQuery> = ({ vacancyPosts }) => {
   const classes = useStyles()
 
   return (
-    <Container maxWidth='lg' className={classes.vacancyList}>
-      {vacancyPosts &&
-        vacancyPosts.map((item) => (
-          <VacancyListItem key={item.id} {...item} className={classes.item} />
-        ))}
-    </Container>
+    <div className={classes.root}>
+      <Container maxWidth='lg'>
+        <div className={classes.list}>
+          {vacancyPosts &&
+            vacancyPosts.map((item) => (
+              <VacancyListItem key={item.id} {...item} className={classes.item} />
+            ))}
+        </div>
+      </Container>
+    </div>
   )
 }
 
