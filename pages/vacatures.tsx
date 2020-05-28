df57bde2751fb3c91510024645a573a7b4e53c03
Carlo Carels <carlo@reachdigital.nl>
2020-05-28 14:53:31 +0200

    [RD-388] add other vacancies overview pages

---
+++ pages/vacatures.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { GetStaticProps } from 'next'
+import getPageLayoutProps from 'components/PageLayout/getPageLayoutProps'
+import getVacancyListProps from 'components/VacancyList/getVacancyListProps'
+import { StaticPageVariables } from 'node/staticParams'
+import LayoutFull, { PageWithLayoutFull, PageLayoutProps } from '../components/PageLayout'
+import ContentRenderer from '../components/ContentRenderer'
+import { useHeaderSpacing } from '../components/Header'
+import VacancyList from '../components/VacancyList'
+
+const Vacancies: PageWithLayoutFull<GQLGetVacancyListQuery> = ({ page, vacancyPosts }) => {
+  const header = useHeaderSpacing()
+
+  return (
+    <div className={header.marginTop}>
+      <VacancyList vacancyPosts={vacancyPosts} />
+      <ContentRenderer content={page.content} />
+    </div>
+  )
+}
+
+Vacancies.layout = LayoutFull
+
+export default Vacancies
+
+export const getStaticProps: GetStaticProps<
+  PageLayoutProps & GQLGetVacancyListQuery
+> = async () => {
+  const params: StaticPageVariables = { url: '/vacatures', locale: 'nl' }
+  const data = await Promise.all([getPageLayoutProps(params), getVacancyListProps(params)])
+
+  return { props: Object.assign(...data) }
+}
