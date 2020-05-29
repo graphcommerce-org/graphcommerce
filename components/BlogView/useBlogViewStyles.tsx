35f0285812300356a32aece181803c270084c864
Carlo Carels <carlo@reachdigital.nl>
2020-05-29 16:14:15 +0200

    refactor(singleblogpost): apply mobile-first + apply theme values

--- components/BlogView/useBlogViewStyles.tsx
+++ components/BlogView/useBlogViewStyles.tsx
@@ -3,34 +3,30 @@ import { vpCalc } from 'components/Theme'
 
 const useBlogViewStyles = makeStyles(
   (theme: Theme) => ({
-    last: {
-      marginTop: '30px',
-    },
     boxed: {
       padding: vpCalc(24, 80),
-      boxShadow: '0 2px 15px 0 rgba(0, 0, 0, 0.04), 0 60px 70px -20px rgba(0, 0, 0, 0.14)',
+      boxShadow: theme.shadows[24],
     },
     featured: {
       display: 'flex',
-      marginBottom: vpCalc(20, 50),
+      flexDirection: 'column',
 
-      [theme.breakpoints.down('sm')]: {
-        flexDirection: 'column',
-      },
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up('md')]: {
         alignItems: 'center',
+        flexDirection: 'row',
+        marginBottom: vpCalc(20, 50),
       },
     },
     assetWrapper: {
-      flexBasis: '46%',
       textAlign: 'right',
-      [theme.breakpoints.down('sm')]: {
-        flexBasis: '100%',
-        width: '100%',
-        marginBottom: '40px',
-      },
+      flexBasis: '100%',
+      width: '100%',
+      marginBottom: '40px',
+
       [theme.breakpoints.up('md')]: {
-        padding: '0 30px',
+        paddingRight: vpCalc(20, 60),
+        flexBasis: '46%',
+        marginBottom: 0,
       },
     },
     asset: {
@@ -39,28 +35,11 @@ const useBlogViewStyles = makeStyles(
     },
     pageTitle: {
       flex: '1 0 50%',
-      marginTop: '-0.2em',
-      padding: vpCalc(10, 30),
+      padding: 0,
 
-      [theme.breakpoints.down('sm')]: {
-        paddingLeft: 0,
-        paddingRight: 0,
-      },
-
-      [theme.breakpoints.up('lg')]: {
-        padding: '0 0 0 30px',
-      },
-    },
-    article: {
-      [theme.breakpoints.up('sm')]: {
-        maxWidth: '800px',
-      },
-      [theme.breakpoints.up('lg')]: {
-        maxWidth: '800px',
-      },
-      '& img': {
-        maxWidth: '100%',
-        height: 'auto',
+      [theme.breakpoints.up('md')]: {
+        margin: 0,
+        paddingRight: vpCalc(10, 30),
       },
     },
   }),
