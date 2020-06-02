87997400b1e483ce5581cd82adbfbf3c68f57e9c
Paul Hachmang <paul@reachdigital.nl>
2020-06-02 11:21:26 +0200

    feat: render product page component

--- shop/venia-ui/RootComponents/ProductPage/index.tsx
+++ shop/venia-ui/RootComponents/ProductPage/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import MagentoDynamic from 'shop/venia-ui/MagentoDynamic/MagentoDynamic'
 import { GetProductPageProps } from './getProductProps'
 
 const ProductPage: React.FC<GetProductPageProps> = ({ product }) => {
-  const { name } = product
-  return <div>Product {name}</div>
+  const { name, description } = product
+
+  return (
+    <MagentoDynamic
+      loader={() => import('@magento/venia-ui/lib/components/ProductFullDetail')}
+      skeleton={(ref) => <div ref={ref}>hoi</div>}
+      product={product}
+    />
+  )
 }
 
 export default ProductPage
