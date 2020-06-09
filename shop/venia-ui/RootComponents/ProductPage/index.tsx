31cd3f5f160440f23a45a89da31d59f4a2f9d30a
Paul Hachmang <paul@reachdigital.nl>
2020-06-09 11:26:56 +0200

    feat: implemented RootComponent: ProductPage

--- shop/venia-ui/RootComponents/ProductPage/index.tsx
+++ shop/venia-ui/RootComponents/ProductPage/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import MagentoDynamic from 'shop/venia-ui/MagentoDynamic/MagentoDynamic'
+import Head from 'next/head'
 import { GetProductPageProps } from './getProductProps'
 
+/**
+ * Replaces RootComponents/Product
+ * https://github.com/magento/pwa-studio/blob/develop/packages/venia-ui/lib/RootComponents/Product/product.js
+ */
 const ProductPage: React.FC<GetProductPageProps> = ({ product }) => {
-  const { name, description } = product
-
+  const { meta_title, meta_description } = product
   return (
-    <MagentoDynamic
-      loader={() => import('@magento/venia-ui/lib/components/ProductFullDetail')}
-      skeleton={(ref) => <div ref={ref}>hoi</div>}
-      product={product}
-    />
+    <>
+      <Head>
+        <title>{meta_title}</title>
+        <meta name='description' content={meta_description} />
+        <meta name='robots' content='INDEX, FOLLOW' />
+      </Head>
+      <MagentoDynamic
+        loader={() => import('@magento/venia-ui/lib/components/ProductFullDetail')}
+        skeleton={(ref) => <div ref={ref}>hoi</div>}
+        product={product}
+      />
+    </>
   )
 }
 
