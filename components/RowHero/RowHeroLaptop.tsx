bee65edda51d9006ce5403a7200195c3989117dd
Carlo Carels <carlo@reachdigital.nl>
2020-06-11 15:21:54 +0200

    refactor(): rename names and remove unused variables

--- components/RowHero/RowHeroLaptop.tsx
+++ components/RowHero/RowHeroLaptop.tsx
@@ -8,7 +8,7 @@ import { ChevronRight } from 'components/Icons'
 import { Container, makeStyles, Theme } from '@material-ui/core'
 import RichText from 'components/RichText'
 import ContactCta from 'components/ContactCta'
-import RowHeroLaptopStyles from './RowHeroLaptopStyles'
+import useRowHeroLaptopStyles from './useRowHeroLaptopStyles'
 import { RowHeroProps } from '.'
 
 const useLaptopStyles = makeStyles((theme: Theme) => ({
@@ -22,8 +22,8 @@ const useLaptopStyles = makeStyles((theme: Theme) => ({
 }))
 
 const RowHeroLaptop: React.FC<RowHeroProps> = (props) => {
-  const { text, asset, links, contactPeople, richTextClasses, children } = props
-  const classes = RowHeroLaptopStyles()
+  const { text, asset, links, children } = props
+  const classes = useRowHeroLaptopStyles()
   const headerSpacing = useHeaderSpacing()
   const DeviceLaptopStyles = useLaptopStyles()
 
@@ -42,14 +42,14 @@ const RowHeroLaptop: React.FC<RowHeroProps> = (props) => {
           )
         )}
         <div className={classes.colTwo}>
-          <RichText {...text} classes={richTextClasses} />
+          <RichText {...text} classes={{ h1: classes.title }} />
           <div>
             {links.map((link) => {
               if (link.__typename === 'LinkInternal' && link.page && link.description)
                 return (
                   <div key={link.id} className={classes.ctaBlock}>
                     <small>
-                      <RichText {...link.description} className={classes.paragraph} />
+                      <RichText {...link.description} classes={{ paragraph: classes.paragraph }} />
                     </small>
                     <Button
                       href={link.page.url}
