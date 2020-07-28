8974f390b5cd31fc46d6efb4ff6492dba465b346
Paul Hachmang <paul@reachdigital.nl>
2020-07-28 14:34:07 +0200

    fix: solve issue where closing cart woudn’t go back to the previous page

--- components/Cart/CartFab.tsx
+++ components/Cart/CartFab.tsx
@@ -5,29 +5,21 @@ import { Badge, Fab } from '@material-ui/core'
 import { useRouter } from 'next/router'
 
 export default function CartFab() {
+  const router = useRouter()
   const { data: cartIdData } = useCartIdQuery()
   const { data: cartData } = useGuestCartQuery({
     variables: { cartId: cartIdData?.cartId || '' },
     fetchPolicy: 'cache-only',
   })
 
-  const router = useRouter()
   const isCart = router.pathname === '/cart'
-  const canGoBack =
-    typeof window !== 'undefined' ? document.referrer.indexOf(window.location.host) !== -1 : false
 
   return (
     <Badge badgeContent={cartData?.cart?.total_quantity || 0} color='primary' overlap='circle'>
       <Fab
         aria-label={isCart ? 'Close Cart' : 'Open Cart'}
         size='medium'
-        onClick={() => {
-          if (isCart && canGoBack) router.back()
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
-          if (isCart && !canGoBack) router.push('/')
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
-          if (!isCart) router.push('/cart')
-        }}
+        onClick={() => (isCart ? router.back() : router.push('/cart'))}
       >
         <CartIcon fontSize='small' />
       </Fab>
