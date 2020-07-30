6bd4a09ed2f882989b59e190895636a78db4b332
Paul Hachmang <paul@reachdigital.nl>
2020-07-30 12:39:01 +0200

    fix(pwa-145): issue where navigation direction was wrong for cached page

--- components/PageTransition/useNavigationDirection.tsx
+++ components/PageTransition/useNavigationDirection.tsx
@@ -1,42 +1,24 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function useNavigationDirection() {
   const router = useRouter()
-  const [navigationState, setNavigationState] = useState<string[]>([router.asPath])
-  const [direction, setDirection] = useState<1 | 0 | -1>(0)
+  const [historyState, setNavigationState] = useState<string[]>([router.asPath])
 
-  useEffect(() => {
-    function history(url: string) {
-      // Navigated to previous page
-      if (navigationState[navigationState.length - 2] === url) {
-        setNavigationState(navigationState.slice(0, -1))
-        if (direction !== -1) setDirection(-1)
-      }
-      // Navigated to same page, do nothing
-      else if (navigationState[navigationState.length - 1] === url) {
-        if (direction !== 0) setDirection(0)
-      }
-      // Navigated to next page
-      else {
-        setNavigationState([...navigationState, url])
-        if (direction !== 1) setDirection(1)
-      }
+  function getDirection(url: string): 1 | 0 | -1 {
+    // Navigated to previous page
+    if (historyState[historyState.length - 2] === url) {
+      setNavigationState(historyState.slice(0, -1))
+      return -1
     }
-
-    function reset() {
-      if (direction !== 0) setDirection(0)
+    // Navigated to same page, do nothing
+    if (historyState[historyState.length - 1] === url) {
+      return 0
     }
+    // Navigated to next page
+    setNavigationState([...historyState, url])
+    return 1
+  }
 
-    router.events.on('routeChangeStart', history)
-    router.events.on('routeChangeComplete', reset)
-    router.events.on('routeChangeError', reset)
-    return () => {
-      router.events.off('routeChangeStart', history)
-      router.events.on('routeChangeComplete', reset)
-      router.events.on('routeChangeError', reset)
-    }
-  }, [direction, navigationState, router])
-
-  return direction
+  return getDirection
 }
