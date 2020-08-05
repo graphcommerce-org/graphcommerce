45d116ececd16e8b598897d2b9827ede1ceadb26
Paul Hachmang <paul@reachdigital.nl>
2020-08-05 17:00:45 +0200

    feat: better error status handling

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -6,10 +6,15 @@ import sharp from 'sharp'
  * Parameters required for the endpoint to work
  */
 function parseQuery(query: NextApiRequest['query']) {
+  const { width, type, url, ...inputOptions } = query
+  if (!width || !type || !url) throw Error('Please provide width, type and url')
+
+  const options: OutputOptions = {}
   return {
     width: Number(query.width),
-    type: String(query.type),
-    url: new URL(String(query.url)),
+    type: query.type as 'jpeg' | 'png' | 'webp' | 'gif' | 'svg',
+    url: query.url as string,
+    options,
   }
 }
 
@@ -17,26 +22,23 @@ function parseQuery(query: NextApiRequest['query']) {
  * PNG to WebP http://localhost:3000/api/image?width=200&height=117&type=webp&url=https://www.sightful.nl/media/catalog/product/8/8/88877d091e5623b76d7fbd7b600e8d8287cc3c4d_air_optix_aqua_6pcs_1200.png
  */
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let status = 500
   try {
-    const { url, width, type } = parseQuery(req.query)
+    const { url, width, type, options } = parseQuery(req.query)
 
-    const image = await fetch(url.toString())
+    const image = await fetch(url)
+    status = image.status
 
     res.setHeader('Cache-Control', image.headers.get('Cache-Control') || '')
     res.setHeader('Etag', image.headers.get('Etag') || '')
 
-    res.end(
-      await sharp(await image.buffer())
-        .resize(width)
-        .toFormat(type)
-        .toBuffer(),
-    )
-    res.status(200)
-
+    const imageBuffer = await image.buffer()
+    res.end(await sharp(imageBuffer).resize(width).toFormat(type, options).toBuffer())
+    res.status(status)
     res.end()
   } catch (e) {
-    console.error(e)
-    res.status(500).end()
+    console.error(e.toString())
+    res.status(status).end(e.toString())
   }
 
   return Promise.resolve()
