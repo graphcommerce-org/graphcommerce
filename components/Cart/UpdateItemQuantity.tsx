562f60f4b6da64d0aef9db4567a434277ca2fbca
Nick de Kleijn <nick@reachdigital.nl>
2020-08-20 15:25:33 +0200

    feat(PWA-36): added TextInputNumber component
    Shows a number input field with increase and decrease buttons

--- components/Cart/UpdateItemQuantity.tsx
+++ components/Cart/UpdateItemQuantity.tsx
@@ -1,37 +1,29 @@
-import { TextField, debounce, createStyles, makeStyles, Theme } from '@material-ui/core'
+import { debounce } from '@material-ui/core'
+import TextInputNumber from 'components/TextInputNumber'
 import { useMutationForm } from 'components/useMutationForm'
 import { UpdateItemQuantityDocument } from 'generated/apollo'
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    quantity: {
-      width: 60,
-    },
-    quantityInput: {
-      textAlign: 'center',
-    },
-  }),
-)
+import React, { useRef } from 'react'
 
 export default function UpdateItemQuantity(props: GQLUpdateItemQuantityMutationVariables) {
   const { register, errors, onSubmit, required, result } = useMutationForm<
     GQLUpdateItemQuantityMutation,
     GQLUpdateItemQuantityMutationVariables
   >({ mutation: UpdateItemQuantityDocument, values: props, mode: 'onChange' })
-  const classes = useStyles()
+  const ref = useRef<HTMLInputElement>(null)
+  register(ref.current, { required: required.quantity })
 
   return (
-    <form noValidate onChange={debounce(onSubmit, 500)}>
-      <TextField
+    <form noValidate onChange={debounce(onSubmit, 600)}>
+      <TextInputNumber
         size='small'
-        type='number'
-        className={classes.quantity}
-        inputProps={{ className: classes.quantityInput, min: 1 }}
+        label='Quantity'
+        variant='outlined'
+        inputProps={{ min: 1 }}
         error={!!errors.quantity}
         id='quantity'
         name='quantity'
         required={required.quantity}
-        inputRef={register({ required: required.quantity })}
+        inputRef={ref}
         helperText={errors?.quantity?.message}
         disabled={result.loading}
       />
