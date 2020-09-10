88e4dc122ecc48403482d1f9ab56ea51a8bb7362
Paul Hachmang <paul@reachdigital.nl>
2020-09-10 11:08:36 +0200

    debug: added performance mark logging

--- next.config.js
+++ next.config.js
@@ -7,12 +7,21 @@ const IntlPolyfill = require('intl')
 const withImages = require('next-images')
 const withPWA = require('next-pwa')
 const withTM = require('next-transpile-modules')(['@apollo/client'])
+const { PerformanceObserver, performance } = require('perf_hooks')
 
 Intl.NumberFormat = IntlPolyfill.NumberFormat
 Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
 if (process.versions.node.split('.')[0] > 12) {
   console.warn("'intl' polyfill isn't required anymore")
 }
+
+const obs = new PerformanceObserver((entryList) => {
+  entryList.getEntries().forEach((item) => {
+    console.log(item.name, item.duration)
+  })
+  performance.clearMarks()
+})
+obs.observe({ entryTypes: ['measure'] })
 
 const nextConfig = {
   webpackStats: process.env.ANALYZE === 'true',
