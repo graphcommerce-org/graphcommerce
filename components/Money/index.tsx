dca053b94a713d1cc5b6b08f724c4cb9178ad9b5
Paul Hachmang <paul@reachdigital.nl>
2020-09-16 10:31:42 +0200

    feat: added correct locale to Money rendering

--- components/Money/index.tsx
+++ components/Money/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useStoreConfigQuery } from 'generated/apollo'
+import React, { useMemo } from 'react'
 
-export default function Money(props: GQLMoneyFragment) {
-  const { currency, value } = props
-  if (!currency || !value) return null
+export default function Money({ currency, value }: GQLMoneyFragment) {
+  const { data: config } = useStoreConfigQuery()
+  const locale = config?.storeConfig?.locale
 
-  const numberFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency })
+  const numberFormatter = useMemo(() => {
+    if (!currency || !locale) return undefined
+    return new Intl.NumberFormat(locale.replace('_', '-'), { style: 'currency', currency })
+  }, [locale, currency])
+
+  if (!numberFormatter || !value) return null
   return <>{numberFormatter.format(value)}</>
 }
