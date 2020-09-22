56df4209b6ea59023a8ad0c6d37d8598676446e0
Paul Hachmang <paul@reachdigital.nl>
2020-09-22 13:10:41 +0200

    fix: redirect to the previous page when signed out

--- components/Customer/SignOutForm.tsx
+++ components/Customer/SignOutForm.tsx
@@ -1,17 +1,19 @@
 import { Button, FormControl, FormHelperText } from '@material-ui/core'
 import { useMutationForm } from 'components/useMutationForm'
 import { SignOutDocument } from 'generated/apollo'
+import { useRouter } from 'next/router'
 
 type SignOutFormProps = Omit<JSX.IntrinsicElements['form'], 'onSubmit' | 'noValidate'>
 
 export default function SignOutForm(props: SignOutFormProps) {
+  const router = useRouter()
   const { onSubmit, result } = useMutationForm<GQLSignOutMutation, GQLSignOutMutationVariables>({
     mutation: SignOutDocument,
+    onComplete: () => router.back(),
   })
-  const { ...formProps } = props
 
   return (
-    <form onSubmit={onSubmit} noValidate {...formProps}>
+    <form onSubmit={onSubmit} noValidate {...props}>
       <FormControl>
         <Button type='submit' disabled={result.loading} variant='outlined'>
           Sign out
