7eb4a3e23045c17b7e90c9fa5afbe3ed17093ea5
Paul Hachmang <paul@reachdigital.nl>
2020-10-26 21:07:46 +0100

    chore: remove build dirs

--- packagesDev/graphql-codegen-relay-optimizer-plugin/dist/main/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PluginFunction } from '@graphql-codegen/plugin-helpers';
-export interface RelayOptimizerPluginConfig {
-}
-export declare const plugin: PluginFunction<RelayOptimizerPluginConfig>;
