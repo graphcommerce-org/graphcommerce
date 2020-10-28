a74fc1016db396c945531a82fe7f4adc596a690b
Paul Hachmang <paul@reachdigital.nl>
2020-10-28 15:19:07 +0100

    fix: add Access-Control-Allow-* headers to API

--- examples/soxbase-api/pages/api/graphql.ts
+++ examples/soxbase-api/pages/api/graphql.ts
@@ -5,4 +5,13 @@ import meshConfig from '../../.meshrc.json'
 const handler = createHandler(meshConfig, '/api/graphql')
 
 export { config }
-export default async (req: NextApiRequest, res: NextApiResponse) => (await handler)(req, res)
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  )
+  return (await handler)(req, res)
+}
