c16117a342229b46c575f8d374897da65215b3d9
Paul Hachmang <paul@reachdigital.nl>
2020-11-11 23:33:22 +0100

    feat: dropshadow on fixed header

--- packages/magento-app-shell/Header.tsx
+++ packages/magento-app-shell/Header.tsx
@@ -1,4 +1,4 @@
-import { Theme, makeStyles } from '@material-ui/core'
+import { Theme, makeStyles, useTheme, useMediaQuery } from '@material-ui/core'
 import CartFab from '@reachdigital/magento-cart/CartFab'
 import CustomerFab from '@reachdigital/magento-customer/AccountFab'
 import SearchButton from '@reachdigital/magento-search/SearchButton'
@@ -7,7 +7,7 @@ import PageLink from '@reachdigital/next-ui/PageTransition/PageLink'
 import { UseStyles } from '@reachdigital/next-ui/Styles'
 import responsiveVal from '@reachdigital/next-ui/Styles/responsiveVal'
 import clsx from 'clsx'
-import { useViewportScroll, m, useTransform, useMotionValue } from 'framer-motion'
+import { useViewportScroll, m, useTransform, useMotionTemplate } from 'framer-motion'
 import React from 'react'
 import MenuFab from './MenuFab'
 import MenuTabs from './MenuTabs'
@@ -16,16 +16,23 @@ import logo from './logo.svg'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
+    wrapper: {
+      position: 'fixed',
+      zIndex: 1,
+      width: '100%',
+    },
     header: {
+      padding: `${theme.page.vertical} ${theme.page.horizontal} 7px`,
       top: 0,
       display: 'flex',
-      padding: `${theme.page.vertical} ${theme.page.horizontal}`,
       pointerEvents: 'none',
       alignItems: 'center',
       justifyContent: 'center',
       width: '100%',
       [theme.breakpoints.down('sm')]: {},
-      [theme.breakpoints.up('md')]: {},
+      [theme.breakpoints.up('md')]: {
+        background: theme.palette.background.default,
+      },
     },
     logo: {
       pointerEvents: 'all',
@@ -112,13 +119,18 @@ export type HeaderProps = HeaderDataProps & HeaderElementProps
 export default function Header(props: HeaderProps) {
   const classes = useStyles(props)
   const { scrollY } = useViewportScroll()
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.up('md'))
   const { menu, urlResolver, ...headerProps } = props
 
-  const y = useTransform(scrollY, [0, 40], [0, -40])
+  const y = useTransform(scrollY, [0, 60], [0, -30])
+  const shadow = useTransform(scrollY, [0, 60], [-40, 30])
+  const filter = useMotionTemplate`drop-shadow(${shadow}px ${shadow}px 20px rgba(0,0,0,0.1))`
+
   // @todo implement with a stable useMemo: 'use-custom-compare'
   return (
-    <m.div style={{ y, position: 'sticky', zIndex: 1, width: '100%' }} layout>
-      <m.header {...headerProps} className={clsx(classes.header)} layout>
+    <m.div style={matches ? { y, filter } : {}} layout className={classes.wrapper}>
+      <m.header {...headerProps} className={clsx(classes.header)} layout drag>
         <PageLink href='/'>
           <a className={classes.logo}>
             <img src={logo} alt='Logo' className={classes.logoImg} width={192} height={72} />
