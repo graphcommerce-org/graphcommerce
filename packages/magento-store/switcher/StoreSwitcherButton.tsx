65610cc1db2929b9203a3b7b25375a8324bc8ce9
Paul Hachmang <paul@reachdigital.nl>
2020-11-23 15:19:13 +0100

    feat: added footer buttons

---
+++ packages/magento-store/switcher/StoreSwitcherButton.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from '@apollo/client'
+import Button from '@reachdigital/next-ui/Button'
+import PageLink from '@reachdigital/next-ui/PageTransition/PageLink'
+import React from 'react'
+import { StoreConfigDocument } from '../StoreConfig.gql'
+
+export default function StoreSwitcherButton() {
+  const config = useQuery(StoreConfigDocument)
+  return (
+    <PageLink href='/switch-stores'>
+      <Button variant='outlined'>
+        {config.data?.storeConfig?.store_name} {config.data?.storeConfig?.base_currency_code}
+      </Button>
+    </PageLink>
+  )
+}
