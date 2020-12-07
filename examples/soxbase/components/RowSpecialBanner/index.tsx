4ec4ec1ad690a98e44ecf94105a7cb3f72818abb
ErwinOtten <erwin@h-o.nl>
2020-12-07 18:00:30 +0100

    feat: rowspecialbanner component

---
+++ examples/soxbase/components/RowSpecialBanner/index.tsx
@@ -0,0 +1,94 @@
+import { Container, Theme } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import RichText from '@reachdigital/graphcms-ui/RichText'
+import PageLinkUrl from '@reachdigital/next-ui/PageTransition/PageLink'
+import responsiveVal from '@reachdigital/next-ui/Styles/responsiveVal'
+import Link from '../PageLink/Link'
+import { RowSpecialBannerFragment } from './RowSpecialBanner.gql'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  wrapper: {
+    display: 'grid',
+    height: '60vw',
+    gridTemplateColumns: '1fr 1fr',
+    justifyItems: 'center',
+    columnGap: `${theme.spacings.lg}`,
+    [theme.breakpoints.up('md')]: {
+      height: '50vw',
+      columnGap: `${theme.spacings.lg}`,
+    },
+  },
+  asset: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+  },
+  copy: {
+    color: '#000',
+    display: 'grid',
+    alignContent: 'center',
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '80%',
+    },
+    '& > *': {
+      maxWidth: 'max-content',
+    },
+  },
+  topic: {
+    textTransform: 'uppercase',
+    color: 'rgba(0,0,0,0.7)',
+  },
+}))
+
+const useRichTextOne = makeStyles((theme: Theme) => ({
+  h2: {
+    textTransform: 'uppercase',
+    maxWidth: '80%',
+    color: 'transparent',
+    WebkitTextStroke: '0.9px #000',
+    fontSize: responsiveVal(18, 50),
+    marginTop: responsiveVal(8, 20),
+    marginBottom: responsiveVal(18, 20),
+    '& strong': {
+      color: '#000',
+      WebkitTextStroke: 'none',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: responsiveVal(18, 60),
+      maxWidth: '100%',
+      WebkitTextStroke: '1.2px #000',
+    },
+  },
+  paragraph: {
+    color: 'red',
+  },
+}))
+
+type RowSpecialBanner = RowSpecialBannerFragment
+
+export default function RowSpecialBanner(props: RowSpecialBanner) {
+  const { copy, asset, topic, pageLinks } = props
+  const classes = useStyles()
+  const richTextOneClasses = useRichTextOne(props)
+
+  return (
+    <Container maxWidth={false}>
+      <div className={classes.wrapper}>
+        {asset.mimeType === 'video/mp4' ? (
+          <video src={asset.url} autoPlay muted loop playsInline className={classes.asset} />
+        ) : (
+          <PageLinkUrl href='#'>
+            <img src={asset.url} className={classes.asset} alt='' />
+          </PageLinkUrl>
+        )}
+        <div className={classes.copy}>
+          {topic ? <span className={classes.topic}>{topic}</span> : false}
+          <RichText classes={richTextOneClasses} {...copy} />
+          {pageLinks.map((pageLink) => (
+            <Link key={pageLink.url} {...pageLink} />
+          ))}
+        </div>
+      </div>
+    </Container>
+  )
+}
