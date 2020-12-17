71121fee9ed7bbd280cff1e0d6d179fa9ec97679
ErwinOtten <erwin@h-o.nl>
2020-12-17 17:02:49 +0100

    style: rowproductgrid margin

--- examples/soxbase/components/RowProductGrid/index.tsx
+++ examples/soxbase/components/RowProductGrid/index.tsx
@@ -1,11 +1,22 @@
 import { Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 import { RowProductGridFragment } from './RowProductGrid.gql'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    marginBottom: `${theme.spacings.lg}`,
+    [theme.breakpoints.up('md')]: {
+      marginBottom: `${theme.spacings.xl}`,
+    },
+  },
+}))
 
 export default function RowProductGrid(props: RowProductGridFragment) {
   const { title, pageLinks } = props
+  const classes = useStyles()
 
   return (
-    <Container maxWidth={false}>
+    <Container maxWidth={false} className={classes.container}>
       <h2>{title}</h2>
     </Container>
   )
