fee623d32520098df2c051edca69ce07705aca8c
Paul Hachmang <paul@reachdigital.nl>
2020-12-21 16:50:56 +0100

    feat: simple product page with add to cart button

--- examples/soxbase/pages/product/[url].tsx
+++ examples/soxbase/pages/product/[url].tsx
@@ -1,17 +1,25 @@
-import { Container } from '@material-ui/core'
+import { Container, Typography } from '@material-ui/core'
 import PageLayout, { PageLayoutProps } from '@reachdigital/magento-app-shell/PageLayout'
 import { PageLayoutDocument } from '@reachdigital/magento-app-shell/PageLayout.gql'
-import useCategoryPageStyles from '@reachdigital/magento-category/useCategoryPageStyles'
+import AddToCartButton from '@reachdigital/magento-cart/AddToCartButton'
+import {
+  ProductSimpleDocument,
+  ProductSimpleQuery,
+} from '@reachdigital/magento-product-simple/ProductSimple.gql'
 import {
   ProductPageDocument,
   ProductPageQuery,
 } from '@reachdigital/magento-product-types/ProductPage.gql'
+import {
+  ProductPageAdditionalDocument,
+  ProductPageAdditionalQuery,
+} from '@reachdigital/magento-product-types/ProductPageAdditional.gql'
+import { ProductAddToCartDocument } from '@reachdigital/magento-product/ProductAddToCart/ProductAddToCart.gql'
 import ProductPageDescription from '@reachdigital/magento-product/ProductPageDescription'
 import ProductPageGallery from '@reachdigital/magento-product/ProductPageGallery'
 import ProductPageMeta from '@reachdigital/magento-product/ProductPageMeta'
-import ProductPageRelated from '@reachdigital/magento-product/ProductPageRelated'
-import ProductPageUpsell from '@reachdigital/magento-product/ProductPageUpsell'
-import getProductStaticPaths from '@reachdigital/magento-product/getProductStaticPaths'
+import getProductStaticPaths from '@reachdigital/magento-product/ProductStaticPaths/getProductStaticPaths'
+import ProductWeight from '@reachdigital/magento-product/ProductWeight'
 import productPageCategory from '@reachdigital/magento-product/productPageCategory'
 import { ResolveUrlDocument } from '@reachdigital/magento-store/ResolveUrl.gql'
 import { StoreConfigDocument } from '@reachdigital/magento-store/StoreConfig.gql'
@@ -19,61 +27,70 @@ import localeToStore from '@reachdigital/magento-store/localeToStore'
 import BottomDrawerUi from '@reachdigital/next-ui/AppShell/BottomDrawerUi'
 import { GetStaticPaths, GetStaticProps } from '@reachdigital/next-ui/Page/types'
 import { registerRouteUi } from '@reachdigital/next-ui/PageTransition/historyHelpers'
-import clsx from 'clsx'
 import NextError from 'next/error'
 import React from 'react'
 import Page from '../../components/Page'
 import { PageByUrlDocument, PageByUrlQuery } from '../../components/Page/PageByUrl.gql'
+import ProductListItems from '../../components/ProductListItems/ProductListItems'
 import apolloClient from '../../lib/apolloClient'
 
-type Props = ProductPageQuery & PageByUrlQuery
+type Props = ProductPageQuery & ProductPageAdditionalQuery & PageByUrlQuery & ProductSimpleQuery
 type RouteProps = { url: string }
 type GetPageStaticPaths = GetStaticPaths<RouteProps>
 type GetPageStaticProps = GetStaticProps<PageLayoutProps, Props, RouteProps>
 
-function ProductPage({ products, pages }: Props) {
-  const classes = useCategoryPageStyles()
-
+function ProductSimple({ products, productAdditionals, simpleProducts, pages }: Props) {
   if (!products) return <NextError statusCode={503} title='Loading skeleton' />
 
   const product = products?.items?.[0]
+  const weight = simpleProducts?.items?.[0]?.weight
 
   if (!product) return <NextError statusCode={404} title='Product not found' />
 
   const category = productPageCategory(product)
   return (
-    <BottomDrawerUi
-      title={product.name ?? ''}
-      backFallbackHref={`/${category?.url_path}`}
-      backFallbackTitle={category?.name}
-    >
-      <Container className={clsx(classes.container)}>
-        <ProductPageMeta
-          canonical_url={product.canonical_url}
-          meta_description={product.meta_description}
-          meta_title={product.meta_title}
-          name={product.name}
-          url_key={product.url_key}
-        />
-        <ProductPageDescription
-          name={product.name}
-          description={product.description}
-          short_description={product.short_description}
-        />
-        <ProductPageGallery media_gallery={product.media_gallery} sku={product.sku} />
-        {pages?.[0] && <Page {...pages?.[0]} />}
-        <ProductPageUpsell upsell_products={product.upsell_products} />
-        <ProductPageRelated related_products={product.related_products} />
-      </Container>
-    </BottomDrawerUi>
+    <>
+      <ProductPageMeta {...product} />
+      <BottomDrawerUi
+        title={product.name ?? ''}
+        backFallbackHref={`/${category?.url_path}`}
+        backFallbackTitle={category?.name}
+      >
+        <Container>
+          <AddToCartButton
+            mutation={ProductAddToCartDocument}
+            variables={{ sku: product.sku ?? '', quantity: 1 }}
+          />
+          {/* <ProductWeight weight={weight} /> */}
+          <ProductPageDescription {...product} />
+          <ProductPageGallery {...product} />
+          {pages?.[0] && <Page {...pages?.[0]} />}
+
+          {productAdditionals?.items?.[0]?.upsell_products ? (
+            <>
+              {/* todo: create a component where we can compose in the ProductListItems */}
+              <Typography variant='h2'>Upsells</Typography>
+              <ProductListItems items={productAdditionals?.items?.[0]?.upsell_products} />
+            </>
+          ) : null}
+          {productAdditionals?.items?.[0]?.related_products ? (
+            <>
+              {/* todo: reate a component where we can compose in the ProductListItems */}
+              <Typography variant='h2'>Related</Typography>
+              <ProductListItems items={productAdditionals?.items?.[0]?.related_products} />
+            </>
+          ) : null}
+        </Container>
+      </BottomDrawerUi>
+    </>
   )
 }
 
-ProductPage.Layout = PageLayout
+ProductSimple.Layout = PageLayout
 
 registerRouteUi('/product/[url]', BottomDrawerUi)
 
-export default ProductPage
+export default ProductSimple
 
 export const getStaticPaths: GetPageStaticPaths = async ({ locales }) => {
   const localePaths =
@@ -87,7 +104,7 @@ export const getStaticPaths: GetPageStaticPaths = async ({ locales }) => {
 }
 
 export const getStaticProps: GetPageStaticProps = async ({ params, locale }) => {
-  let urlKey = params?.url ?? '??'
+  const urlKey = params?.url ?? '??'
 
   const client = apolloClient(localeToStore(locale))
   const staticClient = apolloClient(localeToStore(locale))
@@ -97,11 +114,27 @@ export const getStaticProps: GetPageStaticProps = async ({ params, locale }) => 
     query: PageByUrlDocument,
     variables: { url: `product/${urlKey}` },
   })
-  const productPage = staticClient.query({ query: ProductPageDocument, variables: { urlKey } })
-  const pageLayout = staticClient.query({ query: PageLayoutDocument })
-
-  urlKey = `${urlKey}${(await config)?.data.storeConfig?.product_url_suffix}`
-  const resolveUrl = staticClient.query({ query: ResolveUrlDocument, variables: { urlKey } })
+  const productPage = staticClient.query({
+    query: ProductPageDocument,
+    variables: { urlKey },
+  })
+  const simpleProduct = staticClient.query({
+    query: ProductSimpleDocument,
+    variables: { urlKey },
+  })
+  const productAdditionals = staticClient.query({
+    query: ProductPageAdditionalDocument,
+    variables: { urlKey },
+  })
+  const pageLayout = staticClient.query({
+    query: PageLayoutDocument,
+  })
+  const resolveUrl = staticClient.query({
+    query: ResolveUrlDocument,
+    variables: {
+      urlKey: `${urlKey}${(await config)?.data.storeConfig?.product_url_suffix}`,
+    },
+  })
 
   return {
     props: {
@@ -109,6 +142,8 @@ export const getStaticProps: GetPageStaticProps = async ({ params, locale }) => 
       ...(await pageLayout).data,
       ...(await productPage).data,
       ...(await page).data,
+      ...(await simpleProduct).data,
+      ...(await productAdditionals).data,
       apolloState: client.cache.extract(),
     },
     revalidate: 60 * 20,
