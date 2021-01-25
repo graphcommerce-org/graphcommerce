f2033f8505884fb5c0efb34ea2396454fb223a4d
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-01-25 15:58:06 +0100

    fix: wrong bottom overlay ui animation

--- packages/next-ui/AppShell/useBottomOverlayUiAnimations.tsx
+++ packages/next-ui/AppShell/useBottomOverlayUiAnimations.tsx
@@ -9,53 +9,23 @@ export type OverlayUiAnimationProps = {
 
 export default function useBottomOverlayUiAnimations(props: OverlayUiAnimationProps): MotionProps {
   const { hold, dismissed, z } = props
-  const theme = useTheme()
-  const upMd = useMediaQuery(theme.breakpoints.up('md'), { noSsr: true })
-
-  if (upMd) {
-    return !hold
-      ? {
-          initial: { x: '-20%', y: 0, z, opacity: 0, originY: 0 },
-          animate: {
-            x: '0',
-            y: 0,
-            z,
-            opacity: 1,
-            display: 'block',
-            transition: { type: 'tween', ease: 'easeOut' },
-            ...(dismissed && {
-              x: '-20%',
-              opacity: 0,
-              transition: { type: 'tween', ease: 'easeIn' },
-              transitionEnd: { display: 'none' },
-            }),
-          },
-        }
-      : {
-          initial: { opacity: 1, z, x: 0, y: 0 },
-          animate: { opacity: 1, z, x: 0, y: 0, transition: { type: 'tween', ease: 'easeOut' } },
-          exit: { opacity: 1, z, x: 0, y: 0, transition: { type: 'tween', ease: 'easeIn' } },
-        }
-  }
 
   return !hold
     ? {
         initial: {
-          y: '90%',
+          y: '100%',
           z,
-          x: 0,
           opacity: 0,
           originY: 0,
         },
         animate: {
-          y: '0',
+          y: 0,
           z,
-          x: 0,
           opacity: 1,
           display: 'block',
           transition: { type: 'tween', ease: 'easeOut' },
           ...(dismissed && {
-            y: '90%',
+            y: '100%',
             opacity: 0,
             transition: { type: 'tween', ease: 'easeIn' },
             transitionEnd: { display: 'none' },
@@ -66,21 +36,18 @@ export default function useBottomOverlayUiAnimations(props: OverlayUiAnimationPr
         initial: {
           opacity: 1,
           z,
-          x: 0,
           y: 0,
         },
         animate: {
           opacity: 1,
           z,
-          x: 0,
           y: 0,
           transition: { type: 'tween', ease: 'easeOut' },
         },
         exit: {
           opacity: 1,
-          z,
-          x: 0,
           y: 0,
+          z,
           transition: { type: 'tween', ease: 'easeIn' },
         },
       }
