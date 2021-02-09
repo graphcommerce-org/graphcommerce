02d7fb92fc7238e021240219beb04eb6c789ca98
ErwinOtten <erwin@h-o.nl>
2021-02-09 17:58:39 +0100

    feat: change homepage products to pageSize 8

--- examples/soxbase/pages/page/[url].tsx
+++ examples/soxbase/pages/page/[url].tsx
@@ -99,8 +99,7 @@ export const getStaticProps: GetPageStaticProps = async ({ locale, params }) => 
   const cat = String((await config).data.storeConfig?.root_category_id ?? '')
   const productList = staticClient.query({
     query: ProductListDocument,
-    variables: { rootCategory: cat, filters: { category_id: { eq: cat } },
-    },
+    variables: { pageSize: 8, rootCategory: cat, filters: { category_id: { eq: cat } } },
   })
 
   const { urlResolver } = (await resolveUrl).data
