d58f868ca2308d79640ec1f54216916b000755dd
ErwinOtten <erwin@h-o.nl>
2021-02-10 15:15:38 +0100

    feat: Add Asset component

---
+++ examples/soxbase/components/Asset/index.tsx
@@ -0,0 +1,42 @@
+import { imageMimeTypes, ImageMimeTypes } from '@reachdigital/next-ui/PictureResponsive'
+import PictureResponsiveNext, {
+  PictureResponsiveNextProps,
+} from '@reachdigital/next-ui/PictureResponsiveNext'
+import React from 'react'
+import { AssetFragment } from './Asset.gql'
+
+type ImageAsset = Pick<AssetFragment, 'url'> & {
+  mimeType: ImageMimeTypes
+  width: number
+  height: number
+}
+
+function isImage(asset: AssetFragment): asset is ImageAsset {
+  return !!(
+    asset.width &&
+    asset.height &&
+    imageMimeTypes.includes(asset.mimeType as ImageMimeTypes)
+  )
+}
+
+type AssetProps = {
+  asset: AssetFragment
+} & Omit<PictureResponsiveNextProps, 'type' | 'src' | 'height'>
+
+export default function Asset(props: AssetProps) {
+  const { asset, width, ...imgProps } = props
+
+  if (isImage(asset)) {
+    return (
+      <PictureResponsiveNext
+        type={asset.mimeType}
+        src={asset.url}
+        height={Math.round((asset.height / asset.width) * width)}
+        width={width}
+        {...imgProps}
+      />
+    )
+  }
+
+  return <div>{asset.mimeType} not supported</div>
+}
