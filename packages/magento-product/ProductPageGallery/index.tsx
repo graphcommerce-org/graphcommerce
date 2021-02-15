92bb08680dffd2a6505dfa01a7b381685a911dbf
ErwinOtten <erwin@h-o.nl>
2021-02-15 22:33:50 +0100

    fix: rebuild as grid

--- packages/magento-product/ProductPageGallery/index.tsx
+++ packages/magento-product/ProductPageGallery/index.tsx
@@ -1,4 +1,4 @@
-import { Fab, makeStyles, Theme } from '@material-ui/core'
+import { Fab, makeStyles, Theme, useMediaQuery, useTheme } from '@material-ui/core'
 import RenderType, { TypeRenderer } from '@reachdigital/next-ui/RenderType'
 import responsiveVal from '@reachdigital/next-ui/Styles/responsiveVal'
 import { m } from 'framer-motion'
@@ -10,19 +10,20 @@ import ProductVideo from './ProductVideo'
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     display: 'grid',
+    gridAutoFlow: 'row',
+    [theme.breakpoints.up('md')]: {
+      gridTemplateColumns: '5fr 3fr',
+    },
     marginBottom: theme.spacings.lg,
     paddingLeft: 0,
     background: 'rgba(0,0,0,0.03)',
+    minHeight: responsiveVal(400, 800),
+    '& > div': {
+      position: 'relative',
+    },
   },
   gallery: {
-    position: 'relative',
-    float: 'left',
-    width: '100vw',
-    height: '104vw',
-    [theme.breakpoints.up('md')]: {
-      width: `calc(100% - ${responsiveVal(500, 600)})`,
-      height: '100%',
-    },
+    width: '100%',
     overflowX: 'auto',
     overflowY: 'hidden',
     whiteSpace: 'nowrap',
@@ -38,16 +39,13 @@ const useStyles = makeStyles((theme: Theme) => ({
   expandButton: {
     visibility: 'hidden',
     [theme.breakpoints.up('md')]: {
-      position: 'absolute',
-      transform: 'translateX(-100%)',
-      right: theme.spacings.xs,
       visibility: 'visible',
-      marginTop: theme.spacings.sm,
+      position: 'absolute',
+      top: theme.spacings.sm,
+      right: theme.spacings.sm,
     },
   },
   sidebar: {
-    float: 'right',
-    width: responsiveVal(200, 600),
     height: '100%',
     display: 'grid',
     alignContent: 'center',
@@ -73,23 +71,26 @@ export default function ProductPageGallery(props: ProductPageGalleryProps) {
   const classes = useStyles()
   const { media_gallery, sku, children } = props
   const [expanded, setExpanded] = useState(true)
-  const variants = {
-    collapsed: { width: `calc(100% - ${responsiveVal(500, 600)})` },
-    expanded: { width: '100%', transition: { type: 'spring', mass: 0.5 } },
+  const theme = useTheme()
+  const upMd = useMediaQuery(theme.breakpoints.up('md'), { noSsr: true })
+
+  const gallery = {
+    collapsed: { gridTemplateColumns: '5fr 3fr', transition: { type: 'spring', duration: 0.2 } },
+    expanded: { gridTemplateColumns: '1fr 0fr', transition: { type: 'spring', duration: 0 } },
   }
   const sidebar = {
-    view: { display: 'grid' },
+    view: { display: 'grid', transition: { delay: 0.2 } },
     hide: { display: 'none' },
   }
 
   return (
-    <div className={classes.root}>
+    <m.div
+      className={classes.root}
+      animate={expanded ? 'collapsed' : 'expanded'}
+      variants={gallery}
+    >
       <div>
-        <m.div
-          className={classes.gallery}
-          animate={expanded ? 'collapsed' : 'expanded'}
-          variants={variants}
-        >
+        <div className={classes.gallery}>
           {media_gallery?.map((item) => {
             if (!item?.position) return null
             return (
@@ -101,36 +102,36 @@ export default function ProductPageGallery(props: ProductPageGalleryProps) {
               />
             )
           })}
-          <Fab
-            aria-label='zoom'
-            color='inherit'
-            size='medium'
-            className={classes.expandButton}
-            onClick={() => setExpanded(!expanded)}
-          >
-            {expanded ? (
-              <img
-                src='/icons/desktop_expand.svg'
-                alt='account'
-                width={32}
-                height={32}
-                loading='eager'
-              />
-            ) : (
-              <img
-                src='/icons/desktop_collapse.svg'
-                alt='account'
-                width={32}
-                height={32}
-                loading='eager'
-              />
-            )}
-          </Fab>
-        </m.div>
-        <m.div className={classes.sidebar} animate={expanded ? 'view' : 'hide'} variants={sidebar}>
-          {children}
-        </m.div>
+        </div>
+        <Fab
+          aria-label='zoom'
+          color='inherit'
+          size='medium'
+          className={classes.expandButton}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? (
+            <img
+              src='/icons/desktop_expand.svg'
+              alt='account'
+              width={32}
+              height={32}
+              loading='eager'
+            />
+          ) : (
+            <img
+              src='/icons/desktop_collapse.svg'
+              alt='account'
+              width={32}
+              height={32}
+              loading='eager'
+            />
+          )}
+        </Fab>
       </div>
-    </div>
+      <m.div className={classes.sidebar} animate={expanded ? 'view' : 'hide'} variants={sidebar}>
+        {children}
+      </m.div>
+    </m.div>
   )
 }
