51d88c09687dccd69cdd7db04750472ab052d460
ErwinOtten <erwin@h-o.nl>
2021-02-22 12:31:17 +0100

    feat: add RowProductSpecs component

---
+++ examples/soxbase/components/RowProductSpecs/index.tsx
@@ -0,0 +1,43 @@
+import { Container, Theme, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import ProductSpecs, { ProductSpecsProps } from '@reachdigital/magento-product/ProductSpecs'
+import responsiveVal from '@reachdigital/next-ui/Styles/responsiveVal'
+import { RowProductSpecsFragment } from './RowProductSpecs.gql'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    marginBottom: `${theme.spacings.xl}`,
+    ...theme.typography.body1,
+  },
+  title: {
+    ...theme.typography.caption,
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    marginBottom: theme.spacings.sm,
+    paddingBottom: theme.spacings.sm,
+  },
+  specs: {
+    display: 'grid',
+    justifyContent: 'start',
+  },
+}))
+
+type RowProductSpecsProps = RowProductSpecsFragment & ProductSpecsProps
+
+export default function RowProductSpecs(props: RowProductSpecsProps) {
+  const { aggregations } = props
+  const classes = useStyles()
+  if (!aggregations) {
+    return null
+  }
+
+  return (
+    <Container className={classes.root}>
+      <Typography variant='h3' className={classes.title}>
+        Product specifications
+      </Typography>
+      <div className={classes.specs}>
+        <ProductSpecs aggregations={aggregations} />
+      </div>
+    </Container>
+  )
+}
