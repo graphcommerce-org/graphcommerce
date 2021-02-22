f399c964a36ef1c91e4fa44a3a1db8056499f6cd
ErwinOtten <erwin@h-o.nl>
2021-02-22 12:30:40 +0100

    chore: simplyfy productpage description core component

--- packages/magento-product/ProductPageDescription/index.tsx
+++ packages/magento-product/ProductPageDescription/index.tsx
@@ -1,56 +1,23 @@
-import { makeStyles, Container, Theme, Typography } from '@material-ui/core'
-import React, { PropsWithChildren } from 'react'
+import { makeStyles, Theme } from '@material-ui/core'
+import React from 'react'
 import { ProductPageDescriptionFragment } from './ProductPageDescription.gql'
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    display: 'grid',
-    gap: `${theme.spacings.lg} 0`,
-    gridTemplateAreas: `"title"
-    "description"
-    "usps"`,
-    marginTop: theme.spacings.xxl,
-    marginBottom: theme.spacings.xxl,
-    [theme.breakpoints.up('md')]: {
-      gridTemplateAreas: `"title ."
-      "description usps"`,
-      gridTemplateColumns: '3fr 1fr',
-      gap: `${theme.spacings.lg} ${theme.spacings.xl}`,
-    },
-  },
   description: {
     gridArea: 'description',
     '& p:first-of-type': {
       marginTop: 0,
     },
-    '& p, & li': {
-      ...theme.typography.h4,
-      fontWeight: 400,
-      [theme.breakpoints.up('md')]: {
-        ...theme.typography.h3,
-        fontWeight: 400,
-      },
-    },
-    '& ul': {
-      padding: 0,
-      margin: 0,
-      display: 'inline',
-      listStyleType: 'none',
-    },
-    '& li': {
-      display: 'inline',
-    },
   },
 }))
 
-type ProductPageDescriptionProps = PropsWithChildren<unknown> & ProductPageDescriptionFragment
+export type ProductPageDescriptionProps = ProductPageDescriptionFragment
 
 export default function ProductPageDescription(props: ProductPageDescriptionProps) {
   const classes = useStyles()
-  const { description, name, children } = props
+  const { description } = props
   return (
-    <Container className={classes.root}>
-      <Typography variant='h2'>{name ?? ''}</Typography>
+    <>
       {/* eslint-disable-next-line react/no-danger */}
       {description && (
         <div
@@ -58,7 +25,6 @@ export default function ProductPageDescription(props: ProductPageDescriptionProp
           dangerouslySetInnerHTML={{ __html: description.html }}
         />
       )}
-      {children && children}
-    </Container>
+    </>
   )
 }
