595321b3c20f26ff94c59d42854e3a724b80c3eb
Paul Hachmang <paul@reachdigital.nl>
2021-02-26 10:50:55 +0100

    fix: make sure the image scales correctly

--- packages/next-ui/FramerSlider/SliderImage.tsx
+++ packages/next-ui/FramerSlider/SliderImage.tsx
@@ -1,34 +1,47 @@
 import { makeStyles } from '@material-ui/core'
+import { Portrait } from '@material-ui/icons'
 import clsx from 'clsx'
+import { m } from 'framer-motion'
 import * as React from 'react'
 import { useSliderContext } from './SliderContext'
 
 const useStyles = makeStyles(
   () => ({
     root: {
-      '& picture': {
+      // '& picture': {
+      // },
+    },
+    aspectWrapper: {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    aspect: {
+      overflow: 'hidden',
+      position: 'relative',
+      '& > *': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      },
+      '& img, video': {
+        display: 'block',
         width: '100%',
         height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        pointerEvents: 'none',
       },
     },
     landscape: {
-      '& img, video': {
-        display: 'block',
-        width: '100%',
-        height: 'auto',
-        pointerEvents: 'none',
-      },
+      height: '0',
+      width: '100%',
     },
-    portait: {
-      '& img, video': {
-        display: 'block',
-        width: 'auto',
-        height: '100%',
-        pointerEvents: 'none',
-      },
+    portrait: {
+      height: '100%',
+      width: '0',
     },
   }),
   { name: 'SliderImage' },
@@ -43,16 +56,27 @@ type SliderImageProps = React.PropsWithChildren<{
 export default function SliderImage(props: SliderImageProps) {
   const classes = useStyles()
   const [{ containerSize }] = useSliderContext()
-  const { children, width, height } = props
+  const { children, width, height, animating } = props
 
   const containerRatio = (containerSize?.width ?? 1) / (containerSize?.height ?? 1)
   const ratio = width / height
 
-  const portait = containerRatio > ratio
+  const portrait = containerRatio > ratio
 
   return (
-    <div className={clsx(classes.root, portait ? classes.portait : classes.landscape)}>
-      {children}
+    <div className={clsx(classes.root)}>
+      <div className={classes.aspectWrapper}>
+        <m.div
+          layout={animating}
+          className={clsx(classes.aspect, portrait ? classes.portrait : classes.landscape)}
+          style={{
+            ...(portrait && { paddingLeft: `${Math.round((width / height) * 100)}%` }),
+            ...(!portrait && { paddingTop: `${Math.round((height / width) * 100)}%` }),
+          }}
+        >
+          {children}
+        </m.div>
+      </div>
     </div>
   )
 }
