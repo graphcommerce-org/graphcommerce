fbcb51856afa81188bc292bd49fad5b164b5fcc7
ErwinOtten <erwin@h-o.nl>
2021-02-26 14:38:25 +0100

    feat: inline cart svg

--- packages/magento-cart/CartFab.tsx
+++ packages/magento-cart/CartFab.tsx
@@ -17,7 +17,22 @@ function CartFabContent(props: CartFabProps) {
     <PageLink href='/cart'>
       <Fab aria-label='Cart' color='inherit' size='medium' {...fabProps}>
         <Badge badgeContent={qty || 0} color='primary' variant='dot'>
-          {icon}
+          <svg width='32px' height='32px' viewBox='0 0 32 32' version='1.1'>
+            <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
+              <path
+                d='M22.7211388,13.8851398 L23.5468356,26.00354 L8.74931805,26.00354 L9.57501488,13.8851398 L22.7211388,13.8851398 Z'
+                stroke='#000000'
+                strokeWidth='1.4'
+              />
+              <path
+                d='M12.4444055,14.953 L12.4444055,11.4552028 C12.4444055,9.54694698 13.9458954,8 15.798077,8 C17.6502587,8 19.1517485,9.54694698 19.1517485,11.4552028 L19.1517485,14.9497766'
+                stroke='#000000'
+                strokeWidth='1.4'
+                strokeLinecap='square'
+                strokeLinejoin='bevel'
+              />
+            </g>
+          </svg>
         </Badge>
       </Fab>
     </PageLink>
