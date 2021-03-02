ea1228c7626ac4b0e3614cb8f7d124dfdc8fc191
Paul Hachmang <paul@reachdigital.nl>
2021-03-02 19:57:31 +0100

    refactor: implment RowContentLinks with FramerSlider

--- examples/soxbase/components/RowContentLinks/index.tsx
+++ examples/soxbase/components/RowContentLinks/index.tsx
@@ -1,12 +1,16 @@
-import { Container, Theme, Typography } from '@material-ui/core'
+import { Container, Link, Theme, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import MultiItemSlider from '@reachdigital/next-ui/FramerSlider/variants/MultiItemSlider'
-import NextUiPageLink from '@reachdigital/next-ui/PageTransition/PageLink'
+import PageLink from '@reachdigital/next-ui/PageTransition/PageLink'
 import { RowContentLinksFragment } from './RowContentLinks.gql'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
-    container: {
+    root: {
+      padding: 0,
+    },
+    scroller: {
+      padding: `0 ${theme.page.horizontal}`,
       marginBottom: `${theme.spacings.lg}`,
       display: 'grid',
       gridAutoFlow: 'column',
@@ -14,19 +18,11 @@ const useStyles = makeStyles(
       gap: `${theme.spacings.md}`,
       alignContent: 'center',
       '& > *': {
-        whiteSpace: 'no-wrap',
         minWidth: 'max-content',
       },
     },
     title: {
-      ...theme.typography.body1,
-      color: theme.palette.text.primary,
       fontWeight: 600,
-    },
-    url: {
-      ...theme.typography.body1,
-      color: theme.palette.text.primary,
-      textDecoration: 'none',
     },
   }),
   { name: 'RowContentLinks' },
@@ -39,18 +35,18 @@ export default function RowContentLinks(props: RowContentLinksProps) {
   const classes = useStyles()
 
   return (
-    <Container maxWidth={false}>
-      <MultiItemSlider classes={{ scroller: classes.container }} scrollSnapAlign='start'>
-        <Typography variant='h4' className={classes.title}>
+    <Container maxWidth={false} className={classes.root}>
+      <MultiItemSlider classes={{ scroller: classes.scroller }} scrollSnapAlign={false}>
+        <Typography variant='body1' component='h4' className={classes.title}>
           {title}
         </Typography>
 
         {contentLinks.map((contentLink) => (
-          <NextUiPageLink href={contentLink.url} key={contentLink.url}>
-            <a key={contentLink.url} href={contentLink.url} className={classes.url}>
+          <PageLink href={contentLink.url} key={contentLink.url}>
+            <Link key={contentLink.url} href={contentLink.url} variant='body1' color='inherit'>
               {contentLink.title}
-            </a>
-          </NextUiPageLink>
+            </Link>
+          </PageLink>
         ))}
       </MultiItemSlider>
     </Container>
