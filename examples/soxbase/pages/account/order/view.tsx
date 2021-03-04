e098dfed593c48604f1c3905f294bbec27c3b075
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-03-04 11:57:22 +0100

    feat: ordered items ‘view more’ functionality

--- examples/soxbase/pages/account/order/view.tsx
+++ examples/soxbase/pages/account/order/view.tsx
@@ -1,19 +1,45 @@
 import { useQuery } from '@apollo/client'
-import { Container, NoSsr } from '@material-ui/core'
+import { Button, Container, Link, makeStyles, NoSsr, Theme } from '@material-ui/core'
 import PageLayout from '@reachdigital/magento-app-shell/PageLayout'
 import useOrderCardItemImages from '@reachdigital/magento-customer/OrderCardItemImage/useOrderCardItemImages'
 import { OrderDetailPageDocument } from '@reachdigital/magento-customer/OrderDetailPage/OrderDetailPage.gql'
 import OrderedItem from '@reachdigital/magento-customer/OrderedItem'
 import PageMeta from '@reachdigital/magento-store/PageMeta'
+import AnimatedRow from '@reachdigital/next-ui/AnimatedRow'
 import OverlayUi from '@reachdigital/next-ui/AppShell/OverlayUi'
 import { registerRouteUi } from '@reachdigital/next-ui/PageTransition/historyHelpers'
 import SectionContainer from '@reachdigital/next-ui/SectionContainer'
+import clsx from 'clsx'
+import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    sectionContainer: {
+      marginTop: theme.spacings.md,
+      marginBottom: theme.spacings.md,
+    },
+    orderedItemsInnerContainer: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    viewAllButton: {
+      margin: `${theme.spacings.xs} auto 0 auto`,
+      textAlign: 'center',
+      '& a': {
+        padding: 8,
+      },
+    },
+  }),
+  {
+    name: 'OrderDetailPage',
+  },
+)
 
 const OrderDetailPage = () => {
   const router = useRouter()
   const { orderId } = router.query
+  const classes = useStyles()
 
   const { data, loading } = useQuery(OrderDetailPageDocument, {
     variables: {
@@ -23,8 +49,10 @@ const OrderDetailPage = () => {
 
   const orders = data?.customer?.orders
   const order = orders?.items?.[0]
+  const maxItemsAboveFold = 4
 
   const orderedItemsImages = useOrderCardItemImages(orders)
+  const [collapseOrderedItems, setCollapseOrderedItems] = useState<boolean>(true)
 
   return (
     <OverlayUi title='Orders' variant='bottom' fullHeight>
@@ -37,17 +65,53 @@ const OrderDetailPage = () => {
         <NoSsr>
           {loading && 'Loading'}
 
-          <SectionContainer label='Ordered items' /* endLabel='SHIPPED'*/>
-            {order?.items?.map((orderedItem) => (
-              <div key={`orderedItem-${orderedItem?.product_sku}`}>
-                {orderedItem && (
-                  <OrderedItem
-                    {...orderedItem}
-                    {...orderedItemsImages[orderedItem?.product_url_key ?? '']}
-                  />
-                )}
+          <SectionContainer
+            label='Ordered items'
+            /* endLabel='SHIPPED'*/ className={classes.sectionContainer}
+          >
+            <div className={classes.orderedItemsInnerContainer}>
+              <AnimatePresence initial={false}>
+                {order?.items?.slice(0, maxItemsAboveFold).map((orderedItem) => (
+                  <AnimatedRow key={`orderedItem-${orderedItem?.product_sku}`}>
+                    {orderedItem && (
+                      <OrderedItem
+                        {...orderedItem}
+                        {...orderedItemsImages[orderedItem?.product_url_key ?? '']}
+                      />
+                    )}
+                  </AnimatedRow>
+                ))}
+
+                {!collapseOrderedItems &&
+                  order?.items
+                    ?.slice(maxItemsAboveFold, order?.items?.length)
+                    .map((orderedItem) => (
+                      <AnimatedRow key={`orderedItem-${orderedItem?.product_sku}`}>
+                        {orderedItem && (
+                          <OrderedItem
+                            {...orderedItem}
+                            {...orderedItemsImages[orderedItem?.product_url_key ?? '']}
+                          />
+                        )}
+                      </AnimatedRow>
+                    ))}
+              </AnimatePresence>
+            </div>
+
+            {order?.items && maxItemsAboveFold < order?.items?.length && (
+              <div className={classes.viewAllButton}>
+                <Link
+                  href='#'
+                  color='primary'
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setCollapseOrderedItems(!collapseOrderedItems)
+                  }}
+                >
+                  View all items
+                </Link>
               </div>
-            ))}
+            )}
           </SectionContainer>
         </NoSsr>
       </Container>
