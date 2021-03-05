9324507c3c149fbcb7cd51ac41250a8637521ceb
Paul Hachmang <paul@reachdigital.nl>
2021-03-05 11:31:30 +0100

    feat: support vscode graphql extension

--- examples/soxbase-api/apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    includes: ['./components/**/*.graphql'],
-    service: {
-      name: 'm2-pwa',
-      localSchemaFile: './generated/schema.graphql',
-    },
-  },
-}
