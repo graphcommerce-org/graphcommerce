d3b0134d392eb77b7dd9a1bc7f0818a9936eb972
Paul Hachmang <paul@reachdigital.nl>
2021-03-05 12:05:42 +0100

    fix: use deployed url for the sitemap

--- examples/soxbase/next-sitemap.js
+++ examples/soxbase/next-sitemap.js
@@ -1,6 +1,7 @@
 module.exports = {
-  siteUrl: 'https://graphcommerce.vercel.app',
+  siteUrl: process.env.VERCEL_URL ?? 'http://localhost:3000',
   generateRobotsTxt: true,
+  exclude: ['*/account*', '*/cart*', '*/checkout*', '*/test'],
   robotsTxtOptions: {
     policies: [{ userAgent: '*', disallow: '/' }],
   },
