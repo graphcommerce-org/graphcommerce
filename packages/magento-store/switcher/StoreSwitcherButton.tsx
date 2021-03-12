3e18037b798f9bab909406814e0db55ae59fd9e1
Deam Kop <deam@reachdigital.nl>
2021-03-12 15:31:59 +0100

    refactor: storeswitcher and storeswitcherbutton

--- packages/magento-store/switcher/StoreSwitcherButton.tsx
+++ packages/magento-store/switcher/StoreSwitcherButton.tsx
@@ -1,35 +1,17 @@
 import { useQuery } from '@apollo/client'
-import { Avatar, makeStyles, Theme } from '@material-ui/core'
 import Button from '@reachdigital/next-ui/Button'
+import FlagAvatar from '@reachdigital/next-ui/FlagAvatar'
 import PageLink from '@reachdigital/next-ui/PageTransition/PageLink'
 import React from 'react'
 import { StoreConfigDocument } from '../StoreConfig.gql'
 
-const useStyles = makeStyles(
-  (theme: Theme) => ({
-    avatar: {
-      width: theme.spacing(3),
-      height: theme.spacing(3),
-      marginRight: theme.spacing(1),
-    },
-  }),
-  { name: 'StoreSwitcherButton' },
-)
-
 export default function StoreSwitcherButton() {
   const config = useQuery(StoreConfigDocument)
-  const classes = useStyles()
-
-  const country = config.data?.storeConfig?.locale?.split('_')[1].toLowerCase()
 
   return (
     <PageLink href='/switch-stores'>
       <Button variant='pill' size='medium'>
-        <Avatar
-          className={classes.avatar}
-          alt={country}
-          src={`https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/4x3/${country}.svg`}
-        />
+        <FlagAvatar flagSrc={config.data?.storeConfig?.locale} />
         {config.data?.storeConfig?.store_name} - {config.data?.storeConfig?.base_currency_code}
       </Button>
     </PageLink>
