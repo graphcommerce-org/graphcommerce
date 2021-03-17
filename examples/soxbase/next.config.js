c174841f9b032779fc64d351aa61fdf12101743b
Paul Hachmang <paul@reachdigital.nl>
2021-03-17 17:40:16 +0100

    chore: remove process.env

--- examples/soxbase/next.config.js
+++ examples/soxbase/next.config.js
@@ -6,8 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-console.log(process.env)
-
 require('dotenv').config()
 
 const withImages = require('next-images')
