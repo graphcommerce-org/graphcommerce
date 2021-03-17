d83adef7dd4418ef3153ab77d8be59c0957d2803
Paul Hachmang <paul@reachdigital.nl>
2021-03-17 14:23:11 +0100

    chore: log process.env to see what variables we have available

--- examples/soxbase/next.config.js
+++ examples/soxbase/next.config.js
@@ -6,6 +6,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+console.log(process.env)
+
 require('dotenv').config()
 
 const withImages = require('next-images')
