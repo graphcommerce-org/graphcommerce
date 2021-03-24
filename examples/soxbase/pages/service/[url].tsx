b6356129dcc640164cd98ea0fc0805cbf9bc39b9
Deam <deamus@gmail.com>
2021-03-24 15:21:44 +0100

    feat: change faq pages to service

--- examples/soxbase/pages/service/[url].tsx
+++ examples/soxbase/pages/service/[url].tsx
@@ -1,4 +1,6 @@
+import { Box, makeStyles, Theme, Typography } from '@material-ui/core'
 import PageLayout, { PageLayoutProps } from '@reachdigital/magento-app-shell/PageLayout'
+import PageMeta from '@reachdigital/magento-store/PageMeta'
 import { StoreConfigDocument } from '@reachdigital/magento-store/StoreConfig.gql'
 import { GetStaticProps } from '@reachdigital/next-ui/Page/types'
 import { registerRouteUi } from '@reachdigital/next-ui/PageTransition/historyHelpers'
@@ -6,6 +8,7 @@ import { GetStaticPaths } from 'next'
 import NextError from 'next/error'
 import React from 'react'
 import FullPageUi from '../../components/AppShell/FullPageUi'
+import OverlayPage from '../../components/AppShell/OverlayPage'
 import { DefaultPageDocument, DefaultPageQuery } from '../../components/GraphQL/DefaultPage.gql'
 import PageContent from '../../components/PageContent'
 import apolloClient from '../../lib/apolloClient'
@@ -15,15 +18,42 @@ type RouteProps = { url: string }
 type GetPageStaticPaths = GetStaticPaths<RouteProps>
 type GetPageStaticProps = GetStaticProps<PageLayoutProps, Props, RouteProps>
 
+const useStyles = makeStyles((theme: Theme) => ({
+  title: {
+    textAlign: 'center',
+    padding: `${theme.spacings.md} 0`,
+  },
+  box: {
+    maxWidth: 820,
+    margin: '0 auto',
+  },
+}))
+
 const ServicePage = ({ pages }: Props) => {
+  const classes = useStyles()
   if (!pages) return <NextError statusCode={503} title='Loading skeleton' />
   if (!pages?.[0]) return <NextError statusCode={404} title='Page not found' />
-  const page = pages[0]
+  const title = pages?.[0].title ?? ''
 
   return (
-    <FullPageUi title={page.title ?? ''} backFallbackTitle='Home' backFallbackHref='/'>
+    <OverlayPage
+      title={title}
+      variant='left'
+      backFallbackTitle='Customer Service'
+      backFallbackHref='/service/index'
+    >
+      <PageMeta title={title} metaDescription={title} metaRobots={['noindex']} />
+
+      {title && (
+        <Box component='div' whiteSpace='normal' className={classes.box}>
+          <Typography variant='h2' component='h1' className={classes.title}>
+            {title}
+          </Typography>
+        </Box>
+      )}
+
       <PageContent {...pages[0]} />
-    </FullPageUi>
+    </OverlayPage>
   )
 }
 
@@ -43,19 +73,18 @@ export const getStaticPaths: GetPageStaticPaths = async ({ locales = [] }) => {
 }
 
 export const getStaticProps: GetPageStaticProps = async ({ locale, params }) => {
-  const urlKey = params?.url ?? '??'
+  const url = `service/${params?.url}`
   const client = apolloClient(locale, true)
   const staticClient = apolloClient(locale)
 
   const conf = client.query({ query: StoreConfigDocument })
   const page = staticClient.query({
     query: DefaultPageDocument,
-    variables: {
-      url: `service/${urlKey}`,
-      rootCategory: (await conf).data.storeConfig?.root_category_uid ?? '',
-    },
+    variables: { url, rootCategory: (await conf).data.storeConfig?.root_category_uid ?? '' },
   })
+
   if (!(await page).data.pages?.[0]) return { notFound: true }
+
   return {
     props: {
       ...(await page).data,
