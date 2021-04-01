2eaf131816d3ea7322cf2568b40456082a4b1121
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-04-01 17:17:36 +0200

    fix: sitemap on build
    fix: sitemap still includes test routes

--- examples/soxbase/next-sitemap.js
+++ examples/soxbase/next-sitemap.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteUrl: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
   generateRobotsTxt: true,
-  exclude: ['*/account*', '*/cart*', '*/checkout*', '*/test'],
+  exclude: ['*/account*', '*/cart*', '*/checkout*', '*/test/*', '/test/*'],
   robotsTxtOptions: {
     policies: [{ userAgent: '*', disallow: '/' }],
   },
