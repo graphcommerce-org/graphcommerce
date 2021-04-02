14e4b931060d74eeff60ceee69f546a057efc8c1
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-04-02 09:23:40 +0200

    fix: correct sitemap url on production

--- examples/soxbase/next-sitemap.js
+++ examples/soxbase/next-sitemap.js
@@ -1,5 +1,12 @@
+const siteUrls = {
+  development: process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000',
+  production: 'https://graphcommerce.vercel.app',
+}
+
 module.exports = {
-  siteUrl: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
+  siteUrl: siteUrls[process.env.NODE_ENV] || siteUrls.development,
   generateRobotsTxt: true,
   exclude: ['*/account*', '*/cart*', '*/checkout*', '*/test/*', '/test/*'],
   robotsTxtOptions: {
