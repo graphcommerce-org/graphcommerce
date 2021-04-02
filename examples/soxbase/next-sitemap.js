7d64c34c4a76f9fc00c55f180a392e2f743674d1
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-04-02 10:13:49 +0200

    fix: correct sitemap production url
    chore: added NEXT_PUBLIC_SITE_URL environment variable

--- examples/soxbase/next-sitemap.js
+++ examples/soxbase/next-sitemap.js
@@ -2,7 +2,7 @@ const siteUrls = {
   development: process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : 'http://localhost:3000',
-  production: 'https://graphcommerce.vercel.app',
+  production: process.env.NEXT_PUBLIC_SITE_URL,
 }
 
 module.exports = {
