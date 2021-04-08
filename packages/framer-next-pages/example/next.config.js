b801856d370804616a986a6a66ba88ae4f193b4b
Paul Hachmang <paul@reachdigital.nl>
2021-04-08 13:31:41 +0200

    feat(framer-next-pages): move example to @reachdigital/next-config

--- packages/framer-next-pages/example/next.config.js
+++ packages/framer-next-pages/example/next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withGraphCommerce = require('@reachdigital/next-config').withGraphCommerce()
+
+module.exports = withGraphCommerce({
   experimental: {
     scrollRestoration: true,
   },
   future: {
     webpack5: true,
   },
-}
+})
