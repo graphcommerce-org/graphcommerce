5b687d1cf5a04dfa2f499d1335404cd41992a7aa
Paul Hachmang <paul@reachdigital.nl>
2021-04-11 12:45:23 +0200

    feat(framer-next-pages): animate drag indicator when resizing height

--- packages/framer-next-pages/Sheet/DragIndicator.tsx
+++ packages/framer-next-pages/Sheet/DragIndicator.tsx
@@ -1,4 +1,11 @@
-import { motion, MotionStyle, useSpring, useTransform, useVelocity } from 'framer-motion'
+import {
+  motion,
+  MotionStyle,
+  MotionValue,
+  useSpring,
+  useTransform,
+  useVelocity,
+} from 'framer-motion'
 import { useSheetContext } from './SheetContext'
 import { SPRING_ANIM } from './animation'
 
@@ -10,18 +17,21 @@ const indicator: MotionStyle = {
 }
 
 export default function DragIndicator() {
-  const { y } = useSheetContext()
+  const { y, height } = useSheetContext()
 
-  const rotateLeft = useSpring(
-    useTransform(useVelocity(y), (v) => {
-      let deg = 0
-      if (v > 10) deg = 10
-      if (v < -10) deg = -10
-      return deg
-    }),
-    SPRING_ANIM,
-  )
-  const rotateRight = useTransform(rotateLeft, (v) => v * -1)
+  const vh = useTransform(useVelocity(height), (v) => v * -1)
+  const vy = useVelocity(y)
+
+  const degLeft = useTransform([vh, vy] as MotionValue[], ([dy, dh]: number[]) => {
+    let deg = 0
+    if (dy > 10 || dh > 10) deg = 10
+    if (dy < -10 || dh < -10) deg = -10
+    return deg
+  })
+  const degRight = useTransform(degLeft, (v) => v * -1)
+
+  const rotateLeft = useSpring(degLeft, SPRING_ANIM)
+  const rotateRight = useSpring(degRight, SPRING_ANIM)
 
   return (
     <>
