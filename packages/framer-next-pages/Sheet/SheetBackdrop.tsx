96af2d4dcab87f952f9d3621a7cce9099f9310f6
Paul Hachmang <paul@reachdigital.nl>
2021-04-12 17:10:18 +0200

    feat(framer-next-pages): animate the SheetBackdrop based on the y-value of the sheet

--- packages/framer-next-pages/Sheet/SheetBackdrop.tsx
+++ packages/framer-next-pages/Sheet/SheetBackdrop.tsx
@@ -1,4 +1,5 @@
-import { motion, MotionProps, MotionStyle } from 'framer-motion'
+import { motion, MotionProps, MotionStyle, MotionValue, useTransform } from 'framer-motion'
+import { useSheetContext } from './SheetContext'
 
 const styles: MotionStyle = {
   position: 'fixed',
@@ -15,14 +16,14 @@ type SheetBackdropProps = Omit<MotionProps, 'initial' | 'animate' | 'exit'>
 
 export default function SheetBackdrop(props: SheetBackdropProps) {
   const { style } = props
+  const { y, height } = useSheetContext()
 
-  return (
-    <motion.div
-      {...props}
-      style={{ ...styles, ...style }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    />
-  )
+  // We animate the opacity of the Backdrop based on the position of the sheet
+  const opacity = useTransform([y, height] as MotionValue[], ([yv, hv]: number[]) => {
+    if (hv === 0) return 0
+    if (yv === 0) return 1
+    return 1 - yv / hv
+  })
+
+  return <motion.div {...props} style={{ ...styles, ...style, opacity }} />
 }
