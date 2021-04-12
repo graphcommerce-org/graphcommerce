2d6991fa8aef7f51e2de8d75d4947a8a72802c58
Paul Hachmang <paul@reachdigital.nl>
2021-04-12 16:30:49 +0200

    chore(FramerModalSheet): remove components

--- packages/next-ui/FramerModalSheet/index.tsx
+++
@@ -1,9 +0,0 @@
-export { default as SheetBackdrop } from './SheetBackdrop'
-export { default as SheetContainer } from './SheetContainer'
-export { default as SheetContent } from './SheetContent'
-export { default as SheetHeader } from './SheetHeader'
-export { default as Sheet } from './Sheet'
-
-export type SheetRef = {
-  snapTo: (index: number) => void
-}
