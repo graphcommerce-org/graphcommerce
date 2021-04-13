275563536984573cae6a5133061cd205fb2db4fd
Deam <deamus@gmail.com>
2021-04-13 16:06:21 +0200

    refactor: make slidergallery more abstract

--- packages/magento-product/ProductPageGallery/index.tsx
+++ packages/magento-product/ProductPageGallery/index.tsx
@@ -1,54 +1,10 @@
-import { makeStyles, Theme } from '@material-ui/core'
 import SliderImage from '@reachdigital/next-ui/FramerSlider/SliderImage'
 import SidebarGallery from '@reachdigital/next-ui/FramerSlider/variants/SidebarGallery'
 import RenderType, { TypeRenderer } from '@reachdigital/next-ui/RenderType'
-import responsiveVal from '@reachdigital/next-ui/Styles/responsiveVal'
 import React, { PropsWithChildren } from 'react'
 import ProductImage from './ProductImage'
 import { ProductPageGalleryFragment } from './ProductPageGallery.gql'
 import ProductVideo from './ProductVideo'
-
-const useStyles = makeStyles(
-  (theme: Theme) => ({
-    root: {
-      display: 'grid',
-      gridAutoFlow: 'row',
-      [theme.breakpoints.up('md')]: {
-        gridTemplateColumns: '5.5fr 2.5fr',
-      },
-      background: 'rgba(0,0,0,0.03)',
-      marginBottom: theme.spacings.lg,
-      height: responsiveVal(900, 1200),
-    },
-    container: {
-      position: 'relative',
-      zIndex: 10,
-      background: theme.palette.neutral.default,
-      overflow: 'hidden',
-      width: '100%',
-      height: '100%',
-    },
-    containerZoomed: {
-      zIndex: 12,
-      width: '100vw',
-      height: '100vh',
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-    },
-    sidebar: {
-      width: '100%',
-      minHeight: '20vh',
-      height: '100%',
-      display: 'grid',
-      justifyItems: 'start',
-      alignContent: 'center',
-    },
-  }),
-  { name: 'ProductPageGallery' },
-)
 
 export type ProductPageGalleryRenderers = TypeRenderer<
   NonNullable<NonNullable<ProductPageGalleryFragment['media_gallery']>[0]>
@@ -58,27 +14,18 @@ const renderers: ProductPageGalleryRenderers = { ProductImage, ProductVideo }
 type ProductPageGalleryProps = PropsWithChildren<ProductPageGalleryFragment>
 
 export default function ProductPageGallery(props: ProductPageGalleryProps) {
-  const classes = useStyles()
   const { media_gallery, children } = props
 
   return (
-    <div className={classes.root}>
-      <SidebarGallery
-        sidebar={<div className={classes.sidebar}>{children}</div>}
-        classes={{
-          container: classes.container,
-          containerZoomed: classes.containerZoomed,
-        }}
-      >
-        {media_gallery?.map((item) => {
-          if (!item?.position) return null
-          return (
-            <SliderImage key={item.position} width={1532} height={1678}>
-              <RenderType renderer={renderers} {...item} />
-            </SliderImage>
-          )
-        })}
-      </SidebarGallery>
-    </div>
+    <SidebarGallery sidebar={children}>
+      {media_gallery?.map((item) => {
+        if (!item?.position) return null
+        return (
+          <SliderImage key={item.position} width={1532} height={1678}>
+            <RenderType renderer={renderers} {...item} />
+          </SliderImage>
+        )
+      })}
+    </SidebarGallery>
   )
 }
--- packages/next-ui/FramerSlider/variants/SidebarGallery.tsx
+++ packages/next-ui/FramerSlider/variants/SidebarGallery.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx'
 import { m } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import { UseStyles } from '../../Styles'
+import responsiveVal from '../../Styles/responsiveVal'
 import SliderContainer from '../SliderContainer'
 import { SliderContext } from '../SliderContext'
 import SliderDots from '../SliderDots'
@@ -14,6 +15,16 @@ import SliderScroller from '../SliderScroller'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
+    root: {
+      display: 'grid',
+      gridAutoFlow: 'row',
+      [theme.breakpoints.up('md')]: {
+        gridTemplateColumns: '11fr 5fr',
+      },
+      background: theme.palette.background.highlight,
+      marginBottom: theme.spacings.lg,
+      height: responsiveVal(500, 1200),
+    },
     bottomCenter: {
       display: 'grid',
       gridAutoFlow: 'column',
@@ -47,8 +58,38 @@ const useStyles = makeStyles(
         bottom: theme.spacings.sm,
       },
     },
-    container: {},
-    containerZoomed: {},
+    container: {
+      position: 'relative',
+      zIndex: 10,
+      background: theme.palette.background.highlight,
+      overflow: 'hidden',
+      width: '100%',
+      height: '100%',
+      minHeight: '20vh',
+    },
+    containerZoomed: {
+      zIndex: 12,
+      width: '100vw',
+      height: '100vh',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    },
+    sidebar: {
+      width: '100%',
+      minWidth: '10vw',
+      height: '100%',
+      minHeight: '20vh',
+      display: 'grid',
+      justifyItems: 'start',
+      alignContent: 'center',
+      padding: 10,
+      '& > *': {
+        width: '100%',
+      },
+    },
   }),
   { name: 'ExpandableGallery' },
 )
@@ -59,8 +100,8 @@ type SidebarGalleryProps = {
 } & UseStyles<typeof useStyles>
 
 export default function SidebarGallery(props: SidebarGalleryProps) {
-  const { children, sidebar, classes } = props
-  const expGalleryClasses = useStyles(props)
+  const { children, sidebar } = props
+  const classes = useStyles(props)
   const [zoomed, setZoomed] = useState(false)
 
   /**
@@ -71,7 +112,7 @@ export default function SidebarGallery(props: SidebarGalleryProps) {
   useEffect(() => setLayout(false), [])
 
   return (
-    <>
+    <div className={classes.root}>
       <SliderContext scrollSnapAlign='center'>
         <SliderContainer
           layout={layout}
@@ -82,7 +123,7 @@ export default function SidebarGallery(props: SidebarGalleryProps) {
           <SliderScroller layout={layout}>{children}</SliderScroller>
           <m.div
             layout
-            className={expGalleryClasses.topRight}
+            className={classes.topRight}
             onLayoutAnimationComplete={() => setLayout(false)}
           >
             <Fab
@@ -97,15 +138,14 @@ export default function SidebarGallery(props: SidebarGalleryProps) {
               {zoomed ? <FullscreenExit /> : <Fullscreen />}
             </Fab>
           </m.div>
-          <div className={expGalleryClasses.bottomCenter}>
-            <SliderPrev layout={layout} classes={{ root: expGalleryClasses.sliderButtons }} />
+          <div className={classes.bottomCenter}>
+            <SliderPrev layout={layout} classes={{ root: classes.sliderButtons }} />
             <SliderDots layout={layout} count={React.Children.count(children)} />
-            <SliderNext layout={layout} classes={{ root: expGalleryClasses.sliderButtons }} />
+            <SliderNext layout={layout} classes={{ root: classes.sliderButtons }} />
           </div>
         </SliderContainer>
       </SliderContext>
-
-      {sidebar}
-    </>
+      <div className={classes.sidebar}>{sidebar}</div>
+    </div>
   )
 }
