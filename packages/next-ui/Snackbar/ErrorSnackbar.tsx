4633f840932e31a9512ff7e21f532dae0b99b8a7
Deam <deamus@gmail.com>
2021-04-16 11:44:58 +0200

    refactor: rename snackbarloader, and errorsnackbar

--- packages/next-ui/Snackbar/ErrorSnackbar.tsx
+++ packages/next-ui/Snackbar/ErrorSnackbar.tsx
@@ -1,7 +1,10 @@
-import MessageSnackbar, { MessageSnackbarProps } from './MessageSnackbar'
+import React from 'react'
+import MessageSnackbar from './MessageSnackbar'
+import { MessageSnackbarProps } from './MessageSnackbarImpl'
 
+// We provide a loader for the ErrorSnackbar because it will only be required after an action of the user.
 type ErrorSnackbarProps = Omit<MessageSnackbarProps, 'severity' | 'action'>
 
-export default function ErrorSnackbar(props: ErrorSnackbarProps) {
+export default function ErrorSnackbarImpl(props: ErrorSnackbarProps) {
   return <MessageSnackbar {...props} />
 }
