c52f464a51f1a7105f046f3fecde7c23f7c5492c
Paul Hachmang <paul@reachdigital.nl>
2021-04-19 14:47:06 +0200

    docs(framer-next-pages): add better docs

--- packages/framer-next-pages/PageContext.tsx
+++ packages/framer-next-pages/PageContext.tsx
@@ -7,6 +7,44 @@ export type PageContext = { depth: number; direction: Direction; active: boolean
 export const pageContext = createContext((undefined as unknown) as PageContext)
 export const pageRouterContext = createContext((undefined as unknown) as NextRouter)
 
+/**
+ * - Will return `usePageDirection() === 1` when navigating forward
+ * - Will return `usePageDirection() === -1` when navigating backs
+ */
 export const usePageDirection = () => useContext(pageContext).direction
+
+/**
+ * If we have multiple pages layered on top of each other we get the depth the page has.
+ *
+ * E.g.
+ *
+ * - `/my-regular-page`: `usePageDepth() === 0`
+ *
+ * After navigating to overlay-one:
+ *
+ * - `/my-regular-page`: `usePageDepth() === -1`
+ * - `/overlay-one`: `usePageDepth() === 0`
+ *
+ * After navigation to overlay-two
+ *
+ * - `/my-regular-page`: `usePageDepth() === -2`
+ * - `/overlay-one`: `usePageDepth() === -1`
+ * - `/overlay-two`: `usePageDepth() === 0`
+ */
 export const usePageDepth = () => useContext(pageContext).depth
+
+/**
+ * The pageRouter maintains state for the old page.
+ *
+ * E.g.:
+ *
+ * - `/my-regular-page`:
+ *
+ *   - `useRouter().asPath === '/overlay'`
+ *   - `usePageRouter().asPath === '/my-regular-page'` We maintain the state
+ * - `/overlay`: `usePageDepth() === 0`
+ *
+ *   - `useRouter().asPath === '/overlay'`
+ *   - `usePageRouter().asPath === '/overlay'`
+ */
 export const usePageRouter = () => useContext(pageRouterContext)
