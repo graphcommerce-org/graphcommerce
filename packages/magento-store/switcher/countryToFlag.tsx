8c6bf206f85d44289a8d11d9bcd2178af6cc3445
Paul Hachmang <paul@reachdigital.nl>
2021-04-19 22:52:29 +0200

    fix: footer country flag

--- packages/magento-store/switcher/countryToFlag.tsx
+++
@@ -1,9 +0,0 @@
-// ISO 3166-1 alpha-2
-// ⚠️ No support for IE 11
-export default function countryToFlag(isoCode: string) {
-  return typeof String.fromCodePoint !== 'undefined'
-    ? isoCode
-        .toUpperCase()
-        .replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
-    : isoCode
-}
