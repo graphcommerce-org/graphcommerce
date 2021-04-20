efc32a549fd701b38fd90ff677550eb8da8bcdb3
Deam <deamus@gmail.com>
2021-04-20 13:55:32 +0200

    feat: add input for product quantity

--- packages/magento-cart/AddToCartButton.tsx
+++ packages/magento-cart/AddToCartButton.tsx
@@ -1,4 +1,5 @@
 import { TypedDocumentNode, useQuery } from '@apollo/client'
+import { makeStyles } from '@material-ui/core'
 import Checkmark from '@material-ui/icons/Check'
 import { CustomerTokenDocument } from '@reachdigital/magento-customer/CustomerToken.gql'
 import { ProductInterface } from '@reachdigital/magento-graphql'
@@ -6,10 +7,29 @@ import Button, { ButtonProps } from '@reachdigital/next-ui/Button'
 import ApolloErrorAlert from '@reachdigital/next-ui/Form/ApolloErrorAlert'
 import PictureResponsiveNext from '@reachdigital/next-ui/PictureResponsiveNext'
 import MessageSnackbar from '@reachdigital/next-ui/Snackbar/MessageSnackbar'
-import { DeepPartial, UnpackNestedValue, useFormGqlMutation } from '@reachdigital/react-hook-form'
+import TextInputNumber from '@reachdigital/next-ui/TextInputNumber'
+import {
+  DeepPartial,
+  UnpackNestedValue,
+  Path,
+  useFormGqlMutation,
+} from '@reachdigital/react-hook-form'
 import PageLink from 'next/link'
 import React from 'react'
 import useRequestCartId from './useRequestCartId'
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    button: {
+      marginTop: theme.spacings.sm,
+      width: '100%',
+    },
+    quantityInput: {
+      backgroundColor: theme.palette.background.default,
+    },
+  }),
+  { name: 'AccountMenu' },
+)
 
 export default function AddToCartButton<Q, V extends { cartId: string; [index: string]: unknown }>(
   props: Pick<ProductInterface, 'name'> & {
@@ -19,25 +39,40 @@ export default function AddToCartButton<Q, V extends { cartId: string; [index: s
   } & Omit<ButtonProps, 'type' | 'name'>,
 ) {
   const { name, mutation, variables, ...buttonProps } = props
-
   const requestCartId = useRequestCartId()
   const form = useFormGqlMutation<Q, V>(mutation, {
     defaultValues: { ...variables } as UnpackNestedValue<DeepPartial<V>>,
     onBeforeSubmit: async (vars) => ({ ...vars, cartId: await requestCartId() }),
   })
-  const { handleSubmit, formState, error } = form
+
+  const { handleSubmit, formState, error, muiRegister, required } = form
   const submitHandler = handleSubmit(() => {})
+  const classes = useStyles()
 
   const { data: tokenQuery } = useQuery(CustomerTokenDocument)
-  const requireAuth = Boolean(tokenQuery?.customerToken && !tokenQuery?.customerToken.valid)
 
   return (
     <form onSubmit={submitHandler} noValidate>
+      <TextInputNumber
+        variant='outlined'
+        error={formState.isSubmitted && !!formState.errors.quantity}
+        label='Quantity'
+        required={required.quantity}
+        inputProps={{ min: 1 }}
+        {...muiRegister('quantity' as Path<V>, { required: required.quantity })}
+        helperText={formState.isSubmitted && formState.errors.quantity}
+        disabled={formState.isSubmitting}
+        size='small'
+        classes={{ quantity: classes.quantityInput }}
+      />
+
       <Button
         type='submit'
+        classes={{ root: classes.button }}
         loading={formState.isSubmitting}
         color='primary'
-        variant='contained'
+        variant='pill'
+        size='large'
         {...buttonProps}
       >
         Add to Cart
