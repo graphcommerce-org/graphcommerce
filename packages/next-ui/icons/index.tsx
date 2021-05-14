b9d6a4765dfeaf8fc55c3c0794c9f0dddd61df07
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-05-14 15:18:18 +0200

    chore: cleanup svg imports

--- packages/next-ui/icons/index.tsx
+++ packages/next-ui/icons/index.tsx
@@ -1,71 +1,35 @@
-import addresses from './icon_addresses.svg'
-import arrowBack from './icon_arrow_back.svg'
-import arrowForward from './icon_arrow_forward.svg'
-import box from './icon_box.svg'
-import chat from './icon_chat.svg'
-import checkmark from './icon_checkmark.svg'
-import checkmarkGreen from './icon_checkmark_green.svg'
-import chevronBack from './icon_chevron_back.svg'
-import chevronDown from './icon_chevron_down.svg'
-import chevronLeft from './icon_chevron_left.svg'
-import chevronRight from './icon_chevron_right.svg'
-import chevronUp from './icon_chevron_up.svg'
-import close from './icon_close.svg'
-import closeCircle from './icon_close_circle.svg'
-import collapseVertical from './icon_collapse_vertical.svg'
-import customerService from './icon_customer_service.svg'
-import email from './icon_email.svg'
-import expandVertical from './icon_expand_vertical.svg'
-import heart from './icon_heart.svg'
-import home from './icon_home.svg'
-import invoiceRed from './icon_invoice_red.svg'
-import locationRed from './icon_location_red.svg'
-import menu from './icon_menu.svg'
-import min from './icon_min.svg'
-import person from './icon_person.svg'
-import personAlt from './icon_person_alt.svg'
-import phone from './icon_phone.svg'
-import plus from './icon_plus.svg'
-import sadFace from './icon_sad_face.svg'
-import sadShoppingBag from './icon_sad_shoppingbag.svg'
-import search from './icon_search.svg'
-import shoppingBag from './icon_shopping_bag.svg'
-import shutdown from './icon_shutdown.svg'
-import star from './icon_star.svg'
-import starYellow from './icon_star_yellow.svg'
-
-export { addresses as iconAddresses }
-export { arrowBack as iconArrowBack }
-export { arrowForward as iconArrowForward }
-export { box as iconBox }
-export { chat as iconChat }
-export { checkmark as iconCheckmark }
-export { checkmarkGreen as iconCheckmarkGreen }
-export { chevronBack as iconChevronBack }
-export { chevronDown as iconChevronDown }
-export { chevronLeft as iconChevronLeft }
-export { chevronRight as iconChevronRight }
-export { chevronUp as iconChevronUp }
-export { close as iconClose }
-export { closeCircle as iconCloseCircle }
-export { collapseVertical as iconCollapseVertical }
-export { customerService as iconCustomerService }
-export { email as iconEmail }
-export { expandVertical as iconExpandVertical }
-export { heart as iconHeart }
-export { home as iconHome }
-export { invoiceRed as iconInvoiceRed }
-export { locationRed as iconLocationRed }
-export { menu as iconMenu }
-export { min as iconMin }
-export { person as iconPerson }
-export { personAlt as iconPersonAlt }
-export { phone as iconPhone }
-export { plus as iconPlus }
-export { sadFace as iconSadFace }
-export { sadShoppingBag as iconSadShoppingBag }
-export { search as iconSearch }
-export { shoppingBag as iconShoppingBag }
-export { shutdown as iconShutdown }
-export { star as iconStar }
-export { starYellow as iconStarYellow }
+export { default as iconAddresses } from './icon_addresses.svg'
+export { default as iconArrowBack } from './icon_arrow_back.svg'
+export { default as iconArrowForward } from './icon_arrow_forward.svg'
+export { default as iconBox } from './icon_box.svg'
+export { default as iconChat } from './icon_chat.svg'
+export { default as iconCheckmark } from './icon_checkmark.svg'
+export { default as iconCheckmarkGreen } from './icon_checkmark_green.svg'
+export { default as iconChevronBack } from './icon_chevron_back.svg'
+export { default as iconChevronDown } from './icon_chevron_down.svg'
+export { default as iconChevronLeft } from './icon_chevron_left.svg'
+export { default as iconChevronRight } from './icon_chevron_right.svg'
+export { default as iconChevronUp } from './icon_chevron_up.svg'
+export { default as iconClose } from './icon_close.svg'
+export { default as iconCloseCircle } from './icon_close_circle.svg'
+export { default as iconCollapseVertical } from './icon_collapse_vertical.svg'
+export { default as iconCustomerService } from './icon_customer_service.svg'
+export { default as iconEmail } from './icon_email.svg'
+export { default as iconExpandVertical } from './icon_expand_vertical.svg'
+export { default as iconHeart } from './icon_heart.svg'
+export { default as iconHome } from './icon_home.svg'
+export { default as iconInvoiceRed } from './icon_invoice_red.svg'
+export { default as iconLocationRed } from './icon_location_red.svg'
+export { default as iconMenu } from './icon_menu.svg'
+export { default as iconMin } from './icon_min.svg'
+export { default as iconPerson } from './icon_person.svg'
+export { default as iconPersonAlt } from './icon_person_alt.svg'
+export { default as iconPhone } from './icon_phone.svg'
+export { default as iconPlus } from './icon_plus.svg'
+export { default as iconSadFace } from './icon_sad_face.svg'
+export { default as iconSadShoppingBag } from './icon_sad_shoppingbag.svg'
+export { default as iconSearch } from './icon_search.svg'
+export { default as iconShoppingBag } from './icon_shopping_bag.svg'
+export { default as iconShutdown } from './icon_shutdown.svg'
+export { default as iconStar } from './icon_star.svg'
+export { default as iconStarYellow } from './icon_star_yellow.svg'
