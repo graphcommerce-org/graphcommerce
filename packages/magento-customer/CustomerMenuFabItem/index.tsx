586594cccdef13fb3692366051aa25c27340c27c
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-06-01 10:43:58 +0200

    feat: signed in highlight in menu

--- packages/magento-customer/CustomerMenuFabItem/index.tsx
+++ packages/magento-customer/CustomerMenuFabItem/index.tsx
@@ -1,10 +1,9 @@
 import { useQuery } from '@apollo/client'
-import { Badge, Fab, makeStyles, NoSsr, Theme } from '@material-ui/core'
+import { Badge, makeStyles, NoSsr, Theme } from '@material-ui/core'
+import MenuFabSecondaryItem from '@reachdigital/next-ui/AppShell/MenuFabSecondaryItem'
 import { UseStyles } from '@reachdigital/next-ui/Styles'
-import responsiveVal from '@reachdigital/next-ui/Styles/responsiveVal'
 import SvgImage from '@reachdigital/next-ui/SvgImage'
 import { iconPersonAlt } from '@reachdigital/next-ui/icons'
-import PageLink from 'next/link'
 import React from 'react'
 import { CustomerTokenDocument, CustomerTokenQuery } from '../CustomerToken.gql'
 
@@ -12,50 +11,45 @@ const useStyles = makeStyles((theme: Theme) => ({
   colorError: {
     backgroundColor: theme.palette.grey['500'],
   },
-  fab: {
-    width: responsiveVal(42, 56),
-    height: responsiveVal(42, 56),
-  },
 }))
 
-type CustomerFabContentProps = CustomerTokenQuery & {
+type CustomerMenuFabItemProps = CustomerTokenQuery & {
   icon?: React.ReactNode
+  children: React.ReactNode
   authHref: string
   guestHref: string
 } & UseStyles<typeof useStyles>
 
-function CustomerFabContent(props: CustomerFabContentProps) {
-  const { customerToken, icon, guestHref, authHref } = props
+function CustomerMenuFabItemContent(props: CustomerMenuFabItemProps) {
+  const { customerToken, icon, children, guestHref, authHref } = props
   const classes = useStyles(props)
   const requireAuth = Boolean(!customerToken || !customerToken.valid)
 
   return (
-    <PageLink href={requireAuth ? guestHref : authHref} passHref>
-      <Fab
-        style={{ boxShadow: 'none' }}
-        aria-label='Open Menu'
-        size='medium'
-        classes={{ root: classes.fab }}
-      >
+    <MenuFabSecondaryItem
+      icon={
         <Badge
           badgeContent={customerToken?.token ? 1 : 0}
           color={customerToken?.valid ? 'primary' : 'error'}
           variant='dot'
           classes={{ colorError: classes.colorError }}
         >
-          {icon ?? <SvgImage src={iconPersonAlt} alt='Account' loading='eager' />}
+          {icon ?? <SvgImage src={iconPersonAlt} size='small' alt='Account' />}
         </Badge>
-      </Fab>
-    </PageLink>
+      }
+      href={requireAuth ? guestHref : authHref}
+    >
+      {children}
+    </MenuFabSecondaryItem>
   )
 }
 
-export default function CustomerFab(props: CustomerFabContentProps) {
+export default function CustomerMenuFabItem(props: CustomerMenuFabItemProps) {
   const { data } = useQuery(CustomerTokenDocument)
 
   return (
-    <NoSsr fallback={<CustomerFabContent {...props} />}>
-      <CustomerFabContent customerToken={data?.customerToken} {...props} />
+    <NoSsr fallback={<CustomerMenuFabItemContent {...props} />}>
+      <CustomerMenuFabItemContent customerToken={data?.customerToken} {...props} />
     </NoSsr>
   )
 }
