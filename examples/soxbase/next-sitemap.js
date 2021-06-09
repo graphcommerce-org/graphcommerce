57fbff2ad25fa4eee4ff3e84b0528476924d68b0
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-06-09 11:56:50 +0200

    chore: cleanup

--- examples/soxbase/next-sitemap.js
+++ examples/soxbase/next-sitemap.js
@@ -2,6 +2,7 @@ const isProduction = process.env.VERCEL_ENV === 'production'
 const DEV_SITE_URL = process.env.VERCEL_URL || 'http://localhost:3000'
 const PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || DEV_SITE_URL
 
+/** @link https://github.com/iamvishnusankar/next-sitemap */
 module.exports = {
   siteUrl: isProduction ? PUBLIC_SITE_URL : DEV_SITE_URL,
   generateRobotsTxt: true,
