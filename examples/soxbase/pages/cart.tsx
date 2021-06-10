05becf378569f6b38ee5f2864a5f16f29bdcee22
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-06-10 13:52:46 +0200

    chore: new checkout header wip

--- examples/soxbase/pages/cart.tsx
+++ examples/soxbase/pages/cart.tsx
@@ -1,18 +1,18 @@
 import { Container, NoSsr, Typography } from '@material-ui/core'
 import { PageOptions } from '@reachdigital/framer-next-pages'
 import {
-  useCartQuery,
   CartQuickCheckout,
   CartStartCheckout,
   CartTotals,
   EmptyCart,
+  useCartQuery,
   useClearCurrentCartId,
 } from '@reachdigital/magento-cart'
 import { CartPageDocument } from '@reachdigital/magento-cart-checkout'
 import { CouponAccordion } from '@reachdigital/magento-cart-coupon'
 import { CartItem, CartItems } from '@reachdigital/magento-cart-items'
 import { ConfigurableCartItem } from '@reachdigital/magento-product-configurable'
-import { StoreConfigDocument, PageMeta } from '@reachdigital/magento-store'
+import { PageMeta, StoreConfigDocument } from '@reachdigital/magento-store'
 import AnimatedRow from '@reachdigital/next-ui/AnimatedRow'
 import Button from '@reachdigital/next-ui/Button'
 import ApolloErrorAlert from '@reachdigital/next-ui/Form/ApolloErrorAlert'
@@ -34,60 +34,82 @@ function CartPage() {
     typeof data?.cart?.prices?.grand_total?.value !== 'undefined'
 
   return (
-    <Container maxWidth='md'>
+    <>
       <PageMeta title='Cart' metaDescription='Cart Items' metaRobots={['noindex']} />
       <NoSsr>
-        <Typography variant='h5' component='h1' align='center'>
-          Checkout
-        </Typography>
+        {/* {hasItems && (
+          <>
+            <div style={{ display: 'flex', 'justify-content': 'space-between' }}>
+              <div style={{ 'text-align': 'left', flex: '1' }} />
 
-        <AnimatePresence initial={false}>
-          {hasItems ? (
-            <>
-              <Stepper steps={3} currentStep={1} key='checkout-stepper' />
+              <div style={{ flex: '1' }}>
+                <Typography variant='h5' component='h1' align='center'>
+                  Checkout
+                </Typography>
+                <Stepper steps={3} currentStep={1} key='checkout-stepper' />
+              </div>
 
-              <AnimatedRow key='quick-checkout'>
-                <CartQuickCheckout {...data?.cart} />
-              </AnimatedRow>
-              <CartItems
-                items={data?.cart?.items}
-                id={data?.cart?.id ?? ''}
-                key='cart'
-                renderer={{
-                  BundleCartItem: CartItem,
-                  ConfigurableCartItem,
-                  DownloadableCartItem: CartItem,
-                  SimpleCartItem: CartItem,
-                  VirtualCartItem: CartItem,
-                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                  // @ts-ignore GiftCardProduct is only available in Commerce
-                  GiftCardCartItem: CartItem,
-                }}
-              />
-              <CouponAccordion key='couponform' />
-              <CartTotals
-                key='totals'
-                prices={data?.cart?.prices}
-                shipping_addresses={data?.cart?.shipping_addresses ?? []}
-              />
-              <ApolloErrorAlert error={error} />
-              <AnimatedRow key='checkout-button'>
-                <CartStartCheckout {...data?.cart} />
-              </AnimatedRow>
-            </>
-          ) : (
-            <EmptyCart>
-              {error && (
-                <>
-                  <ApolloErrorAlert error={error} />
-                  <Button onClick={clear}>Reset Cart</Button>
-                </>
-              )}
-            </EmptyCart>
-          )}
-        </AnimatePresence>
+              <div style={{ 'text-align': 'right', flex: '1' }}>
+                <Button color='secondary' variant='pill'>
+                  Right button
+                </Button>
+              </div>
+            </div>
+          </>
+        )} */}
+
+        <Container maxWidth='md'>
+          <Typography variant='h5' component='h1' align='center'>
+            Checkout
+          </Typography>
+          <Stepper steps={3} currentStep={1} key='checkout-stepper' />
+
+          <AnimatePresence initial={false}>
+            {hasItems ? (
+              <>
+                <AnimatedRow key='quick-checkout'>
+                  <CartQuickCheckout {...data?.cart} />
+                </AnimatedRow>
+                <CartItems
+                  items={data?.cart?.items}
+                  id={data?.cart?.id ?? ''}
+                  key='cart'
+                  renderer={{
+                    BundleCartItem: CartItem,
+                    ConfigurableCartItem,
+                    DownloadableCartItem: CartItem,
+                    SimpleCartItem: CartItem,
+                    VirtualCartItem: CartItem,
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore GiftCardProduct is only available in Commerce
+                    GiftCardCartItem: CartItem,
+                  }}
+                />
+                <CouponAccordion key='couponform' />
+                <CartTotals
+                  key='totals'
+                  prices={data?.cart?.prices}
+                  shipping_addresses={data?.cart?.shipping_addresses ?? []}
+                />
+                <ApolloErrorAlert error={error} />
+                <AnimatedRow key='checkout-button'>
+                  <CartStartCheckout {...data?.cart} />
+                </AnimatedRow>
+              </>
+            ) : (
+              <EmptyCart>
+                {error && (
+                  <>
+                    <ApolloErrorAlert error={error} />
+                    <Button onClick={clear}>Reset Cart</Button>
+                  </>
+                )}
+              </EmptyCart>
+            )}
+          </AnimatePresence>
+        </Container>
       </NoSsr>
-    </Container>
+    </>
   )
 }
 
