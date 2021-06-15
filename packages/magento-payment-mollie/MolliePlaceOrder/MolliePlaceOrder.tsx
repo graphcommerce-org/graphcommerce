5c129a930e940a0d1476cd524168c822ca1e9632
Paul Hachmang <paul@reachdigital.nl>
2021-06-15 14:38:37 +0200

    feat(mollie): redirect to cart page when successful

--- packages/magento-payment-mollie/MolliePlaceOrder/MolliePlaceOrder.tsx
+++ packages/magento-payment-mollie/MolliePlaceOrder/MolliePlaceOrder.tsx
@@ -1,4 +1,4 @@
-import { useFormGqlMutationCart } from '@reachdigital/magento-cart'
+import { useCurrentCartId, useFormGqlMutationCart } from '@reachdigital/magento-cart'
 import { PaymentPlaceOrderProps } from '@reachdigital/magento-cart-payment-method'
 import { useFormCompose } from '@reachdigital/react-hook-form'
 import { useRouter } from 'next/router'
@@ -8,21 +8,33 @@ import { MolliePlaceOrderDocument } from './MolliePlaceOrder.gql'
 export default function MolliePlaceOrder(props: PaymentPlaceOrderProps) {
   const { step, paymentDone, code } = props
   const router = useRouter()
+  const cartId = useCurrentCartId()
 
   const form = useFormGqlMutationCart(MolliePlaceOrderDocument, { mode: 'onChange' })
 
   const { handleSubmit, data, formState } = form
+
+  const redirectUrl = data?.placeOrder?.order.mollie_redirect_url
+  const molliePaymentToken = data?.placeOrder?.order.mollie_payment_token
 
   const submit = handleSubmit(() => {
     paymentDone()
   })
 
   useEffect(() => {
-    if (formState.isSubmitSuccessful && data?.placeOrder?.order.mollie_redirect_url) {
+    if (!formState.isSubmitSuccessful || !cartId) return
+
+    if (redirectUrl) {
+      paymentDone()
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      router.push(data?.placeOrder?.order.mollie_redirect_url)
+      router.push(redirectUrl)
     }
-  }, [data?.placeOrder?.order.mollie_redirect_url, formState.isSubmitSuccessful, router])
+    if (!redirectUrl && molliePaymentToken) {
+      paymentDone()
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      router.push({ pathname: '/checkout/success', query: { cartId, molliePaymentToken } })
+    }
+  }, [cartId, formState.isSubmitSuccessful, paymentDone, molliePaymentToken, redirectUrl, router])
 
   useFormCompose({ form, step, submit, key: `PaymentMethodPlaceOrder_${code}` })
 
