49efd8c934ea4e0f8ebcb5ac65f89f9ea2c0fa26
Paul Hachmang <paul@reachdigital.nl>
2021-06-15 14:13:30 +0200

    fix(cart): merge or prices didn’t go well

--- packages/magento-cart/typePolicies.ts
+++ packages/magento-cart/typePolicies.ts
@@ -1,4 +1,5 @@
 import type { QueryCartArgs, ShippingCartAddress, TypedTypePolicies } from '@reachdigital/graphql'
+import { CartPrices } from '@reachdigital/graphql/generated/types'
 
 export const cartTypePolicies: TypedTypePolicies = {
   CurrentCartId: { keyFields: [] },
@@ -10,6 +11,10 @@ export const cartTypePolicies: TypedTypePolicies = {
           incoming: ShippingCartAddress[],
           options,
         ) => [options.mergeObjects(existing?.[0] ?? {}, incoming[0])],
+      },
+      prices: {
+        merge: (existing: CartPrices[] | undefined, incoming: CartPrices[], options) =>
+          options.mergeObjects(existing ?? {}, incoming),
       },
     },
   },
