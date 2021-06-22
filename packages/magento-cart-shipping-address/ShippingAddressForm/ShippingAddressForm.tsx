44e8e778b13dee9843ab5112dedcef8e373dd025
Paul Hachmang <paul@reachdigital.nl>
2021-06-22 21:31:42 +0200

    fix: display empty country in country selector

--- packages/magento-cart-shipping-address/ShippingAddressForm/ShippingAddressForm.tsx
+++ packages/magento-cart-shipping-address/ShippingAddressForm/ShippingAddressForm.tsx
@@ -38,20 +38,20 @@ export default function ShippingAddressForm(props: ShippingAddressFormProps) {
   const currentCustomer = customerQuery?.customer
   const currentCountryCode = currentAddress?.country.code ?? shopCountry ?? 'NLD'
 
+  console.log(currentCountryCode)
   const form = useFormGqlMutationCart(ShippingAddressFormDocument, {
     defaultValues: {
       // todo(paales): change to something more sustainable
-      firstname: currentAddress?.firstname ?? currentCustomer?.firstname ?? undefined, // todo: allow for null values in defaultValues
-      lastname: currentAddress?.lastname ?? currentCustomer?.lastname ?? undefined,
-      telephone:
-        currentAddress?.telephone !== '000 - 000 0000' ? currentAddress?.telephone : undefined,
-      city: currentAddress?.city,
-      company: currentAddress?.company,
+      firstname: currentAddress?.firstname ?? currentCustomer?.firstname ?? '',
+      lastname: currentAddress?.lastname ?? currentCustomer?.lastname ?? '',
+      telephone: currentAddress?.telephone !== '000 - 000 0000' ? currentAddress?.telephone : '',
+      city: currentAddress?.city ?? '',
+      company: currentAddress?.company ?? '',
       postcode: currentAddress?.postcode ?? '',
-      street: currentAddress?.street?.[0] ?? undefined,
-      houseNumber: currentAddress?.street?.[1] ?? undefined,
-      addition: currentAddress?.street?.[2] ?? undefined,
-      regionId: currentAddress?.region?.region_id,
+      street: currentAddress?.street?.[0] ?? '',
+      houseNumber: currentAddress?.street?.[1] ?? '',
+      addition: currentAddress?.street?.[2] ?? '',
+      regionId: currentAddress?.region?.region_id ?? null,
       countryCode: currentCountryCode, // todo: replace by the default shipping country of the store + geoip,
       saveInAddressBook: true,
     },
