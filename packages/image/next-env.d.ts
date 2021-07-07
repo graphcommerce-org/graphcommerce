99cea4f1a985a57059fbd40bdddde5e6f7241ee4
Paul Hachmang <paul@reachdigital.nl>
2021-07-07 13:33:09 +0200

    chore: fix build

--- packages/image/next-env.d.ts
+++ packages/image/next-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-/// <reference types="@reachdigital/next-ui/types" />
 
 declare namespace NodeJS {
   interface Global {
