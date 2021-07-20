5ec54530eaae17554eca1c1f1be6954bfe08922e
Paul Hachmang <paul@reachdigital.nl>
2021-07-20 17:54:10 +0200

    chore: import ts-tiny-variant

--- packages/graphql-mesh/createHandler.ts
+++ packages/graphql-mesh/createHandler.ts
@@ -1,13 +1,14 @@
 import { processConfig } from '@graphql-mesh/config'
 import { getMesh } from '@graphql-mesh/runtime'
 import { YamlConfig } from '@graphql-mesh/types'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import { ApolloServer } from 'apollo-server-micro'
 import '@graphql-mesh/transform-filter-schema'
 import '@graphql-mesh/graphql'
 import '@graphql-mesh/merger-stitching'
 import '@graphql-mesh/transform-cache'
+import 'ts-tiny-invariant'
 import cors from 'micro-cors'
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 
 function injectEnv(json: YamlConfig.Config): YamlConfig.Config {
   let content = JSON.stringify(json)
