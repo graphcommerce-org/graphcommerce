f00462f9abb61a54552c96dbed35ef708fe05608
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-07-21 14:29:04 +0200

    fix: header spacings

--- packages/magento-product/ProductPageGallery/index.tsx
+++ packages/magento-product/ProductPageGallery/index.tsx
@@ -1,4 +1,11 @@
-import { SliderImage, SidebarGallery, RenderType, TypeRenderer } from '@reachdigital/next-ui'
+import { makeStyles, Theme } from '@material-ui/core'
+import {
+  RenderType,
+  SidebarGallery,
+  SliderImage,
+  TypeRenderer,
+  UseStyles,
+} from '@reachdigital/next-ui'
 import React, { PropsWithChildren } from 'react'
 import ProductImage from './ProductImage'
 import { ProductPageGalleryFragment } from './ProductPageGallery.gql'
@@ -9,13 +16,33 @@ export type ProductPageGalleryRenderers = TypeRenderer<
 >
 const renderers: ProductPageGalleryRenderers = { ProductImage, ProductVideo }
 
-type ProductPageGalleryProps = PropsWithChildren<ProductPageGalleryFragment>
+type ProductPageGalleryProps = PropsWithChildren<ProductPageGalleryFragment> &
+  UseStyles<typeof useStyles>
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    wrapper: {
+      marginTop: 'initial',
+      [theme.breakpoints.up('md')]: {
+        marginTop: -96,
+      },
+    },
+  }),
+  {
+    name: 'ProductPageGallery',
+  },
+)
 
 export default function ProductPageGallery(props: ProductPageGalleryProps) {
   const { media_gallery, children } = props
+  const classes = useStyles(props)
 
   return (
-    <SidebarGallery sidebar={children} aspectRatio={[1678, 1532]}>
+    <SidebarGallery
+      sidebar={children}
+      aspectRatio={[1678, 1532]}
+      classes={{ wrapper: classes.wrapper }}
+    >
       {media_gallery?.map((item) => {
         if (!item?.position) return null
         return (
--- packages/next-ui/AppShell/PageShellHeader/index.tsx
+++ packages/next-ui/AppShell/PageShellHeader/index.tsx
@@ -1,24 +1,14 @@
-import { makeStyles } from '@material-ui/core'
 import { useViewportScroll } from 'framer-motion'
 import React from 'react'
-import { UseStyles } from '../../Styles'
 import AppShellHeader, { AppShellHeaderProps } from '../AppShellHeader'
-
-const useStyles = makeStyles(() => ({
-  sheetHeader: {
-    marginBottom: 62,
-  },
-}))
 
 export type PageShellHeaderProps = Omit<
   AppShellHeaderProps,
   'scrollY' | 'hideClose' | 'dragIndicator'
-> &
-  UseStyles<typeof useStyles>
+>
 
 export default function PageShellHeader(props: PageShellHeaderProps) {
   const { scrollY } = useViewportScroll()
-  const classes = useStyles(props)
 
-  return <AppShellHeader {...props} classes={classes} scrollY={scrollY} hideClose />
+  return <AppShellHeader {...props} scrollY={scrollY} hideClose />
 }
