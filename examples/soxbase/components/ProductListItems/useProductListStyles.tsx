af1e85133f8c6681c97cd0d4f066844589dade38
ErwinOtten <erwin@h-o.nl>
2021-07-23 14:37:30 +0200

    fix: simplify xl grid items

--- examples/soxbase/components/ProductListItems/useProductListStyles.tsx
+++ examples/soxbase/components/ProductListItems/useProductListStyles.tsx
@@ -4,35 +4,16 @@ type UseProductListStylesProps = { count: number }
 
 const useProductListStyles = makeStyles(
   (theme: Theme) => ({
-    productList: ({ count }: UseProductListStylesProps) => {
-      let big = 3
-      let index = 0
-      let toggle = false
-      let selector = ''
-      for (index = 0; index <= count; index++) {
-        if (index === big) {
-          selector += `& >:nth-child(${big}),`
-          if (toggle === false) {
-            big = index + 7
-            toggle = !toggle
-          } else {
-            big = index + 11
-            toggle = !toggle
-          }
-        }
-      }
-      selector = selector.slice(0, -1)
-      return {
-        [theme.breakpoints.up('xl')]: {
-          [`${selector}`]: {
-            gridColumn: 'span 2',
-            gridRow: 'span 2;',
-            '& > a > div': {
-              paddingTop: `calc(100% + ${theme.spacings.lg} - 2px)`,
-            },
+    [theme.breakpoints.up('xl')]: {
+      productList: {
+        '& > :nth-child(7n + 3)': {
+          gridColumn: 'span 2',
+          gridRow: 'span 2',
+          '& > a > div': {
+            paddingTop: `calc(100% + ${theme.spacings.lg} - 2px)`,
           },
         },
-      }
+      },
     },
   }),
   { name: 'ProductList' },
