c095213935639548854a12cb00b9bbb62c3917f6
Paul Hachmang <paul@reachdigital.nl>
2021-07-27 14:54:49 +0200

    chore: fix playwright test runner

--- packages/magento-product/__playwright__/fixtures/withProduct.ts
+++ packages/magento-product/__playwright__/fixtures/withProduct.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { gql } from '@apollo/client'
 import { test as base } from '@reachdigital/graphql/__playwright__/fixtures/testGraphQl'
 import { productLink } from '../../ProductLink'
-import { ProductStaticPathsDocument } from '../../ProductStaticPaths/ProductStaticPaths.gql'
 import { ProductTypenames } from '../../ProductStaticPaths/getProductStaticPaths'
 
 type ProductUrls = {
@@ -11,14 +11,29 @@ type ProductUrls = {
 const test = base.extend<{ productURL: ProductUrls }>({
   productURL: async ({ apolloClient, baseURL }, use) => {
     const query = await apolloClient.query({
-      query: ProductStaticPathsDocument,
+      query: gql`
+        query ProductStaticPaths($currentPage: Int!, $pageSize: Int!) {
+          products(filter: {}, pageSize: $pageSize, currentPage: $currentPage) {
+            page_info {
+              current_page
+              total_pages
+            }
+            total_count
+            items {
+              __typename
+              uid
+              url_key
+            }
+          }
+        }
+      `,
       variables: { currentPage: 1, pageSize: 1000 },
     })
 
     const productUrls: Partial<Omit<ProductUrls, 'all'>> = {}
     const urls = (query.data.products?.items ?? []).map((p) => {
-      productUrls[`${p?.__typename}`] = `${baseURL}${productLink(p!)}`
-      return `${baseURL}${productLink(p!)}`
+      productUrls[`${p?.__typename}`] = `${baseURL}${productLink(p)}`
+      return `${baseURL}${productLink(p)}`
     })
 
     await use({ all: urls, ...productUrls } as ProductUrls)
