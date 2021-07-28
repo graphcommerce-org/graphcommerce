57098a56f50f175cc753339aeba7c577288e20d6
Paul Hachmang <paul@reachdigital.nl>
2021-07-28 08:56:03 +0200

    fix: playwright

--- packages/magento-product/__playwright__/fixtures/withProduct.ts
+++ packages/magento-product/__playwright__/fixtures/withProduct.ts
@@ -1,46 +1,24 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { gql } from '@apollo/client'
 import { test as base } from '@reachdigital/graphql/__playwright__/fixtures/testGraphQl'
 import { productLink } from '../../ProductLink'
+import { ProductStaticPathsDocument } from '../../ProductStaticPaths/ProductStaticPaths.gql'
+import { ProductTypenames } from '../../ProductStaticPaths/getProductStaticPaths'
 
 type ProductUrls = {
   all: string[]
-} & Record<
-  | 'BundleProduct'
-  | 'ConfigurableProduct'
-  | 'DownloadableProduct'
-  | 'SimpleProduct'
-  | 'VirtualProduct'
-  | 'GroupedProduct',
-  string
->
+} & Record<`${ProductTypenames}`, string>
 
 const test = base.extend<{ productURL: ProductUrls }>({
   productURL: async ({ apolloClient, baseURL }, use) => {
     const query = await apolloClient.query({
-      query: gql`
-        query ProductStaticPaths($currentPage: Int!, $pageSize: Int!) {
-          products(filter: {}, pageSize: $pageSize, currentPage: $currentPage) {
-            page_info {
-              current_page
-              total_pages
-            }
-            total_count
-            items {
-              __typename
-              uid
-              url_key
-            }
-          }
-        }
-      `,
+      query: ProductStaticPathsDocument,
       variables: { currentPage: 1, pageSize: 1000 },
     })
 
     const productUrls: Partial<Omit<ProductUrls, 'all'>> = {}
     const urls = (query.data.products?.items ?? []).map((p) => {
-      productUrls[`${p?.__typename}`] = `${baseURL}${productLink(p)}`
-      return `${baseURL}${productLink(p)}`
+      productUrls[`${p?.__typename}`] = `${baseURL}${productLink(p!)}`
+      return `${baseURL}${productLink(p!)}`
     })
 
     await use({ all: urls, ...productUrls } as ProductUrls)
