76471ab5079ac09c0d8d40488e180f74f55bc54e
Paul Hachmang <paul@reachdigital.nl>
2021-07-29 10:08:22 +0200

    test(purchaseorder): validate that we're able to place a purchaseorder

---
+++ packages/magento-payment-included/_playwright/purchaseorder.spec.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { expect } from '@playwright/test'
+import { waitForGraphQlResponse } from '@reachdigital/graphql/_playwright/apolloClient.fixture'
+import { PaymentMethodPlaceOrderNoopDocument } from '@reachdigital/magento-cart-payment-method/PaymentMethodPlaceOrderNoop/PaymentMethodPlaceOrderNoop.gql'
+import { fillShippingAddressForm } from '@reachdigital/magento-cart-shipping-address/_playwright/fillShippingAddressForm'
+import { addConfigurableProductToCart } from '@reachdigital/magento-product-configurable/_playwright/addConfigurableProductToCart'
+import { test } from '@reachdigital/magento-product/_playwright/productURL.fixture'
+
+test.only('place order', async ({ page, productURL }) => {
+  await addConfigurableProductToCart(page, productURL.ConfigurableProduct)
+
+  await page.click('button:has-text("View shopping cart")')
+
+  await page.click('a[href="/checkout"]:last-of-type')
+
+  await page.click('input[name="email"]')
+  await page.fill('input[name="email"]', 'test@test.com')
+
+  await fillShippingAddressForm(page)
+
+  await page.click('button[value=flatrate-flatrate]')
+  await page.click('button:has-text("Next")')
+
+  await page.click('button[value=purchaseorder___]')
+
+  await page.click('input[name="poNumber"]')
+  await page.fill('input[name="poNumber"]', '1234567890')
+
+  await page.click('button:has-text("Pay (Purchase Order)")')
+
+  const result = await waitForGraphQlResponse(page, PaymentMethodPlaceOrderNoopDocument)
+  expect(result.errors).toBeUndefined()
+  expect(result.data?.placeOrder?.order.order_number).toBeDefined()
+})
