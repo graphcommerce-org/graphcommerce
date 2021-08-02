f4b0a8098adfc9d00bf72a0a2ae834aa2b8c38b6
Deam <deamus@gmail.com>
2021-08-02 10:13:20 +0200

    style: set backgroundColor for options

--- packages/magento-cart-payment-method/PaymentMethodOptions/PaymentMethodOptions.tsx
+++ packages/magento-cart-payment-method/PaymentMethodOptions/PaymentMethodOptions.tsx
@@ -1,15 +1,29 @@
+import { makeStyles, Theme } from '@material-ui/core'
 import { AnimatedRow } from '@reachdigital/next-ui'
 import { AnimatePresence } from 'framer-motion'
 import { PaymentMethodOptionsProps } from '../Api/PaymentMethod'
 import { usePaymentMethodContext } from '../PaymentMethodContext/PaymentMethodContext'
 
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    root: {
+      '& > div': {
+        padding: theme.spacings.sm,
+        background: `${theme.palette.secondary.main}12`,
+      },
+    },
+  }),
+  { name: 'PaymentMethodOptions' },
+)
+
 export default function PaymentMethodOptions(props: PaymentMethodOptionsProps) {
   const { selectedMethod, selectedModule } = usePaymentMethodContext()
+  const classes = useStyles()
 
   return (
     <AnimatePresence initial={false}>
       {selectedModule && selectedMethod && (
-        <AnimatedRow key={selectedMethod.code}>
+        <AnimatedRow key={selectedMethod.code} className={classes.root}>
           <selectedModule.PaymentOptions {...selectedMethod} {...props} />
         </AnimatedRow>
       )}
