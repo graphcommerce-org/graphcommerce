20a28a5c4fdbb4cd883b69459e13ac481bdf3a64
ErwinOtten <erwin@h-o.nl>
2021-08-03 14:27:35 +0200

    feat: add Chip with author and publish date

---
+++ examples/soxbase/components/Blog/BlogAuthor.tsx
@@ -0,0 +1,15 @@
+import { useQuery } from '@apollo/client'
+import { StoreConfigDocument } from '@reachdigital/magento-store'
+import { BlogAuthor as NextBlogAuthor } from '@reachdigital/next-ui'
+import { BlogAuthorFragment } from './BlogAuthor.gql'
+
+type BlogAuthorProps = BlogAuthorFragment
+
+export default function BlogAuthor(props: BlogAuthorProps) {
+  const { author, date } = props
+
+  const { data: config } = useQuery(StoreConfigDocument)
+  const locale = config?.storeConfig?.locale?.replace('_', '-')
+
+  return <NextBlogAuthor author={author ?? 'Admin'} date={date} locale={locale ?? ''} />
+}
---
+++ packages/next-ui/Blog/BlogAuthor/index.tsx
@@ -0,0 +1,65 @@
+import { Avatar, Chip, makeStyles, Theme } from '@material-ui/core'
+import React from 'react'
+import Row from '../../Row'
+import { UseStyles } from '../../Styles'
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    root: {
+      marginBottom: 0,
+    },
+    chipWrapper: {
+      display: 'flex',
+      justifyContent: 'left',
+      maxWidth: 800,
+      margin: `0 auto`,
+      marginBottom: theme.spacings.md,
+      '& > div': {
+        height: 52,
+        borderRadius: 30,
+        '& > div': {
+          width: '44px !important',
+          height: '44px !important',
+        },
+      },
+    },
+    date: {
+      color: theme.palette.text.disabled,
+    },
+    author: {},
+  }),
+  { name: 'BlogAuthor' },
+)
+
+export type BlogAuthorProps = UseStyles<typeof useStyles> & {
+  author: string
+  date: string
+  locale: string
+}
+
+export default function BlogAuthor(props: BlogAuthorProps) {
+  const { author, date, locale } = props
+  const classes = useStyles()
+
+  const formatter = new Intl.DateTimeFormat(locale, {
+    month: 'long',
+    day: 'numeric',
+  })
+
+  return (
+    <Row className={classes.root}>
+      <div className={classes.chipWrapper}>
+        <Chip
+          variant='outlined'
+          avatar={<Avatar>{author.charAt(0).toUpperCase()}</Avatar>}
+          label={
+            <section>
+              <div className={classes.author}>{author}</div>
+              <div className={classes.date}>{formatter.format(new Date(date))}</div>
+            </section>
+          }
+        />
+      </div>
+    </Row>
+  )
+}
