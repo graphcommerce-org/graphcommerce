e02c74341be195e90fa290fc6ff54a4a44c29bed
Paul Hachmang <paul@reachdigital.nl>
2021-08-09 12:20:51 +0200

    fix: product image alt tag was the URL

--- packages/magento-product/components/ProductPageGallery/ProductImage.tsx
+++ packages/magento-product/components/ProductPageGallery/ProductImage.tsx
@@ -2,9 +2,9 @@ import { Image } from '@reachdigital/image'
 import { ProductImageFragment } from './ProductImage.gql'
 
 export default function ProductImage(props: ProductImageFragment) {
-  const { url } = props
+  const { url, label } = props
 
   if (!url) return null
 
-  return <Image src={url} width={328} height={328} alt={url} dontReportWronglySizedImages />
+  return <Image src={url} width={328} height={328} alt={label ?? ''} dontReportWronglySizedImages />
 }
