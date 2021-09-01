00472e5fe3c3c5408db358c0c78a3559cea902ca
Paul Hachmang <paul@reachdigital.nl>
2021-09-01 12:14:21 +0200

    feat(framer-scroller): add prev/next buttons to SidebarSlider

--- packages/next-ui/FramerScroller/components/SidebarSlider.tsx
+++ packages/next-ui/FramerScroller/components/SidebarSlider.tsx
@@ -1,7 +1,14 @@
+import { relative } from 'path/posix'
 import { Theme } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
-import { Scroller, ScrollerPageCounter, ScrollerProvider } from '@reachdigital/framer-scroller'
+import {
+  Scroller,
+  ScrollerButton,
+  ScrollerPageCounter,
+  ScrollerProvider,
+} from '@reachdigital/framer-scroller'
 import React, { ReactNode } from 'react'
+import { SvgImageSimple, iconChevronLeft, iconChevronRight } from '../..'
 import { UseStyles } from '../../Styles'
 import responsiveVal from '../../Styles/responsiveVal'
 
@@ -18,17 +25,42 @@ const useStyles = makeStyles(
       alignContent: 'space-between',
       padding: `0 ${theme.spacings.lg} 0 ${theme.page.horizontal}`,
     },
+    scrollerContainer: {
+      position: 'relative',
+      minWidth: 1,
+    },
     scroller: {
+      width: '100%',
       display: 'grid',
       gridAutoFlow: 'column',
       gridColumnGap: theme.spacings.md,
       gridRowGap: theme.spacings.lg,
       alignContent: 'space-around',
       paddingRight: theme.page.horizontal,
-      minWidth: 1,
+
       '& > *': {
         minWidth: responsiveVal(200, 400),
       },
+    },
+    sliderButtons: {
+      [theme.breakpoints.down('sm')]: {
+        display: 'none',
+      },
+    },
+    centerLeft: {
+      display: 'grid',
+      gridAutoFlow: 'row',
+      gap: theme.spacings.xxs,
+      position: 'absolute',
+      left: theme.spacings.sm,
+      top: `calc(50% - 28px)`,
+    },
+    centerRight: {
+      display: 'grid',
+      gap: theme.spacings.xxs,
+      position: 'absolute',
+      right: theme.spacings.sm,
+      top: `calc(50% - 28px)`,
     },
   }),
   { name: 'SidebarSlider' },
@@ -50,9 +82,21 @@ export default function SidebarSlider(props: SidebarSliderProps) {
           <ScrollerPageCounter />
         </div>
 
-        <Scroller className={classes.scroller} hideScrollbar>
-          {children}
-        </Scroller>
+        <div className={classes.scrollerContainer}>
+          <Scroller className={classes.scroller} hideScrollbar>
+            {children}
+          </Scroller>
+          <div className={classes.centerLeft}>
+            <ScrollerButton layout direction='left' className={classes.sliderButtons}>
+              <SvgImageSimple src={iconChevronLeft} />
+            </ScrollerButton>
+          </div>
+          <div className={classes.centerRight}>
+            <ScrollerButton layout direction='right' className={classes.sliderButtons}>
+              <SvgImageSimple src={iconChevronRight} />
+            </ScrollerButton>
+          </div>
+        </div>
       </div>
     </ScrollerProvider>
   )
