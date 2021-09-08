ec298a0d8c1055d0b27e0b7a6b33f4ec66b3623b
ErwinOtten <erwin@h-o.nl>
2021-09-08 17:10:39 +0200

    feat: buttons

--- examples/soxbase/components/RowHeroAnimation/index.tsx
+++ examples/soxbase/components/RowHeroAnimation/index.tsx
@@ -1,4 +1,4 @@
-import { Container, makeStyles, Theme, Typography } from '@material-ui/core'
+import { Button, Container, makeStyles, Theme, Typography } from '@material-ui/core'
 import { responsiveVal } from '@reachdigital/next-ui'
 import React from 'react'
 import Animation from './Animation'
@@ -8,9 +8,13 @@ const useStyles = makeStyles(
     hero: {
       background: '#001727',
       position: 'relative',
+      '& *': {
+        // border: '1px solid red',
+      },
     },
     copy: {
-      padding: `${responsiveVal(20, 40)} 0 ${responsiveVal(60, 300)} 0`,
+      padding: `${responsiveVal(20, 100)} 0 ${responsiveVal(60, 300)} 0`,
+      margin: '0 auto',
       color: '#fff',
       display: 'grid',
       gridTemplateRows: 'auto auto',
@@ -23,15 +27,62 @@ const useStyles = makeStyles(
       },
     },
     title: {
-      width: '80%',
-      fontSize: responsiveVal(30, 140),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '80%',
+      },
+      fontWeight: 700,
+      fontSize: responsiveVal(20, 130),
       lineHeight: responsiveVal(40, 160),
     },
     body: {
+      fontWeight: 600,
+      marginBottom: responsiveVal(10, 50),
+    },
+    main: {
       paddingLeft: '20%',
       [theme.breakpoints.up('md')]: {
         paddingLeft: '50%',
       },
+    },
+    styledButton: {
+      borderRadius: 14,
+      background: 'linear-gradient(45deg, #85FFFC 0%, #85FFFC 10%, #CEFF99 100%)',
+      fontSize: responsiveVal(15, 21),
+      fontWeight: 600,
+      border: '1px solid #001727',
+      padding: `${responsiveVal(2, 6)} ${responsiveVal(20, 35)}`,
+      '&:before': {
+        content: '""',
+        position: 'absolute',
+        left: '-4px',
+        top: '-4px',
+        background:
+          'linear-gradient(45deg, #55A3A1 30%, #CEFF99 36%, #55A3A1 42%, #55A3A1 80%, #CEFF99 86%, #55A3A1 92% )',
+        backgroundSize: '400%',
+        width: 'calc(100% + 8px)',
+        height: 'calc(100% + 8px)',
+        zIndex: '-1',
+        borderRadius: '16px',
+        animation: `$glow 40s linear infinite`,
+      },
+    },
+    '@keyframes glow': {
+      '0%': {
+        backgroundPosition: '0 0',
+      },
+      '100%': {
+        backgroundPosition: '-400% 0%',
+      },
+    },
+    copyButton: {
+      borderRadius: 15,
+      fontSize: responsiveVal(14, 17),
+      padding: `${responsiveVal(2, 12)} ${responsiveVal(20, 38)}`,
+      fontFamily: 'Menlo,ui-monospace,SFMono-Regular,monospace',
+      borderColor: 'rgba(255,255,255,0.4)',
+      background: 'rgba(255,255,255,0.1)',
+      margin: '0 0 0 40px',
     },
   }),
   { name: 'RowHeroAnimation' },
@@ -50,10 +101,24 @@ export default function RowHeroAnimation() {
             <Typography variant='h2' className={classes.title}>
               Where the future of e-commerce is headless.
             </Typography>
-            <Typography variant='h3' className={classes.body}>
-              GraphCommerce® is an open source, headless e-commerce storefront build with GraphQL,
-              React, Typescript and Next.js.
-            </Typography>
+            <div className={classes.main}>
+              <Typography variant='h3' className={classes.body}>
+                GraphCommerce® is an open source, headless e-commerce storefront build with GraphQL,
+                React, Typescript and Next.js.
+              </Typography>
+              <Button href='/' variant='contained' size='large' className={classes.styledButton}>
+                View live demo
+              </Button>
+              <Button
+                href='/'
+                variant='outlined'
+                size='large'
+                color='inherit'
+                className={classes.copyButton}
+              >
+                $ npm install graphcommerce
+              </Button>
+            </div>
           </Container>
         </div>
       </div>
