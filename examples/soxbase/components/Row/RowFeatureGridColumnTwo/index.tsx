a01e92f360e71a168d5218f726648f9be1fbe034
ErwinOtten <erwin@h-o.nl>
2021-09-27 15:29:59 +0200

    fix: responsive styling

--- examples/soxbase/components/Row/RowFeatureGridColumnTwo/index.tsx
+++ examples/soxbase/components/Row/RowFeatureGridColumnTwo/index.tsx
@@ -11,38 +11,53 @@ const useStyles = makeStyles(
       padding: `${responsiveVal(50, 200)} 0`,
       display: 'grid',
       gridTemplateAreas: `
+      "copy"
+      "graphic"
+      "usps"
+    `,
+      gridTemplateColumns: '80%',
+      justifyContent: 'center',
+      [theme.breakpoints.up('md')]: {
+        gridTemplateAreas: `
       "copy graphic"
       "usps graphic"
     `,
-      gridTemplateColumns: 'minmax(400px, 700px) minmax(200px, 450px)',
+        gridTemplateColumns: 'minmax(400px, 700px) minmax(200px, 450px)',
+        justifyContent: 'space-between',
+      },
       columnGap: theme.spacings.md,
-      justifyContent: 'space-between',
     },
     copy: {
       gridArea: 'copy',
-      margin: `0 0 ${responsiveVal(30, 80)} 0`,
+      margin: `0 0 ${responsiveVal(30, 70)} 0`,
     },
     grid: {
       display: 'grid',
       gridArea: 'usps',
-      gridTemplateColumns: `repeat(2, minmax(200px, 300px))`,
+      gridTemplateColumns: `repeat(2, minmax(200px, 320px))`,
       justifyContent: 'space-between',
-      columnGap: responsiveVal(20, 50),
-      rowGap: responsiveVal(20, 50),
+      columnGap: responsiveVal(20, 40),
+      rowGap: responsiveVal(20, 40),
       '& > *': {
         display: 'grid',
         gridTemplateColumns: `50px auto`,
         alignItems: 'center',
       },
     },
+    h4: {
+      fontSize: responsiveVal(14, 20),
+    },
     phone: {
-      width: '100%',
+      margin: '0 auto',
+      width: '50%',
+      [theme.breakpoints.up('md')]: {
+        width: '100%',
+      },
       aspectRatio: '450/900',
       border: '1px solid #bbb',
       gridArea: 'graphic',
       justifySelf: 'end',
       borderRadius: 60,
-      margin: `${responsiveVal(30, 80)} 0 0 0`,
     },
     overline: {
       backgroundImage: 'none',
@@ -79,35 +94,51 @@ export default function RowFeatureGridColumnTwo() {
         <div className={classes.grid}>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>Hybrid Static Site Generation (SSG)</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              Hybrid Static Site Generation (SSG)
+            </Typography>
           </div>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>Page-based Javascript bundling and splitting</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              Page-based Javascript bundling and splitting
+            </Typography>
           </div>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>CSS critical rendering path</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              CSS critical rendering path
+            </Typography>
           </div>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>Advanced CDN serving both assets and pages</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              Advanced CDN serving both assets and pages
+            </Typography>
           </div>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>Image and font rendering and download optimization</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              Image and font rendering and download optimization
+            </Typography>
           </div>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>Fetch only required data using GraphQL</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              Fetch only required data using GraphQL
+            </Typography>
           </div>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>Viewport URL prefetching</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              Viewport URL prefetching
+            </Typography>
           </div>
           <div>
             <SvgImage src={iconUspCheck} alt='box' loading='eager' size='medium' />
-            <Typography variant='h4'>Long-term browser caching, even between builds</Typography>
+            <Typography variant='h4' className={classes.h4}>
+              Long-term browser caching, even between builds
+            </Typography>
           </div>
         </div>
       </Container>
