c4761cf6d1810c140fd56f6eac8fca922f8c0edc
Paul Hachmang <paul@reachdigital.nl>
2021-09-27 13:40:15 +0200

    fix: build packages before releasing

--- packagesDev/next-config/js/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { NextConfig } from 'next/dist/server/config-shared';
-export declare type WorkspaceInfo = {
-    [name: string]: {
-        location: string;
-        workspaceDependencies: string[];
-        mismatchedWorkspaceDependencies: string[];
-    };
-};
-export declare function withYarn1Workspaces(modules?: string[]): (config: NextConfig) => NextConfig;
