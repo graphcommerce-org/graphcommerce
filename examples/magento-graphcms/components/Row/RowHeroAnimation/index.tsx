0607ca97d1a5c2a153d4bb3fbcd88f6b2f44e8b1
ErwinOtten <erwin@h-o.nl>
2021-10-02 10:23:08 +0200

    fix: import

--- examples/magento-graphcms/components/Row/RowHeroAnimation/index.tsx
+++ examples/magento-graphcms/components/Row/RowHeroAnimation/index.tsx
@@ -1,5 +1,5 @@
+import { responsiveVal } from '@graphcommerce/next-ui'
 import { Button, Container, makeStyles, Theme, Typography } from '@material-ui/core'
-import { responsiveVal } from '@reachdigital/next-ui'
 import React from 'react'
 import Animation from './Animation'
 
