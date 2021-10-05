345e60d93a16b98eda404fe1547a0e1c020eb0d4
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-10-05 10:05:19 +0200

    fix: adjust animation speeds

--- packages/framer-sheet/animation.tsx
+++ packages/framer-sheet/animation.tsx
@@ -1,21 +1,29 @@
 /**
+ * Value between 0.1 - 2.0
+ *
+ * Will be multiplied by animation values to adjust animation speed while preserving the animation feel
+ */
+const inertiaAnimationSpeed = 3.0
+const springAnimationSpeed = 2.0
+
+/**
  * Copy of the internal inertia animation for dragging
  *
  * https://github.com/framer/motion/blob/main/src/gestures/drag/VisualElementDragControls.ts#L545-L558
  */
 export const INERTIA_ANIM = {
   type: 'inertia',
-  bounceStiffness: 200,
-  bounceDamping: 40,
-  timeConstant: 750,
+  bounceStiffness: 200 * inertiaAnimationSpeed,
+  bounceDamping: 40 * inertiaAnimationSpeed,
+  timeConstant: 750 * inertiaAnimationSpeed,
   restDelta: 1,
 }
 
 /** Copy of INERTIA_ANIM, but converted to `type = 'spring'` */
 export const SPRING_ANIM = {
   type: 'spring',
-  stiffness: 200,
-  damping: 40,
+  stiffness: 200 * springAnimationSpeed,
+  damping: 40 * springAnimationSpeed,
   restDelta: 1,
-  restSpeed: 10,
+  restSpeed: 10 * springAnimationSpeed,
 }
