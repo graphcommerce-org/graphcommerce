c85a897733d4f2d93556fc68b4145d922efeea13
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-10-06 10:52:28 +0200

    style(cart-fab): more subtle box shadow

--- packages/next-ui/AppShell/useFixedFabAnimation.tsx
+++ packages/next-ui/AppShell/useFixedFabAnimation.tsx
@@ -7,9 +7,9 @@ export default function useFixedFabAnimation() {
   const { scrollY } = useViewportScroll()
   const scrollTo = isMobile ? 0 : 60
   const opacity = useTransform(scrollY, [50, scrollTo], [0, 1])
-  const opacity1 = useTransform(scrollY, [0, scrollTo], [0, 0.1])
+  const opacity1 = useTransform(scrollY, [0, scrollTo], [0, 0.08])
   const boxShadow = useMotionTemplate`
-    0 4px 12px 0 rgba(0, 0, 0, ${opacity1})
+    0 2px 10px 0 rgba(0, 0, 0, ${opacity1})
     `
 
   return { boxShadow, opacity }
