316d2b2a99189f093e74db98521c27e8e7267dd4
Paul Hachmang <paul@reachdigital.nl>
2021-10-08 16:13:15 +0200

    fix(mollie): make sure we're redirected to the success page

--- packages/mollie-magento-payment/_playwright/mollie_methods_ideal.spec.ts
+++ packages/mollie-magento-payment/_playwright/mollie_methods_ideal.spec.ts
@@ -1,16 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { waitForGraphQlResponse } from '@graphcommerce/graphql/_playwright/apolloClient.fixture'
-import { PaymentMethodPlaceOrderNoopDocument } from '@graphcommerce/magento-cart-payment-method/PaymentMethodPlaceOrderNoop/PaymentMethodPlaceOrderNoop.gql'
 import { fillShippingAddressForm } from '@graphcommerce/magento-cart-shipping-address/_playwright/fillShippingAddressForm'
 import { fillCartAgreementsForm } from '@graphcommerce/magento-cart/_playwright/fillCartAgreementsForm'
 import { addConfigurableProductToCart } from '@graphcommerce/magento-product-configurable/_playwright/addConfigurableProductToCart'
 import { test } from '@graphcommerce/magento-product/_playwright/productURL.fixture'
-import { expect } from '@playwright/test'
+import { expect, Page } from '@playwright/test'
+import { MolliePlaceOrderDocument } from '../components/MolliePlaceOrder/MolliePlaceOrder.gql'
 import { UseMolliePaymentTokenHandlerDocument } from '../hooks/UseMolliePaymentTokenHandler.gql'
 
-test('place order failed', async ({ page, productURL }) => {
-  await addConfigurableProductToCart(page, productURL.ConfigurableProduct)
-
+const goToPayment = async (page: Page) => {
   await page.click('a:has-text("View shopping cart")')
 
   await page.click('a[href="/checkout"]:last-of-type')
@@ -30,17 +28,74 @@ test('place order failed', async ({ page, productURL }) => {
   await page.selectOption('select[name="issuer"]', 'ideal_RABONL2U')
 
   await fillCartAgreementsForm(page)
+}
 
-  // Place the order and wait for the the redirect to the new page.
-  await Promise.all([page.waitForNavigation(), page.click('button[name="placeOrder"]')])
+test.describe('mollie ideal place order', () => {
+  test('CANCELED', async ({ page, productURL }) => {
+    test.fixme()
+    await addConfigurableProductToCart(page, productURL.ConfigurableProduct)
+    await goToPayment(page)
 
-  // Let the order fail
-  await page.click('input[name="final_state"][value=failed]')
+    // Place the order and wait for the the redirect to the new page.
+    await Promise.all([page.waitForNavigation(), page.click('button[name="placeOrder"]')])
 
-  // Return to the website.
-  await Promise.all([page.waitForNavigation(), page.click('.footer button')])
+    // Let the order fail
+    await page.click('input[name="final_state"][value=canceled]')
 
-  const result = await waitForGraphQlResponse(page, UseMolliePaymentTokenHandlerDocument)
-  expect(result.errors).toBeUndefined()
-  expect(result.data?.mollieProcessTransaction?.paymentStatus).toBeDefined()
+    // Return to the website.
+    await Promise.all([page.waitForNavigation(), page.click('.footer button')])
+
+    const result = await waitForGraphQlResponse(page, UseMolliePaymentTokenHandlerDocument)
+    expect(result.errors).toBeUndefined()
+    expect(result.data?.mollieProcessTransaction?.paymentStatus).toBe('CANCELED')
+
+    // Select Rabobank
+    await page.selectOption('select[name="issuer"]', 'ideal_RABONL2U')
+
+    // Place the order and wait for the the redirect to the new page.
+    await Promise.all([page.waitForNavigation(), page.click('button[name="placeOrder"]')])
+
+    const placeOrder = await waitForGraphQlResponse(page, MolliePlaceOrderDocument)
+    expect(placeOrder.errors).toBeUndefined()
+  })
+
+  test('PAID', async ({ page, productURL }) => {
+    await addConfigurableProductToCart(page, productURL.ConfigurableProduct)
+    await goToPayment(page)
+
+    await page.pause()
+
+    // Place the order and wait for the the redirect to the new page.
+    await Promise.all([page.waitForNavigation(), page.click('button[name="placeOrder"]')])
+
+    // Let the order fail
+    await page.click('input[name="final_state"][value=paid]')
+
+    // Return to the website.
+    await Promise.all([page.waitForNavigation(), page.click('.footer button')])
+
+    const result = await waitForGraphQlResponse(page, UseMolliePaymentTokenHandlerDocument)
+    expect(result.errors).toBeUndefined()
+    expect(result.data?.mollieProcessTransaction?.paymentStatus).toBe('PAID')
+
+    await page.waitForNavigation()
+  })
 })
+
+// test('place order failed', async ({ page, productURL }) => {
+//   await addConfigurableProductToCart(page, productURL.ConfigurableProduct)
+//   await goToPayment(page)
+
+//   // Place the order and wait for the the redirect to the new page.
+//   await Promise.all([page.waitForNavigation(), page.click('button[name="placeOrder"]')])
+
+//   // Let the order fail
+//   await page.click('input[name="final_state"][value=failed]')
+
+//   // Return to the website.
+//   await Promise.all([page.waitForNavigation(), page.click('.footer button')])
+
+//   const result = await waitForGraphQlResponse(page, UseMolliePaymentTokenHandlerDocument)
+//   expect(result.errors).toBeUndefined()
+//   expect(result.data?.mollieProcessTransaction?.paymentStatus).toBeDefined()
+// })
