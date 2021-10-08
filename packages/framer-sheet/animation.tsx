5b9ece293fb7e12663386f9f9cbc99bc4e22aaa9
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-10-08 10:37:30 +0200

    fix(menu-fab): animation duration
    fix(framer-sheet): animation duration

--- packages/framer-sheet/animation.tsx
+++ packages/framer-sheet/animation.tsx
@@ -1,29 +1,18 @@
 /**
- * Value between 0.1 - 3.0
- *
- * Will be multiplied by animation values to adjust animation speed while preserving the animation feel
- */
-const inertiaAnimationModifier = 3.0
-const springAnimationModifier = 2.0
-
-/**
  * Copy of the internal inertia animation for dragging
  *
  * https://github.com/framer/motion/blob/main/src/gestures/drag/VisualElementDragControls.ts#L545-L558
  */
 export const INERTIA_ANIM = {
   type: 'inertia',
-  bounceStiffness: 200 * inertiaAnimationModifier,
-  bounceDamping: 40 * inertiaAnimationModifier,
-  timeConstant: 750 * inertiaAnimationModifier,
+  bounceStiffness: 200,
+  bounceDamping: 40,
+  timeConstant: 750,
   restDelta: 1,
 }
 
-/** Copy of INERTIA_ANIM, but converted to `type = 'spring'` */
 export const SPRING_ANIM = {
   type: 'spring',
-  stiffness: 200 * springAnimationModifier,
-  damping: 40 * springAnimationModifier,
-  restDelta: 1,
-  restSpeed: 10 * springAnimationModifier,
+  stiffness: 580,
+  damping: 75,
 }
