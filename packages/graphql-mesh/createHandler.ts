53226e1a276116e6a7b4321294b411ae3b15b27f
Paul Hachmang <paul@reachdigital.nl>
2021-10-20 01:07:28 +0200

    chore: add missing operationHeaders for future compatibility

--- packages/graphql-mesh/createHandler.ts
+++ packages/graphql-mesh/createHandler.ts
@@ -67,13 +67,15 @@ export async function createHandler(meshInstance: MeshInstance, path: string) {
       'X-Api-Version',
       'Access-Control-Allow-Origin',
       'X-HTTP-Method-Override',
+      'x-apollo-tracing',
+      'apollographql-client-name',
+
+      // Magento 2 related headers
       'Authorization',
       'Store',
       'Preview-Version',
       'Content-Currency',
       'X-ReCaptcha',
-      'x-apollo-tracing',
-      'apollographql-client-name',
     ],
   })
 
