1064c17122ba4a6b0a140f805968122ef4913fc0
Paul Hachmang <paul@reachdigital.nl>
2021-10-19 23:20:10 +0200

    fix(mesh): make sure we're using an inmemory store when running the mesh

--- examples/magento-graphcms-api/pages/api/graphql.ts
+++ examples/magento-graphcms-api/pages/api/graphql.ts
@@ -2,14 +2,23 @@ import { cwd } from 'process'
 import { createHandler, config, injectEnv } from '@graphcommerce/graphql-mesh'
 import { processConfig } from '@graphql-mesh/config'
 import { getMesh } from '@graphql-mesh/runtime'
+import { MeshStore, InMemoryStoreStorageAdapter } from '@graphql-mesh/store'
+import { YamlConfig } from '@graphql-mesh/types'
 import { NextApiRequest, NextApiResponse } from 'next'
-import meshConfig from '../../.meshrc.json'
+import meshConfig from '../../.meshrc.yml'
 
-const handler = (async () =>
-  createHandler(
-    await getMesh(await processConfig(injectEnv(meshConfig), { dir: cwd() })),
+const handler = (async () => {
+  const store = new MeshStore('.mesh', new InMemoryStoreStorageAdapter(), {
+    validate: true,
+    readonly: false,
+  })
+  return createHandler(
+    await getMesh(
+      await processConfig(injectEnv(meshConfig as YamlConfig.Config), { dir: cwd(), store }),
+    ),
     '/api/graphql',
-  ))()
+  )
+})()
 
 export { config }
 
