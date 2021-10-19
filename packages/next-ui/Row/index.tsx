954656ad813171511848a284b27206c1defdc768
ErwinOtten <erwin@h-o.nl>
2021-10-19 16:03:58 +0200

    feat: more spacious row margins for mobile

--- packages/next-ui/Row/index.tsx
+++ packages/next-ui/Row/index.tsx
@@ -2,7 +2,7 @@ import { Container, styled } from '@material-ui/core'
 
 const Row = styled(Container)(
   ({ theme }) => ({
-    marginBottom: theme.spacings.lg,
+    marginBottom: theme.spacings.xl,
     [theme.breakpoints.up('md')]: {
       marginBottom: `${theme.spacings.xl}`,
     },
