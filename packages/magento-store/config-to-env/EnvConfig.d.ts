249f1a0e10dce9341f13bdbf32151e65bd8e3b41
Paul Hachmang <paul@reachdigital.nl>
2021-10-20 15:16:29 +0200

    chore: remove unused files

--- packages/magento-store/config-to-env/EnvConfig.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NEXT_PUBLIC_MAGENTO_CONFIG: {
-      [storeCode: string]: NonNullable<NonNullable<EnvConfigQuery['envConfig']>[0]>
-    }
-  }
-}
