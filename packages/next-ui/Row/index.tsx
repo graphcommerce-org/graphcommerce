19dc4c4bde18bec74c8f3e13e3769b929f2e9d57
ErwinOtten <erwin@h-o.nl>
2021-10-25 17:49:57 +0200

    fix: remove unused breakpoint

--- packages/next-ui/Row/index.tsx
+++ packages/next-ui/Row/index.tsx
@@ -3,9 +3,6 @@ import { Container, styled } from '@material-ui/core'
 const Row = styled(Container)(
   ({ theme }) => ({
     marginBottom: theme.spacings.xl,
-    [theme.breakpoints.up('md')]: {
-      marginBottom: `${theme.spacings.xl}`,
-    },
   }),
   { name: 'Row' },
 )
