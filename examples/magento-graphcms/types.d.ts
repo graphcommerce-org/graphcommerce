c8c037d489b4b0f51fb18dc49cb8e97f1ad75734
Paul Hachmang <paul@reachdigital.nl>
2021-10-26 10:15:24 +0200

    chore: fix build

--- examples/magento-graphcms/types.d.ts
+++ examples/magento-graphcms/types.d.ts
@@ -2,3 +2,12 @@ declare module '*.yml' {
   const content: unknown
   export default content
 }
+
+declare module '*.po' {
+  const messages: Record<
+    string,
+    string | Array<string | Array<string | (string | undefined) | Record<string, unknown>>>
+  >
+
+  export const messages
+}
