95ce54d7fd41d11120847f2fdf6b9097a2c93871
ErwinOtten <erwin@h-o.nl>
2021-10-28 22:11:11 +0200

    fix: move checkmark icons on select fields

--- packages/next-ui/Form/InputCheckmark.tsx
+++ packages/next-ui/Form/InputCheckmark.tsx
@@ -1,14 +1,18 @@
 import { makeStyles, Theme } from '@material-ui/core'
+import clsx from 'clsx'
 import React, { PropsWithChildren } from 'react'
 import SvgImage from '../SvgImage'
 import SvgImageSimple from '../SvgImage/SvgImageSimple'
 import { iconCheckmark } from '../icons'
 
-export type InputCheckmarkProps = PropsWithChildren<{ show?: boolean }>
+export type InputCheckmarkProps = PropsWithChildren<{ show?: boolean; select?: boolean }>
 const useStyles = makeStyles(
   (theme: Theme) => ({
     iconCheckmark: {
       stroke: '#01D26A',
+    },
+    select: {
+      marginRight: 15,
     },
   }),
   { name: 'InputCheckmark' },
@@ -22,9 +26,14 @@ const useStyles = makeStyles(
  * ```
  */
 export default function InputCheckmark(props: InputCheckmarkProps) {
-  const { show: valid, children } = props
+  const { show: valid, children, select = false } = props
   const classes = useStyles()
 
   if (!valid) return <>{children}</>
-  return <SvgImageSimple src={iconCheckmark} className={classes.iconCheckmark} />
+  return (
+    <SvgImageSimple
+      src={iconCheckmark}
+      className={clsx(classes.iconCheckmark, select && classes.select)}
+    />
+  )
 }
