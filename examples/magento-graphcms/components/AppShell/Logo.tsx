9b4909e6711cadcc5af76fb0e8f437f038366b69
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-11-02 14:20:44 +0100

    refactor(logo): remove use styles

--- examples/magento-graphcms/components/AppShell/Logo.tsx
+++ examples/magento-graphcms/components/AppShell/Logo.tsx
@@ -1,76 +1,16 @@
-import { Image } from '@graphcommerce/image'
-import { UseStyles } from '@graphcommerce/next-ui/Styles'
-import { makeStyles, Theme, useTheme } from '@material-ui/core'
-import clsx from 'clsx'
-import PageLink from 'next/link'
-import React from 'react'
+import { Logo as NextLogo } from '@graphcommerce/next-ui'
 import svgLogo from './graphcommerce.svg'
 
-const useStyles = makeStyles(
-  (theme: Theme) => ({
-    logo: {
-      width: 'auto',
-      height: 16,
-      pointerEvents: 'all',
-      paddingLeft: 10,
-      [theme.breakpoints.up('md')]: {
-        width: 'auto',
-        height: 28,
-        paddingLeft: 0,
-        marginTop: '-5px',
-      },
-    },
-    dark: {
-      filter: 'invert(100%)',
-    },
-    link: {
-      height: '100%',
-      width: 'max-content',
-      display: 'flex',
-      alignItems: 'center',
-      margin: '0 auto',
-      justifyContent: 'center',
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
-        margin: 'unset',
-        justifyContent: 'left',
-      },
-    },
-    hideLogoOnMobile: {
-      display: 'none',
-      [theme.breakpoints.up('md')]: {
-        display: 'unset',
-      },
-    },
-  }),
-  { name: 'Logo' },
-)
-
-type LogoProps = UseStyles<typeof useStyles> & {
-  alwaysShow?: boolean
-}
-
-export default function Logo(props: LogoProps) {
-  const { alwaysShow } = props
-  const classes = useStyles(props)
-  const theme = useTheme()
-
+export default function Logo() {
   return (
-    <PageLink href='/' passHref>
-      <a className={classes.link}>
-        <Image
-          layout='fixed'
-          alt='logo'
-          src={svgLogo}
-          unoptimized
-          loading='eager'
-          className={clsx(
-            classes.logo,
-            theme.palette.type === 'dark' && classes.dark,
-            !alwaysShow && classes.hideLogoOnMobile,
-          )}
-        />
-      </a>
-    </PageLink>
+    <NextLogo
+      image={{
+        layout: 'fixed',
+        alt: 'logo',
+        src: svgLogo,
+        unoptimized: true,
+        loading: 'eager',
+      }}
+    />
   )
 }
---
+++ packages/next-ui/AppShell/Logo.tsx
@@ -0,0 +1,73 @@
+import { Image, ImageProps } from '@graphcommerce/image'
+import { makeStyles, Theme, useTheme } from '@material-ui/core'
+import clsx from 'clsx'
+import PageLink from 'next/link'
+import React from 'react'
+import { UseStyles } from '../Styles'
+
+const useStyles = makeStyles(
+  (theme: Theme) => ({
+    logo: {
+      width: 'auto',
+      height: 16,
+      pointerEvents: 'all',
+      paddingLeft: 10,
+      [theme.breakpoints.up('md')]: {
+        width: 'auto',
+        height: 28,
+        paddingLeft: 0,
+        marginTop: '-5px',
+      },
+    },
+    dark: {
+      filter: 'invert(100%)',
+    },
+    link: {
+      height: '100%',
+      width: 'max-content',
+      display: 'flex',
+      alignItems: 'center',
+      margin: '0 auto',
+      justifyContent: 'center',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+        margin: 'unset',
+        justifyContent: 'left',
+      },
+    },
+    logoAlwaysShow: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'unset',
+      },
+    },
+  }),
+  { name: 'Logo' },
+)
+
+type LogoProps = {
+  href?: `/${string}`
+  image: ImageProps
+  alwaysShow?: boolean
+} & UseStyles<typeof useStyles>
+
+export default function Logo(props: LogoProps) {
+  const { href, alwaysShow, image } = props
+  const classes = useStyles(props)
+  const theme = useTheme()
+
+  return (
+    <PageLink href={href ?? '/'} passHref>
+      <a className={classes.link}>
+        <Image
+          {...{ ...image }}
+          className={clsx(
+            classes.logo,
+            theme.palette.type === 'dark' && classes.dark,
+            alwaysShow && classes.logoAlwaysShow,
+          )}
+        />
+      </a>
+    </PageLink>
+  )
+}
