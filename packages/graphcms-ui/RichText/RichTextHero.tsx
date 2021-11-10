34cf81eed6c5996f70be8b2c10888e44fad4d57f
ErwinOtten <erwin@h-o.nl>
2021-11-10 13:21:11 +0100

    fix: must have smaller sizes for mobile

--- packages/graphcms-ui/RichText/RichTextHero.tsx
+++ packages/graphcms-ui/RichText/RichTextHero.tsx
@@ -4,11 +4,14 @@ import RichText from '.'
 const RichTextHero = withStyles((theme: Theme) => ({
   h1: {
     ...theme.typography.h1,
-    fontSize: `calc(${theme.typography.h1.fontSize}*1.5)`,
+    fontSize: `calc(${theme.typography.h1.fontSize}*1.1)`,
     textTransform: 'uppercase',
     maxWidth: '70%',
     textAlign: 'center',
     marginBottom: theme.spacings.md,
+    [theme.breakpoints.up('sm')]: {
+      fontSize: `calc(${theme.typography.h1.fontSize}*1.4)`,
+    },
     [theme.breakpoints.up('md')]: {
       textAlign: 'left',
       maxWidth: '100%',
