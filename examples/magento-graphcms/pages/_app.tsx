d8ec22a80295af854a4cf6f357c4fb137c5b550d
Paul Hachmang <paul@reachdigital.nl>
2021-11-11 12:57:31 +0100

    feat: lingui configuration and integration greatly simplified and fixed ssr
    fix: server side rendering

--- examples/magento-graphcms/pages/_app.tsx
+++ examples/magento-graphcms/pages/_app.tsx
@@ -1,47 +1,27 @@
 import { ApolloProvider } from '@apollo/client'
 import { GoogleRecaptchaV3Script } from '@graphcommerce/googlerecaptcha'
 import { GoogleTagManagerScript } from '@graphcommerce/googletagmanager'
+import { LinguiProvider } from '@graphcommerce/lingui-next'
 import { App, AppProps } from '@graphcommerce/next-ui'
-import { i18n } from '@lingui/core'
-import { I18nProvider } from '@lingui/react'
-import { en, cs, nl } from 'make-plural/plurals'
 import { useRouter } from 'next/router'
-import React, { useEffect, useMemo } from 'react'
+import React from 'react'
 import ThemedProvider from '../components/Theme/ThemedProvider'
 import apolloClient from '../lib/apolloClientBrowser'
-
-type Messages = Record<
-  string,
-  string | Array<string | Array<string | (string | undefined) | Record<string, unknown>>>
->
 
 export default function ThemedApp(props: AppProps) {
   const { pageProps } = props
   const { locale } = useRouter()
-
-  useEffect(() => {
-    import(/* webpackPreload: true */ `../locales/${locale}.po`)
-      .then(({ messages }: { messages: Messages }) => {
-        if (!locale) return
-        i18n.loadLocaleData({
-          nl: { plurals: nl },
-        })
-        i18n.load(locale, messages)
-        i18n.activate(locale)
-      })
-      .catch(console.error)
-  }, [locale])
 
   return (
     <>
       <GoogleRecaptchaV3Script />
       <GoogleTagManagerScript />
       <ApolloProvider client={apolloClient(locale, true, pageProps.apolloState)}>
-        <ThemedProvider>
-          <I18nProvider i18n={i18n}>
+        <LinguiProvider loader={(locale) => import(`../locales/${locale}.po`)}>
+          <ThemedProvider>
             <App {...props} />
-          </I18nProvider>
-        </ThemedProvider>
+          </ThemedProvider>
+        </LinguiProvider>
       </ApolloProvider>
     </>
   )
---
+++ packages/lingui-next/components/LinguiProvider.tsx
@@ -0,0 +1,52 @@
+import { I18nProvider, I18nProviderProps } from '@lingui/react'
+import { useRouter } from 'next/router'
+import React, { useMemo } from 'react'
+import { i18n, Messages } from '@lingui/core'
+import { nl, en, fr } from 'make-plural/plurals'
+import { MessageLoader } from '../types'
+
+type LinguiProviderProps = Omit<I18nProviderProps, 'i18n'> & {
+  children: React.ReactNode
+  loader: MessageLoader
+}
+
+export default function LinguiProvider(props: LinguiProviderProps) {
+  const { loader, ...i18nProviderPRops } = props
+
+  const { locale = 'en' } = useRouter()
+  useMemo(() => {
+    const localeOnly = locale?.split('-')?.[0]
+
+    if (!localeOnly) return
+    const data = globalThis.document?.getElementById('lingui')
+
+    if (data?.lang === localeOnly && data.textContent) {
+      // @todo: We're not loading the plurals dynamically, but we can't because it will load the complete module.
+      i18n.loadLocaleData({
+        nl: { plurals: nl },
+        fr: { plurals: fr },
+        en: { plurals: en },
+      })
+      i18n.load(localeOnly, JSON.parse(data.textContent))
+      i18n.activate(localeOnly)
+    } else if (i18n.locale !== locale) {
+      ;(async () => {
+        try {
+          const messages = (await loader(localeOnly)).messages as Messages
+          i18n.load(localeOnly, messages)
+          i18n.activate(localeOnly)
+        } catch (e) {
+          if (process.env.NODE_ENV !== 'production')
+            throw new Error(
+              `Can not find the .po file for the locale '${localeOnly}'. Possible reasons:
+- If you've added a new locale, please run 'yarn lingui:extact' to generate the .po file.
+- Make sure the passed load function is correct.`,
+            )
+          if (process.env.NODE_ENV === 'production') console.error(e)
+        }
+      })()
+    }
+  }, [locale, loader])
+
+  return <I18nProvider i18n={i18n} {...i18nProviderPRops} />
+}
