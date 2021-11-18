54aea4c177c708de07df9ef9c590da6bff43aea8
ErwinOtten <erwin@h-o.nl>
2021-11-18 11:29:20 +0100

    fix: make stylable

--- packages/magento-review/components/ProductReviewSummary/index.tsx
+++ packages/magento-review/components/ProductReviewSummary/index.tsx
@@ -1,10 +1,12 @@
 import { SvgImageSimple, iconStar, responsiveVal } from '@graphcommerce/next-ui'
-import { ChipProps, makeStyles, Theme } from '@material-ui/core'
-import { Rating } from '@material-ui/lab'
+import { makeStyles, Theme } from '@material-ui/core'
+import { Rating, RatingProps } from '@material-ui/lab'
 import { ProductReviewSummaryFragment } from './ProductReviewSummary.gql'
 import React from 'react'
+import clsx from 'clsx'
 
-export type ProductReviewSummaryProps = ProductReviewSummaryFragment & ChipProps
+export type ProductReviewSummaryProps = ProductReviewSummaryFragment &
+  Omit<RatingProps, 'defaultValue' | 'readOnly'>
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -16,7 +18,7 @@ const useStyles = makeStyles(
 )
 
 export default function ProductReviewSummary(props: ProductReviewSummaryProps) {
-  const { rating_summary } = props
+  const { rating_summary, className, ...ratingProps } = props
   const classes = useStyles()
 
   if (!rating_summary) return null
@@ -25,15 +27,15 @@ export default function ProductReviewSummary(props: ProductReviewSummaryProps) {
   const normalizedRating = Math.round(rating_summary / (10 / max)) / 10
 
   return (
-    <div className={classes.ratingContainer}>
-      <Rating
-        name='customized-icons'
-        defaultValue={normalizedRating}
-        precision={0.1}
-        readOnly
-        size='small'
-        icon={<SvgImageSimple src={iconStar} size='small' />}
-      />
-    </div>
+    <Rating
+      name='customized-icons'
+      defaultValue={normalizedRating}
+      precision={0.1}
+      readOnly
+      size='small'
+      className={clsx(classes.ratingContainer, className)}
+      icon={<SvgImageSimple src={iconStar} size='small' />}
+      {...ratingProps}
+    />
   )
 }
