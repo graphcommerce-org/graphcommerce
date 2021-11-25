cbe59d9e753cc2ab76b2de048139e0319f08d035
Paul Hachmang <paul@reachdigital.nl>
2021-11-25 13:58:27 +0100

    feat(framer-scroller): provide promise with scrollTo

--- packages/framer-scroller/hooks/useScrollTo.ts
+++ packages/framer-scroller/hooks/useScrollTo.ts
@@ -2,43 +2,54 @@ import { useElementScroll } from '@graphcommerce/framer-utils'
 import { Point2D } from 'framer-motion'
 import { animate } from 'popmotion'
 import { useScrollerContext } from './useScrollerContext'
-
 export function useScrollTo() {
   const { scrollerRef, register, disableSnap, enableSnap } = useScrollerContext()
   const scroll = useElementScroll(scrollerRef)
 
   return (to: Point2D) => {
-    if (!scrollerRef.current) return
+    const ref = scrollerRef.current
+    if (!ref) return Promise.resolve()
 
     // const notCompletelyVisible = items.filter((item) => item.visibility.get() < 0.9)
     // todo get the target element and move one right, keep the current velocity
 
     disableSnap()
-    register(
-      animate({
-        from: scrollerRef.current.scrollLeft,
-        to: to.x,
-        velocity: scroll.x.getVelocity(),
-        onUpdate: (v) => {
-          if (!scrollerRef.current) return
-          scrollerRef.current.scrollLeft = v
-        },
-        onComplete: enableSnap,
-        bounce: 50,
-      }),
-    )
-    register(
-      animate({
-        from: scrollerRef.current.scrollTop,
-        to: to.y,
-        velocity: scroll.y.getVelocity(),
-        onUpdate: (v) => {
-          if (!scrollerRef.current) return
-          scrollerRef.current.scrollTop = v
-        },
-        onComplete: enableSnap,
-        bounce: 50,
-      }),
-    )
+
+    const xDone = new Promise<void>((onComplete) => {
+      register(
+        animate({
+          from: ref.scrollLeft,
+          to: to.x,
+          velocity: scroll.x.getVelocity(),
+          onUpdate: (v) => {
+            ref.scrollLeft = v
+          },
+          onComplete: () => {
+            enableSnap()
+            onComplete()
+          },
+          bounce: 50,
+        }),
+      )
+    })
+
+    const yDone = new Promise<void>((onComplete) => {
+      register(
+        animate({
+          from: ref.scrollTop,
+          to: to.y,
+          velocity: scroll.y.getVelocity(),
+          onUpdate: (v) => {
+            ref.scrollTop = v
+          },
+          onComplete: () => {
+            enableSnap()
+            onComplete()
+          },
+          bounce: 50,
+        }),
+      )
+    })
+    return Promise.all<void>([xDone, yDone])
   }
 }
