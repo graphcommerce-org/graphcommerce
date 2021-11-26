8cfe44434a8c37075470927b176c7dabcb9e06ab
ErwinOtten <erwin@h-o.nl>
2021-11-26 19:56:43 +0100

    fix: correct footer button color

--- examples/magento-graphcms/components/AppShell/Footer.tsx
+++ examples/magento-graphcms/components/AppShell/Footer.tsx
@@ -43,7 +43,7 @@ export default function Footer(props: FooterProps) {
       storeSwitcher={<StoreSwitcherButton />}
       customerService={
         <PageLink href='/service' passHref>
-          <Button variant='pill' color='inherit'>
+          <Button variant='pill' color='default'>
             <Trans>Customer Service</Trans>
           </Button>
         </PageLink>
