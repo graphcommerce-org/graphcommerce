48be01340ec0dad99d821401a9f95de51698a565
Paul Hachmang <paul@reachdigital.nl>
2021-11-29 17:08:33 +0100

    refactor: simplify renderer

--- examples/magento-graphcms/components/ProductListItems/renderers.tsx
+++ examples/magento-graphcms/components/ProductListItems/renderers.tsx
@@ -14,83 +14,60 @@ const Subtitle = (props: TypographyProps) => (
 )
 
 const renderers: ProductListItemRenderer = {
-  SimpleProduct: (props) => {
-    const { rating_summary } = props
-    return (
-      <ProductListItemSimple
-        {...props}
-        subTitle={<Subtitle>BY GC</Subtitle>}
-        aspectRatio={[1, 1]}
-        bottomLeft={<ProductReviewSummary rating_summary={rating_summary} />}
-      />
-    )
-  },
-  ConfigurableProduct: (props) => {
-    const { rating_summary } = props
-
-    return (
-      <ProductListItemConfigurable
-        {...props}
-        subTitle={<Subtitle>BY GC</Subtitle>}
-        aspectRatio={[1, 1]}
-        swatchLocations={{
-          topLeft: [],
-          topRight: [], // ['size']
-          bottomLeft: [],
-          bottomRight: ['dominant_color'],
-        }}
-        bottomLeft={<ProductReviewSummary rating_summary={rating_summary} />}
-      />
-    )
-  },
-  BundleProduct: (props) => {
-    const { rating_summary } = props
-
-    return (
-      <ProductListItemBundle
-        {...props}
-        subTitle={<Subtitle>BY GC</Subtitle>}
-        aspectRatio={[1, 1]}
-        bottomLeft={<ProductReviewSummary rating_summary={rating_summary} />}
-      />
-    )
-  },
-  VirtualProduct: (props) => {
-    const { rating_summary } = props
-
-    return (
-      <ProductListItemVirtual
-        {...props}
-        subTitle={<Subtitle>BY GC</Subtitle>}
-        aspectRatio={[1, 1]}
-        bottomLeft={<ProductReviewSummary rating_summary={rating_summary} />}
-      />
-    )
-  },
-  DownloadableProduct: (props) => {
-    const { rating_summary } = props
-
-    return (
-      <ProductListItemDownloadable
-        {...props}
-        subTitle={<Subtitle>BY GC</Subtitle>}
-        aspectRatio={[1, 1]}
-        bottomLeft={<ProductReviewSummary rating_summary={rating_summary} />}
-      />
-    )
-  },
-  GroupedProduct: (props) => {
-    const { rating_summary } = props
-
-    return (
-      <ProductListItemGrouped
-        {...props}
-        subTitle={<Subtitle>BY GC</Subtitle>}
-        aspectRatio={[1, 1]}
-        bottomLeft={<ProductReviewSummary rating_summary={rating_summary} />}
-      />
-    )
-  },
+  SimpleProduct: (props) => (
+    <ProductListItemSimple
+      {...props}
+      subTitle={<Subtitle>BY GC</Subtitle>}
+      aspectRatio={[1, 1]}
+      bottomLeft={<ProductReviewSummary rating_summary={props.rating_summary} />}
+    />
+  ),
+  ConfigurableProduct: (props) => (
+    <ProductListItemConfigurable
+      {...props}
+      subTitle={<Subtitle>BY GC</Subtitle>}
+      aspectRatio={[1, 1]}
+      swatchLocations={{
+        topLeft: [],
+        topRight: [], // ['size']
+        bottomLeft: [],
+        bottomRight: ['dominant_color'],
+      }}
+      bottomLeft={<ProductReviewSummary rating_summary={props.rating_summary} />}
+    />
+  ),
+  BundleProduct: (props) => (
+    <ProductListItemBundle
+      {...props}
+      subTitle={<Subtitle>BY GC</Subtitle>}
+      aspectRatio={[1, 1]}
+      bottomLeft={<ProductReviewSummary rating_summary={props.rating_summary} />}
+    />
+  ),
+  VirtualProduct: (props) => (
+    <ProductListItemVirtual
+      {...props}
+      subTitle={<Subtitle>BY GC</Subtitle>}
+      aspectRatio={[1, 1]}
+      bottomLeft={<ProductReviewSummary rating_summary={props.rating_summary} />}
+    />
+  ),
+  DownloadableProduct: (props) => (
+    <ProductListItemDownloadable
+      {...props}
+      subTitle={<Subtitle>BY GC</Subtitle>}
+      aspectRatio={[1, 1]}
+      bottomLeft={<ProductReviewSummary rating_summary={props.rating_summary} />}
+    />
+  ),
+  GroupedProduct: (props) => (
+    <ProductListItemGrouped
+      {...props}
+      subTitle={<Subtitle>BY GC</Subtitle>}
+      aspectRatio={[1, 1]}
+      bottomLeft={<ProductReviewSummary rating_summary={props.rating_summary} />}
+    />
+  ),
   // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // // @ts-ignore GiftCardProduct is only available in Commerce
   // GiftCardProduct: (props) => (
