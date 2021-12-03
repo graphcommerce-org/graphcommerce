421ac89be7639aad1e073a5aa4a7b25d4acea4cb
ErwinOtten <erwin@h-o.nl>
2021-12-03 14:50:18 +0100

    fix: build

--- packages/next-ui/Styles/responsiveVal.tsx
+++ packages/next-ui/Styles/responsiveVal.tsx
@@ -3,6 +3,14 @@
  *
  * E.g.: `responsiveVal(16, 22)` -> Will render 16px at 320px window width, 22 ad 1280 window width
  */
-export function responsiveVal(min: number, max: number, maxBreakpoint = 1280) {
-  return `max(${min}px, min(${min}, ${max}px))`
+export function responsiveVal(
+  min: number,
+  max: number,
+  maxBreakpoint = 1280,
+): `max(${number}px, min(${string}, ${number}px))` {
+  const round = (x: number, n: number): number => Math.round(x * 10 ** n) / 10 ** n
+
+  const minBreakpoint = 320
+
+  return `max(${min}px, min(${max}, ${max}px))`
 }
