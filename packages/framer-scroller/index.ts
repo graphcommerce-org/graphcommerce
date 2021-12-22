0cf60669b2547d2c421eb07c1ba23d7718df74aa
Paul Hachmang <paul@reachdigital.nl>
2021-12-22 14:39:48 +0100

    fix(framer-scroller): gallery didn't align images in the center

--- packages/framer-scroller/index.ts
+++ packages/framer-scroller/index.ts
@@ -4,7 +4,6 @@ export * from './components/MotionImageAspect'
 export { default as MotionImageAspect } from './components/MotionImageAspect'
 
 export { default as Scroller } from './components/Scroller'
-export { default as CenterSlide } from './components/CenterSlide'
 
 export * from './components/ScrollerButton'
 export { default as ScrollerButton } from './components/ScrollerButton'
