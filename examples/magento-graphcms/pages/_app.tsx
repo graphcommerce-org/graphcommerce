2aef0618eedda4302cf83eca23092ed4c1d22bca
Paul Hachmang <paul@reachdigital.nl>
2021-12-22 15:32:27 +0100

    fix: darkmode would not initialize properly

--- examples/magento-graphcms/pages/_app.tsx
+++ examples/magento-graphcms/pages/_app.tsx
@@ -7,14 +7,18 @@ import { StoreConfigDocument } from '@graphcommerce/magento-store'
 import { App, AppProps, GlobalHead } from '@graphcommerce/next-ui'
 import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import { useRouter } from 'next/router'
-import React, { useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import { lightTheme, darkTheme } from '../components/Theme/ThemedProvider'
 import apolloClient from '../lib/apolloClientBrowser'
 
 export default function ThemedApp(props: AppProps) {
   const { pageProps } = props
   const { locale, asPath } = useRouter()
-  const darkMode = useRef(asPath.includes('dark'))
+
+  // Hack for the demo to allow using darkmode without any fancy darkmode toggles
+  const [darkMode, setDarkMode] = useState(false)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => setDarkMode(asPath.includes('dark')), [])
 
   const client = apolloClient(locale, true, pageProps.apolloState)
   const storeConfig = useQuery(StoreConfigDocument, { client })
@@ -28,7 +32,7 @@ export default function ThemedApp(props: AppProps) {
       {/* <GoogleTagManagerScript /> */}
       <ApolloProvider client={client}>
         <LinguiProvider loader={(l) => import(`../locales/${l}.po`)}>
-          <ThemeProvider theme={darkMode.current ? darkTheme : lightTheme}>
+          <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
             <CssBaseline />
             <App {...props} key={locale} />
           </ThemeProvider>
