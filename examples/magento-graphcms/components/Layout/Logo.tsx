7c10c5f81b5ab27a34abb3213ca07eea6a4d7808
Paul Hachmang <paul@reachdigital.nl>
2022-01-20 14:54:58 +0100

    fix: layout inverted styles

--- examples/magento-graphcms/components/Layout/Logo.tsx
+++ examples/magento-graphcms/components/Layout/Logo.tsx
@@ -4,15 +4,15 @@ import svgLogo from './graphcommerce.svg'
 export function Logo() {
   return (
     <LogoBase
-      sx={(theme) => ({
+      sx={{
         '& .GcLogo-logo': {
           width: 'auto',
           height: { xs: '16px', md: '28px' },
           paddingLeft: { xs: '10px', md: 0 },
           marginTop: { xs: 0, md: '-5px' },
-          fitler: theme.palette.mode === 'dark' ? 'invert(100%)' : 'invert(0%)',
+          filter: (theme) => (theme.palette.mode === 'dark' ? 'invert(100%)' : 'invert(0%)'),
         },
-      })}
+      }}
       image={{ alt: 'GraphCommerce Logo', src: svgLogo, unoptimized: true }}
     />
   )
