80455ab1deaad97b3fb92eeebc846e41f39da18f
ErwinOtten <erwin@h-o.nl>
2022-01-21 17:02:51 +0100

    fix: darkMode logo

--- examples/magento-graphcms/components/Layout/Logo.tsx
+++ examples/magento-graphcms/components/Layout/Logo.tsx
@@ -1,5 +1,5 @@
 import { Logo as NextLogo, LogoProps as CoreLogoProps } from '@graphcommerce/next-ui'
-import { makeStyles, Theme, useTheme } from '@material-ui/core'
+import { makeStyles, Theme } from '@material-ui/core'
 import clsx from 'clsx'
 import svgLogo from './graphcommerce.svg'
 
@@ -17,9 +17,7 @@ const useStyles = makeStyles(
         paddingLeft: 0,
         marginTop: -2,
       },
-    },
-    dark: {
-      filter: 'invert(50%) brightness(200%)',
+      filter: theme.palette.type === 'dark' ? 'invert(50%) brightness(200%)' : undefined,
     },
   }),
   { name: 'Logo' },
@@ -27,12 +25,11 @@ const useStyles = makeStyles(
 
 export default function Logo(props: LogoProps) {
   const classes = useStyles(props)
-  const inverted = useTheme().palette.type === 'dark'
 
   return (
     <NextLogo
       {...props}
-      classes={{ logo: clsx(classes.logo, inverted && classes.dark) }}
+      classes={{ logo: clsx(classes.logo) }}
       image={{ alt: 'logo', src: svgLogo, unoptimized: true }}
     />
   )
