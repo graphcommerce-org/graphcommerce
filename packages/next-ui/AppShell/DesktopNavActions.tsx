db3136931d2ace1bfb6e7fecad0e01758aa2b397
ErwinOtten <erwin@h-o.nl>
2022-02-01 15:02:42 +0100

    fix: make DesktopNavActions stylable

--- packages/next-ui/AppShell/DesktopNavActions.tsx
+++ packages/next-ui/AppShell/DesktopNavActions.tsx
@@ -1,5 +1,6 @@
 import { makeStyles, Theme } from '@material-ui/core'
 import React from 'react'
+import { UseStyles } from '../Styles'
 
 const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -20,8 +21,12 @@ const useStyles = makeStyles(
   { name: 'DesktopNavActions' },
 )
 
-export default function DesktopNavActions(props: { children?: React.ReactNode }) {
+export default function DesktopNavActions(
+  props: {
+    children?: React.ReactNode
+  } & UseStyles<typeof useStyles>,
+) {
   const { children } = props
-  const classes = useStyles()
+  const classes = useStyles(props)
   return <div className={classes.actions}>{children}</div>
 }
