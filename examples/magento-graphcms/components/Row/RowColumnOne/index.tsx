ae2681bf95dcc640933b2d872626236cd7b25ceb
Leander Matse <leander@reachdigital.nl>
2022-02-11 12:27:02 +0100

    fix: Removal of UseRichTextStyles passed as prop. This is already handled in de <RichText /> component.

--- examples/magento-graphcms/components/Row/RowColumnOne/index.tsx
+++ examples/magento-graphcms/components/Row/RowColumnOne/index.tsx
@@ -1,19 +1,15 @@
 import RichText from '@graphcommerce/graphcms-ui/RichText'
-import { UseRichTextStyles } from '@graphcommerce/graphcms-ui/RichText/useRichTextStyles'
 import { ColumnOne } from '@graphcommerce/next-ui'
 import React from 'react'
 import type { RowColumnOneFragment } from './RowColumnOne.gql'
 
-export type RowColumnOneProps = RowColumnOneFragment & {
-  richTextOneClasses?: UseRichTextStyles['classes']
-}
 
-export default function RowColumnOne(props: RowColumnOneProps) {
+export default function RowColumnOne(props: RowColumnOneFragment) {
   const { colOne, richTextOneClasses } = props
 
   return (
     <ColumnOne>
-      <RichText {...colOne} classes={richTextOneClasses} />
+      <RichText {...colOne} />
     </ColumnOne>
   )
 }
