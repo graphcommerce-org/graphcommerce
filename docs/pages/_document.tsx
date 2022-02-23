8671a8b4e69f70afda4e1504c0f15b3863920474
Paul Hachmang <paul@reachdigital.nl>
2022-02-23 15:58:13 +0100

    fix: fix ssr content rendering

---
+++ docs/pages/_document.tsx
@@ -0,0 +1,22 @@
+// import { GoogleTagManagerNoScript } from '@graphcommerce/googletagmanager'
+import { EmotionCacheProps, withEmotionCache } from '@graphcommerce/next-ui'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+class Document extends NextDocument<EmotionCacheProps> {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Inject MUI styles first to match with the prepend: true configuration. */}
+          {this.props.emotionStyleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default withEmotionCache(Document)
