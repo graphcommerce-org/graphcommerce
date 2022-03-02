44e793d4773605d671833b53d99dd436ce863d78
Laurens Franken <laurens@reachdigital.nl>
2022-03-02 15:42:51 +0100

    feat: add sx prop to UspsList component

--- packages/next-ui/UspList/index.tsx
+++ packages/next-ui/UspList/index.tsx
@@ -1,8 +1,9 @@
-import { Box } from '@mui/material'
+import { Box, SxProps, Theme } from '@mui/material'
 import { extendableComponent } from '../Styles'
 
 export type UspListProps = OwnerState & {
   children: React.ReactNode
+  sx?: SxProps<Theme>
 }
 
 type OwnerState = { size?: 'small' | 'medium' }
@@ -11,24 +12,27 @@ const parts = ['root'] as const
 const { withState } = extendableComponent<OwnerState, typeof name, typeof parts>(name, parts)
 
 export function UspList(props: UspListProps) {
-  const { children, size } = props
+  const { children, size, sx = [] } = props
   const classes = withState({ size })
 
   return (
     <Box
       component='ul'
       className={classes.root}
-      sx={(theme) => ({
-        listStyleType: 'none',
-        padding: 0,
-        margin: 0,
-        display: 'grid',
-        alignContent: 'start',
-        rowGap: theme.spacings.xs,
-        '&.sizeSmall': {
-          rowGap: '3px',
-        },
-      })}
+      sx={[
+        (theme) => ({
+          listStyleType: 'none',
+          padding: 0,
+          margin: 0,
+          display: 'grid',
+          alignContent: 'start',
+          rowGap: theme.spacings.xs,
+          '&.sizeSmall': {
+            rowGap: '3px',
+          },
+        }),
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
     >
       {children}
     </Box>
