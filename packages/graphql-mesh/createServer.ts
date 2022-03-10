2915650cc5a57e4613791b78ed6958282b93cb32
Paul Hachmang <paul@reachdigital.nl>
2022-03-10 10:06:42 +0100

    feat: moved to the new @graphql-yoga

--- packages/graphql-mesh/createServer.ts
+++ packages/graphql-mesh/createServer.ts
@@ -1,66 +1,25 @@
 import { processConfig } from '@graphql-mesh/config'
 import { getMesh as getGraphQLMesh, MeshInstance } from '@graphql-mesh/runtime'
 import { YamlConfig } from '@graphql-mesh/types'
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
-import { ApolloServer } from 'apollo-server-micro'
-import '@graphql-mesh/transform-filter-schema'
-import '@graphql-mesh/graphql'
-import '@graphql-mesh/merger-stitching'
-import '@graphql-mesh/transform-cache'
-import '@graphql-mesh/cache-inmemory-lru'
-import '@vue/compiler-sfc'
-import 'ts-tiny-invariant'
-import 'micro'
-import { plugin as apolloTracingPlugin } from 'apollo-tracing'
-import cors from 'micro-cors'
+import { createServer as createYogaServer } from '@graphql-yoga/node'
 
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export async function getMesh(config: unknown): Promise<MeshInstance> {
   return getGraphQLMesh(await processConfig(config as YamlConfig.Config, { dir: process.cwd() }))
 }
 
 export async function createServer(mesh: Promise<MeshInstance>, path: string) {
   const meshInstance = await mesh
-  const apolloServer = new ApolloServer({
-    context: ({ req }) => req,
-    introspection: true,
+
+  const server = createYogaServer({
     ...meshInstance,
-    plugins: [
-      ApolloServerPluginLandingPageGraphQLPlayground({
-        // @ts-expect-error https://github.com/graphql/graphql-playground/issues/1289
-        shareEnabled: true,
-      }),
-      // @ts-expect-error apolloTracingPlugin is not officially supported anymore
-      apolloTracingPlugin(),
-    ],
+    cors: false,
+    graphiql: {},
+    endpoint: '/api/graphql',
+    logging: {
+      prettyLog: false,
+    },
   })
-  await apolloServer.start()
 
-  const apolloHandler = apolloServer.createHandler({ path })
-
-  const corsHandler = cors({
-    allowMethods: ['GET', 'POST', 'OPTIONS'],
-    allowHeaders: [
-      'X-CSRF-Token',
-      'X-Requested-With',
-      'Accept',
-      'Accept-Version',
-      'Content-Length',
-      'Content-MD5',
-      'Content-Type',
-      'Date',
-      'X-Api-Version',
-      'Access-Control-Allow-Origin',
-      'X-HTTP-Method-Override',
-      'x-apollo-tracing',
-      'apollographql-client-name',
-
-      // Magento 2 related headers
-      'Authorization',
-      'Store',
-      'Preview-Version',
-      'Content-Currency',
-      'X-ReCaptcha',
-    ],
-  })
-  return corsHandler((req, res) => (req.method === 'OPTIONS' ? res.end() : apolloHandler(req, res)))
+  return server.requestListener
 }
--- packages/graphql-mesh/index.ts
+++ packages/graphql-mesh/index.ts
@@ -1,3 +1,8 @@
 export * from './createServer'
 
-export const config = { api: { bodyParser: false } }
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+}
