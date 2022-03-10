22ea2a0af373dbd427e85f6ac82a074b1928a289
Paul Hachmang <paul@reachdigital.nl>
2022-03-10 10:37:22 +0100

    Moved all rendering of docs to https://www.graphcommerce.org/docs and use this only as a content package.

---
+++ .changeset/purple-tigers-cross.md
@@ -0,0 +1,6 @@
+---
+'@graphcommerce/docs': major
+---
+
+Moved all rendering of docs to https://www.graphcommerce.org/docs and use this
+only as a content package.
--- docs/pages/api/reindex.ts
+++
@@ -1,4 +0,0 @@
-import { DocumentIndexer } from '../../lib/DocumentIndexer'
-import { typesenseIndexerHandler } from '../../lib/typesense/typesenseIndexerHandler'
-
-export default typesenseIndexerHandler([new DocumentIndexer('content')])
