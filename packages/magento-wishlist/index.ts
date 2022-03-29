da818b6113077ceaef26a6f94e7c394c78df2000
Tim Hofman <tim@reachdigital.nl>
2022-03-29 14:46:39 +0200

    fix: adhere pascalcase for graphql documents

--- packages/magento-wishlist/index.ts
+++ packages/magento-wishlist/index.ts
@@ -3,8 +3,8 @@ export * from './hooks'
 
 export * from './queries/GetGuestWishlistProducts.gql'
 export * from './queries/GetWishlistProducts.gql'
-export * from './queries/addProductToWishlist.gql'
-export * from './queries/removeProductFromWishlist.gql'
+export * from './queries/AddProductToWishlist.gql'
+export * from './queries/RemoveProductFromWishlist.gql'
 export * from './queries/WishlistSummaryFragment.gql'
 export * from './queries/GetIsInWishlists.gql'
 
