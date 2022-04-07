81c662e4f6fe0fe71c54e2f3ad36f210e057b05a
Tim Hofman <tim@reachdigital.nl>
2022-04-07 14:47:57 +0200

    chore: show status dot on top of wishlist icon
    Known defect: header icon does not update for guest wishlists
    at this moment. Customer wishlist works fine.
    
    Menu icon works for both types of wishlist.
    
    Changes to localStorage do trigger re-renders of components
    like Apollo does.

--- packages/magento-wishlist/components/WishlistFab/WishlistFab.tsx
+++ packages/magento-wishlist/components/WishlistFab/WishlistFab.tsx
@@ -1,9 +1,14 @@
-import { useQuery } from '@graphcommerce/graphql'
+import { useQuery } from '@apollo/client'
+import { CustomerTokenDocument } from '@graphcommerce/magento-customer'
+import {
+  GUEST_WISHLIST_STORAGE_NAME,
+  GetIsInWishlistsDocument,
+} from '@graphcommerce/magento-wishlist'
 import { iconHeart, DesktopHeaderBadge, IconSvg, extendableComponent } from '@graphcommerce/next-ui'
 import { t } from '@lingui/macro'
 import { Fab, FabProps as FabPropsType, NoSsr, SxProps, Theme } from '@mui/material'
 import PageLink from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 type WishlistFabContentProps = {
   icon?: React.ReactNode
@@ -18,6 +23,19 @@ const { classes } = extendableComponent(name, parts)
 function WishlistFabContent(props: WishlistFabContentProps) {
   const { icon, FabProps, sx } = props
 
+  const { data: token } = useQuery(CustomerTokenDocument)
+  const isLoggedIn = token?.customerToken && token?.customerToken.valid
+
+  const { data: GetCustomerWishlistData, loading } = useQuery(GetIsInWishlistsDocument, {
+    skip: !isLoggedIn,
+  })
+
+  let activeWishlist = false
+  if (isLoggedIn) {
+    const wishlistItemCount = GetCustomerWishlistData?.customer?.wishlists[0]?.items_count || 0
+    activeWishlist = wishlistItemCount > 0
+  }
+
   return (
     <PageLink href='/wishlist' passHref>
       <Fab
@@ -30,8 +48,8 @@ function WishlistFabContent(props: WishlistFabContentProps) {
         sx={sx}
       >
         <DesktopHeaderBadge
-          badgeContent={true ? 1 : 0}
-          color={true ? 'primary' : 'error'}
+          badgeContent={activeWishlist ? 1 : 0}
+          color='primary'
           variant='dot'
           overlap='circular'
         >
--- packages/magento-wishlist/components/WishlistMenuFabItem/WishlistMenuFabItem.tsx
+++ packages/magento-wishlist/components/WishlistMenuFabItem/WishlistMenuFabItem.tsx
@@ -1,3 +1,9 @@
+import { useQuery } from '@apollo/client'
+import { CustomerTokenDocument } from '@graphcommerce/magento-customer'
+import {
+  GUEST_WISHLIST_STORAGE_NAME,
+  GetIsInWishlistsDocument,
+} from '@graphcommerce/magento-wishlist'
 import { MenuFabSecondaryItem, iconHeart, IconSvg } from '@graphcommerce/next-ui'
 import { Badge, NoSsr, SxProps, Theme } from '@mui/material'
 import React from 'react'
@@ -11,13 +17,29 @@ type WishlistMenuFabItemProps = {
 function WishlistMenuFabItemContent(props: WishlistMenuFabItemProps) {
   const { icon, children, sx = [] } = props
 
+  const { data: token } = useQuery(CustomerTokenDocument)
+  const isLoggedIn = token?.customerToken && token?.customerToken.valid
+
+  const { data: GetCustomerWishlistData, loading } = useQuery(GetIsInWishlistsDocument, {
+    skip: !isLoggedIn,
+  })
+
+  let activeWishlist
+  if (isLoggedIn) {
+    const wishlistItemCount = GetCustomerWishlistData?.customer?.wishlists[0]?.items_count || 0
+    activeWishlist = wishlistItemCount > 0
+  } else {
+    const wishlist = JSON.parse(localStorage.getItem(GUEST_WISHLIST_STORAGE_NAME) || '[]')
+    activeWishlist = wishlist.length > 0
+  }
+
   return (
     <MenuFabSecondaryItem
       sx={sx}
       icon={
         <Badge
-          badgeContent={true ? 1 : 0}
-          color={true ? 'primary' : 'error'}
+          badgeContent={activeWishlist ? 1 : 0}
+          color='primary'
           variant='dot'
           overlap='circular'
         >
