dc1cf838bc5739e267bf524bb62afbd98554b8d4
Tim Hofman <tim@reachdigital.nl>
2022-04-14 11:33:16 +0200

    chore: validate guest wishlist items before migrating to db storage
    Some items might have been in client cache for a long time, and
    by the time of login could not be available anymore.

--- packages/magento-wishlist/hooks/useMergeGuestWishlistWithCustomer.tsx
+++ packages/magento-wishlist/hooks/useMergeGuestWishlistWithCustomer.tsx
@@ -3,28 +3,39 @@ import { CustomerTokenDocument } from '@graphcommerce/magento-customer'
 import {
   AddProductToWishlistDocument,
   GuestWishlistDocument,
+  GetGuestWishlistProductsDocument,
 } from '@graphcommerce/magento-wishlist'
 import { useEffect } from 'react'
 
 /** Merge guest wishlist items to customer session upon login */
 export function useMergeGuestWishlistWithCustomer() {
-  const { data: token } = useQuery(CustomerTokenDocument)
-  const isLoggedIn = token?.customerToken && token?.customerToken.valid
+  const customerToken = useQuery(CustomerTokenDocument)?.data?.customerToken
+  const isLoggedIn = customerToken?.token && customerToken?.valid
   const { cache } = useApolloClient()
 
-  const { data: guestWishlistData, loading: loadingGuestWishlistData } = useQuery(
-    GuestWishlistDocument,
-    {
+  const guestWishlistData = useQuery(GuestWishlistDocument, {
+    ssr: false,
+  }).data?.guestWishlist
+
+  const guestData = guestWishlistData?.items.map((item) => item?.sku) || []
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const validatedItems =
+    useQuery(GetGuestWishlistProductsDocument, {
       ssr: false,
-    },
-  )
+      variables: {
+        filters: { sku: { in: guestData } },
+      },
+      skip: guestData.length === 0,
+    }).data?.products?.items || []
 
   const [addWishlistItem] = useMutation(AddProductToWishlistDocument)
 
   useEffect(() => {
     if (!isLoggedIn) return
 
-    const wishlist = guestWishlistData?.guestWishlist?.items.map((item) => item?.sku) || []
+    if (!validatedItems.length) return
+
+    const wishlist = validatedItems.map((item) => item?.sku) || []
 
     if (!wishlist.length) return
 
@@ -37,5 +48,5 @@ export function useMergeGuestWishlistWithCustomer() {
       id: cache.identify({ __typename: 'GuestWishlist' }),
     })
     addWishlistItem({ variables: { input: payload } })
-  })
+  }, [validatedItems, addWishlistItem, cache, isLoggedIn])
 }
