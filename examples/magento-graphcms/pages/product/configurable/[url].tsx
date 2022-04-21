de25736e9be02497271f6552bba0b0d10b01561e
ErwinOtten <erwin@h-o.nl>
2022-04-21 21:49:13 +0200

    Revert "fix: nofollow sizechart model"
    This reverts commit 410379b860c116cfffe686a4cdd72b031890dc60.

--- examples/magento-graphcms/pages/product/configurable/[url].tsx
+++ examples/magento-graphcms/pages/product/configurable/[url].tsx
@@ -91,7 +91,7 @@ function ProductConfigurable(props: Props) {
             optionEndLabels={{
               size: (
                 <PageLink href='/modal/product/global/size' passHref>
-                  <Link color='primary' underline='hover' rel='nofollow'>
+                  <Link color='primary' underline='hover'>
                     <Trans>Which size is right?</Trans>
                   </Link>
                 </PageLink>
