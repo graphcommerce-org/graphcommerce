73f8778d2e73a09905a9eb2b55a45af1725be23b
Paul Hachmang <paul@reachdigital.nl>
2022-05-17 15:03:42 +0200

    chore: try and simplify hook

--- packages/magento-wishlist/hooks/useMergeGuestWishlistWithCustomer.tsx
+++ packages/magento-wishlist/hooks/useMergeGuestWishlistWithCustomer.tsx
@@ -1,24 +1,9 @@
 import { useMutation, useQuery, useApolloClient } from '@graphcommerce/graphql'
 import { CustomerTokenDocument } from '@graphcommerce/magento-customer'
-import { useCallback, useEffect, useMemo } from 'react'
+import { useEffect } from 'react'
 import { AddProductToWishlistDocument } from '../queries/AddProductToWishlist.gql'
 import { GetGuestWishlistProductsDocument } from '../queries/GetGuestWishlistProducts.gql'
 import { GuestWishlistDocument } from '../queries/GuestWishlist.gql'
-
-function useValidatedItems(guestDataSkus: string[]) {
-  const guestWishList = useQuery(GetGuestWishlistProductsDocument, {
-    ssr: false,
-    variables: {
-      filters: { sku: { in: guestDataSkus } },
-    },
-    skip: guestDataSkus.length === 0,
-  })
-  const items = useMemo(
-    () => guestWishList.data?.products?.items?.map((item) => item?.sku) || [],
-    [guestWishList.data?.products?.items],
-  )
-  return items
-}
 
 /** Merge guest wishlist items to customer session upon login */
 export function useMergeGuestWishlistWithCustomer() {
@@ -26,71 +11,33 @@ export function useMergeGuestWishlistWithCustomer() {
   const isLoggedIn = customerToken?.token && customerToken?.valid
   const { cache } = useApolloClient()
 
-  const guestWishlistData = useQuery(GuestWishlistDocument, {
+  const guestSkus = useQuery(GuestWishlistDocument, { ssr: false }).data?.guestWishlist?.items
+
+  const guestProducts = useQuery(GetGuestWishlistProductsDocument, {
     ssr: false,
-  }).data?.guestWishlist
-
-  const guestDataSkus = useMemo(
-    () => guestWishlistData?.items.map((item) => item?.sku) || [],
-    [guestWishlistData?.items],
-  )
-
-  const validatedItems = useValidatedItems(guestDataSkus)
+    variables: { filters: { sku: { in: guestSkus?.map((item) => item?.sku) } } },
+    skip: guestSkus && guestSkus?.length === 0,
+  }).data?.products?.items
 
   const [addWishlistItem] = useMutation(AddProductToWishlistDocument)
 
-  const addWishlist = useCallback(
-    (
-      payload: {
-        sku: string
-        selected_options: (string | null)[] | null | undefined
-        quantity: number
-      }[],
-    ) =>
-      addWishlistItem({ variables: { input: payload } }).then(() =>
-        cache.evict({
-          id: cache.identify({ __typename: 'GuestWishlist' }),
-        }),
-      ),
-    [addWishlistItem, cache],
-  )
-
-  const purgeOutdatedItems = useCallback(() => {
-    /** Only outdated items were found, purge them */
-    cache.evict({
-      id: cache.identify({ __typename: 'GuestWishlist' }),
-    })
-  }, [cache])
-
   useEffect(() => {
-    if (!isLoggedIn) return
+    if (!isLoggedIn || !guestSkus || guestSkus.length === 0) return
 
-    if (!guestDataSkus.length) return
-
-    if (!validatedItems.length) {
-      purgeOutdatedItems()
+    if (guestProducts?.length === 0) {
+      cache.evict({ id: cache.identify({ __typename: 'GuestWishlist' }) })
       return
     }
 
-    const wishlist =
-      guestWishlistData?.items.filter((item) => validatedItems.includes(item.sku)) || []
+    const input = guestSkus
+      .filter((item) => guestProducts?.find((i) => i?.sku === item.sku))
+      .map(({ sku, selected_options, quantity }) => ({ sku, selected_options, quantity }))
 
-    if (!wishlist.length) return
-
-    const payload = wishlist.map((item) => ({
-      sku: item.sku,
-      selected_options: item.selected_options,
-      quantity: item.quantity,
-    }))
+    if (!input.length) return
 
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    addWishlist(payload)
-  }, [
-    isLoggedIn,
-    validatedItems,
-    guestDataSkus,
-    addWishlist,
-    guestWishlistData?.items,
-    purgeOutdatedItems,
-  ])
+    addWishlistItem({ variables: { input } }).then(() =>
+      cache.evict({ id: cache.identify({ __typename: 'GuestWishlist' }) }),
+    )
+  }, [addWishlistItem, cache, guestProducts, guestSkus, isLoggedIn])
 }
