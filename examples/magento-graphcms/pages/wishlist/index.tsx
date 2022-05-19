f069c4b196c21ec4d02f170360c40f3de1780013
Tim Hofman <tim@reachdigital.nl>
2022-05-19 11:02:15 +0200

    chore: display loading screen when wishlist data is being fetched

--- examples/magento-graphcms/pages/wishlist/index.tsx
+++ examples/magento-graphcms/pages/wishlist/index.tsx
@@ -30,10 +30,6 @@ type GetPageStaticProps = GetStaticProps<LayoutOverlayProps, Props>
 function WishlistPage(props: Props) {
   const wishlistItemsData = useWishlistItems()
 
-  if (wishlistItemsData.loading) {
-    return null
-  }
-
   return (
     <>
       <PageMeta title={i18n._(/* i18n */ `Wishlist`)} metaRobots={['noindex']} />
@@ -44,45 +40,56 @@ function WishlistPage(props: Props) {
           </LayoutTitle>
         </LayoutOverlayHeader>
 
-        <Container maxWidth='md'>
-          <AnimatePresence initial={false}>
-            {!wishlistItemsData.items || wishlistItemsData.items?.length === 0 ? (
-              <FullPageMessage
-                title={<Trans id='Your wishlist is empty' />}
-                icon={<IconSvg src={iconHeart} size='xxl' />}
-                button={
-                  <Link href='/' passHref>
-                    <Button variant='contained' color='primary' size='large'>
-                      <Trans id='Continue shopping' />
-                    </Button>
-                  </Link>
-                }
-              >
-                <Trans id='Discover our collection and add items to your wishlist!' />
-              </FullPageMessage>
-            ) : (
-              <>
-                <LayoutTitle icon={iconHeart}>
-                  <Trans id='Wishlist' />
-                </LayoutTitle>
-                <Container maxWidth='md'>
-                  <WishlistItems
-                    renderer={{
-                      BundleProduct: WishlistItemBase,
-                      ConfigurableProduct: WishlistItemBase,
-                      DownloadableProduct: WishlistItemBase,
-                      SimpleProduct: WishlistItem,
-                      VirtualProduct: WishlistItem,
-                      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                      // @ts-ignore GiftCardProduct is only available in Commerce
-                      GiftCardProduct: WishlistItemBase,
-                    }}
-                  />
-                </Container>
-              </>
-            )}
-          </AnimatePresence>
-        </Container>
+        {wishlistItemsData.loading ? (
+          <Container maxWidth='md'>
+            <FullPageMessage
+              title={<Trans id='Loading wishlist' />}
+              icon={<IconSvg src={iconHeart} size='xxl' />}
+            >
+              <Trans id='We are fetching your favorite products, one moment please!' />
+            </FullPageMessage>
+          </Container>
+        ) : (
+          <Container maxWidth='md'>
+            <AnimatePresence initial={false}>
+              {!wishlistItemsData.items || wishlistItemsData.items?.length === 0 ? (
+                <FullPageMessage
+                  title={<Trans id='Your wishlist is empty' />}
+                  icon={<IconSvg src={iconHeart} size='xxl' />}
+                  button={
+                    <Link href='/' passHref>
+                      <Button variant='contained' color='primary' size='large'>
+                        <Trans id='Continue shopping' />
+                      </Button>
+                    </Link>
+                  }
+                >
+                  <Trans id='Discover our collection and add items to your wishlist!' />
+                </FullPageMessage>
+              ) : (
+                <>
+                  <LayoutTitle icon={iconHeart}>
+                    <Trans id='Wishlist' />
+                  </LayoutTitle>
+                  <Container maxWidth='md'>
+                    <WishlistItems
+                      renderer={{
+                        BundleProduct: WishlistItemBase,
+                        ConfigurableProduct: WishlistItemBase,
+                        DownloadableProduct: WishlistItemBase,
+                        SimpleProduct: WishlistItem,
+                        VirtualProduct: WishlistItem,
+                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                        // @ts-ignore GiftCardProduct is only available in Commerce
+                        GiftCardProduct: WishlistItemBase,
+                      }}
+                    />
+                  </Container>
+                </>
+              )}
+            </AnimatePresence>
+          </Container>
+        )}
       </NoSsr>
     </>
   )
