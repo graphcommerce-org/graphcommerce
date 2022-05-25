e0ae537222b828becc506b9609cba13577e72797
Paul Hachmang <paul@reachdigital.nl>
2022-05-25 14:53:39 +0200

    core: remove default label

--- packages/next-ui/TextInputNumber/TextInputNumber.tsx
+++ packages/next-ui/TextInputNumber/TextInputNumber.tsx
@@ -88,7 +88,6 @@ export function TextInputNumber(props: TextInputNumberProps) {
 
   return (
     <TextField
-      label={' '}
       {...textFieldProps}
       type='number'
       inputRef={forkRef}
