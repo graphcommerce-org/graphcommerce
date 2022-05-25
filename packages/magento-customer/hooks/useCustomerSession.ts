238aa4d3478773b8cb0973f4112c9829e59e16d6
Paul Hachmang <paul@reachdigital.nl>
2022-05-25 17:51:27 +0200

    fix: When @injecting mandatory fields into the CustomerTokenFragment it would throw a typescript error.

--- packages/magento-customer/hooks/useCustomerSession.ts
+++ packages/magento-customer/hooks/useCustomerSession.ts
@@ -4,10 +4,8 @@ import { CustomerTokenDocument, CustomerTokenQuery } from './CustomerToken.gql'
 type TokenResponse = Omit<NonNullable<CustomerTokenQuery['customerToken']>, '__typename'>
 
 export type UseCustomerSessionReturn =
-  | TokenResponse & {
-      loggedIn: boolean
-      requireAuth: boolean
-    }
+  | { loggedIn: false; requireAuth: false }
+  | (TokenResponse & { loggedIn: boolean; requireAuth: boolean })
 
 export function useCustomerSession(): UseCustomerSessionReturn {
   const token = useQuery(CustomerTokenDocument, { ssr: false, fetchPolicy: 'cache-only' }).data
