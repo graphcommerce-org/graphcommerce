54dc3d9ab82f635e1da27c0886b535fa9da82ade
ErwinOtten <erwin@h-o.nl>
2022-06-20 17:05:45 +0200

    fix: visible flash on pageload

--- packages/next-ui/LayoutOverlay/components/Overlay.tsx
+++ packages/next-ui/LayoutOverlay/components/Overlay.tsx
@@ -1,7 +1,6 @@
 import { ScrollerProvider, ScrollSnapType } from '@graphcommerce/framer-scroller'
 import { Box } from '@mui/material'
-import { AnimatePresence } from 'framer-motion'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { SetOptional } from 'type-fest'
 import { LayoutOverlayBase, LayoutOverlayBaseProps } from './LayoutOverlayBase'
 
@@ -18,19 +17,29 @@ export function Overlay(props: OverlayProps) {
   const scrollSnapTypeMd: ScrollSnapType =
     variantMd === 'left' || variantMd === 'right' ? 'inline mandatory' : 'block proximity'
 
+  // todo: The overlay is always present in the DOM and the initial scroll position is set to 0.
+  //       This means in this case that the overlay is visisble. LayoutOverlayBase sets the scroll position to 320 with JS.
+  //       This would cause the overlay to be visisble for a moment before the scroll position is set.
+  //       The solution is to set the the first scroll-snap-align and scroll-snap-stop to the open position of the overlay.
+  //       However: We have control of the LayoutOverlayBase, we do not have control of all the child components so that solution will not work..
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => setLoaded(true), [])
+
   return (
     <Box
       className='Overlay'
-      // hidden={!active}
       sx={{
         position: 'fixed',
         top: 0,
         left: 0,
-        // left: active ? 0 : '-120vw',
+        transform: loaded ? undefined : 'translateX(-200vw)',
         pointerEvents: active ? undefined : 'none',
         right: 0,
         bottom: 0,
         zIndex: 'drawer',
+        '& .LayoutOverlayBase-overlayPane': {
+          boxShadow: active ? undefined : 0,
+        },
       }}
     >
       <ScrollerProvider scrollSnapTypeSm={scrollSnapTypeSm} scrollSnapTypeMd={scrollSnapTypeMd}>
