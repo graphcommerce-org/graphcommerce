1a454acdb7810dcb40d241f07ce24292bbcba5ac
ErwinOtten <erwin@h-o.nl>
2022-08-01 11:07:54 +0200

    Default to left full

--- examples/magento-graphcms/components/Layout/LayoutNavigation.tsx
+++ examples/magento-graphcms/components/Layout/LayoutNavigation.tsx
@@ -90,10 +90,10 @@ export function LayoutNavigation(props: LayoutNavigationProps) {
           <NavigationOverlay
             active={navigationActive}
             stretchColumns={false}
-            variantSm='bottom'
+            variantSm='left'
             sizeSm='full'
-            // justifySm='start'
-            itemWidthSm='100vw'
+            justifySm='start'
+            itemWidthSm='70vw'
             variantMd='left'
             sizeMd='full'
             justifyMd='start'
