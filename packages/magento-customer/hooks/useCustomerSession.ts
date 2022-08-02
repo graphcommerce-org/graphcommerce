3c809a3a438995503f6d2290d6c0bb90fbc489be
Paul Hachmang <paul@reachdigital.nl>
2022-08-02 12:52:21 +0200

    Make sure the useCustomerSession returns the full query so it can be awaited

--- packages/magento-customer/hooks/useCustomerSession.ts
+++ packages/magento-customer/hooks/useCustomerSession.ts
@@ -1,35 +1,39 @@
 import { useIsomorphicLayoutEffect } from '@graphcommerce/framer-utils'
-import { useQuery } from '@graphcommerce/graphql'
+import { QueryResult, useQuery } from '@graphcommerce/graphql'
 import { startTransition, useState } from 'react'
-import { CustomerTokenDocument, CustomerTokenQuery } from './CustomerToken.gql'
+import {
+  CustomerTokenDocument,
+  CustomerTokenQuery,
+  CustomerTokenQueryVariables,
+} from './CustomerToken.gql'
 
 export type UseCustomerSessionOptions = { hydration?: boolean }
 
 export type UseCustomerSessionReturn =
-  | Partial<
-      Omit<NonNullable<CustomerTokenQuery['customerToken']>, '__typename'> & {
-        called: boolean
-      }
-    > & { loggedIn: boolean; requireAuth: boolean }
+  | {
+      loggedIn: boolean
+      requireAuth: boolean
+      query: QueryResult<CustomerTokenQuery, CustomerTokenQueryVariables>
+    } & Partial<Omit<NonNullable<CustomerTokenQuery['customerToken']>, '__typename'>>
 
 export function useCustomerSession(
   options: UseCustomerSessionOptions = {},
 ): UseCustomerSessionReturn {
-  const { hydration = false } = options
+  const { hydration = true } = options
   const [hydrating, setHydrating] = useState(!hydration)
+
   useIsomorphicLayoutEffect(() => startTransition(() => setHydrating(false)), [])
+
   const skip = hydrating
 
-  const { called, data } = useQuery(CustomerTokenDocument, { skip })
+  const query = useQuery(CustomerTokenDocument, { skip })
 
-  const token = data?.customerToken
-
-  if (!token) return { called, loggedIn: false, requireAuth: true }
+  const token = query.data?.customerToken
 
   return {
     ...token,
-    called,
     loggedIn: Boolean(token?.token && token.valid),
     requireAuth: Boolean(!token || !token.valid),
+    query,
   }
 }
