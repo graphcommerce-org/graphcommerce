f4b015234bb56ecaccc66df5bd501e2c0efedc25
ErwinOtten <erwin@h-o.nl>
2022-08-12 15:49:32 +0200

    Fix quotes

--- packages/next-ui/Navigation/components/NavigationOverlay.tsx
+++ packages/next-ui/Navigation/components/NavigationOverlay.tsx
@@ -196,7 +196,7 @@ export function NavigationOverlay(props: NavigationOverlayProps) {
                 ? selected.length >= 1
                   ? `calc(${itemWidthMd} + 1px)`
                   : itemWidthMd
-                : `-webkit-fill-available`,
+                : '-webkit-fill-available',
             },
             [theme.breakpoints.down('md')]: {
               width:
