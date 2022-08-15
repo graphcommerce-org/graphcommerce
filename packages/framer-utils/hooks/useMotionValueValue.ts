5c5645e6eaf5314c063f05547707fcd4b34a8717
Paul Hachmang <paul@reachdigital.nl>
2022-08-15 12:46:24 +0200

    Reduce the required properties for useMotionValueValue

--- packages/framer-utils/hooks/useMotionValueValue.ts
+++ packages/framer-utils/hooks/useMotionValueValue.ts
@@ -1,14 +1,17 @@
 import { MotionValue } from 'framer-motion'
-import { useState } from 'react'
+import { startTransition, useState } from 'react'
 import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect'
 
 /** Get the MotionValue's value and return the value as a state update. */
-export function useMotionValueValue<T, R>(motionValue: MotionValue<T>, effect: (v: T) => R) {
+export function useMotionValueValue<T, R>(
+  motionValue: Pick<MotionValue<T>, 'onChange' | 'get'>,
+  effect: (v: T) => R,
+) {
   const [result, setResult] = useState<R>(effect(motionValue.get()))
 
   useIsomorphicLayoutEffect(() => {
-    const set = (v: T) => setResult(effect(v))
-    set(motionValue.get())
+    const set = (v: T) => startTransition(() => setResult(effect(v)))
+    setResult(effect(motionValue.get()))
     return motionValue.onChange(set)
     // we're not recalculating
     // eslint-disable-next-line react-hooks/exhaustive-deps
