30956922e3065bc4127b28dd6c53e5973c3156fb
ErwinOtten <erwin@h-o.nl>
2022-08-17 11:06:47 +0200

    fontSize

--- examples/magento-graphcms/components/theme.ts
+++ examples/magento-graphcms/components/theme.ts
@@ -134,7 +134,7 @@ const createThemeWithPalette = (palette: PaletteOptions) =>
       },
       fontWeightBold: 600,
       body1: {
-        ...fontSize(15, 18),
+        ...fontSize(15, 17),
         lineHeight: 1.7,
       },
       subtitle2: {
