452aa9dec15f9591845534d67f40d74ef6352deb
Mike Keehnen <mikekeehnen@me.com>
2022-09-09 09:26:40 +0200

    Changed package to apollo3-cache-persist and used memo for hook

--- packages/magento-customer/hooks/useCachePersistor.ts
+++ packages/magento-customer/hooks/useCachePersistor.ts
@@ -1,14 +1,17 @@
 import { useApolloClient } from '@graphcommerce/graphql'
-import { CachePersistor } from 'apollo-cache-persist'
-import { PersistedData, PersistentStorage } from 'apollo-cache-persist/types'
-import { useEffect, useRef } from 'react'
+import { CachePersistor, PersistentStorage } from 'apollo3-cache-persist'
+import { useMemo } from 'react'
 
 export function useCachePersistor() {
   const client = useApolloClient()
-  const persistor = new CachePersistor({
-    cache: client.cache,
-    storage: window.localStorage as PersistentStorage<PersistedData<string>>,
-  })
+  const persistor = useMemo(
+    () =>
+      new CachePersistor({
+        cache: client.cache,
+        storage: window.localStorage as PersistentStorage<string>,
+      }),
+    [client.cache],
+  )
 
   return {
     persistor,
