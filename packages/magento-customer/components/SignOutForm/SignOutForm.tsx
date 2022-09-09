5072fd2aae9cb06b6ce163655076c4651514321c
Mike Keehnen <mikekeehnen@me.com>
2022-09-09 11:07:26 +0200

    Added listener on init new CachePersistor

--- packages/magento-customer/components/SignOutForm/SignOutForm.tsx
+++ packages/magento-customer/components/SignOutForm/SignOutForm.tsx
@@ -2,7 +2,6 @@ import { useApolloClient } from '@graphcommerce/graphql'
 import { FormState, useFormGqlMutation } from '@graphcommerce/react-hook-form'
 import { useRouter } from 'next/router'
 import React from 'react'
-import { useCachePersistor } from '../../hooks/useCachePersistor'
 import { ApolloCustomerErrorAlert } from '../ApolloCustomerError/ApolloCustomerErrorAlert'
 import { SignOutFormDocument } from './SignOutForm.gql'
 
@@ -14,14 +13,12 @@ export function SignOutForm(props: SignOutFormProps) {
   const { button } = props
   const router = useRouter()
   const client = useApolloClient()
-  const { persistor } = useCachePersistor()
 
   const { handleSubmit, formState, error } = useFormGqlMutation(
     SignOutFormDocument,
     {
       onComplete: async () => {
         await client.clearStore()
-        await persistor.purge()
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
         router.push('/')
       },
