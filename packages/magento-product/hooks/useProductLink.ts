9635f4016bd6a9be78918a00d8a704fb469708b3
Paul Hachmang <paul@reachdigital.nl>
2022-09-19 20:13:32 +0200

    Allow using legacy product pages NEXT_PUBLIC_LEGACY_PRODUCT_PAGES

--- packages/magento-product/hooks/useProductLink.ts
+++ packages/magento-product/hooks/useProductLink.ts
@@ -3,6 +3,21 @@ import { ProductLinkFragment } from './ProductLink.gql'
 export type ProductLinkProps = Omit<ProductLinkFragment, 'uid'>
 
 export function productLink(link: ProductLinkProps) {
+  if (process.env.NEXT_PUBLIC_LEGACY_PRODUCT_PAGES) {
+    const { __typename, url_key } = link
+
+    const productRoute = __typename
+      .split(/(?=[A-Z])/)
+      .map((s) => s.toLowerCase())
+      .reverse()
+
+    if (__typename === 'SimpleProduct')
+      // For Simple and Virtual products we're not navigating to a type specific page
+      productRoute.splice(1, 1)
+
+    return `/${productRoute.join('/')}/${url_key}`
+  }
+
   return `/p/${link.url_key}`
 }
 
