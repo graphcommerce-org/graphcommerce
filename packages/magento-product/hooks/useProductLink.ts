cb53f1374a63b57397dd7f1e588a8ea652fc6648
Paul Hachmang <paul@reachdigital.nl>
2022-09-20 15:15:30 +0200

    Disable single product page by defau;t

--- packages/magento-product/hooks/useProductLink.ts
+++ packages/magento-product/hooks/useProductLink.ts
@@ -3,7 +3,7 @@ import { ProductLinkFragment } from './ProductLink.gql'
 export type ProductLinkProps = Omit<ProductLinkFragment, 'uid'>
 
 export function productLink(link: ProductLinkProps) {
-  if (process.env.NEXT_PUBLIC_LEGACY_PRODUCT_PAGES) {
+  if (process.env.NEXT_PUBLIC_SINGLE_PRODUCT_PAGE !== '1') {
     const { __typename, url_key } = link
 
     const productRoute = __typename
