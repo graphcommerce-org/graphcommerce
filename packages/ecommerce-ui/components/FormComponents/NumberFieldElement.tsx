e5048c5ec52b83dbe70a246ffdcea65b55a884c6
Paul Hachmang <paul@reachdigital.nl>
2022-09-26 11:10:03 +0200

    Flatten the NumberFieldElement to use the Controller directly

--- packages/ecommerce-ui/components/FormComponents/NumberFieldElement.tsx
+++ packages/ecommerce-ui/components/FormComponents/NumberFieldElement.tsx
@@ -5,32 +5,33 @@ import {
   IconSvg,
   responsiveVal,
 } from '@graphcommerce/next-ui'
-import { FieldValues } from '@graphcommerce/react-hook-form'
+import { Controller, ControllerProps, FieldValues } from '@graphcommerce/react-hook-form'
 import { i18n } from '@lingui/core'
 import {
   IconButton,
   IconButtonProps,
   SxProps,
-  useForkRef,
   Theme,
-  useEventCallback,
+  TextField,
+  TextFieldProps,
 } from '@mui/material'
-import { ChangeEvent, Ref, useRef } from 'react'
-import { TextFieldElement, TextFieldElementProps } from './TextFieldElement'
 
 export type NumberFieldElementProps<T extends FieldValues = FieldValues> = Omit<
-  TextFieldElementProps<T>,
-  'type'
+  TextFieldProps,
+  'type' | 'defaultValue'
 > & {
   DownProps?: IconButtonProps
   UpProps?: IconButtonProps
   sx?: SxProps<Theme>
-}
+} & Omit<ControllerProps<T>, 'render'>
 
 type OwnerState = { size?: 'small' | 'medium' }
-const name = 'TextInputNumber' as const
+const componentName = 'TextInputNumber' as const
 const parts = ['quantity', 'quantityInput', 'button'] as const
-const { withState } = extendableComponent<OwnerState, typeof name, typeof parts>(name, parts)
+const { withState } = extendableComponent<OwnerState, typeof componentName, typeof parts>(
+  componentName,
+  parts,
+)
 
 export function NumberFieldElement<T extends FieldValues = FieldValues>(
   props: NumberFieldElementProps<T>,
@@ -39,81 +40,100 @@ export function NumberFieldElement<T extends FieldValues = FieldValues>(
     DownProps = {},
     UpProps = {},
     inputProps = {},
-    inputRef,
     sx = [],
     size = 'medium',
+    control,
+    name,
+    rules = {},
+    required,
+    defaultValue,
     ...textFieldProps
   } = props
 
   const classes = withState({ size })
-  const ref = useRef<HTMLInputElement>(null)
-  const forkRef = useForkRef(ref, inputRef as Ref<HTMLInputElement>)
+
+  if (required && !rules.required) {
+    rules.required = i18n._(/* i18n */ 'This field is required')
+  }
 
   return (
-    <TextFieldElement
-      {...textFieldProps}
-      size={size}
-      type='number'
-      inputRef={forkRef}
-      className={`${textFieldProps.className ?? ''} ${classes.quantity}`}
-      sx={[{ width: responsiveVal(80, 120) }, ...(Array.isArray(sx) ? sx : [sx])]}
-      autoComplete='off'
-      InputProps={{
-        ...textFieldProps.InputProps,
-        startAdornment: (
-          <IconButton
-            aria-label={i18n._(/* i18n */ 'Decrease')}
-            size='medium'
-            edge='start'
-            onClick={useEventCallback(() => {
-              if ((ref.current?.value ?? Infinity) >= inputProps.max) return
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      defaultValue={defaultValue}
+      render={({ field: { value, onChange, onBlur }, fieldState: { invalid, error } }) => {
+        const valueAsNumber = value ? parseFloat(value) : 0
 
-              ref.current?.stepUp()
-              ref.current?.dispatchEvent(new Event('change', { bubbles: true }))
-            })}
-            tabIndex={-1}
-            color='inherit'
-            {...DownProps}
-            className={`${classes.button} ${DownProps.className ?? ''}`}
-          >
-            {DownProps.children ?? <IconSvg src={iconMin} size='small' />}
-          </IconButton>
-        ),
-        endAdornment: (
-          <IconButton
-            aria-label={i18n._(/* i18n */ 'Increase')}
-            size='medium'
-            edge='end'
-            onClick={useEventCallback(() => {
-              if ((ref.current?.value ?? 0) <= inputProps.min) return
-
-              ref.current?.stepDown()
-              ref.current?.dispatchEvent(new Event('change', { bubbles: true }))
-            })}
-            tabIndex={-1}
-            color='inherit'
-            {...UpProps}
-            className={`${classes.button} ${UpProps.className ?? ''}`}
-          >
-            {UpProps.children ?? <IconSvg src={iconPlus} size='small' />}
-          </IconButton>
-        ),
-      }}
-      onChange={(e: ChangeEvent<HTMLInputElement>) => {
-        if (textFieldProps.onChange) textFieldProps.onChange(e)
-      }}
-      inputProps={{
-        ...inputProps,
-        'aria-label': i18n._(/* i18n */ 'Number'),
-        sx: [
-          {
-            textAlign: 'center',
-            '&::-webkit-inner-spin-button,&::-webkit-outer-spin-button': {
-              appearance: 'none',
-            },
-          },
-        ],
-        className: `${inputProps?.className ?? ''} ${classes.quantityInput}`,
+        return (
+          <TextField
+            {...textFieldProps}
+            name={name}
+            value={value ?? ''}
+            onChange={(ev) => {
+              const newValue = (ev.target as HTMLInputElement).valueAsNumber
+              onChange(Number.isNaN(newValue) ? '' : newValue)
+              textFieldProps.onChange?.(ev)
+            }}
+            onBlur={onBlur}
+            required={required}
+            error={invalid}
+            helperText={error ? error.message : textFieldProps.helperText}
+            size={size}
+            type='number'
+            className={`${textFieldProps.className ?? ''} ${classes.quantity}`}
+            sx={[{ width: responsiveVal(80, 120) }, ...(Array.isArray(sx) ? sx : [sx])]}
+            autoComplete='off'
+            InputProps={{
+              ...textFieldProps.InputProps,
+              startAdornment: (
+                <IconButton
+                  aria-label={i18n._(/* i18n */ 'Decrease')}
+                  size='medium'
+                  edge='start'
+                  onClick={() => {
+                    if ((valueAsNumber || Infinity) <= inputProps.min) return
+                    onChange(value - 1)
+                  }}
+                  tabIndex={-1}
+                  color='inherit'
+                  {...DownProps}
+                  className={`${classes.button} ${DownProps.className ?? ''}`}
+                >
+                  {DownProps.children ?? <IconSvg src={iconMin} size='small' />}
+                </IconButton>
+              ),
+              endAdornment: (
+                <IconButton
+                  aria-label={i18n._(/* i18n */ 'Increase')}
+                  size='medium'
+                  edge='end'
+                  onClick={() => {
+                    if (valueAsNumber >= (inputProps.max ?? Infinity)) return
+                    onChange(valueAsNumber + 1)
+                  }}
+                  tabIndex={-1}
+                  color='inherit'
+                  {...UpProps}
+                  className={`${classes.button} ${UpProps.className ?? ''}`}
+                >
+                  {UpProps.children ?? <IconSvg src={iconPlus} size='small' />}
+                </IconButton>
+              ),
+            }}
+            inputProps={{
+              ...inputProps,
+              'aria-label': i18n._(/* i18n */ 'Number'),
+              className: `${inputProps?.className ?? ''} ${classes.quantityInput}`,
+              sx: {
+                textAlign: 'center',
+                '&::-webkit-inner-spin-button,&::-webkit-outer-spin-button': {
+                  appearance: 'none',
+                },
+              },
+            }}
+          />
+        )
       }}
     />
   )
--- packages/magento-product/components/AddProductsToCart/AddProductsToCartQuantity.tsx
+++ packages/magento-product/components/AddProductsToCart/AddProductsToCartQuantity.tsx
@@ -3,12 +3,12 @@ import { useFormAddProductsToCart } from './AddProductsToCartForm'
 
 type AddToCartQuantityProps = Omit<
   NumberFieldElementProps,
-  'error' | 'required' | 'inputProps' | 'inputRef' | 'helperText' | 'name'
+  'error' | 'required' | 'inputProps' | 'helperText' | 'name' | 'control' | 'name'
 > & { index?: number }
 
 export function AddProductsToCartQuantity(props: AddToCartQuantityProps) {
   const { index = 0 } = props
-  const { formState, control } = useFormAddProductsToCart()
+  const { control } = useFormAddProductsToCart()
 
   return (
     <NumberFieldElement
@@ -17,10 +17,9 @@ export function AddProductsToCartQuantity(props: AddToCartQuantityProps) {
       {...props}
       required
       inputProps={{ min: 1 }}
-      defaultValue='1'
+      defaultValue={1}
       control={control}
       name={`cartItems.${index}.quantity`}
-      helperText={formState.isSubmitted && formState.errors.cartItems?.[index].quantity?.message}
     />
   )
 }
