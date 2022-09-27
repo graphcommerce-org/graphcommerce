94f6c008dd481ba4fa5125b3bc302fa67bd04b69
Mike Keehnen <mikekeehnen@me.com>
2022-09-27 12:32:29 +0200

    Changes first filtertype to actioncarlist
    # Conflicts:
    #	packages/next-ui/ChipMenu/ChipMenu.tsx

--- packages/next-ui/ChipMenu/ChipMenu.tsx
+++ packages/next-ui/ChipMenu/ChipMenu.tsx
@@ -1,9 +1,8 @@
-import { Chip, ChipProps, Menu, MenuProps, menuClasses, SxProps, Theme } from '@mui/material'
+import { Chip, ChipProps, SxProps, Theme } from '@mui/material'
 import React, { useState } from 'react'
 import { IconSvg } from '../IconSvg'
-import { SectionHeader } from '../SectionHeader/SectionHeader'
-import { responsiveVal } from '../Styles/responsiveVal'
 import { iconChevronDown, iconChevronUp, iconCancelAlt } from '../icons'
+import { ResponsiveMenu } from './ResponsiveMenu'
 
 export type ChipMenuProps = Omit<ChipProps<'button'>, 'children' | 'component'> & {
   selectedLabel?: React.ReactNode
@@ -11,22 +10,12 @@ export type ChipMenuProps = Omit<ChipProps<'button'>, 'children' | 'component'> 
   onClose?: () => void
   labelRight?: React.ReactNode
   sx?: SxProps<Theme>
-  menuProps?: Partial<MenuProps>
   children?: React.ReactNode
 }
 
 export function ChipMenu(props: ChipMenuProps) {
-  const {
-    children,
-    selected,
-    onDelete,
-    label,
-    labelRight,
-    onClose,
-    selectedLabel,
-    menuProps,
-    ...chipProps
-  } = props
+  const { children, selected, onDelete, label, labelRight, onClose, selectedLabel, ...chipProps } =
+    props
 
   const [openEl, setOpenEl] = useState<null | HTMLElement>(null)
 
@@ -37,62 +26,32 @@ export function ChipMenu(props: ChipMenuProps) {
   const selectedAndMenuHidden = selected && !openEl && !!selectedLabel
 
   return (
-    <>
-      <Chip
-        component='button'
-        size='responsive'
-        color={selectedAndMenuHidden ? 'primary' : 'default'}
-        clickable
-        onDelete={
-          onDelete ||
-          ((event: React.MouseEvent<HTMLButtonElement>) =>
-            setOpenEl(event.currentTarget.parentElement))
-        }
-        onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
-          setOpenEl(event.currentTarget)
-        }}
-        deleteIcon={deleteIcon}
-        {...chipProps}
-        label={selectedLabel ?? label}
-      />
-
-      <Menu
-        anchorEl={openEl}
-        open={!!openEl}
-        onClose={() => {
-          if (onClose) onClose()
-          setOpenEl(null)
-        }}
-        anchorPosition={{ top: 6, left: 0 }}
-        anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-        {...menuProps}
-        sx={[
-          (theme) => ({
-            marginTop: theme.spacings.xxs,
-            [`& .${menuClasses.list}`]: {
-              padding: 0,
-              '&:focus': { outline: 'none' },
-            },
-            [`& .${menuClasses.paper}`]: {
-              minWidth: responsiveVal(200, 560),
-              maxWidth: 560,
-              padding: `0 ${theme.spacings.xs} ${theme.spacings.xs}`,
-              margin: 0,
-              [theme.breakpoints.down('sm')]: {
-                minWidth: 0,
-                width: '100%',
-                maxWidth: `calc(100% - (${theme.page.horizontal} * 2))`,
-                margin: '0 auto',
-              },
-            },
-          }),
-          // eslint-disable-next-line no-nested-ternary
-          ...(menuProps?.sx ? (Array.isArray(menuProps.sx) ? menuProps.sx : [menuProps.sx]) : []),
-        ]}
-      >
-        <SectionHeader labelLeft={label ?? ''} labelRight={labelRight ?? ''} usePadding />
-        {children}
-      </Menu>
-    </>
+    <ResponsiveMenu
+      {...props}
+      openEl={openEl}
+      setOpenEl={setOpenEl}
+      onDelete={onDelete}
+      chip={
+        <Chip
+          component='button'
+          size='responsive'
+          color={selectedAndMenuHidden ? 'primary' : 'default'}
+          clickable
+          onDelete={
+            onDelete ||
+            ((event: React.MouseEvent<HTMLButtonElement>) =>
+              setOpenEl(event.currentTarget.parentElement))
+          }
+          onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+            setOpenEl(event.currentTarget)
+          }}
+          deleteIcon={deleteIcon}
+          {...chipProps}
+          label={selectedLabel ?? label}
+        />
+      }
+    >
+      {children}
+    </ResponsiveMenu>
   )
 }
