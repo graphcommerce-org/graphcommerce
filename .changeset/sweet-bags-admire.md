0bd9ea58230dde79c5fe2cdb07e9860151460270
Paul Hachmang <paul@reachdigital.nl>
2022-10-06 12:44:32 +0200

    Added a new Fab component which adds a loading state

---
+++ .changeset/sweet-bags-admire.md
@@ -0,0 +1,5 @@
+---
+'@graphcommerce/next-ui': minor
+---
+
+Added a new Fab component which adds a loading state
---
+++ packages/next-ui/Fab/Fab.tsx
@@ -0,0 +1,38 @@
+import { Box, CircularProgress, Fab as FabBase, FabProps as FabPropsBase } from '@mui/material'
+import { IconSvg, IconSvgProps } from '../IconSvg'
+import { useFabSize } from '../Theme/MuiFab'
+
+export type FabProps = Omit<FabPropsBase<'button'>, 'variant' | 'children'> & {
+  loading?: boolean
+  icon: IconSvgProps['src']
+}
+
+/** Adds loading functionality to the Fab component. */
+export function Fab(props: FabProps) {
+  const { size = 'responsive', disabled, loading, sx = [], icon, ...fabProps } = props
+
+  const fabSize = useFabSize(size)
+
+  return (
+    <FabBase
+      type='submit'
+      color='primary'
+      size={size}
+      {...fabProps}
+      disabled={disabled}
+      sx={[{ display: 'grid' }, ...(Array.isArray(sx) ? sx : [sx])]}
+    >
+      <Box sx={{ display: 'flex', placeContent: 'center', gridArea: '1/1' }}>
+        <IconSvg src={icon} size='medium' />
+      </Box>
+      {loading && (
+        <CircularProgress
+          sx={{ display: 'flex', placeContent: 'center', gridArea: '1/1' }}
+          thickness={1.5}
+          size={fabSize}
+          color='inherit'
+        />
+      )}
+    </FabBase>
+  )
+}
