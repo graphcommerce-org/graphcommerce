2ad2ea2056c2c147626ccf1329fc15ca4bbb02c0
Paul Hachmang <paul@reachdigital.nl>
2022-10-07 16:27:49 +0200

    Move primary color from example to AddProductsToCartQuantity

--- packages/magento-product/components/AddProductsToCart/AddProductsToCartQuantity.tsx
+++ packages/magento-product/components/AddProductsToCart/AddProductsToCartQuantity.tsx
@@ -14,6 +14,7 @@ export function AddProductsToCartQuantity(props: AddToCartQuantityProps) {
     <NumberFieldElement
       variant='outlined'
       size='small'
+      color='primary'
       {...props}
       required
       inputProps={{ min: 1 }}
