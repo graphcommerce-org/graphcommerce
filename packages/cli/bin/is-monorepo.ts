da20e4e72ca3f29216592e0ecfb59c0e44bcbe20
Paul Hachmang <paul@reachdigital.nl>
2022-10-11 13:14:02 +0200

    Created an is-monorepo command to exectute scripts based if they are in the monorepo or in a project

---
+++ packages/cli/bin/is-monorepo.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import { ChildProcess, spawn } from 'node:child_process'
+import { isMonorepo } from '@graphcommerce/next-config'
+import { detect } from 'detect-package-manager'
+
+/**
+ * Executes a command dependening if we're running in a monorepo or not Usage:
+ *
+ *     is-monorepo '[pkgrun] run my-script' '[pkgrun] run my-other-script'
+ *
+ * We're using the `[pkgrun]` placeholder to replace it with the package manager we're using. For
+ * example, if we're using `yarn` it will replace `[pkgrun]` with `yarn`. If we're using `npm` it
+ * will replace `[pkgrun]` with `npm run`.
+ */
+async function main() {
+  const isMono = isMonorepo()
+  const command = isMono ? process.argv.slice(2)[0] : process.argv.slice(2)[1]
+  const packageManager = await detect()
+
+  const commandArray = command
+    .split(' ')
+    .map((arg) => arg.replace('[pkgrun]', `${packageManager} run`))
+
+  if (isMono) commandArray.unshift('cd', '../..', '&&')
+
+  const [cmd, ...args] = commandArray
+  const childProcess: ChildProcess = spawn(cmd, args, { shell: true, stdio: 'inherit' })
+
+  childProcess.on('exit', (code) => {
+    process.exit(code ?? 0)
+  })
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
