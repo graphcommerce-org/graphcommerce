dcc0630bc1b7b3e9c40f7666c3d3ab9759297321
Paul Hachmang <paul@reachdigital.nl>
2022-12-02 10:45:25 +0100

    Move to @ardatan/relay-compiler

---
+++ packagesDev/graphql-codegen-relay-optimizer-plugin/src/types.d.ts
@@ -0,0 +1,37 @@
+declare module '@ardatan/relay-compiler/lib/core/Schema' {
+  export function create(schema: string): import('relay-compiler').Schema
+}
+
+declare module '@ardatan/relay-compiler/lib/core/IRPrinter' {
+  export function print(schema: import('relay-compiler').Schema, document: any): string
+}
+
+declare module '@ardatan/relay-compiler/lib/core/CompilerContext' {
+  let CompilerContext: typeof import('relay-compiler').CompilerContext
+  export = CompilerContext
+}
+
+declare module '@ardatan/relay-compiler/lib/transforms/SkipRedundantNodesTransform' {
+  let transform: typeof import('relay-compiler/lib/transforms/SkipRedundantNodesTransform.js')
+  export = transform
+}
+
+declare module '@ardatan/relay-compiler/lib/transforms/InlineFragmentsTransform' {
+  let transform: typeof import('relay-compiler/lib/transforms/InlineFragmentsTransform.js')
+  export = transform
+}
+
+declare module '@ardatan/relay-compiler/lib/transforms/ApplyFragmentArgumentTransform' {
+  let transform: typeof import('relay-compiler/lib/transforms/ApplyFragmentArgumentTransform.js')
+  export = transform
+}
+
+declare module '@ardatan/relay-compiler/lib/transforms/FlattenTransform' {
+  let transform: typeof import('relay-compiler/lib/transforms/FlattenTransform.js')
+  export = transform
+}
+
+declare module '@ardatan/relay-compiler/lib/core/RelayParser' {
+  let RelayParser: typeof import('relay-compiler/lib/core/RelayParser.js')
+  export = RelayParser
+}
