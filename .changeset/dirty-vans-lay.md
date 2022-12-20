1540c61953532871aa18616a2752399ff37e852d
Paul Hachmang <paul@reachdigital.nl>
2022-12-20 10:31:57 +0100

    Revert "yarn can not completele successfully when runnin in a monorepo setup if codegen hasn’t ran"
    This reverts commit 0aab0bcc2048793f43a76bf981ca18d9f3ccaf16.

--- .changeset/dirty-vans-lay.md
+++
@@ -1,5 +0,0 @@
----
-'@graphcommerce/next-config': patch
----
-
-yarn can not completele successfully when runnin in a monorepo setup if codegen hasn’t ran
--- packagesDev/next-config/src/utils/isMonorepo.ts
+++ packagesDev/next-config/src/utils/isMonorepo.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 
 export function isMonorepo() {
   const root = process.cwd()
-  const meshDir = path.dirname(require.resolve('@graphcommerce/next-config'))
+  const meshDir = path.dirname(require.resolve('@graphcommerce/graphql-mesh'))
   const relativePath = path.join(path.relative(meshDir, root), '/')
   return relativePath.startsWith(`..${path.sep}..${path.sep}examples`)
 }
