21422e2fcb92cf56f96703db53dcfe82147e08a0
Laurens Franken <laurens@reachdigital.nl>
2023-01-30 14:15:59 +0100

    PayPal didn’t show the ‘processing payment’ message giving the impression it wasn’t doing anything

--- examples/magento-graphcms/pages/checkout/payment.tsx
+++ examples/magento-graphcms/pages/checkout/payment.tsx
@@ -33,6 +33,7 @@ import {
 import { i18n } from '@lingui/core'
 import { Trans } from '@lingui/react'
 import { CircularProgress, Container, Dialog, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
 import { LayoutMinimal, LayoutMinimalProps } from '../../components'
 import { LayoutDocument } from '../../components/Layout/Layout.gql'
 import { DefaultPageDocument } from '../../graphql/DefaultPage.gql'
@@ -42,7 +43,10 @@ type GetPageStaticProps = GetStaticProps<LayoutMinimalProps>
 
 function PaymentPage() {
   const billingPage = useCartQuery(BillingPageDocument, { fetchPolicy: 'cache-and-network' })
-  const [{ locked }] = useCartLock()
+
+  const router = useRouter()
+  const [cartLock] = useCartLock()
+  const locked = cartLock.locked || !router.isReady || router.query.PayerID
 
   const cartExists =
     typeof billingPage.data?.cart !== 'undefined' && (billingPage.data.cart?.items?.length ?? 0) > 0
