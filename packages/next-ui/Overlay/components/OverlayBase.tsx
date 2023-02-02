graphcommerce-org/graphcommerce

commit 29e15cf63251cf98cf42325322fcf09fb7a6c0b7

Fix scroll issue with bottom overlay on Android
This prevent scrolling Android browser UI in and out of view causing the browser to resize, which in turn causes the bottom overlay to jump around

packages/next-ui/Overlay/components/OverlayBase.tsx
@@ -287,7 +287,7 @@ export function OverlayBase(incomingProps: LayoutOverlayBaseProps) {
                 borderTopLeftRadius: theme.shape.borderRadius * 3,
                 borderTopRightRadius: theme.shape.borderRadius * 3,
                 gridTemplate: `"beforeOverlay" "overlay"`,
-                height: dvh(100),
+                height: `calc(${dvh(100)} - 1px)`,
               },
             },
             [theme.breakpoints.up('md')]: {

commit 7a5c0230f40249a1948d8d264dffa6fcf6635949

Hide bottom overlay being 1px too short (on iOS)
caused in 29e15cf63251cf98cf42325322fcf09fb7a6c0b7

packages/next-ui/Overlay/components/OverlayBase.tsx
@@ -288,6 +288,17 @@ export function OverlayBase(incomingProps: LayoutOverlayBaseProps) {
                 borderTopRightRadius: theme.shape.borderRadius * 3,
                 gridTemplate: `"beforeOverlay" "overlay"`,
                 height: `calc(${dvh(100)} - 1px)`,
+
+                '&::after': {
+                  content: `""`,
+                  display: 'block',
+                  position: 'absolute',
+                  width: '100%',
+                  height: '1px',
+                  top: 'calc(100% - 1px)',
+                  left: '0',
+                  background: theme.palette.background.paper,
+                },
               },
             },
             [theme.breakpoints.up('md')]: {