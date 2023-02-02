984de29a1114a898db67323876e16fc3756f170f
ErwinOtten <erwin@h-o.nl>
2023-02-02 14:13:02 +0100

    Add newsletter subscribe form

--- examples/magento-graphcms/pages/newsletter/index.tsx
+++ examples/magento-graphcms/pages/newsletter/index.tsx
@@ -2,7 +2,6 @@ import { PageOptions } from '@graphcommerce/framer-next-pages'
 import { StoreConfigDocument } from '@graphcommerce/magento-store'
 import { PageMeta, GetStaticProps, LayoutOverlayHeader, LayoutTitle } from '@graphcommerce/next-ui'
 import { Container } from '@mui/material'
-import { GetStaticPaths } from 'next'
 import {
   LayoutOverlay,
   LayoutOverlayProps,
@@ -10,17 +9,17 @@ import {
   RowRenderer,
 } from '../../components'
 import { LayoutDocument } from '../../components/Layout/Layout.gql'
+import { GuestNewsletter } from '../../components/Newsletter/GuestNewsletter'
 import { DefaultPageDocument, DefaultPageQuery } from '../../graphql/DefaultPage.gql'
-import { PagesStaticPathsDocument } from '../../graphql/PagesStaticPaths.gql'
 import { graphqlSsrClient, graphqlSharedClient } from '../../lib/graphql/graphqlSsrClient'
 
 type Props = DefaultPageQuery
 type RouteProps = { url: string[] }
-type GetPageStaticPaths = GetStaticPaths<RouteProps>
 type GetPageStaticProps = GetStaticProps<LayoutNavigationProps, Props, RouteProps>
 
-function ServicePage({ pages }: Props) {
-  const title = pages?.[0].title ?? ''
+function NewsletterSubscribe({ pages }: Props) {
+  const page = pages?.[0]
+  const title = page.title ?? ''
 
   return (
     <>
@@ -29,17 +28,25 @@ function ServicePage({ pages }: Props) {
         metaDescription={title}
         canonical={pages?.[0]?.url ? `/${pages[0].url}` : undefined}
       />
-      <LayoutOverlayHeader>
+      <LayoutOverlayHeader
+        switchPoint={0}
+        noAlign
+        sx={(theme) => ({
+          '&.noAlign': { marginBottom: theme.spacings.sm },
+          '& + .MuiContainer-root': {
+            marginBottom: theme.spacings.sm,
+          },
+        })}
+      >
         <LayoutTitle component='span' size='small'>
           {title}
         </LayoutTitle>
       </LayoutOverlayHeader>
 
-      <Container maxWidth='md'>
-        <LayoutTitle>{title}</LayoutTitle>
-      </Container>
-      <Container maxWidth='md'>
-        <RowRenderer {...pages[0]} />
+      <RowRenderer {...pages[0]} />
+
+      <Container maxWidth={false}>
+        <GuestNewsletter />
       </Container>
     </>
   )
@@ -48,34 +55,21 @@ function ServicePage({ pages }: Props) {
 const pageOptions: PageOptions<LayoutOverlayProps> = {
   overlayGroup: 'left',
   Layout: LayoutOverlay,
-  layoutProps: { variantMd: 'left' },
+  layoutProps: {
+    variantMd: 'right',
+    sizeMd: 'floating',
+    justifyMd: 'end',
+    widthMd: '500px',
+
+    sizeSm: 'floating',
+  },
 }
-ServicePage.pageOptions = pageOptions
+NewsletterSubscribe.pageOptions = pageOptions
 
-export default ServicePage
+export default NewsletterSubscribe
 
-// eslint-disable-next-line @typescript-eslint/require-await
-export const getStaticPaths: GetPageStaticPaths = async ({ locales = [] }) => {
-  if (process.env.NODE_ENV === 'development') return { paths: [], fallback: 'blocking' }
-
-  const path = async (locale: string) => {
-    const client = graphqlSharedClient(locale)
-    const { data } = await client.query({
-      query: PagesStaticPathsDocument,
-      variables: {
-        first: process.env.VERCEL_ENV !== 'production' ? 1 : 1000,
-        urlStartsWith: 'service',
-      },
-    })
-    return data.pages.map((page) => ({ params: { url: page.url.split('/').slice(1) }, locale }))
-  }
-  const paths = (await Promise.all(locales.map(path))).flat(1)
-
-  return { paths, fallback: 'blocking' }
-}
-
-export const getStaticProps: GetPageStaticProps = async ({ locale, params }) => {
-  const url = params?.url ? `service/${params?.url.join('/')}` : `service`
+export const getStaticProps: GetPageStaticProps = async ({ locale }) => {
+  const url = `newsletter`
   const client = graphqlSharedClient(locale)
   const staticClient = graphqlSsrClient(locale)
   const conf = client.query({ query: StoreConfigDocument })
@@ -84,13 +78,10 @@ export const getStaticProps: GetPageStaticProps = async ({ locale, params }) => 
 
   if (!(await page).data.pages?.[0]) return { notFound: true }
 
-  const isRoot = url === 'service'
-
   return {
     props: {
       ...(await page).data,
       ...(await layout).data,
-      up: isRoot ? null : { href: '/service', title: 'Customer Service' },
       apolloState: await conf.then(() => client.cache.extract()),
     },
     revalidate: 60 * 20,
