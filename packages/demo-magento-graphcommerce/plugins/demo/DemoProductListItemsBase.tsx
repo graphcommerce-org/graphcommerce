e6bf824918227452239c597ca36f3167cb95836f
ErwinOtten <erwin@h-o.nl>
2023-02-03 14:54:09 +0100

    Only show sxLargeItems on category pages

--- packages/demo-magento-graphcommerce/plugins/demo/DemoProductListItemsBase.tsx
+++ packages/demo-magento-graphcommerce/plugins/demo/DemoProductListItemsBase.tsx
@@ -8,16 +8,18 @@ export const ifEnv = 'DEMO_MAGENTO_GRAPHCOMMERCE'
 
 export const sxLargeItem = (theme: Theme) => ({
   [theme.breakpoints.up('xl')]: {
-    '& > :nth-of-type(7n + 3)': {
-      gridColumn: 'span 2',
-      gridRow: 'span 2',
-      display: 'grid',
-      gridAutoFlow: 'row',
-      gridTemplateColumns: '100%',
-      gridTemplateRows: '1fr auto',
-      '& > div:first-of-type': {
-        position: 'relative',
-        height: '100%',
+    ':not(.ContainerWithHeader-root &)': {
+      '& > :nth-of-type(7n + 3)': {
+        gridColumn: 'span 2',
+        gridRow: 'span 2',
+        display: 'grid',
+        gridAutoFlow: 'row',
+        gridTemplateColumns: '100%',
+        gridTemplateRows: '1fr auto',
+        '& > div:first-of-type': {
+          position: 'relative',
+          height: '100%',
+        },
       },
     },
   },
