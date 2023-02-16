d80f85ed23b1439bf7ebfd9f297b3b76d9dedc9f
ErwinOtten <erwin@h-o.nl>
2023-02-16 12:12:36 +0100

    GalleryNavigation optional

--- packages/magento-product/components/ProductPageGallery/ProductPageGallery.tsx
+++ packages/magento-product/components/ProductPageGallery/ProductPageGallery.tsx
@@ -39,7 +39,6 @@ export function ProductPageGallery(props: ProductPageGalleryProps) {
       sidebar={children}
       aspectRatio={[width, height]}
       images={images}
-      galleryNavigation={<ScrollerDots />}
     />
   )
 }
