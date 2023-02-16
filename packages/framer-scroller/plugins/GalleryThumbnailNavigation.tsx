c5292a45daeee63d8a74df527849f2826856bd9d
ErwinOtten <erwin@h-o.nl>
2023-02-16 12:04:14 +0100

    relocate plugin

--- packages/framer-scroller/plugins/GalleryThumbnailNavigation.tsx
+++ packages/framer-scroller/plugins/GalleryThumbnailNavigation.tsx
@@ -1,15 +1,15 @@
-import { ScrollerThumbnails } from '@graphcommerce/framer-scroller'
-import type { SidebarGalleryProps } from '@graphcommerce/next-ui/FramerScroller/SidebarGallery'
 import type { PluginProps } from '@graphcommerce/next-config'
+import type { SidebarGalleryProps } from '@graphcommerce/next-ui/FramerScroller/SidebarGallery'
 import { useCallback } from 'react'
+import { ScrollerThumbnails } from '../components/ScrollerThumbnails'
 
 export const component = 'SidebarGallery'
 export const exported = '@graphcommerce/next-ui'
-export const ifEnv = 'DEMO_MAGENTO_GRAPHCOMMERCE'
+export const ifEnv = 'GALLERY_THUMBNAILS'
 
-function DemoSidebarGallery(props: PluginProps<SidebarGalleryProps>) {
+function GalleryThumbnailNavigation(props: PluginProps<SidebarGalleryProps>) {
   const { Prev, ...rest } = props
   const navigation = useCallback(() => <ScrollerThumbnails images={rest.images} />, [rest.images])
   return <Prev {...rest} GalleryNavigation={navigation} />
 }
-export const Plugin = DemoSidebarGallery
+export const Plugin = GalleryThumbnailNavigation
