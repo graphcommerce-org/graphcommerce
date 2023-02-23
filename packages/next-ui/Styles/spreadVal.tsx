8178775f6e5b6c0eb2034c9953e299cbb19c9f48
Paul Hachmang <paul@reachdigital.nl>
2023-02-23 16:55:11 +0100

    Cleanup

--- packages/next-ui/Styles/spreadVal.tsx
+++ packages/next-ui/Styles/spreadVal.tsx
@@ -5,19 +5,13 @@ type Interpolation = (from: number, to: number, t: number) => number
 export const linear: Interpolation = (from, to, t) => (1 - t) * from + t * to
 export const easeIn: Interpolation = (from, to, t) => linear(from, to, t * t)
 
-export type SpreadValReturn = [xs: number, sm: number, md?: number, lg?: number, xl?: number]
-
-type Until = 'sm' | 'md' | 'lg' | 'xl'
-
-export type SpreadVal = (
+const spread = (
   bpValues: Breakpoints['values'],
   interpolation: Interpolation,
   from: number,
   to: number,
-  until: Until,
-) => SpreadValReturn
-
-const spread: SpreadVal = (bpValues, interpolation, from, to, until) => {
+  until: 'sm' | 'md' | 'lg' | 'xl',
+) => {
   const xs = 320
   const { sm, md, lg, xl } = bpValues
 
@@ -29,7 +23,7 @@ const spread: SpreadVal = (bpValues, interpolation, from, to, until) => {
     const val = interpolation(from, to, (bp - xs) / (bps[untilIdx] - xs))
     return Math.round(val * 100) / 100
   })
-  return values as SpreadValReturn
+  return values as [xs: number, sm: number, md?: number, lg?: number, xl?: number]
 }
 
 export type FromTo = [number, number]
@@ -42,39 +36,23 @@ export type ResponsiveTemplate = (
   ...expressions: Expression[]
 ) => ResponsiveValue
 
+function isFromToValue(val: unknown): val is FromTo {
+  return Array.isArray(val) && val.length === 2
+}
+
 export function createResponsiveTemplate(bpValues: Breakpoints['values']): ResponsiveTemplate {
   return (template: TemplateStringsArray, ...expressions: Expression[]): ResponsiveValue => {
     const breakpoints = Object.keys(bpValues)
 
-    const expressionsExpanded = expressions.map((value: Expression) => {
-      if (
-        typeof value === 'string' ||
-        typeof value === 'number' ||
-        value.length === breakpoints.length
-      )
-        return value
-
-      const [from, to] = value
-      return spread(bpValues, easeIn, from, to, 'xl')
-    })
+    const values = expressions.map((value: Expression) =>
+      isFromToValue(value) ? spread(bpValues, easeIn, ...value, 'xl') : value,
+    )
 
     return breakpoints.map((_, breakpoint) =>
-      template.reduce((prev, curr, i) => {
-        const value = expressionsExpanded[i]?.[breakpoint] ?? expressions[i]
-        return `${prev}${curr}${value}`
-      }, ''),
+      template.reduce(
+        (prev, curr, i) => `${prev}${curr}${values[i]?.[breakpoint] ?? expressions[i]}`,
+        '',
+      ),
     ) as ResponsiveValue
-  }
-}
-
-export function createResponsiveBreakpoints(bpValues: Breakpoints['values']): Breakpoints {
-  const templater = createResponsiveTemplate(bpValues)
-  return (template: TemplateStringsArray, ...expressions: Expression[]): Record<Breakpoints, string> => {
-    const result = templater(template, ...expressions)
-
-    return result.reduce((prev, curr, i) => {
-      const key = Object.keys(bpValues)[i]
-      return { ...prev, [key]: curr }
-    }
   }
 }
