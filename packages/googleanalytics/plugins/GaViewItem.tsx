d463d787d5d09a6d1f0b45502625dd6e8755b16e
Mike Keehnen <mikekeehnen@me.com>
2023-02-28 11:37:19 +0100

    Added useDeepMemo hook to viewItem ga event

--- packages/googleanalytics/plugins/GaViewItem.tsx
+++ packages/googleanalytics/plugins/GaViewItem.tsx
@@ -1,5 +1,6 @@
 import { ProductPageMetaFragment } from '@graphcommerce/magento-product/components/ProductPageMeta/ProductPageMeta.gql'
 import { PluginProps } from '@graphcommerce/next-config'
+import { useMemoDeep } from '@graphcommerce/next-ui'
 import { useEffect } from 'react'
 import { gtagViewItem } from '../events/gtagViewItem/gtagViewItem'
 
@@ -10,12 +11,9 @@ export const exported = '@graphcommerce/magento-product'
 function GaViewItem(props: PluginProps<ProductPageMetaFragment>) {
   const { Prev, url_key, ...rest } = props
 
-  useEffect(
-    () => gtagViewItem(rest),
-    // We're disabling eslint rule to prevent this event from being triggerd on every rerender
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  )
+  const viewItems = useMemoDeep(() => rest, [rest])
+
+  useEffect(() => gtagViewItem(viewItems), [viewItems])
 
   return <Prev {...props} />
 }
