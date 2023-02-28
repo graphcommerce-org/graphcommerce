059eea4f05e821b556f3c99bf7830ec833dbd88f
Paul Hachmang <paul@reachdigital.nl>
2023-02-28 15:55:49 +0100

    Makes ure Gtag is only included when configured

--- packages/googletagmanager/hooks/useGoogleTagmanagerId.ts
+++ packages/googletagmanager/hooks/useGoogleTagmanagerId.ts
@@ -1,5 +1,7 @@
 import { useI18nConfig } from '@graphcommerce/next-ui'
 
 export function useGoogleTagmanagerId() {
-  return useI18nConfig().googleTagmanagerId ?? import.meta.graphCommerce.googleTagmanagerId
+  const id = useI18nConfig().googleTagmanagerId ?? import.meta.graphCommerce.googleTagmanagerId
+  if (!id) throw new Error('No googleTagmanagerId found in config')
+  return id
 }
