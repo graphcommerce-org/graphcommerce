18ea8e64e85f3bef64f1270308afb8bd97413bc1
Paul Hachmang <paul@reachdigital.nl>
2023-03-01 13:25:42 +0100

    Demo config update

--- packagesDev/next-config/dist/config/demoConfig.js
+++ packagesDev/next-config/dist/config/demoConfig.js
@@ -6,4 +6,6 @@ exports.demoConfig = {
     hygraphEndpoint: 'https://eu-central-1.cdn.hygraph.com/content/ckhx7xadya6xs01yxdujt8i80/master',
     magentoEndpoint: 'https://backend.reachdigital.dev/graphql',
     i18n: [{ locale: 'en', magentoStoreCode: 'en_US' }],
+    productFiltersPro: true,
+    legacyProductRoute: false,
 };
