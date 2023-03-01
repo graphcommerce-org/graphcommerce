a7d89a5ee372a9df0da67d7c09d3418341200ee7
Paul Hachmang <paul@reachdigital.nl>
2023-03-01 13:28:17 +0100

    All stores for demo content

--- packagesDev/next-config/src/config/demoConfig.ts
+++ packagesDev/next-config/src/config/demoConfig.ts
@@ -4,7 +4,38 @@ export const demoConfig: GraphCommerceConfig = {
   canonicalBaseUrl: 'https://graphcommerce.vercel.app',
   hygraphEndpoint: 'https://eu-central-1.cdn.hygraph.com/content/ckhx7xadya6xs01yxdujt8i80/master',
   magentoEndpoint: 'https://backend.reachdigital.dev/graphql',
-  i18n: [{ locale: 'en', magentoStoreCode: 'en_US' }],
+  i18n: [
+    {
+      locale: 'en',
+      magentoStoreCode: 'en_US',
+      defaultLocale: true,
+    },
+    {
+      locale: 'nl',
+      magentoStoreCode: 'nl_NL',
+      hygraphLocales: ['nl', 'en'],
+      cartDisplayPricesInclTax: true,
+    },
+    {
+      locale: 'fr-be',
+      magentoStoreCode: 'fr_BE',
+      cartDisplayPricesInclTax: true,
+    },
+    {
+      locale: 'nl-be',
+      magentoStoreCode: 'nl_BE',
+      cartDisplayPricesInclTax: true,
+    },
+    {
+      locale: 'en-gb',
+      magentoStoreCode: 'en_GB',
+      cartDisplayPricesInclTax: true,
+    },
+    {
+      locale: 'en-ca',
+      magentoStoreCode: 'en_CA',
+    },
+  ],
   productFiltersPro: true,
-  legacyProductRoute: false,
+  robotsAllow: false,
 }
