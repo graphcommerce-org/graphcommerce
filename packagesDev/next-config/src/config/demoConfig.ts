bacfe309d1cb31cf67387701e07aafc03528f5cd
Paul Hachmang <paul@reachdigital.nl>
2023-03-01 16:51:40 +0100

    Config and demoConfig

--- packagesDev/next-config/src/config/demoConfig.ts
+++ packagesDev/next-config/src/config/demoConfig.ts
@@ -5,36 +5,12 @@ export const demoConfig: GraphCommerceConfig = {
   hygraphEndpoint: 'https://eu-central-1.cdn.hygraph.com/content/ckhx7xadya6xs01yxdujt8i80/master',
   magentoEndpoint: 'https://backend.reachdigital.dev/graphql',
   i18n: [
-    {
-      locale: 'en',
-      magentoStoreCode: 'en_US',
-      defaultLocale: true,
-    },
-    {
-      locale: 'nl',
-      magentoStoreCode: 'nl_NL',
-      hygraphLocales: ['nl', 'en'],
-      cartDisplayPricesInclTax: true,
-    },
-    {
-      locale: 'fr-be',
-      magentoStoreCode: 'fr_BE',
-      cartDisplayPricesInclTax: true,
-    },
-    {
-      locale: 'nl-be',
-      magentoStoreCode: 'nl_BE',
-      cartDisplayPricesInclTax: true,
-    },
-    {
-      locale: 'en-gb',
-      magentoStoreCode: 'en_GB',
-      cartDisplayPricesInclTax: true,
-    },
-    {
-      locale: 'en-ca',
-      magentoStoreCode: 'en_CA',
-    },
+    { locale: 'en', magentoStoreCode: 'en_US', defaultLocale: true },
+    { locale: 'nl', magentoStoreCode: 'nl_NL', cartDisplayPricesInclTax: true },
+    { locale: 'fr-be', magentoStoreCode: 'fr_BE', cartDisplayPricesInclTax: true },
+    { locale: 'nl-be', magentoStoreCode: 'nl_BE', cartDisplayPricesInclTax: true },
+    { locale: 'en-gb', magentoStoreCode: 'en_GB', cartDisplayPricesInclTax: true },
+    { locale: 'en-ca', magentoStoreCode: 'en_CA' },
   ],
   productFiltersPro: true,
   robotsAllow: false,
