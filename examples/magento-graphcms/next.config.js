23286ae3277f85f35aed5361fd15fadf164c6e4e
Giovanni Schroevers <giovie2000@hotmail.com>
2023-03-29 10:43:07 +0200

    fix(GCOM-1028): allow cache header to be set in config, change next-data cache handler

--- examples/magento-graphcms/next.config.js
+++ examples/magento-graphcms/next.config.js
@@ -3,9 +3,26 @@
 require('dotenv').config({ path: `${__dirname}/.env` })
 
 const { withGraphCommerce } = require('@graphcommerce/next-config')
+const cache = require('next-pwa/cache')
+
+// eslint-disable-next-line import/order
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'next-data',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+      },
+    },
+    ...cache,
+  ],
 })
 
 /** @type {import('next').NextConfig} */
@@ -25,6 +42,58 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // eslint-disable-next-line @typescript-eslint/require-await
+  headers: async () => [
+    {
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, no-cache, no-store, max-age=0, must-revalidate',
+        },
+        {
+          key: 'CDN-Cache-Control',
+          value: 'public, max-age=31536000, stale-while-revalidate',
+        },
+      ],
+
+      source: '/:path*',
+    },
+    {
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable',
+        },
+        {
+          key: 'CDN-Cache-Control',
+          value: 'public, max-age=31536000, stale-while-revalidate',
+        },
+      ],
+
+      source:
+        '/:path(.+\\.(?:ico|png|svg|jpg|jpeg|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)',
+    },
+    {
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+        },
+      ],
+
+      source: '/_next/data/:path*',
+    },
+    {
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+        },
+      ],
+
+      source: '/_next/:path(.+\\.(?:json)$)',
+    },
+  ],
 }
 
 module.exports = withGraphCommerce(withPWA(nextConfig), __dirname)
