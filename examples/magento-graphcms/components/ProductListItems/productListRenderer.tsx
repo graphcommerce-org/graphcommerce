a06abb9b4834453225f88c7e7a0f9eba3816abf4
Paul Hachmang <paul@reachdigital.nl>
2023-03-29 12:20:13 +0200

    fix build

--- examples/magento-graphcms/components/ProductListItems/productListRenderer.tsx
+++ examples/magento-graphcms/components/ProductListItems/productListRenderer.tsx
@@ -79,7 +79,7 @@ export const productListRenderer: ProductListItemRenderer = {
       topRight={<ProductWishlistChip {...props} />}
     />
   ),
-  // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // // @ts-ignore GiftCardProduct is only available in Commerce
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore GiftCardProduct is only available in Commerce
   GiftCardProduct: (props) => <ProductListItem {...props} aspectRatio={[1, 1]} />,
 }
