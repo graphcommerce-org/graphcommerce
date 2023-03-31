54b133cdd709e0b878cf3e8980464eacc5376ce5
Giovanni Schroevers <giovie2000@hotmail.com>
2023-03-31 12:20:07 +0200

    fix(GCOM-1028): change cache headers, import cache from next-pwa

--- packagesDev/next-config/src/config/headers.ts
+++ packagesDev/next-config/src/config/headers.ts
@@ -4,29 +4,10 @@ export const headers = [
     headers: [
       {
         key: 'Cache-Control',
-        value: 'public, max-age=0, must-revalidate',
-      },
-      {
-        key: 'CDN-Cache-Control',
-        value: 'public, max-age=31536000, stale-while-revalidate',
+        value: 'public, no-cache, no-store, max-age=0, must-revalidate',
       },
     ],
 
     source: '/:path*',
-  },
-  {
-    headers: [
-      {
-        key: 'Cache-Control',
-        value: 'public, max-age=31536000, immutable',
-      },
-      {
-        key: 'CDN-Cache-Control',
-        value: 'public, max-age=31536000, stale-while-revalidate',
-      },
-    ],
-
-    source:
-      '/:path(.+\\.(?:ico|png|svg|jpg|jpeg|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)',
   },
 ]
---
+++ packagesDev/next-config/src/types/cache.d.ts
@@ -0,0 +1,4 @@
+declare module 'next-pwa/cache' {
+  const cache: import('workbox-build').RuntimeCaching[]
+  export default cache
+}
