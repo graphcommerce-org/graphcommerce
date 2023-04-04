e6dbddc019358f13886288ae065da2def0c1b335
Paul Hachmang <paul@reachdigital.nl>
2023-04-04 12:29:00 +0200

    useScrollTo improvements for easier debugging

--- packages/framer-scroller/hooks/useScrollTo.ts
+++ packages/framer-scroller/hooks/useScrollTo.ts
@@ -11,7 +11,7 @@ export function useScrollTo() {
   const duration = (useContext(MotionConfigContext).transition as Tween | undefined)?.duration ?? 0
 
   const scrollTo = useCallback(
-    async (incoming: Point | [number, number]) => {
+    async (incoming: Point | [number, number], retrigger = 0) => {
       const ref = scrollerRef.current
       if (!ref) return
 
@@ -22,6 +22,24 @@ export function useScrollTo() {
         to = { x: x[incoming[0]] ?? 0, y: y[incoming[1]] ?? 0 }
       } else {
         to = incoming
+      }
+
+      if (process.env.NODE_ENV === 'development' && scroll.animating.get() && retrigger === 0) {
+        throw Error(
+          `scrollTo triggered while another animation is in progress. Maybe wrap in 'if(!scroll.animating.get())'?`,
+        )
+      }
+
+      if (process.env.NODE_ENV === 'development' && retrigger > 5) {
+        throw Error(
+          `scrollTo triggered more than 5 times, is the element resizing constantly? Bailing out.`,
+        )
+      }
+
+      if (process.env.NODE_ENV === 'development' && retrigger > 0) {
+        console.warn(
+          `scrollTo detect a resize while animating, this can cause the animation to be inaccurate.`,
+        )
       }
 
       const xDone = new Promise<void>((onComplete) => {
@@ -73,7 +91,7 @@ export function useScrollTo() {
       if (Array.isArray(incoming)) {
         const checkPositions = getScrollSnapPositions()
         if (checkPositions.x[incoming[0]] !== to.x || checkPositions.y[incoming[1]] !== to.y)
-          await scrollTo(incoming)
+          await scrollTo(incoming, retrigger++)
       }
       enableSnap()
     },
