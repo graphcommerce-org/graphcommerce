ee2f814f4d91898f106036e92be4e560d638509d
Paul Hachmang <paul@reachdigital.nl>
2023-04-25 10:50:19 +0200

    When the customer is logging in and the merge cart functionality throws an error, it would not set the current customer's cart as active, but keep the faulty cart active.

--- packages/magento-cart/hooks/useMergeCustomerCart.ts
+++ packages/magento-cart/hooks/useMergeCustomerCart.ts
@@ -1,5 +1,5 @@
-import { useMutation, useQuery } from '@graphcommerce/graphql'
-import { useCustomerSession } from '@graphcommerce/magento-customer'
+import { useMutation } from '@graphcommerce/graphql'
+import { useCustomerQuery } from '@graphcommerce/magento-customer'
 import { useEffect } from 'react'
 import { CustomerCartDocument } from './CustomerCart.gql'
 import { UseMergeCustomerCartDocument } from './UseMergeCustomerCart.gql'
@@ -9,39 +9,29 @@ import { useCurrentCartId } from './useCurrentCartId'
 /**
  * - Automatically assign the customer cart as the current cart
  * - Merge the guest cart into the customer cart
- *
- * @todo: Implement the assignCustomerToGuestCart when available: https://github.com/magento/magento2/pull/33106
  */
 export function useMergeCustomerCart() {
   const { currentCartId } = useCurrentCartId()
   const assignCurrentCartId = useAssignCurrentCartId()
   const [merge] = useMutation(UseMergeCustomerCartDocument, { errorPolicy: 'all' })
 
-  const { loggedIn } = useCustomerSession()
-  const destinationCartId = useQuery(CustomerCartDocument, {
-    skip: !loggedIn,
-  })?.data?.customerCart.id
+  const destinationCartId = useCustomerQuery(CustomerCartDocument)?.data?.customerCart.id
 
   useEffect(() => {
     // If we don't have a customer cart, we're done
-    if (!destinationCartId) return
-
     // If the vistor cart is the same as the customer cart, we're done
-    if (currentCartId === destinationCartId) return
+    if (!destinationCartId || currentCartId === destinationCartId) return
 
-    // If there is no visitor cart, assign the customer cart as the current cart
-    if (!currentCartId) {
-      assignCurrentCartId(destinationCartId)
-      return
+    // Assign the customer cart as the new cart id
+    assignCurrentCartId(destinationCartId)
+
+    // If the visitor has a guest cart, try merging it into the customer cart
+    if (currentCartId) {
+      // We're not handling exceptions here:
+      // If the merge returns an error, we'll use the customer cart without merging the guest cart.
+
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      merge({ variables: { sourceCartId: currentCartId, destinationCartId } })
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    ;(async () => {
-      const result = await merge({ variables: { sourceCartId: currentCartId, destinationCartId } })
-
-      if (!result.data?.mergeCarts.id) return
-
-      assignCurrentCartId(result.data?.mergeCarts.id)
-    })()
   }, [assignCurrentCartId, destinationCartId, merge, currentCartId])
 }
