df2b3efb2f906232a302218678c1524753c5a32c
Paul Hachmang <paul@reachdigital.nl>
2023-05-26 16:36:17 +0200

    Customer related information was stored in the users local storage indefintely, now it stores a lot less

---
+++ .changeset/young-chairs-switch.md
@@ -0,0 +1,5 @@
+---
+'@graphcommerce/graphql': patch
+---
+
+Customer related information was stored in the users local storage indefintely, now it stores a lot less
---
+++ packages/graphql/components/GraphQLProvider/persistenceMapper.ts
@@ -0,0 +1,43 @@
+import { InMemoryCache, NormalizedCacheObject } from '@apollo/client'
+
+function pruneKey(cacheValue: unknown, path: string[]) {
+  if (typeof cacheValue !== 'object' || cacheValue === null) return
+
+  const [segment, ...rest] = path
+  const regexp = new RegExp(
+    segment.replace(/\*/g, '.*?').replace(/\(/g, '\\(').replace(/\)/g, '\\)').replace(/=/g, '\\='),
+  )
+
+  Object.keys(cacheValue).forEach((key) => {
+    if (!regexp.test(key)) return
+
+    if (rest.length === 0) {
+      delete cacheValue[key]
+    } else pruneKey(cacheValue[key], rest)
+  })
+}
+
+function pruneCache(cacheObject: object, patterns: string[]) {
+  patterns.forEach((pattern) => pruneKey(cacheObject, pattern.split('.')))
+}
+
+export const persistenceMapper = (data: string): Promise<string> => {
+  const parsedCache = JSON.parse(data) as NormalizedCacheObject
+
+  pruneCache(parsedCache, [
+    'ROOT_MUTATION',
+    'ROOT_QUERY.products*',
+    'ROOT_QUERY.countries',
+    'ROOT_QUERY.checkoutAgreements',
+    'ROOT_QUERY.storeConfig',
+    'ROOT_QUERY.__type*',
+    'ConfigurableProduct:*.configurable_product_options_selection*',
+    '*Product:{"uid":"*"}.crosssell_products',
+  ])
+
+  const cache = new InMemoryCache({})
+  cache.restore(parsedCache)
+  cache.gc()
+  const newCache = cache.extract()
+  return Promise.resolve(JSON.stringify(newCache))
+}
