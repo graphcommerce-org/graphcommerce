fe5ad9565e78a017e017082510a94e1aca02d771
Carlo Carels <carlo@reachdigital.nl>
2023-06-13 21:38:00 +0200

    refactor: remove unused import

--- packages/magento-customer/components/ResetPasswordForm/ResetPasswordForm.tsx
+++ packages/magento-customer/components/ResetPasswordForm/ResetPasswordForm.tsx
@@ -6,7 +6,6 @@ import {
 import { Button, Form, FormActions, FormRow } from '@graphcommerce/next-ui'
 import { useFormGqlMutation } from '@graphcommerce/react-hook-form'
 import { Trans } from '@lingui/react'
-import { TextField } from '@mui/material'
 import { useRouter } from 'next/router'
 import { ApolloCustomerErrorAlert } from '../ApolloCustomerError/ApolloCustomerErrorAlert'
 import {
