a85fbeca07bb0adf7b52dcb0bcfa654572fcf458
Jonatan G <jogala14@gmail.com>
2023-06-19 01:08:36 -0600

    23-6-19-config

--- examples/magento-graphcms/next.config.js
+++ examples/magento-graphcms/next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
   },
   optimizeFonts: false,
   images: {
+    domains: ['backend.reachdigital.dev'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   productionBrowserSourceMaps: true,
