324c757a9a8541cb3fedd64c4fc3072c620daab0
Joshua Bolk <joshuabolk@gmail.com>
2023-06-23 08:46:49 +0200

    wip

--- packages/hygraph-app/components/Setup.tsx
+++ packages/hygraph-app/components/Setup.tsx
@@ -1,22 +1,45 @@
 import React from 'react'
 import { useApp, Wrapper } from '@hygraph/app-sdk-react'
-import { Button } from '@mui/material'
+
+function useLogin() {
+  const [accessToken, setAccessToken] = React.useState<string>('')
+  function login() {
+    setAccessToken('123456')
+    localStorage.setItem('appToken', accessToken)
+  }
+  return { accessToken, login }
+}
 
 function Install() {
-  const { updateInstallation } = useApp()
+  const { accessToken, login } = useLogin()
+  const { updateInstallation, installation } = useApp()
+
+  if (accessToken) {
+    return <div>Connected as name@example.com</div>
+  }
+
+  if (installation.status === 'COMPLETED') {
+    return <div>App installation successful</div>
+  }
 
   return (
-    <Button
+    <button
       type='button'
-      onClick={async () => {
-        console.log(1)
-        await updateInstallation({ status: 'COMPLETED', config: {} }).catch((e) => {
-          console.log(e)
+      onClick={() =>
+        updateInstallation({
+          config: {},
+          status: 'COMPLETED',
         })
-      }}
+          .then((updatedInstallation) => {
+            console.log('Installation updated', updatedInstallation)
+          })
+          .catch((error) => {
+            console.error('Error updating installation', error)
+          })
+      }
     >
       Install App
-    </Button>
+    </button>
   )
 }
 
@@ -24,16 +47,29 @@ function Configure() {
   return <div>Configure</div>
 }
 
-export function Page() {
+function Setup() {
   const { installation } = useApp()
-
-  if (installation?.status === 'COMPLETED') {
-    return (
-      <Wrapper>
-        <Configure />
-      </Wrapper>
-    )
+  if (installation.status === 'COMPLETED') {
+    return <h1>Hello</h1>
   }
-
-  return <Wrapper>TRALASLAL</Wrapper>
+  return <Install />
 }
+
+export function Page() {
+  return (
+    <>
+      <div>
+        <h1>Hygraph Setup</h1>
+      </div>
+      <Wrapper>
+        <Install />
+      </Wrapper>
+    </>
+  )
+}
+
+// import { Wrapper } from '@hygraph/app-sdk-react'
+
+// export function Page() {
+//   return <Wrapper>Hello Hygraph</Wrapper>
+// }
