d9d1e8b4e9c256d87eeded7bd71a7f92178a4f22
Jonatan Garces <jogala14@gmail.com>
2023-06-27 20:26:36 -0600

    feature-232706 Remove menu item

--- examples/magento-graphcms/components/Layout/LayoutNavigation.tsx
+++ examples/magento-graphcms/components/Layout/LayoutNavigation.tsx
@@ -58,16 +58,6 @@ export function LayoutNavigation(props: LayoutNavigationProps) {
               <Trans id='Search...' />
             </SearchLink>,
             { id: 'home', name: <Trans id='Home' />, href: '/' },
-            /*{
-              id: 'manual-item-one',
-              href: `/${menu?.items?.[0]?.children?.[0]?.url_path}`,
-              name: menu?.items?.[0]?.children?.[0]?.name ?? '',
-            },
-            {
-              id: 'manual-item-two',
-              href: `/${menu?.items?.[0]?.children?.[1]?.url_path}`,
-              name: menu?.items?.[0]?.children?.[1]?.name ?? '',
-            },*/
             ...magentoMenuToNavigation(menu, false),
             { id: 'blog', name: 'Blog', href: '/blog' },
             <Divider sx={(theme) => ({ my: theme.spacings.xs })} />,
