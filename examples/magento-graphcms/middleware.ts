91cb259260136f8f7eecbafed461fe32858d4bdd
Hans Nieser <hans@reachdigital.nl>
2023-06-29 15:28:40 +0200

    WIP

---
+++ examples/magento-graphcms/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// Currently, it does not appear to be possible to rewrite from the root (/) like below through
+// next.config.js, so we use middleware instead. Leaving this for future reference:
+// rewrites: async () => ({
+//   afterFiles: [
+//     {
+//       locale: false,
+//       source: '/:path*',
+//       has: [
+//         {
+//           type: 'header',
+//           key: 'x-magento-tags-pattern',
+//         },
+//       ],
+//       destination: '/api/varnish-purge',
+//     },
+//   ],
+// }),
+
+export function middleware(request: NextRequest) {
+  // fixme: Should check remote IP against some configured value, so we don't allow anyone to send PURGE requests
+  if (request.method !== 'PURGE' || !request.headers.get('X-Magento-Tags-Pattern')) {
+    return undefined
+  }
+
+  return NextResponse.rewrite(new URL('/api/varnish-purge', request.url))
+}
+
+export const config = { matcher: '/' }
