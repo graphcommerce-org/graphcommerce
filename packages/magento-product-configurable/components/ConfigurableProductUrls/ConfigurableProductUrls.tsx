832622db4249c5618827e4c01f3c492ba3aec619
Carlo Carels <carlo@reachdigital.nl>
2023-07-04 15:00:22 +0200

    chore: use the useProductLink hook to make it compatible with custom product page urls

--- packages/magento-product-configurable/components/ConfigurableProductUrls/ConfigurableProductUrls.tsx
+++ packages/magento-product-configurable/components/ConfigurableProductUrls/ConfigurableProductUrls.tsx
@@ -1,30 +1,35 @@
+import { ProductLinkFragment } from '@graphcommerce/magento-product/hooks/ProductLink.gql'
+import { useProductLink } from '@graphcommerce/magento-product/hooks/useProductLink'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useConfigurableOptionsSelection } from '../../hooks/useConfigurableOptionsSelection'
 
-type ConfigurableProductUrlsProps = {
-  url_key?: string | null
+type ConfigurableProductUrlsProps = ProductLinkFragment & {
   index?: number
 }
 
 export function ConfigurableProductUrls(props: ConfigurableProductUrlsProps) {
-  const { url_key, index = 0 } = props
+  const { url_key, __typename: productType, index = 0 } = props
   const router = useRouter()
   const { configured } = useConfigurableOptionsSelection({ url_key, index })
+  const urlKey = configured?.configurable_product_options_selection?.variant?.url_key ?? url_key
+
+  const productLink = useProductLink({
+    url_key: urlKey,
+    __typename: productType,
+  })
 
   useEffect(() => {
     const currentUrlPath = router.asPath.split('/')
     const currentUrlKey = currentUrlPath[currentUrlPath.length - 1]
-    const simpleProductUrlKey = configured?.configurable_product_options_selection?.variant?.url_key
+    const optionUrlKey = configured?.configurable_product_options_selection?.variant?.url_key
 
-    if (simpleProductUrlKey && simpleProductUrlKey !== currentUrlKey) {
-      router
-        .replace(`/p/${simpleProductUrlKey}`, undefined, { scroll: false, shallow: true })
-        .catch((error) => {
-          console.error('Failed to replace URL:', error)
-        })
+    if (optionUrlKey && optionUrlKey !== currentUrlKey) {
+      router.replace(productLink, undefined, { scroll: false, shallow: true }).catch((error) => {
+        console.error('Failed to replace URL:', error)
+      })
     }
-  }, [configured, router])
+  }, [configured, router, productLink])
 
   return undefined
 }
