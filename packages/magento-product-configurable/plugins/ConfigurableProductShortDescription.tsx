d17854c69e62146d11c04278543891ac0510cc57
Carlo Carels <carlo@reachdigital.nl>
2023-07-04 12:00:23 +0200

    refactor: resolve type errors

--- packages/magento-product-configurable/plugins/ConfigurableProductShortDescription.tsx
+++ packages/magento-product-configurable/plugins/ConfigurableProductShortDescription.tsx
@@ -8,21 +8,18 @@ export const exported = '@graphcommerce/magento-product'
 
 function ConfigurableProductShortDescription(
   props: PluginProps<
-    ProductShortDescriptionProps & { product?: ConfigurableOptionsFragment; index?: number }
+    ProductShortDescriptionProps & ConfigurableOptionsFragment & { index?: number }
   >,
 ) {
-  const { Prev, product, index = 0, short_description, ...rest } = props
-  const { configured } = useConfigurableOptionsSelection({ url_key: product?.url_key, index })
-
-  const isConfigurable = product?.__typename === 'ConfigurableProduct'
+  const { Prev, url_key, __typename, index = 0, short_description, ...rest } = props
+  const { configured } = useConfigurableOptionsSelection({ url_key, index })
 
   const getConfigurableShortDescription =
     configured?.configurable_product_options_selection?.variant?.short_description
-  const configurableShortDescription =
-    isConfigurable && getConfigurableShortDescription?.html.length
-      ? getConfigurableShortDescription
-      : short_description
+  const configurableShortDescription = getConfigurableShortDescription?.html.length
+    ? getConfigurableShortDescription
+    : short_description
 
-  return <Prev {...rest} short_description={configurableShortDescription} />
+  return <Prev short_description={configurableShortDescription} __typename={__typename} {...rest} />
 }
 export const Plugin = ConfigurableProductShortDescription
