ac610f945c2f0a0fa881f00eab53bce2e2b0441f
Carlo Carels <carlo@reachdigital.nl>
2023-07-06 11:06:40 +0200

    wip: configurableJsonLdProduct plugin

---
+++ packages/magento-product-configurable/plugins/ConfigurableJsonLdProduct.tsx
@@ -0,0 +1,44 @@
+import { mergeDeep } from '@graphcommerce/graphql'
+import { JsonLdProductFragment } from '@graphcommerce/magento-product/components/JsonLdProduct/JsonLdProduct.gql'
+import type { PluginProps } from '@graphcommerce/next-config'
+import { ConfigurableOptionsFragment } from '../graphql/ConfigurableOptions.gql'
+import { useConfigurableOptionsSelection } from '../hooks'
+
+export const component = 'jsonLdProduct'
+export const exported = '@graphcommerce/magento-product'
+
+type ConfigurableJsonLdProductProps = JsonLdProductFragment & {
+  index?: number
+}
+
+export function ConfigurableJsonLdProduct(props: PluginProps<ConfigurableJsonLdProductProps>) {
+  const {
+    Prev,
+    name,
+    sku,
+    media_gallery,
+    categories,
+    description,
+    url_key,
+    index = 0,
+    ...rest
+  } = props
+  const { configured } = useConfigurableOptionsSelection({ url_key, index })
+  const configurableOption = mergeDeep(
+    props,
+    configured?.configurable_product_options_selection?.variant,
+  )
+
+  const configurableMediaGallery = mergeDeep(configurableOption?.media_gallery, media_gallery)
+
+  return (
+    <Prev
+      name={configurableOption?.name ?? name}
+      sku={configurableOption?.sku ?? sku}
+      media_gallery={configurableMediaGallery ?? media_gallery}
+      categories={configurableOption?.categories ?? categories}
+      description={configurableOption?.description ?? description}
+      {...rest}
+    />
+  )
+}
