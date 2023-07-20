a03825b4e4ea66f41e44242b53e3059ca544f4f3
Paul Hachmang <paul@reachdigital.nl>
2023-07-20 09:11:42 +0200

    Simplify ConfigurableProductPagePrice

--- packages/magento-product-configurable/hooks/useConfigurableOptionsSelection.ts
+++ packages/magento-product-configurable/hooks/useConfigurableOptionsSelection.ts
@@ -4,13 +4,11 @@ import { findByTypename, nonNullable } from '@graphcommerce/next-ui'
 import { useWatch } from '@graphcommerce/react-hook-form'
 import { GetConfigurableOptionsSelectionDocument } from '../graphql/GetConfigurableOptionsSelection.gql'
 
-export function useConfigurableOptionsSelection({
-  url_key,
-  index = 0,
-}: {
-  url_key?: string | null
-  index: number
-}) {
+export type UseConfigurableOptionsSelection = { url_key?: string | null; index?: number }
+
+export function useConfigurableOptionsSelection(props: UseConfigurableOptionsSelection) {
+  const { url_key, index = 0 } = props
+
   const { control } = useFormAddProductsToCart()
   const selectedOptions = (useWatch({ control, name: `cartItems.${index}.selected_options` }) ?? [])
     .filter(nonNullable)
--- packages/magento-product-configurable/plugins/ConfigurableProductPagePrice.tsx
+++ packages/magento-product-configurable/plugins/ConfigurableProductPagePrice.tsx
@@ -1,35 +1,17 @@
-import {
-  type ProductPagePriceProps,
-  getProductTierPrice,
-  useFormAddProductsToCart,
-} from '@graphcommerce/magento-product'
-import { Money } from '@graphcommerce/magento-store'
+import { type ProductPagePrice } from '@graphcommerce/magento-product'
 import type { PluginProps } from '@graphcommerce/next-config'
-import { useWatch } from '@graphcommerce/react-hook-form'
-import type { ConfigurableOptionsFragment } from '../graphql/ConfigurableOptions.gql'
 import { useConfigurableOptionsSelection } from '../hooks'
 
 export const component = 'ProductPagePrice'
 export const exported = '@graphcommerce/magento-product'
 
-type ConfigurablePriceProps = ProductPagePriceProps & {
-  product: Partial<ConfigurableOptionsFragment>
-  index?: number
-}
-
-function ConfigurableProductPagePrice(props: PluginProps<ConfigurablePriceProps>) {
-  const { product, index = 0 } = props
+function ConfigurableProductPagePrice(
+  props: PluginProps<React.ComponentProps<typeof ProductPagePrice>>,
+) {
+  const { Prev, product, index = 0 } = props
   const { configured } = useConfigurableOptionsSelection({ url_key: product.url_key, index })
-  const { control } = useFormAddProductsToCart()
-  const quantity = useWatch({ control, name: `cartItems.${index}.quantity` })
+  const variant = configured?.configurable_product_options_selection?.variant
 
-  const regular_price =
-    configured?.configurable_product_options_selection?.variant?.price_range.minimum_price
-      .final_price ?? product.price_range?.minimum_price.final_price
-
-  const price_tiers = configured?.configurable_product_options_selection?.variant?.price_tiers
-  const price = getProductTierPrice({ price_tiers }, quantity) ?? regular_price
-
-  return <Money {...price} />
+  return <Prev product={variant ?? product} />
 }
 export const Plugin = ConfigurableProductPagePrice
