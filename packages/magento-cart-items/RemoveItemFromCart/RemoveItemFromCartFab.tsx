0ad4abdd5fffdcc7e4a8410735343a47d31d32ff
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-07-20 12:29:32 +0200

    Changed RemoveItemFromCartFab to a simple textbutton

--- packages/magento-cart-items/RemoveItemFromCart/RemoveItemFromCartFab.tsx
+++ packages/magento-cart-items/RemoveItemFromCart/RemoveItemFromCartFab.tsx
@@ -1,37 +1,25 @@
 import { ApolloCartErrorSnackbar, useFormGqlMutationCart } from '@graphcommerce/magento-cart'
-import { iconClose, IconSvg } from '@graphcommerce/next-ui'
-import { i18n } from '@lingui/core'
-import { Fab, SxProps, Theme, styled } from '@mui/material'
+import { Trans } from '@lingui/react'
+import { Button, SxProps, Theme, styled } from '@mui/material'
 import { CartItemFragment } from '../Api/CartItem.gql'
 import { RemoveItemFromCartDocument } from './RemoveItemFromCart.gql'
 
-export type RemoveItemFromCartProps = CartItemFragment &
+export type RemoveItemFromCartProps = Omit<CartItemFragment, 'product' | '__typename'> &
   Omit<JSX.IntrinsicElements['form'], 'onSubmit' | 'noValidate'> & { sx?: SxProps<Theme> }
 
 const Form = styled('form')({})
 
 export function RemoveItemFromCartFab(props: RemoveItemFromCartProps) {
-  const {
-    product: {},
-    uid,
-    quantity,
-    prices,
-    ...formProps
-  } = props
+  const { uid, quantity, prices, ...formProps } = props
   const form = useFormGqlMutationCart(RemoveItemFromCartDocument, { defaultValues: { uid } })
   const { handleSubmit, formState, error } = form
   const submitHandler = handleSubmit(() => {})
 
   return (
     <Form noValidate onSubmit={submitHandler} {...formProps}>
-      <Fab
-        aria-label={i18n._(/* i18n */ 'Remove Product')}
-        size='small'
-        type='submit'
-        disabled={formState.isSubmitting}
-      >
-        <IconSvg src={iconClose} />
-      </Fab>
+      <Button type='submit' variant='inline' color='secondary' disabled={formState.isSubmitting}>
+        <Trans id='Remove' />
+      </Button>
       <ApolloCartErrorSnackbar error={error} />
     </Form>
   )
