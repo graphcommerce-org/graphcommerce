94e1ae811fe9eb0051863e8be91c6399ddcdf22f
Bram van der Holst <bram@h-o.nl>
2023-07-21 13:40:59 +0200

    Added storageType prop to MessageSnackbar to allow messages to be shown only once

---
+++ .changeset/shiny-radios-cover.md
@@ -0,0 +1,5 @@
+---
+'@graphcommerce/next-ui': patch
+---
+
+Added storageType prop to MessageSnackbar to allow messages to be shown only once
--- packages/next-ui/Snackbar/MessageSnackbar.tsx
+++ packages/next-ui/Snackbar/MessageSnackbar.tsx
@@ -1,4 +1,36 @@
 import dynamic from 'next/dynamic'
+import { MessageSnackbarProps as MessageSnackbarImpl } from './MessageSnackbarImpl'
 
 /** Always load the MessageSnackbar dynamically */
-export const MessageSnackbar = dynamic(() => import('./MessageSnackbarImpl'), { ssr: false })
+export const MessageSnackbarImplLoader = dynamic(() => import('./MessageSnackbarImpl'), {
+  ssr: false,
+})
+
+export type MessageSnackbarProps = MessageSnackbarImpl & {
+  id?: string
+  storageType?: 'localStorage' | 'sessionStorage'
+}
+export function MessageSnackbar(props: MessageSnackbarProps) {
+  const { storageType, id, onClose, ...rest } = props
+  const messageId = `MessageSnackBar_${id}`
+
+  if (storageType && !id && process.env.NODE_ENV !== 'production') {
+    console.error('MessageSnackbar with a storageType set must has an id.')
+  }
+
+  if (storageType && id && globalThis[storageType].getItem(messageId)) {
+    return null
+  }
+
+  return (
+    <MessageSnackbarImplLoader
+      {...rest}
+      onClose={() => {
+        if (id && storageType) {
+          globalThis[storageType].setItem(messageId, `${Date.now()}`)
+        }
+        onClose?.()
+      }}
+    />
+  )
+}
