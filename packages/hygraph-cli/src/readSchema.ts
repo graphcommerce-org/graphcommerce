88dcf71782ffa82c6a2691eaf43030ab7af65433
Paul Hachmang <paul@reachdigital.nl>
2023-08-10 11:28:25 +0200

    Cleanup dependencies

--- packages/hygraph-cli/src/readSchema.ts
+++ packages/hygraph-cli/src/readSchema.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, gql, HttpLink } from '@apollo/client'
 import { GraphCommerceConfig } from '@graphcommerce/next-config'
-import fetch from 'cross-fetch'
+import { fetch } from '@whatwg-node/fetch'
 
 export const readSchema = async (config: GraphCommerceConfig) => {
   if (!config.hygraphProjectId) {
