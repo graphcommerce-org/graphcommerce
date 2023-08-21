1b4f8af428cd5a7fde09be936087c65c780385a2
Mike Keehnen <mikekeehnen@me.com>
2023-08-21 15:51:16 +0200

    Add runtime type and ignore VARY header

--- examples/magento-graphcms/next.config.js
+++ examples/magento-graphcms/next.config.js
@@ -2,13 +2,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config({ path: `${__dirname}/.env` })
 
+const PWA = require('@ducanh2912/next-pwa')
 const { withGraphCommerce, runtimeCachingOptimizations } = require('@graphcommerce/next-config')
 
 // eslint-disable-next-line import/order
-const withPWA = require('@ducanh2912/next-pwa').default({
+const withPWA = PWA.default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: runtimeCachingOptimizations,
+  runtimeCaching: PWA.runtimeCaching.concat(runtimeCachingOptimizations),
 })
 
 /** @type {import('next').NextConfig} */
--- packagesDev/next-config/src/runtimeCachingOptimizations.ts
+++ packagesDev/next-config/src/runtimeCachingOptimizations.ts
@@ -1,9 +1,12 @@
-export const runtimeCachingOptimizations = [
+import { RuntimeCaching } from 'workbox-build'
+
+export const runtimeCachingOptimizations: RuntimeCaching[] = [
   {
     urlPattern: /\/_next\/image?url=.*$/i,
     handler: 'StaleWhileRevalidate',
     options: {
       cacheName: 'next-image',
+      matchOptions: { ignoreVary: true },
       expiration: {
         // Currently experimenting with max cache entries to optimize service worker cache
         maxEntries: 100, // 100 images
