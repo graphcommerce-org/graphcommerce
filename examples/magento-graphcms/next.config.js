c46ce11a30e57d7e28ad571f98f71140671a6b49
Mike Keehnen <mikekeehnen@me.com>
2023-08-21 15:04:12 +0200

    Add new next/pwa package and configuration

--- examples/magento-graphcms/next.config.js
+++ examples/magento-graphcms/next.config.js
@@ -3,9 +3,24 @@
 require('dotenv').config({ path: `${__dirname}/.env` })
 
 const { withGraphCommerce } = require('@graphcommerce/next-config')
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  mode: 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /\/_next\/image?url=.*$/i,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'next-image',
+        expiration: {
+          // Currently experimenting with max cache entries to optimize service worker cache
+          maxEntries: 100, // 100 images
+          maxAgeSeconds: 48 * 60 * 60, // 48 hours
+        },
+      },
+    },
+  ],
 })
 
 /** @type {import('next').NextConfig} */
