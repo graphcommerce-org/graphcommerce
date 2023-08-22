c2d94cf8b3f60f19f276650c30c9d3fe19b20a9b
Mike Keehnen <mikekeehnen@me.com>
2023-08-22 16:12:15 +0200

    Added ignoreVary header

--- examples/magento-graphcms/next.config.js
+++ examples/magento-graphcms/next.config.js
@@ -5,9 +5,28 @@ require('dotenv').config({ path: `${__dirname}/.env` })
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  extendDefaultRuntimeCaching: true,
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: /\/_next\/image?url=.*$/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'next-image',
+          matchOptions: { ignoreVary: true },
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 48 * 60 * 60, // 48 hours
+            purgeOnQuotaError: true,
+            matchOptions: { ignoreVary: true },
+          },
+        },
+      },
+    ],
+  },
 })
 
-const { withGraphCommerce } = require('@graphcommerce/next-config')
+const { withGraphCommerce, runtimeCachingOptimizations } = require('@graphcommerce/next-config')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
