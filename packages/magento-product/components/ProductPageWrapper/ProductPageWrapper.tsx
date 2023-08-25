2f22eba09d895ec1ca87b9f7b73227603f8fe901
Carlo Carels <carlo@reachdigital.nl>
2023-08-25 11:50:17 +0200

    refactor: remove unused files

--- packages/magento-product/components/ProductPageWrapper/ProductPageWrapper.tsx
+++ packages/magento-product/components/ProductPageWrapper/ProductPageWrapper.tsx
@@ -2,12 +2,11 @@ import { Box } from '@mui/material'
 import { AddProductsToCartButtonProps } from '../AddProductsToCart'
 
 export type ProductPageWrapperProps = {
-  product?: Pick<AddProductsToCartButtonProps, 'product'>
   children?: React.ReactNode
 }
 
 export function ProductPageWrapper(props: ProductPageWrapperProps) {
-  const { product, children } = props
+  const { children } = props
 
-  return <Box sx={{ position: 'relative', border: '1px solid red' }}>{children}</Box>
+  return <Box>{children}</Box>
 }
