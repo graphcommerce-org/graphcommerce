dd5c94eb4d211eff383abb13bb3de6e385580cd1
Carlo Carels <carlo@reachdigital.nl>
2023-09-05 14:38:53 +0200

    chore(GCOM-1186): remove the selected method from the Payment button

--- packages/magento-cart-payment-method/PaymentMethodButton/PaymentMethodButton.tsx
+++ packages/magento-cart-payment-method/PaymentMethodButton/PaymentMethodButton.tsx
@@ -27,24 +27,8 @@ function PaymentMethodButtonRenderer(
           {...buttonProps}
           onClick={submit}
           loading={buttonState.isSubmitting || (buttonState.isSubmitSuccessful && !error)}
-          button={{
-            sx: [
-              (theme) => ({
-                [theme.breakpoints.down('md')]: {
-                  fontSize: theme.typography.body2.fontSize,
-                },
-              }),
-            ],
-            ...buttonProps?.button,
-          }}
         >
           {buttonProps.children}
-          {selectedMethod?.title && (
-            <>
-              {' '}
-              (<em>{selectedMethod?.title}</em>)
-            </>
-          )}
         </LinkOrButton>
       ) : (
         <PaymentButton
