0c6f6b7c3c1b40b66927b6a2b19d34012f5c7766
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-09-05 13:23:25 +0200

    Support configurable wishlistItems

--- packages/magento-wishlist/components/WishlistItem/WishlistItem.tsx
+++ packages/magento-wishlist/components/WishlistItem/WishlistItem.tsx
@@ -1,8 +1,10 @@
 import { InputMaybe } from '@graphcommerce/graphql-mesh'
-import { SxProps, Theme } from '@mui/material'
+import { Box, Button, SxProps, Theme } from '@mui/material'
 import { ProductAddToCart } from './ProductAddToCart'
 import { WishlistItemBase } from './WishlistItemBase'
 import { WishlistItemProductFragment } from './WishlistItemProduct.gql'
+import { Trans } from '@lingui/react'
+import { useProductLink } from '@graphcommerce/magento-product'
 
 type OptionalProductWishlistParent = {
   wishlistItemId?: string
@@ -12,18 +14,43 @@ export type WishlistItemProps = WishlistItemProductFragment & {
   sx?: SxProps<Theme>
   selectedOptions?: InputMaybe<string[]> | undefined
   children?: React.ReactNode
-} & OptionalProductWishlistParent
+} & OptionalProductWishlistParent & { isConfigurableUncompleted?: boolean }
 
 export function WishlistItem(props: WishlistItemProps) {
-  const { sku, name, price_range, selectedOptions } = props
+  const {
+    sku,
+    name,
+    price_range,
+    selectedOptions,
+    isConfigurableUncompleted = false,
+    __typename: productType,
+    url_key,
+  } = props
+
+  const productLink = useProductLink({ url_key, __typename: productType })
 
   return (
     <WishlistItemBase {...props}>
-      <ProductAddToCart
-        variables={{ sku: sku ?? '', quantity: 1, selectedOptions }}
-        name={name ?? ''}
-        price={price_range.minimum_price.final_price}
-      />
+      {isConfigurableUncompleted ? (
+        <Button
+          color='primary'
+          variant='text'
+          size='medium'
+          sx={{ width: '100px' }}
+          href={productLink}
+        >
+          <Trans id='Configure' />
+        </Button>
+      ) : (
+        <Box>
+          <ProductAddToCart
+            variables={{ sku: sku ?? '', quantity: 1, selectedOptions }}
+            name={name ?? ''}
+            price={price_range.minimum_price.final_price}
+          />
+          {selectedOptions}
+        </Box>
+      )}
     </WishlistItemBase>
   )
 }
--- packages/magento-wishlist/components/WishlistItems/WishlistItems.tsx
+++ packages/magento-wishlist/components/WishlistItems/WishlistItems.tsx
@@ -1,7 +1,8 @@
-import { RenderType, TypeRenderer } from '@graphcommerce/next-ui'
+import { RenderType, TypeRenderer, nonNullable } from '@graphcommerce/next-ui'
 import { Box } from '@mui/material'
 import { useWishlistItems } from '../../hooks'
 import { WishlistItemsFragment } from './WishlistItems.gql'
+import { useCustomerSession } from '@graphcommerce/magento-customer'
 
 export type WishlistItemRenderer = TypeRenderer<
   NonNullable<
@@ -14,17 +15,40 @@ export type WishlistProps = { renderer: WishlistItemRenderer }
 export function WishlistItems(props: WishlistProps) {
   const { renderer } = props
   const wishlistItemsData = useWishlistItems()
+  const { loggedIn } = useCustomerSession()
+
+  const wishlist = loggedIn
+    ? wishlistItemsData.data
+    : wishlistItemsData.guestWishlist.data?.guestWishlist?.items.map((guestItem) =>
+        wishlistItemsData.data?.find((joe) => guestItem.sku === joe.sku),
+      )
 
   /** Structure between guest and customer wishlist differs */
   return (
     <>
-      {wishlistItemsData.data?.map((item) => {
+      {wishlist?.map((item, i) => {
         if (!item?.uid && !item?.id) return null
 
         const productData = item?.product ? item?.product : item
+        const configurable_options =
+          wishlistItemsData.guestWishlist.data && !loggedIn
+            ? wishlistItemsData.guestWishlist.data?.guestWishlist?.items?.[i].selected_options
+            : item.configurable_options?.map(
+                (option) => option.configurable_product_option_value_uid,
+              )
+
+        const isConfigurableUncompleted =
+          productData.__typename === 'ConfigurableProduct' &&
+          productData.configurable_options.length !== configurable_options.length
         return (
-          <Box key={item.id || item.uid}>
-            <RenderType renderer={renderer} wishlistItemId={item.id || null} {...productData} />
+          <Box key={loggedIn ? item.id || item.uid : i}>
+            <RenderType
+              renderer={renderer}
+              wishlistItemId={loggedIn ? item.id || null : i}
+              selectedOptions={configurable_options}
+              isConfigurableUncompleted={isConfigurableUncompleted}
+              {...productData}
+            />
           </Box>
         )
       })}
