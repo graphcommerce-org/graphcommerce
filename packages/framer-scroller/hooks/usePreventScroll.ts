925e75f062c0c46b666757baa511e51cc7af63d5
Joshua Bolk <joshuabolk@gmail.com>
2023-09-26 12:40:55 +0200

    [GCOM-1219] hide scrollbar so it's no longer draggable

--- packages/framer-scroller/hooks/usePreventScroll.ts
+++ packages/framer-scroller/hooks/usePreventScroll.ts
@@ -5,15 +5,32 @@ export function usePreventScroll(shouldPrevent: boolean) {
     const preventScrolling = (e) => e.preventDefault()
 
     if (shouldPrevent) {
-      // When the effect runs, add the event listeners.
+      // Block scrolling event
       window.addEventListener('wheel', preventScrolling, { passive: false })
       window.addEventListener('touchmove', preventScrolling, { passive: false })
+
+      // Hide scrollbar
+      const style = document.createElement('remove-scrollbar')
+      style.innerHTML = `
+        body::-webkit-scrollbar {
+            width: 0px; 
+            background: transparent; 
+        }
+      `
+      document.head.appendChild(style)
     }
 
     return () => {
       // When the effect is cleaned up, remove the event listeners.
       window.removeEventListener('wheel', preventScrolling)
       window.removeEventListener('touchmove', preventScrolling)
+
+      const styles = Array.from(document.head.getElementsByTagName('remove-scrollbar'))
+      styles.forEach((style) => {
+        if (style.innerHTML.includes('body::-webkit-scrollbar')) {
+          document.head.removeChild(style)
+        }
+      })
     }
-  }, [shouldPrevent]) // The effect will re-run whenever `shouldPrevent` changes.
+  }, [shouldPrevent])
 }
