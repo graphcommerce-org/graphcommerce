c10b84eae0a76d50076c7c5d2985a13d57c4f4bd
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-09-27 15:45:03 +0200

    Updated wishlistFab to work with the new wishlist

--- packages/magento-wishlist/components/WishlistFab/WishlistFab.tsx
+++ packages/magento-wishlist/components/WishlistFab/WishlistFab.tsx
@@ -1,15 +1,9 @@
-import {
-  useCustomerQuery,
-  useCustomerSession,
-  useGuestQuery,
-} from '@graphcommerce/magento-customer'
+import { useCustomerSession } from '@graphcommerce/magento-customer'
 import { iconHeart, DesktopHeaderBadge, IconSvg, extendableComponent } from '@graphcommerce/next-ui'
 import { i18n } from '@lingui/core'
 import { Fab, FabProps as FabPropsType, NoSsr, SxProps, Theme } from '@mui/material'
 import React from 'react'
-import { useWishlistEnabled } from '../../hooks'
-import { GetIsInWishlistsDocument } from '../../queries/GetIsInWishlists.gql'
-import { GuestWishlistDocument } from '../../queries/GuestWishlist.gql'
+import { useWishlistEnabled, useWishlistItems } from '../../hooks'
 
 type WishlistFabContentProps = {
   icon?: React.ReactNode
@@ -60,22 +54,16 @@ export function WishlistFab(props: WishlistFabProps) {
 
   const { loggedIn } = useCustomerSession()
 
-  const { data: GetCustomerWishlistData } = useCustomerQuery(GetIsInWishlistsDocument)
-  const { data: guestWishlistData } = useGuestQuery(GuestWishlistDocument)
+  const wishlistData = useWishlistItems()
 
-  let activeWishlist = false
-  if (loggedIn) {
-    const wishlistItemCount = GetCustomerWishlistData?.customer?.wishlists[0]?.items_count || 0
-    activeWishlist = wishlistItemCount > 0
-  } else {
-    const wishlist = guestWishlistData?.guestWishlist?.items || []
-    activeWishlist = wishlist.length > 0
-  }
+  const activeWishlist = wishlistData.data?.length !== 0
 
   if (!isWishlistEnabled || (hideForGuest && !loggedIn)) return null
 
   return (
-    <NoSsr fallback={<WishlistFabContent {...props} activeWishlist={false} />}>
+    <NoSsr
+      fallback={<WishlistFabContent {...props} activeWishlist={wishlistData.data?.length !== 0} />}
+    >
       <WishlistFabContent {...props} activeWishlist={activeWishlist} />
     </NoSsr>
   )
