c1dde53c20913298fb2cc3db97cc1e8b0faea0ef
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-10-03 14:39:00 +0200

    Removed logic from ConfigurableWishlistItemActionCard to ConfigurableWishlistitemAction

--- packages/magento-product-configurable/plugins/ConfigurableWishlistItemActionCard.tsx
+++ packages/magento-product-configurable/plugins/ConfigurableWishlistItemActionCard.tsx
@@ -1,7 +1,7 @@
 import type { ConfigurableOptions, WishlistItemActionCard } from '@graphcommerce/magento-wishlist'
-import { ProductAddToCart } from '@graphcommerce/magento-wishlist/components/WishlistItem/ProductAddToCart'
 import type { PluginProps } from '@graphcommerce/next-config'
-import { nonNullable } from '@graphcommerce/next-ui'
+import { Box } from '@mui/material'
+import { ConfigurableWishlistItemAction } from '../components/ConfigurableWishlistItemAction/ConfigurableWishlistItemAction'
 
 export const component = 'WishlistItemActionCard'
 export const exported =
@@ -10,36 +10,24 @@ export const exported =
 export function ConfigurableWishlistItemActionCard(
   props: PluginProps<React.ComponentProps<typeof WishlistItemActionCard> & ConfigurableOptions>,
 ) {
-  const { Prev, configurable_options, product, ...rest } = props
+  const { Prev, configurable_options, ...rest } = props
 
-  const selectedOptions = configurable_options
-    ?.map((option) => option?.configurable_product_option_value_uid)
-    .filter(nonNullable)
-  const isConfigurableUncompleted =
-    (product?.__typename === 'ConfigurableProduct' &&
-      product?.configurable_options?.length !== configurable_options?.length) ||
-    configurable_options?.some(
-      (option) =>
-        option?.configurable_product_option_value_uid === null ||
-        option?.configurable_product_option_value_uid === undefined,
-    )
   if (rest.__typename !== 'ConfigurableWishlistItem') return <Prev {...props} />
 
   return (
     <Prev
       {...props}
       secondaryAction={
-        isConfigurableUncompleted
-          ? 'uncompleted'
-          : product?.name &&
-            product?.sku &&
-            product?.price_range.minimum_price.final_price && (
-              <ProductAddToCart
-                variables={{ sku: product.sku, quantity: 1, selectedOptions }}
-                name={product.name}
-                price={product.price_range.minimum_price.final_price}
-              />
-            )
+        <ConfigurableWishlistItemAction configurable_options={configurable_options} {...rest} />
+      }
+      details={
+        configurable_options && (
+          <Box sx={(theme) => ({ mb: `calc(${theme.spacings.xxs} / 2)` })}>
+            {configurable_options?.map((option) => (
+              <Box key={option?.configurable_product_option_uid}>{option?.value_label}</Box>
+            ))}
+          </Box>
+        )
       }
     />
   )
