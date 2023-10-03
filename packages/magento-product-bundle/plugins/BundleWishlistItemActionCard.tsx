8230014a9c0918fed019f4a563675ae3246a0411
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-10-03 15:05:45 +0200

    Edited BundleWishlistItemActionCard

--- packages/magento-product-bundle/plugins/BundleWishlistItemActionCard.tsx
+++ packages/magento-product-bundle/plugins/BundleWishlistItemActionCard.tsx
@@ -1,6 +1,9 @@
+import { useProductLink } from '@graphcommerce/magento-product/hooks/useProductLink'
 import type { WishlistItemActionCard } from '@graphcommerce/magento-wishlist'
 import type { PluginProps } from '@graphcommerce/next-config'
+import { IconSvg, iconChevronRight } from '@graphcommerce/next-ui'
 import { Trans } from '@lingui/react'
+import { Button } from '@mui/material'
 
 export const component = 'WishlistItemActionCard'
 export const exported =
@@ -9,14 +12,29 @@ export const exported =
 export function BundleWishlistItemActionCard(
   props: PluginProps<React.ComponentProps<typeof WishlistItemActionCard>>,
 ) {
-  const { Prev, ...rest } = props
+  const { Prev, product, ...rest } = props
+
+  const productLink = useProductLink({
+    url_key: product?.url_key,
+    __typename: product?.__typename ?? 'BundleProduct',
+  })
 
   if (rest.__typename !== 'BundleWishlistItem') return <Prev {...props} />
 
   return (
     <Prev
       {...props}
-      secondaryAction={<Trans id='Bundled products cannot be added to the cart directly.' />}
+      secondaryAction={
+        <Button
+          variant='text'
+          color='primary'
+          size='medium'
+          href={productLink}
+          endIcon={<IconSvg src={iconChevronRight} />}
+        >
+          <Trans id='Configure' />
+        </Button>
+      }
     />
   )
 }
