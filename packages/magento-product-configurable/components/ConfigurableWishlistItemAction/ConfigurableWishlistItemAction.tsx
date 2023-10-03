0e8e7300b9477da14e730c3ba7a140cd49376e2a
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-10-03 14:37:58 +0200

    Added Action to be used in the configurableWishlistItemActionCard

---
+++ packages/magento-product-configurable/components/ConfigurableWishlistItemAction/ConfigurableWishlistItemAction.tsx
@@ -0,0 +1,50 @@
+import { useProductLink } from '@graphcommerce/magento-product/hooks/useProductLink'
+import { ConfigurableOptions, WishlistItemActionCardProps } from '@graphcommerce/magento-wishlist'
+import { ProductAddToCart } from '@graphcommerce/magento-wishlist/components/WishlistItem/ProductAddToCart'
+import { IconSvg, iconChevronRight, nonNullable } from '@graphcommerce/next-ui'
+import { Trans } from '@lingui/react'
+import { Button } from '@mui/material'
+
+type ConfigurableWishlistItemActionProps = WishlistItemActionCardProps & ConfigurableOptions
+
+export function ConfigurableWishlistItemAction(props: ConfigurableWishlistItemActionProps) {
+  const { configurable_options, product } = props
+
+  const productLink = useProductLink({
+    url_key: product?.url_key,
+    __typename: product?.__typename ?? 'SimpleProduct',
+  })
+
+  const selectedOptions = configurable_options
+    ?.map((option) => option?.configurable_product_option_value_uid)
+    .filter(nonNullable)
+
+  const isConfigurableUncompleted =
+    (product?.__typename === 'ConfigurableProduct' &&
+      product?.configurable_options?.length !== configurable_options?.length) ||
+    configurable_options?.some(
+      (option) =>
+        option?.configurable_product_option_value_uid === null ||
+        option?.configurable_product_option_value_uid === undefined,
+    )
+
+  return isConfigurableUncompleted ? (
+    <Button
+      variant='text'
+      color='primary'
+      size='medium'
+      href={productLink}
+      endIcon={<IconSvg src={iconChevronRight} />}
+    >
+      <Trans id='Configure' />
+    </Button>
+  ) : (
+    product?.name && product?.sku && product?.price_range.minimum_price.final_price && (
+      <ProductAddToCart
+        variables={{ sku: product.sku, quantity: 1, selectedOptions }}
+        name={product.name}
+        price={product.price_range.minimum_price.final_price}
+      />
+    )
+  )
+}
