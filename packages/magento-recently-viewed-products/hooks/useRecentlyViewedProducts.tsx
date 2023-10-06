564178de99771ac3812cc174db496ff75941215b
Bram van der Holst <bram@h-o.nl>
2023-10-06 16:39:31 +0200

    feat(GCOM-535) sort recently viewed products based on array order

--- packages/magento-recently-viewed-products/hooks/useRecentlyViewedProducts.tsx
+++ packages/magento-recently-viewed-products/hooks/useRecentlyViewedProducts.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from '@graphcommerce/graphql'
 import { ProductListDocument } from '@graphcommerce/magento-product'
 import { useRecentlyViewedSkus } from './useRecentlyViewedSkus'
+import { nonNullable } from '@graphcommerce/next-ui'
 
 export function useRecentlyViewedProducts({ exclude }: { exclude?: string[] } = {}) {
   const { skus, loading } = useRecentlyViewedSkus({ exclude })
-  const getTimeBySku = (sku: string) => skus.find((s) => s.sku === sku)?.time || 0
 
   const {
     loading: loadingProducts,
@@ -23,9 +23,7 @@ export function useRecentlyViewedProducts({ exclude }: { exclude?: string[] } = 
 
   let products = data?.products?.items || previousData?.products?.items || []
   // Sort products based on the time they were viewed. Last viewed item should be the first item in the array
-  products = [...products].sort((a, b) =>
-    getTimeBySku(b?.sku || '') > getTimeBySku(a?.sku || '') ? 1 : -1,
-  )
+  products = skus.map((sku) => products.find((p) => (p?.sku || '') === sku.sku)).filter(nonNullable)
 
   return {
     products,
