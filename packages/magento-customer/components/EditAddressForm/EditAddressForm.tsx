014fbfe03ef11a70f097011b481b22aab63fdd40
Joshua Bolk <joshuabolk@gmail.com>
2023-10-19 10:44:53 +0200

    [GCOM-1266] Fix editAddressForm when addition is nullable

--- packages/magento-customer/components/EditAddressForm/EditAddressForm.tsx
+++ packages/magento-customer/components/EditAddressForm/EditAddressForm.tsx
@@ -50,7 +50,7 @@ export function EditAddressForm(props: EditAddressFormProps) {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         houseNumber: address?.street?.[1],
-        addition: address?.street?.[2],
+        addition: address?.street?.[2] ?? '',
       },
       onBeforeSubmit: (formData) => {
         const region = countries
