123da48c8946941ea40bddf9d08bf87866ba4ae0
Joshua Bolk <joshuabolk@gmail.com>
2023-10-23 16:07:11 +0200

    [GCOM-1108] Deep introspection query

--- packages/hygraph-app/lib/functions.ts
+++ packages/hygraph-app/lib/functions.ts
@@ -39,3 +39,19 @@ export const findProperties = (
   }
   return inputs
 }
+
+export const createRecursiveIntrospectionQuery = (type, depth) => {
+  let baseQuery = `__type(name: "${type}") { name kind fields { name `
+  let endQuery = ' } }'
+
+  for (let i = 0; i < depth; i++) {
+    baseQuery += 'type { ofType { fields { name '
+
+    endQuery += ' } } }'
+  }
+
+  const result = baseQuery + endQuery
+
+  console.log(85, baseQuery + endQuery)
+  return result
+}
