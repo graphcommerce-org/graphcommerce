6fdf2f49ccf061dec4c7b8f751b7d52304729bac
Joshua Bolk <joshuabolk@gmail.com>
2023-10-24 16:24:33 +0200

    [GCOM-1108] cleanup

--- packages/hygraph-app/types/index.ts
+++ packages/hygraph-app/types/index.ts
@@ -4,20 +4,5 @@ export type ProductProperty = {
   type?: string
 }
 
-export type Condition = {
-  property?: string
-  value?: string | number
-  type: 'ConditionText' | 'ConditionNumber' | 'ConditionAnd'
-  operator?: string
-  conditions?: Condition[]
-  id?: string
-}
-
-export type ConditionAnd = {
-  type: 'ConditionAnd'
-  conditions: Condition[]
-  id: string
-}
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type InterfaceObject = { [s: string]: any }
