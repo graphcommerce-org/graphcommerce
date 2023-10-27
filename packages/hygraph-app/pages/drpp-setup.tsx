03e31fad1aa1aef5048880fb565730032c087251
Joshua Bolk <joshuabolk@gmail.com>
2023-10-27 15:21:59 +0200

    [GCOM-1108] fix types

--- packages/hygraph-app/pages/drpp-setup.tsx
+++ packages/hygraph-app/pages/drpp-setup.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
+import React, { MutableRefObject, ReactNode, RefObject } from 'react'
 import { Page } from '..'
 
 export default function Setup() {
-  const appContainer = React.useRef(null)
+  const appContainer = React.useRef<HTMLDivElement | null>(null)
 
   /**
    * This is a hack to fix the height of the iframe, which was malfunctioning because of a conflict
    * with FramerNextPages
    */
   React.useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     const framerParent = appContainer?.current?.parentElement
     if (framerParent) {
       framerParent.style.position = 'static'
       framerParent.style.minHeight = 'unset'
     }
 
-    const framerParent2 = framerParent?.previousSibling
+    const framerParent2 = framerParent?.previousSibling as HTMLDivElement | null
     if (framerParent2) {
       framerParent2.style.minHeight = 'unset'
     }
