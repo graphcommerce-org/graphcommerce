c1250b1c78a7fb381d9c18489e7ff0d58ff058f8
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-10-30 10:15:55 +0100

    solved any ovverrides al other attributes error

--- packages/magento-wishlist/components/WishlistItemActionCard/WishlistItemActionCard.tsx
+++ packages/magento-wishlist/components/WishlistItemActionCard/WishlistItemActionCard.tsx
@@ -77,123 +77,122 @@ export function WishlistItemActionCard(props: WishlistItemActionCardProps) {
   const filteredItems = oldItems?.filter((oldItem) => oldItem?.id !== wishlistItemId)
   const [removeWishlistItem] = useMutation(RemoveProductFromWishlistDocument)
 
+  if (!wishlistItemId) return null
+
   return (
-    wishlistItemId && (
-      <AddProductsToCartForm key={wishlistItemId}>
-        <ActionCard
-          variant={variant}
-          value={wishlistItemId}
-          sx={[
-            (theme) => ({
-              '&.ActionCard-root': {
-                px: 0,
-                py: theme.spacings.xs,
-              },
-              '& .MuiBox-root': {
-                justifyContent: 'space-between',
-                alignItems: 'stretch',
-              },
-              '&.sizeSmall': {
-                px: 0,
-              },
-              '& .ActionCard-action': {
-                alignSelf: 'flex-end',
-              },
-              '& .ActionCard-image': {
-                alignSelf: 'flex-start',
-              },
-              '& .ActionCard-secondaryAction': {
-                typography: typographySizes[size],
-                display: 'flex',
-                alignItems: 'center',
-                color: 'text.secondary',
-                mt: 1,
-                gap: '10px',
-                justifyContent: 'start',
-              },
-              '& .ActionCard-price': {
-                typography: typographySizes[size],
-                mb: { xs: 0.5, sm: 0 },
-              },
-            }),
-            ...(Array.isArray(sx) ? sx : [sx]),
-          ]}
-          image={
-            product?.small_image?.url && (
-              <Image
-                className={classes.image}
-                layout='fill'
-                src={product?.small_image?.url}
-                sx={{
-                  width: productImageSizes[size],
-                  height: productImageSizes[size],
-                  display: 'block',
-                  borderRadius: 1,
-                }}
-                sizes={productImageSizes[size]}
-              />
-            )
-          }
-          title={
-            productLink ? (
-              <Link
-                href={`${productLink}?wishlistItemId=${wishlistItemId}`}
-                className={classes.itemName}
-                underline='hover'
-                sx={{
-                  color: 'inherit',
-                  flexWrap: 'nowrap',
-                  maxWidth: 'max-content',
-                }}
-              >
-                {product?.name}
-              </Link>
-            ) : (
-              product?.name
-            )
-          }
-          secondaryAction={
-            secondaryAction ||
-            ((product?.__typename === 'SimpleProduct' ||
-              product?.__typename === 'VirtualProduct') &&
-              product?.sku &&
-              product?.name && <AddWishlistItemToCart product={product} selectedOptions={[]} />)
-          }
-          price={<Money {...product?.price_range.minimum_price.final_price} />}
-          action={
-            <Button
-              variant='inline'
-              color='secondary'
-              size='medium'
-              type='submit'
-              onClick={
-                loggedIn
-                  ? () => removeWishlistItem({ variables: { wishlistItemId } })
-                  : () =>
-                      cache.writeQuery({
-                        query: GuestWishlistDocument,
-                        data: {
-                          customer: {
-                            wishlists: [
-                              {
-                                items_v2: {
-                                  items: filteredItems || [],
-                                },
-                              },
-                            ],
-                          },
-                        },
-                        broadcast: true,
-                      })
-              }
+    <AddProductsToCartForm key={wishlistItemId}>
+      <ActionCard
+        variant={variant}
+        value={wishlistItemId}
+        sx={[
+          (theme) => ({
+            '&.ActionCard-root': {
+              px: 0,
+              py: theme.spacings.xs,
+            },
+            '& .MuiBox-root': {
+              justifyContent: 'space-between',
+              alignItems: 'stretch',
+            },
+            '&.sizeSmall': {
+              px: 0,
+            },
+            '& .ActionCard-action': {
+              alignSelf: 'flex-end',
+            },
+            '& .ActionCard-image': {
+              alignSelf: 'flex-start',
+            },
+            '& .ActionCard-secondaryAction': {
+              typography: typographySizes[size],
+              display: 'flex',
+              alignItems: 'center',
+              color: 'text.secondary',
+              mt: 1,
+              gap: '10px',
+              justifyContent: 'start',
+            },
+            '& .ActionCard-price': {
+              typography: typographySizes[size],
+              mb: { xs: 0.5, sm: 0 },
+            },
+          }),
+          ...(Array.isArray(sx) ? sx : [sx]),
+        ]}
+        image={
+          product?.small_image?.url && (
+            <Image
+              className={classes.image}
+              layout='fill'
+              src={product?.small_image?.url}
+              sx={{
+                width: productImageSizes[size],
+                height: productImageSizes[size],
+                display: 'block',
+                borderRadius: 1,
+              }}
+              sizes={productImageSizes[size]}
+            />
+          )
+        }
+        title={
+          productLink ? (
+            <Link
+              href={`${productLink}?wishlistItemId=${wishlistItemId}`}
+              className={classes.itemName}
+              underline='hover'
+              sx={{
+                color: 'inherit',
+                flexWrap: 'nowrap',
+                maxWidth: 'max-content',
+              }}
             >
-              <Trans id='Remove' />
-            </Button>
-          }
-          size={size}
-          {...rest}
-        />
-      </AddProductsToCartForm>
-    )
+              {product?.name}
+            </Link>
+          ) : (
+            product?.name
+          )
+        }
+        secondaryAction={
+          secondaryAction ||
+          ((product?.__typename === 'SimpleProduct' || product?.__typename === 'VirtualProduct') &&
+            product?.sku &&
+            product?.name && <AddWishlistItemToCart product={product} selectedOptions={[]} />)
+        }
+        price={<Money {...product?.price_range.minimum_price.final_price} />}
+        action={
+          <Button
+            variant='inline'
+            color='secondary'
+            size='medium'
+            type='submit'
+            onClick={
+              loggedIn
+                ? () => removeWishlistItem({ variables: { wishlistItemId } })
+                : () =>
+                    cache.writeQuery({
+                      query: GuestWishlistDocument,
+                      data: {
+                        customer: {
+                          wishlists: [
+                            {
+                              items_v2: {
+                                items: filteredItems || [],
+                              },
+                            },
+                          ],
+                        },
+                      },
+                      broadcast: true,
+                    })
+            }
+          >
+            <Trans id='Remove' />
+          </Button>
+        }
+        size={size}
+        {...rest}
+      />
+    </AddProductsToCartForm>
   )
 }
