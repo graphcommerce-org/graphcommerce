b7a2b09a816365dc1d647e48cb0316b823a77c89
StefanAngenent <stefan@reachdigital.nl>
2023-11-27 12:18:31 +0100

    use ProductScroller

--- packages/magento-cart-items/components/CartItemCrosssells/CartItemCrosssells.tsx
+++ packages/magento-cart-items/components/CartItemCrosssells/CartItemCrosssells.tsx
@@ -1,6 +1,10 @@
 import { useCrosssellItems } from '@graphcommerce/magento-cart/components/CartAdded/useCrosssellItems'
-import { AddProductsToCartForm, ProductListItemRenderer } from '@graphcommerce/magento-product'
-import { responsiveVal, SidebarSlider, RenderType } from '@graphcommerce/next-ui'
+import {
+  AddProductsToCartForm,
+  ProductListItemRenderer,
+  ProductScroller,
+} from '@graphcommerce/magento-product'
+import { responsiveVal } from '@graphcommerce/next-ui'
 import { Trans } from '@lingui/react'
 import { Box, SxProps, Theme, Typography } from '@mui/material'
 
@@ -11,7 +15,6 @@ export type CartItemCrosssellsProps = {
 
 export function CartItemCrosssells(props: CartItemCrosssellsProps) {
   const { renderer, sx = [] } = props
-  // const sku = cart?.items?.[cart.items.length - 1]?.product?.sku
   const [addedItem, crossSellItems] = useCrosssellItems()
 
   const crossSellsHideCartItems = Boolean(import.meta.graphCommerce.crossSellsHideCartItems)
@@ -19,43 +22,26 @@ export function CartItemCrosssells(props: CartItemCrosssellsProps) {
 
   return (
     <Box sx={[(theme) => ({ mt: theme.spacings.sm }), ...(Array.isArray(sx) ? sx : [sx])]}>
-      <Typography variant='h6' sx={{ mb: '8px' }}>
+      <Typography variant='h6' sx={(theme) => ({ mb: theme.spacings.xxs })}>
         <Trans id='Complete your purchase' />
       </Typography>
 
       <AddProductsToCartForm redirect={false} disableSuccessSnackbar>
-        <SidebarSlider
-          buttonSize='responsiveMedium'
-          showButtons='auto'
+        <ProductScroller
+          productListRenderer={renderer}
+          items={crossSellItems}
           sx={(theme) => ({
-            marginBottom: 0,
+            mb: 0,
             '& .Scroller-root': {
-              gap: theme.spacings.xs,
-              paddingX: 0,
+              px: 0,
+              gap: theme.spacings.xxs,
               gridAutoColumns: responsiveVal(125, 175),
             },
             '& .ProductListItem-titleContainer': {
               gridTemplateAreas: `"title title" "subtitle price"`,
             },
-            // '& .ProductListItem-title, .ProductListItem-subtitle': {
-            //   textOverflow: 'ellipsis',
-            //   overflow: 'hidden',
-            //   whiteSpace: 'nowrap',
-            // },
           })}
-        >
-          {crossSellItems.map(
-            (item) =>
-              item && (
-                <RenderType
-                  key={item.uid ?? ''}
-                  renderer={renderer}
-                  titleComponent='h3'
-                  {...item}
-                />
-              ),
-          )}
-        </SidebarSlider>
+        />
       </AddProductsToCartForm>
     </Box>
   )
