9e68610cbabf66d7615eef8742e8d06357fb3386
Jesse van der Poel <jessevdpoel@hotmail.com>
2023-12-04 15:53:32 +0100

    Moved hydration to footerBase

--- examples/magento-graphcms/components/Layout/Footer.tsx
+++ examples/magento-graphcms/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 import { Image } from '@graphcommerce/image'
 import { StoreSwitcherButton } from '@graphcommerce/magento-store'
-import { Footer as FooterBase, LazyHydrate } from '@graphcommerce/next-ui'
+import { Footer as FooterBase } from '@graphcommerce/next-ui'
 import { Trans } from '@lingui/react'
 import { Button, IconButton, Link } from '@mui/material'
 import { FooterQueryFragment } from './FooterQueryFragment.gql'
@@ -11,44 +11,42 @@ export function Footer(props: FooterProps) {
   const { footer } = props
 
   return (
-    <LazyHydrate>
-      <FooterBase
-        socialLinks={footer?.socialLinks?.map((link) => (
-          <IconButton key={link.title} href={link.url} color='inherit' size='medium' edge='start'>
-            {link.asset ? (
-              <Image
-                layout='fill'
-                src={link.asset.url}
-                width={24}
-                height={24}
-                unoptimized
-                alt={link.title}
-                sx={(theme) => ({
-                  filter: theme.palette.mode === 'dark' ? 'invert(100%)' : 'invert(0%)',
-                })}
-              />
-            ) : (
-              link.title
-            )}
-          </IconButton>
-        ))}
-        storeSwitcher={<StoreSwitcherButton />}
-        customerService={
-          <Button href='/service' variant='pill'>
-            <Trans id='Customer Service' />
-          </Button>
-        }
-        copyright={
-          <>
-            <span>{footer?.copyright}</span>
-            {footer?.legalLinks?.map((link) => (
-              <Link key={link.title} href={link.url} color='textPrimary' underline='always'>
-                {link.title}
-              </Link>
-            ))}
-          </>
-        }
-      />
-    </LazyHydrate>
+    <FooterBase
+      socialLinks={footer?.socialLinks?.map((link) => (
+        <IconButton key={link.title} href={link.url} color='inherit' size='medium' edge='start'>
+          {link.asset ? (
+            <Image
+              layout='fill'
+              src={link.asset.url}
+              width={24}
+              height={24}
+              unoptimized
+              alt={link.title}
+              sx={(theme) => ({
+                filter: theme.palette.mode === 'dark' ? 'invert(100%)' : 'invert(0%)',
+              })}
+            />
+          ) : (
+            link.title
+          )}
+        </IconButton>
+      ))}
+      storeSwitcher={<StoreSwitcherButton />}
+      customerService={
+        <Button href='/service' variant='pill'>
+          <Trans id='Customer Service' />
+        </Button>
+      }
+      copyright={
+        <>
+          <span>{footer?.copyright}</span>
+          {footer?.legalLinks?.map((link) => (
+            <Link key={link.title} href={link.url} color='textPrimary' underline='always'>
+              {link.title}
+            </Link>
+          ))}
+        </>
+      }
+    />
   )
 }
