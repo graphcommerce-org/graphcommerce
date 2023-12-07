d8dd04c9c67fa7bd013248336c7ed4a6eae31b28
Joshua Bolk <joshuabolk@gmail.com>
2023-12-07 16:59:33 +0100

    [GCOM-1260] refactor RowColOne variants

--- examples/magento-graphcms/components/GraphCMS/RowColumnOne/variant/Default.tsx
+++ examples/magento-graphcms/components/GraphCMS/RowColumnOne/variant/Default.tsx
@@ -2,13 +2,13 @@ import { RichText } from '@graphcommerce/graphcms-ui'
 import { ColumnOne } from '@graphcommerce/next-ui'
 
 type RowColumnOneProps = {
+  __typename: string
   id: string
   rowColumnOneVariant?: 'Default' | 'Message' | null | undefined
   colOne: {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     raw: any
   }
-  __typename: string
 }
 
 export function Default(props: RowColumnOneProps) {
