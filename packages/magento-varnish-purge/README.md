36e7d0bb79e0732a1368f1032e8e51a78ec3674e
Hans Nieser <hans@reachdigital.nl>
2023-12-08 14:48:06 +0100

    Refactor into separate package, handle multiple locales

--- packages/magento-varnish-purge/README.md
+++ packages/magento-varnish-purge/README.md
@@ -1,21 +1,43 @@
 # Magento Varnish Purge
 
-This package contains the neccesary logic for some limited support for handling
-the PURGE requests that Magento sends when using varnish as the full page cache
+This package contains the neccesary logic for limited support for handling the
+PURGE requests that Magento sends when using varnish as the full page cache
 backend. We use these requests to invalidate statically generated content for
 product and category pages (see
 https://nextjs.org/docs/pages/building-your-application/data-fetching/incremental-static-regeneration#using-on-demand-revalidation)
 
-While this doesn't work entirely similar to Magento, where cache entries are
-saved along with the tags relevant for all the models that were involved with
-generating the content, (allowing very precise and fine-grained flushing of
-cache entries by directly referring to the tags), it does allow us to handle
-some common tag patterns for product and category pages, where we can parse the
-IDs from the tags, and map them to the product/category URLs, and revalidate
-those.
+While this doesn't work entirely similar to Magento, where cache tags are used
+for fine-grained flushing of cache entries, it does allow us to handle some
+common tag patterns for product and category pages, where we can parse the IDs
+from the tags, and map them to the product/category URLs, and revalidate those.
 
 Because Magento allows us to configure multiple cache hosts, this allows both
 the Varnish backend as well as the graphcommerce backend to be included as
 targets for sending PURGE requests. As Magento allows only a host + port number,
 we employ a middleware file to rewrite the PURGE request to the
 api/varnish-purge route.
+
+## Enabling handling of PURGE requests in your GraphCommerce project
+
+TODO, but in short:
+
+- Add middleware for rewriting PURGE requests to api route
+- Implement api route that invokes handlePurgeRequest for each store
+
+## Configuring Magento to send PURGE request to your GraphCommerce application
+
+Simply add your GraphCommerce hostname to the `http_cache_hosts` configuration
+in your `app/etc/env.php`:
+
+```php
+'http_cache_hosts' => [
+    [
+        'host' => '127.0.0.1', // Your varnish instance
+        'port' => '6081'
+    ],
+    [
+        'host' => 'myshop.example.com', // Your GraphCommerce application
+        'port' => '443'
+    ]
+],
+```
---
+++ packages/magento-varnish-purge/lib/handlePurgeRequest.ts
@@ -0,0 +1,132 @@
+/* eslint-disable no-console */
+/* eslint-disable no-cond-assign */
+import { NormalizedCacheObject, ApolloClient } from '@graphcommerce/graphql'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { PurgeGetProductPathDocument } from '../graphql/PurgeGetProductPath.gql'
+
+function parseTagsFromPatterns(tagsPatterns: string[]): string[] {
+  const fullTags = tagsPatterns.join('|')
+
+  // Tag matching patterns are concatenated into the following form:
+  //   ((^|,)TAG1(,|$))|((^|,)TAG2(,|$))|((^|,)TAG3(,|$))
+  // So we can match on )TAG1(.
+  const regex = /\)([a-zA-Z0-9_]+)\(/g
+
+  const matches = fullTags.matchAll(regex)
+  const parsedTags: string[] = []
+
+  for (const tag of matches) {
+    parsedTags.push(tag[1])
+  }
+
+  return parsedTags
+}
+
+async function invalidateProductById(
+  client: ApolloClient<NormalizedCacheObject>,
+  id: number,
+  res: NextApiResponse,
+) {
+  console.log(`varnish-purge: invalidating for product ${id}`)
+
+  const internalUrl = `catalog/product/view/id/${id}`
+
+  const routeData = await client.query({
+    query: PurgeGetProductPathDocument,
+    variables: {
+      internalUrl,
+    },
+  })
+
+  let urlPath = routeData.data.route?.relative_url
+  if (urlPath) {
+    // Construct product URL based on Magento path. We can safely (and must)
+    // strip off the .html suffix, as we already redirect to URLs without this
+    // suffix
+
+    // FIXME: We must build the correct URL for current locale, based on possible locale prefix/locale domain
+    urlPath = `/p/${urlPath}`
+    urlPath = urlPath.replace(/\.html$/, '')
+
+    console.log(`varnish-purge: invalidating URL ${urlPath}`)
+    try {
+      await res.revalidate(urlPath)
+    } catch (err) {
+      console.log(`varnish-purge: failed to revalidate ${urlPath}: ${err}`)
+    }
+  } else {
+    console.log(`varnish-purge: no URL found for product ID ${id}`)
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function invalidateCategoryById(
+  client: ApolloClient<NormalizedCacheObject>,
+  id: number,
+  res: NextApiResponse,
+) {
+  console.log(`varnish-purge: invalidating for category ${id}`)
+  // TODO
+}
+
+function invalidateByTags(
+  client: ApolloClient<NormalizedCacheObject>,
+  tagsPatterns: string | string[] | undefined,
+  res: NextApiResponse,
+) {
+  if (typeof tagsPatterns === 'undefined') {
+    return
+  }
+
+  const tags = parseTagsFromPatterns(
+    typeof tagsPatterns === 'string' ? [tagsPatterns] : tagsPatterns,
+  )
+
+  // Tags to support:
+  // cat_c_2 (category was changed)
+  // cat_c_p_2 (category/product relation was changed)
+  // cat_p_27130 (product was changed)
+
+  const categoryTagPattern = /^cat_c_([0-9]+)$/
+  const categoryRelationTagPattern = /^cat_c_p_([0-9]+)$/
+  const productTagPattern = /^cat_p_([0-9]+)$/
+
+  tags.forEach((tag) => {
+    let match: RegExpMatchArray | null
+
+    if ((match = tag.match(categoryTagPattern))) {
+      invalidateCategoryById(client, +match[1], res)
+    } else if ((match = tag.match(categoryRelationTagPattern))) {
+      invalidateCategoryById(client, +match[1], res)
+    } else if ((match = tag.match(productTagPattern))) {
+      void invalidateProductById(client, +match[1], res)
+    } else {
+      console.log(`varnish-purge: unsupported tag: ${tag}}`)
+    }
+  })
+}
+
+export function doFullPurge() {
+  // @todo
+}
+
+export function handlePurgeRequest(
+  client: ApolloClient<NormalizedCacheObject>,
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const rawTags = req.headers['x-magento-tags-pattern']
+
+  console.log(`varnish-purge: purge request from ${req.socket.remoteAddress}`)
+  console.log(`varnish-purge: purge requested for: ${rawTags}`)
+
+  if (rawTags === '.*') {
+    doFullPurge()
+  } else {
+    invalidateByTags(client, rawTags, res)
+  }
+
+  res.status(200)
+  res.send('OK')
+  res.end()
+}
