e5efb9d318358f700e8268cda7a670c6c863590c
Joshua Bolk <joshuabolk@gmail.com>
2023-12-11 15:46:06 +0100

    [GCOM-1260] create a JSON duplicate of current homepage as generic content

---
+++ examples/magento-graphcms/lib/generic-content/homepage.ts
@@ -0,0 +1,641 @@
+import { RowHeroBannerProps } from '../../components/GraphCMS/RowHeroBanner'
+import { RowLinksProps } from '../../components/GraphCMS/RowLinks'
+import { RowProductProps } from '../../components/GraphCMS/RowProduct'
+import { RowQuoteProps } from '../../components/GraphCMS/RowQuote'
+import { RowSpecialBannerProps } from '../../components/GraphCMS/RowSpecialBanner'
+
+const heroBanner: RowHeroBannerProps = {
+  __typename: 'RowHeroBanner',
+  id: 'cldx741qp7u0i0bun77oo5prx',
+  heroAsset: {
+    url: 'https://media.graphassets.com/zTUkCXlFTshLDzcp9Omg',
+    width: null,
+    height: null,
+    mimeType: 'video/mp4',
+    size: 1761151,
+    alt: 'Pexels tea',
+  },
+  copy: {
+    raw: {
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: 'Generic title',
+            },
+          ],
+        },
+        {
+          type: 'heading-one',
+          children: [
+            {
+              bold: true,
+              text: 'Discover',
+            },
+            {
+              text: ' the art collection.',
+            },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: '',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  pageLinks: [
+    {
+      title: 'Shop Art',
+      url: '/men/art',
+      description: null,
+      asset: null,
+    },
+  ],
+}
+
+const rowLinks1: RowLinksProps = {
+  __typename: 'RowLinks',
+  id: 'cley772kumkgl0bt9ldgx3cv8',
+  title: 'Hot & New',
+  rowLinksCopy: {
+    raw: {
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: '',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  linksVariant: 'ImageLabelSwiper',
+  pageLinks: [
+    {
+      id: 'cley575u0k8jg09tadqmjyn2c',
+      title: 'Bedazzle',
+      url: '/p/bedazzled-gc-112-sock',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: 'Nothing says boho-chic like a flowy swirl',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/osQkBcCbQxGkCVBa1Gvt',
+        width: 800,
+        height: 1200,
+        mimeType: 'image/jpeg',
+        size: 538666,
+      },
+    },
+    {
+      id: 'cley0f7mbeuvm0bulo0ph36uj',
+      title: 'Art',
+      url: '/men/art',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: 'The ultimate expressions of individuality',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/Ff1fxAUQg6ebTzGqmuwD',
+        width: 800,
+        height: 1200,
+        mimeType: 'image/jpeg',
+        size: 876600,
+      },
+    },
+    {
+      id: 'clf0z3znxrmzg0bupenfqiq2m',
+      title: 'Seamless service',
+      url: '/service',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: 'Prepare to get your socks knocked off',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/l0mmYMNSYyq7kLvJzDgg',
+        width: null,
+        height: null,
+        mimeType: 'video/mp4',
+        size: 348411,
+      },
+    },
+    {
+      id: 'cley68c1elhbx0buo4yp85x8c',
+      title: 'Black & White',
+      url: '/p/super-nummy-gc-74-sock',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: 'Celebrate the timeless beauty of monochrome fashion',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/1bNjsZkgQoiaowSa4OoS',
+        width: 800,
+        height: 1200,
+        mimeType: 'image/jpeg',
+        size: 291628,
+      },
+    },
+    {
+      id: 'cley69nn8lnit09ta5m7b1ey2',
+      title: "70's",
+      url: '/men/70s',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: 'Tie-dye your way to groovy fashion',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/inVAUZvQTdSjQK9heg9M',
+        width: 800,
+        height: 1200,
+        mimeType: 'image/jpeg',
+        size: 729068,
+      },
+    },
+    {
+      id: 'cley6b8vqkz6o0bte7snzrw09',
+      title: 'Special occasions',
+      url: '/p/wearing-my-pjs-gc-226-sock',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: 'Get ready to dazzle with impeccable taste',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/CTkHiA24S1uEcFSvvITK',
+        width: 800,
+        height: 1200,
+        mimeType: 'image/jpeg',
+        size: 412915,
+      },
+    },
+    {
+      id: 'cley8xcop00e20bt6ntrmiuhb',
+      title: 'Modest',
+      url: '/women/business',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: "Have a 'sole'-ful celebration",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/3YEkPr6QIx36AQgGBmsg',
+        width: 800,
+        height: 1200,
+        mimeType: 'image/jpeg',
+        size: 495180,
+      },
+    },
+  ],
+}
+
+const rowProduct1: RowProductProps = {
+  id: 'ckusavki80za30d05deomd7qn',
+  __typename: 'RowProduct',
+  variant: 'Grid',
+  identity: 'home-favorites',
+  asset: null,
+  title: 'Our Season favorites',
+  productCopy: {
+    raw: {
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: '',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  pageLinks: [
+    {
+      title: 'View favorites',
+      url: '/men/art',
+      description: null,
+      asset: null,
+    },
+  ],
+}
+
+const rowProduct2: RowProductProps = {
+  id: 'ckusb2pg81bqh0d09ezbhbf77',
+  __typename: 'RowProduct',
+  variant: 'Swipeable',
+  identity: 'home-swipable',
+  asset: null,
+  title: 'Power to the Women',
+  productCopy: null,
+  pageLinks: [],
+}
+
+const rowProduct3: RowProductProps = {
+  id: 'ckusapwsw10ep0d05g14ggkv6',
+  __typename: 'RowProduct',
+  variant: 'Backstory',
+  identity: 'home-product-backstory',
+  asset: {
+    url: 'https://media.graphassets.com/5g1PZkFcRwmuX0si62Yr',
+    width: 1192,
+    height: 1589,
+    mimeType: 'image/jpeg',
+    size: 1766866,
+  },
+  title: 'Home Product Backstory',
+  productCopy: {
+    raw: {
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: 'Once upon a time, in a world where art and fashion collided, there lived a small team of designers who dreamed of creating the most unique and intricate sock designs. They took inspiration from modern art and combined it with traditional patterns to create one-of-a-kind prints.',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  pageLinks: [],
+}
+
+const rowProduct4: RowProductProps = {
+  id: 'ckusapwsw10ep0d05g14ggkv6',
+  __typename: 'RowProduct',
+  variant: 'Backstory',
+  identity: 'home-product-backstory',
+  asset: {
+    url: 'https://media.graphassets.com/5g1PZkFcRwmuX0si62Yr',
+    width: 1192,
+    height: 1589,
+    mimeType: 'image/jpeg',
+    size: 1766866,
+  },
+  title: 'Home Product Backstory',
+  productCopy: {
+    raw: {
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: 'Once upon a time, in a world where art and fashion collided, there lived a small team of designers who dreamed of creating the most unique and intricate sock designs. They took inspiration from modern art and combined it with traditional patterns to create one-of-a-kind prints.',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  pageLinks: [],
+}
+
+const rowLinks2: RowLinksProps = {
+  id: 'cleyagh991cle0bujghsoszkl',
+  __typename: 'RowLinks',
+  title: 'Brands',
+  rowLinksCopy: {
+    raw: {
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: '',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  linksVariant: 'LogoSwiper',
+  pageLinks: [
+    {
+      id: 'cleyagf9e1okt09uoxxb3ilrw',
+      title: 'Moma',
+      url: '/',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: '',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/34q67ooES1K0PiGF10mc',
+        width: 209,
+        height: 53,
+        mimeType: 'image/svg+xml',
+        size: 777,
+      },
+    },
+    {
+      id: 'cleyahklf1pz909uopzt9cbda',
+      title: 'Nike',
+      url: '/',
+      description: {
+        raw: {
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: '',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      asset: {
+        url: 'https://media.graphassets.com/Y6vfJiw0S0aq04VGG4AI',
+        width: 1000,
+        height: 356,
+        mimeType: 'image/svg+xml',
+        size: 966,
+      },
+    },
+    {
+      id: 'cleyajm7n1g2d0bujj1anpa8m',
+      title: 'McDonalds',
+      url: '/',
+      description: null,
+      asset: {
+        url: 'https://media.graphassets.com/tVbVR01EQ2u2JsMyEXWz',
+        width: 296,
+        height: 238,
+        mimeType: 'image/svg+xml',
+        size: 1714,
+      },
+    },
+    {
+      id: 'clf0vc22vmco70bupz40i0bxd',
+      title: 'Marvel',
+      url: '/',
+      description: null,
+      asset: {
+        url: 'https://media.graphassets.com/3bQ33k13RvKZENhCpGJc',
+        width: 1001,
+        height: 403,
+        mimeType: 'image/svg+xml',
+        size: 3229,
+      },
+    },
+  ],
+}
+
+const rowLinks3: RowLinksProps = {
+  id: 'clf1648hhza3f0buqqg6grd6o',
+  __typename: 'RowLinks',
+  title: 'Customer service',
+  rowLinksCopy: null,
+  linksVariant: 'Inline',
+  pageLinks: [
+    {
+      id: 'ckv6jk7jszql80b49np6cptgy',
+      title: 'Order',
+      url: '/service/order',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6kbfhknt3h0c5946xpj83m',
+      title: 'Brand/Sizes',
+      url: '/service/brands-and-sizes',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6kbslshuri0c504fi41rur',
+      title: 'Newsletter',
+      url: '/service/newsletter',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6kc2mwidam0d57h3pq54e3',
+      title: 'Payment Information',
+      url: '/service/payment-information',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6kcacontqg0c598vcjg24w',
+      title: 'Returns',
+      url: '/service/returns',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6kciu807s40b49i15zw6rz',
+      title: 'Shipping',
+      url: '/service/shipping',
+      description: null,
+      asset: null,
+    },
+  ],
+}
+
+export const rowLinks4: RowLinksProps = {
+  id: 'clf1690q0zjkw0bup6ozkp8q6',
+  __typename: 'RowLinks',
+  title: 'About Us',
+  rowLinksCopy: null,
+  linksVariant: 'Inline',
+  pageLinks: [
+    {
+      id: 'ckv6jps60b3n10b069ne485fa',
+      title: 'Contact',
+      url: '#',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6jq954hhry0c50awn89wxr',
+      title: 'Careers',
+      url: '#',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6jqrnsb4710b06rrugxm2w',
+      title: 'About Us',
+      url: '#',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6jrd9khigq0c50ji1f41ax',
+      title: 'Loyalty program',
+      url: '#',
+      description: null,
+      asset: null,
+    },
+    {
+      id: 'ckv6js1yghit10c50oijzupia',
+      title: 'Corporate responsibility',
+      url: '#',
+      description: null,
+      asset: null,
+    },
+  ],
+}
+
+const rowSpecialBanner: RowSpecialBannerProps = {
+  id: 'ckiek99iw429w0a55hmnggxym',
+  __typename: 'RowSpecialBanner',
+  asset: {
+    url: 'https://media.graphassets.com/jD667rD9RkCoaaGhInnD',
+    width: 1532,
+    height: 1678,
+    mimeType: 'image/jpeg',
+    size: 1247616,
+  },
+  copy: {
+    raw: {
+      children: [
+        {
+          type: 'heading-two',
+          children: [
+            {
+              bold: true,
+              text: 'Impressionists',
+            },
+            {
+              text: ' and modern ',
+            },
+            {
+              bold: true,
+              text: 'masters',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  pageLinks: [
+    {
+      title: 'A complete collection',
+      url: '/men/art',
+      description: null,
+      asset: null,
+    },
+  ],
+  topic: 'A peek into history',
+}
+
+const rowQuote: RowQuoteProps = {
+  id: 'ckifwcfa80xs30a55gmcqz5hu',
+  __typename: 'RowQuote',
+  quote: {
+    raw: {
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: "Whether you're looking for something stylish for a night time party with friends or something professional for a conference call in the boardroom, GraphCommerce has you covered with socks that are both comfortable and versatile. So why settle for boring and plain socks when you can upgrade your footwear game?",
+            },
+          ],
+        },
+      ],
+    },
+  },
+}
+
+export const homepageContent = [
+  heroBanner,
+  rowLinks1,
+  rowProduct1,
+  rowSpecialBanner,
+  rowQuote,
+  rowProduct2,
+  rowLinks2,
+  rowProduct3,
+  rowProduct4,
+  rowLinks3,
+  rowLinks4,
+]
