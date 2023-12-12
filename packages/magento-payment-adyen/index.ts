d8a5f3a6b801dce46ced5719aa51e52becb09fe3
Joshua Bolk <joshuabolk@gmail.com>
2023-12-12 16:08:50 +0100

    [GCOM-1299] RM out of scope files

--- packages/magento-payment-adyen/index.ts
+++ packages/magento-payment-adyen/index.ts
@@ -2,12 +2,10 @@ import { PaymentModule } from '@graphcommerce/magento-cart-payment-method'
 import { AdyenPaymentActionCard } from './components/AdyenPaymentActionCard/AdyenPaymentActionCard'
 import { AdyenPaymentHandler } from './components/AdyenPaymentHandler/AdyenPaymentHandler'
 import { HppOptions } from './components/AdyenPaymentOptionsAndPlaceOrder/AdyenPaymentOptionsAndPlaceOrder'
-import { adyenHppExpandMethods } from './methods/adyen_hpp/adyenHppExpandMethods'
 
 export const adyen_hpp: PaymentModule = {
   PaymentOptions: HppOptions,
   PaymentPlaceOrder: () => null,
   PaymentHandler: AdyenPaymentHandler,
   PaymentActionCard: AdyenPaymentActionCard,
-  expandMethods: adyenHppExpandMethods,
 }
--- packages/magento-payment-adyen/plugins/AddAdyenMethods.tsx
+++ packages/magento-payment-adyen/plugins/AddAdyenMethods.tsx
@@ -1,7 +1,6 @@
 import { PaymentMethodContextProviderProps } from '@graphcommerce/magento-cart-payment-method'
 import type { PluginProps } from '@graphcommerce/next-config'
 import { adyen_cc } from '../methods/adyen_cc'
-import { adyen_hpp } from '../methods/adyen_hpp'
 
 export const component = 'PaymentMethodContextProvider'
 export const exported = '@graphcommerce/magento-cart-payment-method'
@@ -15,7 +14,6 @@ function AddAdyenMethods(props: PluginProps<PaymentMethodContextProviderProps>) 
       modules={{
         ...modules,
         adyen_cc,
-        adyen_hpp,
       }}
     />
   )
