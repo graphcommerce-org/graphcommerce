a13f9aa5718b7027393fc086fc39796e36ddf7d3
Joshua Bolk <joshuabolk@gmail.com>
2023-12-19 15:52:06 +0100

    [GCOM-1260] add todo

--- packages/hygraph-ui/plugins/test.ts
+++ packages/hygraph-ui/plugins/test.ts
@@ -7,6 +7,19 @@ export const exported = '@graphcommerce/graphcms-ui/lib'
 const test: MethodPlugin<typeof parseHygraph> = (prev) => {
   console.log('alalala')
 
+  /**
+   * Todo:
+   * - hook into next-ui package or examples folder
+   * - make hygraph ggl files for rowColOne, rowLinks, rowQuote under components in this folder.
+   * - inject them into the RowRenderer query
+   *
+   * - if hygraph-ui is installed:
+   *  - parse query outcomes with parser for Hygraph
+   *
+   * - else
+   *  - use default JSON inputs
+   *
+   **/
   return prev
 }
 
