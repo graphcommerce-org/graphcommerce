ccda77b1c47567098019e3b170d014a1eb894d6c
Joshua Bolk <joshuabolk@gmail.com>
2023-12-29 12:11:47 +0100

    [GCOM-1260] remove Asset, RichText from hygraph-ui

--- packages/hygraph-ui/components/index.ts
+++ packages/hygraph-ui/components/index.ts
@@ -1,5 +1,3 @@
-export * from './RichText'
-export * from './Asset/Asset'
 export * from './RowColumnOne/RowColumnOne.gql'
 export * from './RowLinks/RowLinks.gql'
 export * from './RowQuote/RowQuote.gql'
