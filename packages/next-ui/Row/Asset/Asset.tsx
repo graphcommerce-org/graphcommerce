8919dcf9b3efc360b682276ade3da38a410a75e0
Joshua Bolk <joshuabolk@gmail.com>
2023-12-29 12:14:09 +0100

    [GCOM-1260] move asset to next-ui

--- packages/next-ui/Row/Asset/Asset.tsx
+++ packages/next-ui/Row/Asset/Asset.tsx
@@ -1,24 +1,22 @@
 import { Image, ImageProps } from '@graphcommerce/image'
 import { styled, SxProps, Theme } from '@mui/material'
-import { AssetFragment } from './Asset.gql'
+import { AssetProps } from './type'
 
-export type { AssetFragment } from './Asset.gql'
-
-type ImageAsset = Omit<AssetFragment, 'width' | 'height'> & {
+type ImageAsset = Omit<AssetProps, 'width' | 'height'> & {
   width: number
   height: number
 }
 
-function isImage(asset: AssetFragment): asset is ImageAsset {
+function isImage(asset: AssetProps): asset is ImageAsset {
   return !!(asset.width && asset.height)
 }
 
-type AssetProps = {
-  asset: AssetFragment
-  sx?: SxProps<Theme>
-} & Omit<ImageProps, 'src' | 'width' | 'height' | 'alt' | 'sx'>
-
-export function Asset(props: AssetProps) {
+export function Asset(
+  props: { asset: AssetProps; sx?: SxProps<Theme> } & Omit<
+    ImageProps,
+    'src' | 'width' | 'height' | 'alt' | 'sx'
+  >,
+) {
   const { asset, sx = [], ...imgProps } = props
 
   if (isImage(asset)) {
---
+++ packages/next-ui/Row/Asset/type.ts
@@ -0,0 +1,8 @@
+export type AssetProps = {
+  url: string
+  width?: number | null
+  height?: number | null
+  mimeType?: string | null
+  size?: number | null
+  alt?: string | null
+}
