367ce9cd8f6f037bff3c973c3caf7eb24fb0634e
Paul Hachmang <paul@reachdigital.nl>
2024-01-05 14:11:49 +0100

    Simplify LazyHydrate

--- packages/next-ui/LazyHydrate/LazyHydrate.tsx
+++ packages/next-ui/LazyHydrate/LazyHydrate.tsx
@@ -1,21 +1,47 @@
-import React, { useState, useRef, useLayoutEffect, startTransition } from 'react'
+import React, { useState, useRef, startTransition, useLayoutEffect, useEffect } from 'react'
 
-function LazyHydrateClient(props: LazyHydrateProps) {
+// Make sure the server doesn't choke on the useLayoutEffect
+export const useLayoutEffect2 = typeof window !== 'undefined' ? useLayoutEffect : useEffect
+
+export type LazyHydrateProps = {
+  /**
+   * The content is always rendered on the server and on the client it uses the server rendered HTML until it is hydrated.
+   */
+  children: React.ReactNode
+
+  /**
+   * When a boolean is provided, the IntersectionObserver is disabled and hydrates the component when the value becomes true.
+   *
+   * For example:
+   * - Disable the hydration functionality completely: `<LazyHydrate hydrated={true}>`
+   * - Hydrate the component on some state `<LazyHydrate hydrated={someState}>` where someState initially is false and later becomes true.
+   */
+  hydrated?: boolean
+}
+
+/**
+ * LazyHydrate can defer the hydration of a component until it becomes visible.
+ * OR manually by using the hydrated prop.
+ * This can be a way to improve the TBT of a page.
+ */
+export function LazyHydrate(props: LazyHydrateProps) {
   const { hydrated, children } = props
   const rootRef = useRef<HTMLElement>(null)
 
   const [isHydrated, setIsHydrated] = useState(hydrated || false)
   if (!isHydrated && hydrated) setIsHydrated(true)
 
-  useLayoutEffect(() => {
+  useLayoutEffect2(() => {
     // If we are manually hydrating, we watch that value and do not use the IntersectionObserver
     if (isHydrated || !rootRef.current) return undefined
-    const wasRenderedServerSide = rootRef.current?.hasAttribute('data-lazy-hydrate')
-    if (!wasRenderedServerSide) {
+
+    // If the element wasn't rendered on the server, we hydrate it immediately
+    if (!rootRef.current?.hasAttribute('data-lazy-hydrate')) {
       setIsHydrated(true)
       return undefined
     }
 
+    // The user has opted to manually hydrate the component
     if (hydrated !== undefined) return undefined
 
     const observer = new IntersectionObserver(
@@ -31,48 +57,16 @@ function LazyHydrateClient(props: LazyHydrateProps) {
     return () => observer.disconnect()
   }, [hydrated, isHydrated])
 
-  return !isHydrated ? (
-    <section
-      ref={rootRef}
+  if (typeof window === 'undefined') {
+    return <section data-lazy-hydrate>{children}</section>
+  }
+
+  if (!isHydrated) {
+    return (
       // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: '' }}
-      suppressHydrationWarning
-    />
-  ) : (
-    <section suppressHydrationWarning>{children}</section>
-  )
-}
+      <section ref={rootRef} dangerouslySetInnerHTML={{ __html: '' }} suppressHydrationWarning />
+    )
+  }
 
-function LazyHydrateServer(props: { children: React.ReactNode }) {
-  const { children } = props
-  return <section data-lazy-hydrate>{children}</section>
-}
-
-export type LazyHydrateProps = {
-  /**
-   * The content is always rendered on the server. And on the client it keeps reference to the server state until the component is hydrated.
-   */
-  children: React.ReactNode
-
-  /**
-   * When a boolean is provided, the IntersectionObserver is disabled and hydrates the component when the value becomes true.
-   *
-   * For example:
-   * - To Skip hydration altogether (when rendering a list of items and you want to skip the hydration for some of them): `<LazyHydrate hydrated={false}>`
-   */
-  hydrated?: boolean
-}
-
-/**
- * The LazyHydrate component is used to delay the hydration of a component until it is visible in the viewport OR until the hydrated prop becomes true.
- *
- * This improved the TBT of the page.
- */
-export function LazyHydrate(props: LazyHydrateProps) {
-  const { children, hydrated } = props
-  return typeof window === 'undefined' ? (
-    <LazyHydrateServer>{children}</LazyHydrateServer>
-  ) : (
-    <LazyHydrateClient hydrated={hydrated}>{children}</LazyHydrateClient>
-  )
+  return <section suppressHydrationWarning>{children}</section>
 }
