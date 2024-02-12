d4377227b7283985b25bb1f6f0ba38374971f8eb
Paul Hachmang <paul@reachdigital.nl>
2024-02-12 12:00:04 +0100

    Working on PayPal

---
+++ packages/magento-payment-braintree/hooks/useBraintreePaypal.ts
@@ -0,0 +1,39 @@
+import braintree, { PayPalCheckout } from 'braintree-web'
+import { useEffect, useState } from 'react'
+import { useBraintreeClient } from './useBraintree'
+
+let teardownPromise: Promise<void> | undefined | void
+
+export function useBraintreePaypal() {
+  const braintreePromise = useBraintreeClient()
+  const [paypalCheckout, setPaypalCheckout] = useState<PayPalCheckout | undefined>(undefined)
+
+  useEffect(() => {
+    if (!paypalCheckout && !teardownPromise) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      braintreePromise.then(async (client) => {
+        if (teardownPromise) {
+          await teardownPromise
+          teardownPromise = undefined
+        }
+
+        const checkout = await braintree.paypalCheckout.create({ client })
+
+        const loaded = await checkout.loadPayPalSDK({
+          debug: true,
+          currency: 'EUR',
+          locale: 'en-US',
+          intent: 'capture',
+          vault: false,
+        })
+        setPaypalCheckout(loaded)
+      })
+    }
+
+    return () => {
+      teardownPromise = paypalCheckout?.teardown()
+    }
+  }, [braintreePromise, paypalCheckout])
+
+  return teardownPromise ? undefined : paypalCheckout
+}
