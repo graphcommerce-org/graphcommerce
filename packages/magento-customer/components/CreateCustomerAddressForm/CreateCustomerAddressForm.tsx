6b61a93476505e66a3135844b9bce62ccdd1bdcf
Paul Hachmang <paul@reachdigital.nl>
2024-02-20 14:34:07 +0100

    Solve an issue where the customer could not save their region in the customer account section.

--- packages/magento-customer/components/CreateCustomerAddressForm/CreateCustomerAddressForm.tsx
+++ packages/magento-customer/components/CreateCustomerAddressForm/CreateCustomerAddressForm.tsx
@@ -38,7 +38,7 @@ export function CreateCustomerAddressForm() {
       onBeforeSubmit: (formData) => {
         const region = countries
           ?.find((country) => country?.two_letter_abbreviation === formData.countryCode)
-          ?.available_regions?.find((r) => r?.id === formData.region)
+          ?.available_regions?.find((r) => r?.id === formData.region.region_id)
 
         return {
           ...formData,
@@ -68,7 +68,7 @@ export function CreateCustomerAddressForm() {
     <>
       <Form onSubmit={submitHandler} noValidate>
         <NameFields form={form} prefix />
-        <AddressFields form={form} />
+        <AddressFields form={form} regionId='region.region_id' />
 
         <FormRow>
           <TextField
