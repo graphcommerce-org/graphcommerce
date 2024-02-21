04b79ed5027c1f4a7cfd1950e8e32e81fd2ffc0c
Paul Hachmang <paul@reachdigital.nl>
2024-02-21 11:28:42 +0100

    Cleanup CategoryChildren, simpler url check

--- packages/magento-category/components/CategoryChildren/CategoryChildren.tsx
+++ packages/magento-category/components/CategoryChildren/CategoryChildren.tsx
@@ -1,10 +1,8 @@
 import { Scroller, ScrollerProvider } from '@graphcommerce/framer-scroller'
-import { ProductListParams } from '@graphcommerce/magento-product'
-import { productListLink } from '@graphcommerce/magento-product/hooks/useProductListLink'
-import { extendableComponent } from '@graphcommerce/next-ui'
+import { ProductListParams, productListLink } from '@graphcommerce/magento-product'
+import { extendableComponent, filterNonNullableKeys } from '@graphcommerce/next-ui'
 import { Box, Link, SxProps, Theme } from '@mui/material'
 import { CategoryChildrenFragment } from './CategoryChildren.gql'
-import { useRouter } from 'next/router'
 
 type CategoryChildrenProps = Omit<CategoryChildrenFragment, 'uid'> & {
   params: ProductListParams
@@ -18,17 +16,22 @@ const { classes } = extendableComponent(name, parts)
 export function CategoryChildren(props: CategoryChildrenProps) {
   const { children, params, sx = [] } = props
 
-  const { currentPage, ...paramsWithoutCurrentPage } = params
-
-  const router = useRouter()
-
-  if (
-    !children ||
-    children.length === 0 ||
-    (children?.[0]?.uid === paramsWithoutCurrentPage.filters.category_uid?.in?.[0] &&
-      children.length === 1)
+  const childItems = filterNonNullableKeys(children, ['url_path', 'name', 'include_in_menu']).map(
+    (category) => ({
+      name: category.name,
+      href: productListLink({
+        ...params,
+        currentPage: 0,
+        url: category.url_path,
+        filters: { category_uid: { eq: category.uid } },
+      }),
+      active: params.url === category.url_path,
+    }),
   )
-    return null
+
+  const hasNavigatableChildren = childItems.some((cat) => !cat.active)
+  if (!hasNavigatableChildren) return null
+
   return (
     <ScrollerProvider scrollSnapAlign='none'>
       <Box
@@ -47,53 +50,45 @@ export function CategoryChildren(props: CategoryChildrenProps) {
           hideScrollbar
           sx={{ gridAutoColumns: `max-content` }}
         >
-          {children.map((cat) => {
-            if (!cat?.url_path || !cat.name || !cat.include_in_menu) return null
-
-            return (
-              <Link
-                key={cat.url_path}
-                underline='none'
-                color='inherit'
-                href={productListLink({
-                  ...paramsWithoutCurrentPage,
-                  url: cat.url_path,
-                  filters: { category_uid: { eq: cat.uid } },
-                })}
-                className={classes.link}
-                sx={(theme) => ({
-                  whiteSpace: 'nowrap',
-                  display: 'block',
-                  marginRight: `${theme.spacings.xxs}`,
-                  marginLeft: `${theme.spacings.xxs}`,
-                  typography: 'h6',
-                  position: 'relative',
-                  paddingBottom: '8px',
+          {childItems.map((category) => (
+            <Link
+              key={category.href}
+              underline='none'
+              color='inherit'
+              href={category.href}
+              className={classes.link}
+              sx={(theme) => ({
+                whiteSpace: 'nowrap',
+                display: 'block',
+                marginRight: theme.spacings.xxs,
+                marginLeft: theme.spacings.xxs,
+                typography: 'h6',
+                position: 'relative',
+                paddingBottom: '8px',
+                '&:before': {
+                  content: '""',
+                  width: 40,
+                  height: 2,
+                  background: theme.palette.primary.main,
+                  position: 'absolute',
+                  left: 0,
+                  right: 0,
+                  margin: '0 auto',
+                  opacity: category.active ? 1 : 0,
+                  transition: 'opacity .2s ease, bottom .2s ease',
+                  bottom: category.active ? 5 : 0,
+                },
+                '&:hover': {
                   '&:before': {
-                    content: '""',
-                    width: 40,
-                    height: 2,
-                    background: theme.palette.primary.main,
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    margin: '0 auto',
-                    opacity: cat.url_path && router.asPath.includes(cat.url_path) ? 1 : 0,
-                    transition: 'opacity .2s ease, bottom .2s ease',
-                    bottom: cat.url_path && router.asPath.includes(cat.url_path) ? 5 : 0,
+                    opacity: 1,
+                    bottom: 5,
                   },
-                  '&:hover': {
-                    '&:before': {
-                      opacity: 1,
-                      bottom: 5,
-                    },
-                  },
-                })}
-              >
-                {cat.name}
-              </Link>
-            )
-          })}
+                },
+              })}
+            >
+              {category.name}
+            </Link>
+          ))}
         </Scroller>
       </Box>
     </ScrollerProvider>
