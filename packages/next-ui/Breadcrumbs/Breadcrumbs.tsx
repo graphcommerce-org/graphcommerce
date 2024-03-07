4369cad89f3d771fed5c28fdf17cbaf773913389
Laurens Franken <laurens@reachdigital.nl>
2024-03-07 16:55:11 +0100

    feat(GCOM-1347): add breadcrumbs json-ld to normal breadcrumbs

--- packages/next-ui/Breadcrumbs/Breadcrumbs.tsx
+++ packages/next-ui/Breadcrumbs/Breadcrumbs.tsx
@@ -1,19 +1,32 @@
 import { Trans } from '@lingui/react'
 import { Breadcrumbs as BreadcrumbsBase, Link, Typography } from '@mui/material'
+import { BreadcrumbsJsonLd } from './BreadcrumbsJsonLd'
 import { BreadcrumbsProps } from './BreadcrumbsType'
+import { jsonLdBreadcrumb } from './jsonLdBreadcrumb'
 
 export function Breadcrumbs(props: BreadcrumbsProps) {
-  const { breadcrumbs, name, ...breadcrumbsProps } = props
+  const { breadcrumbs, ...breadcrumbsProps } = props
 
   return (
-    <BreadcrumbsBase {...breadcrumbsProps}>
-      <Link href='/' underline='hover' color='inherit'>
-        <Trans id='Home' />
-      </Link>
-      {breadcrumbs.slice(0, breadcrumbs.length - 1).map((breadcrumb) => (
-        <Link {...breadcrumb} />
-      ))}
-      <Typography color='text.primary'>{breadcrumbs[breadcrumbs.length - 1].children}</Typography>
-    </BreadcrumbsBase>
+    <>
+      {breadcrumbs.length && (
+        <BreadcrumbsJsonLd
+          breadcrumbs={breadcrumbs}
+          render={(bc) => ({
+            '@context': 'https://schema.org',
+            ...jsonLdBreadcrumb(bc),
+          })}
+        />
+      )}
+      <BreadcrumbsBase {...breadcrumbsProps}>
+        <Link href='/' underline='hover' color='inherit'>
+          <Trans id='Home' />
+        </Link>
+        {breadcrumbs.slice(0, breadcrumbs.length - 1).map((breadcrumb) => (
+          <Link {...breadcrumb} />
+        ))}
+        <Typography color='text.primary'>{breadcrumbs[breadcrumbs.length - 1].children}</Typography>
+      </BreadcrumbsBase>
+    </>
   )
 }
