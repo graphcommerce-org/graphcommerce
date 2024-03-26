ed217c50c16b590887eee8d3680b43b8a0cc8caa
Paul Hachmang <paul@reachdigital.nl>
2024-03-26 13:24:21 +0100

    Set default format to GA4. Track ‘wrong’ pageview event on routeChangeComplete. Make sure we use gtag/datalayer correctly

--- packages/google-datalayer/lib/event.ts
+++ packages/google-datalayer/lib/event.ts
@@ -1,7 +1,7 @@
 import { EventFormatSchema } from '@graphcommerce/next-config'
 
 type EventMapFunctionType = (
-  eventName: string,
+  eventName: Gtag.EventNames | (string & Record<never, never>),
   eventData: {
     [key: string]: unknown
   },
@@ -10,12 +10,30 @@ type EventMapFunctionType = (
 type EventType = keyof (typeof EventFormatSchema)['Enum']
 
 const eventMap: { [key in EventType]: EventMapFunctionType } = {
-  GA4: (eventName, eventData) => globalThis.gtag('event', eventName, eventData),
-  GA3: (eventName, eventData) =>
-    globalThis.dataLayer.push({ event: eventName, ecommerce: eventData }),
+  GA4: (eventName, eventData) => {
+    if (import.meta.graphCommerce.googleAnalyticsId) {
+      globalThis.gtag('event', eventName, eventData)
+    }
+
+    if (import.meta.graphCommerce.googleTagmanagerId) {
+      globalThis.dataLayer.push({ event: eventName, ...eventData })
+    }
+  },
+  GA3: (eventName, eventData) => {
+    if (import.meta.graphCommerce.googleAnalyticsId) {
+      console.warn(
+        "Google Analytics 3 format is not supported for Google Analytics 4. Please update your event format to 'GA4'.",
+      )
+    }
+
+    if (import.meta.graphCommerce.googleTagmanagerId) {
+      globalThis.dataLayer.push({ event: eventName, ecommerce: eventData })
+    }
+  },
 }
 
+const eventsToBeFired = import.meta.graphCommerce.analytics?.eventFormat ?? ['GA4']
+
 export const event: EventMapFunctionType = (eventName, eventData) => {
-  const eventsToBeFired = import.meta.graphCommerce.analytics?.eventFormat
-  if (eventsToBeFired) eventsToBeFired.map((e) => eventMap[e](eventName, eventData))
+  eventsToBeFired.map((e) => eventMap[e](eventName, eventData))
 }
---
+++ packages/google-datalayer/plugins/AllPagesPageview.tsx
@@ -0,0 +1,36 @@
+import type { PagesProps } from '@graphcommerce/framer-next-pages'
+import type { IfConfig, PluginProps } from '@graphcommerce/next-config'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { event } from '../lib/event'
+
+export const component = 'FramerNextPages'
+export const exported = '@graphcommerce/framer-next-pages'
+export const ifConfig: IfConfig = 'analytics'
+
+function AllPagesPageview(props: PluginProps<PagesProps>) {
+  const { Prev, ...rest } = props
+
+  const { events } = useRouter()
+
+  useEffect(() => {
+    const onRouteChangeComplete = (url: string) => {
+      /**
+       * Todo: the actual page_view event is currently disabled, because we run the risk of double counting page views.
+       * https://developers.google.com/analytics/devguides/collection/ga4/views?client_type=gtag#manually_send_page_view_events
+       */
+      // event('page_view', {
+      //   page_title: '<Page Title>',
+      //   page_location: '<Page Location>',
+      // })
+      event('pageview', { page: url })
+    }
+
+    events.on('routeChangeComplete', onRouteChangeComplete)
+    return () => events.off('routeChangeComplete', onRouteChangeComplete)
+  }, [events])
+
+  return <Prev {...rest} />
+}
+
+export const Plugin = AllPagesPageview
