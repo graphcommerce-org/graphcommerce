5146f5de5c64cc899b16005710b48dd6eb4e8719
Paul Hachmang <paul@reachdigital.nl>
2024-03-26 14:31:11 +0100

    Fix UpdateItemQuantity

--- packages/google-datalayer/plugins/UpdateItemQuantity.tsx
+++ packages/google-datalayer/plugins/UpdateItemQuantity.tsx
@@ -11,49 +11,52 @@ export const exported =
  * event
  */
 function UpdateItemQuantity(props: PluginProps<UpdateItemQuantityProps>) {
-  const { Prev, onComplete, quantity, ...rest } = props
+  const { Prev, formOptions, quantity, ...rest } = props
 
   return (
     <Prev
       {...rest}
       quantity={quantity}
-      onComplete={(data, variables) => {
-        const original = onComplete?.(data, variables)
-        const diffQuantity = variables.quantity - quantity
-        if (diffQuantity === 0) return original
+      formOptions={{
+        ...formOptions,
+        onComplete: (data, variables) => {
+          const original = formOptions?.onComplete?.(data, variables)
+          const diffQuantity = variables.quantity - quantity
+          if (diffQuantity === 0) return original
 
-        const itemId = variables.uid
-        const addedItem = data.data?.updateCartItems?.cart.items?.find(
-          (item) => item?.uid === itemId,
-        )
+          const itemId = variables.uid
+          const addedItem = data.data?.updateCartItems?.cart.items?.find(
+            (item) => item?.uid === itemId,
+          )
 
-        if (addedItem && addedItem.prices && addedItem.prices.row_total_including_tax.value) {
-          // we need to manually calculate pricePerItemInclTax (https://github.com/magento/magento2/issues/33848)
-          const pricePerItemInclTax =
-            addedItem.prices.row_total_including_tax.value / addedItem.quantity
-          const addToCartValue = pricePerItemInclTax * diffQuantity
+          if (addedItem && addedItem.prices && addedItem.prices.row_total_including_tax.value) {
+            // we need to manually calculate pricePerItemInclTax (https://github.com/magento/magento2/issues/33848)
+            const pricePerItemInclTax =
+              addedItem.prices.row_total_including_tax.value / addedItem.quantity
+            const addToCartValue = pricePerItemInclTax * diffQuantity
 
-          event('add_to_cart', {
-            currency: addedItem?.prices?.price.currency,
-            value: addToCartValue,
-            items: [
-              {
-                item_id: addedItem?.product.sku,
-                item_name: addedItem?.product.name,
-                currency: addedItem?.prices?.price.currency,
-                price: pricePerItemInclTax,
-                quantity: variables.quantity,
-                discount: addedItem?.prices?.discounts?.reduce(
-                  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-                  (sum, discount) => sum + (discount?.amount?.value ?? 0),
-                  0,
-                ),
-              },
-            ],
-          })
-        }
+            event('add_to_cart', {
+              currency: addedItem?.prices?.price.currency,
+              value: addToCartValue,
+              items: [
+                {
+                  item_id: addedItem?.product.sku,
+                  item_name: addedItem?.product.name,
+                  currency: addedItem?.prices?.price.currency,
+                  price: pricePerItemInclTax,
+                  quantity: variables.quantity,
+                  discount: addedItem?.prices?.discounts?.reduce(
+                    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+                    (sum, discount) => sum + (discount?.amount?.value ?? 0),
+                    0,
+                  ),
+                },
+              ],
+            })
+          }
 
-        return original
+          return original
+        },
       }}
     />
   )
