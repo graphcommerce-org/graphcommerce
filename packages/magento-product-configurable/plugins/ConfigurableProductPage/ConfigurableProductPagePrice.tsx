a95b0ec11974505f585043003545a6c1e2cdd457
Jesse van der Poel <jessevdpoel@hotmail.com>
2024-04-11 09:58:16 +0200

    Added options of main product to the configurable variant

--- packages/magento-product-configurable/plugins/ConfigurableProductPage/ConfigurableProductPagePrice.tsx
+++ packages/magento-product-configurable/plugins/ConfigurableProductPage/ConfigurableProductPagePrice.tsx
@@ -1,4 +1,8 @@
-import { AddToCartItemSelector, type ProductPagePrice } from '@graphcommerce/magento-product'
+import {
+  AddToCartItemSelector,
+  ProductPagePriceFragment,
+  type ProductPagePrice,
+} from '@graphcommerce/magento-product'
 import type { ReactPlugin } from '@graphcommerce/next-config'
 import { useConfigurableSelectedVariant } from '../../hooks'
 
@@ -12,7 +16,12 @@ const ConfigurableProductPagePrice: PluginType = (props) => {
   const { Prev, product, index, ...rest } = props
   const variant = useConfigurableSelectedVariant({ url_key: product.url_key, index })
 
-  return <Prev product={variant ?? product} index={index} {...rest} />
+  if (product.__typename === 'GroupedProduct') return <Prev {...props} />
+  const finalProduct: ProductPagePriceFragment = variant
+    ? { ...variant, options: product.options }
+    : product
+
+  return <Prev product={finalProduct} index={index} {...rest} />
 }
 
 export const Plugin = ConfigurableProductPagePrice
