fc86886db97be905c19c4cb62320c06f165e91d0
Jesse van der Poel <jessevdpoel@hotmail.com>
2024-04-15 16:56:00 +0200

    Added pagination to AllHygraphPages

---
+++ packages/hygraph-ui/lib/GetAllHygraphPages.ts
@@ -0,0 +1,41 @@
+import { ApolloClient, NormalizedCacheObject, ApolloQueryResult } from '@apollo/client'
+import { HygraphAllPagesDocument, HygraphAllPagesQuery } from '@graphcommerce/graphcms-ui'
+
+type Urls = { url: string }
+
+export async function getAllHygraphPages(
+  client: ApolloClient<NormalizedCacheObject>,
+  options: { pageSize?: number } = {},
+) {
+  const { pageSize = 100 } = options
+  const query = client.query({
+    query: HygraphAllPagesDocument,
+    variables: { first: pageSize },
+    fetchPolicy: process.env.NODE_ENV !== 'development' ? 'cache-first' : undefined,
+  })
+  const pages: Promise<ApolloQueryResult<HygraphAllPagesQuery>>[] = [query]
+
+  const { data } = await query
+  const totalPages = Math.ceil(data.pagesConnection.aggregate.count / pageSize) ?? 1
+
+  if (totalPages > 1) {
+    for (let i = 2; i <= totalPages; i++) {
+      pages.push(
+        client.query({
+          query: HygraphAllPagesDocument,
+          variables: { first: pageSize, skip: pageSize * (i - 1) },
+          fetchPolicy: process.env.NODE_ENV !== 'development' ? 'cache-first' : undefined,
+        }),
+      )
+    }
+  }
+
+  const paths: Urls[] = (await Promise.all(pages))
+    .map((q) => q.data.pages)
+    .flat(1)
+    .map((page) => ({
+      url: page.url,
+    }))
+
+  return paths
+}
