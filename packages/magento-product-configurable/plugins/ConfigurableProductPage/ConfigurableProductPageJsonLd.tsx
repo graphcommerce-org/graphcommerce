c99d71d90073723628af94cdc1dd96d486588ff9
Paul Hachmang <paul@reachdigital.nl>
2024-04-22 21:19:42 +0200

    Fix build

--- packages/magento-product-configurable/plugins/ConfigurableProductPage/ConfigurableProductPageJsonLd.tsx
+++ packages/magento-product-configurable/plugins/ConfigurableProductPage/ConfigurableProductPageJsonLd.tsx
@@ -1,4 +1,8 @@
-import type { AddToCartItemSelector, ProductPageJsonLdProps } from '@graphcommerce/magento-product'
+import type {
+  AddToCartItemSelector,
+  JsonLdProductFragment,
+  ProductPageJsonLdProps,
+} from '@graphcommerce/magento-product'
 import type { IfConfig, PluginProps } from '@graphcommerce/next-config'
 import { useConfigurableSelectedVariant } from '../../hooks'
 
@@ -6,12 +10,15 @@ export const component = 'ProductPageJsonLd'
 export const exported = '@graphcommerce/magento-product'
 export const ifConfig: IfConfig = 'configurableVariantValues.content'
 
-const ConfigurableProductPageJsonLd = <T extends { '@type': string }>(
-  props: PluginProps<ProductPageJsonLdProps<T>> & AddToCartItemSelector,
+const ConfigurableProductPageJsonLd = <
+  T extends { '@type': string },
+  P extends JsonLdProductFragment,
+>(
+  props: PluginProps<ProductPageJsonLdProps<T, P>> & AddToCartItemSelector,
 ) => {
   const { Prev, product, index, ...rest } = props
   const variant = useConfigurableSelectedVariant({ url_key: product.url_key, index })
-  return <Prev product={variant ?? product} {...rest} />
+  return <Prev product={(variant ?? product) as P} {...rest} />
 }
 
 export const Plugin = ConfigurableProductPageJsonLd
