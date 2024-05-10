aa3505ac22b92f1ca14a0fcc7911786aa49b899c
Jesse van der Poel <jessevdpoel@hotmail.com>
2024-05-10 14:46:35 +0200

    Added sign in button to AddProductsToCartButton

--- packages/magento-product/components/AddProductsToCart/AddProductsToCartButton.tsx
+++ packages/magento-product/components/AddProductsToCart/AddProductsToCartButton.tsx
@@ -1,5 +1,7 @@
-import { Button, ButtonProps } from '@graphcommerce/next-ui'
+import { useCustomerSession } from '@graphcommerce/magento-customer'
+import { Button, ButtonProps, useStorefrontConfig } from '@graphcommerce/next-ui'
 import { Trans } from '@lingui/react'
+import { useRouter } from 'next/router'
 import {
   useAddProductsToCartAction,
   UseAddProductsToCartActionProps,
@@ -22,8 +24,23 @@ export type AddProductsToCartButtonProps = UseAddProductsToCartActionProps &
 export function AddProductsToCartButton(props: AddProductsToCartButtonProps) {
   const { children, product, ...rest } = props
   const { showSuccess, ...action } = useAddProductsToCartAction(props)
+  const router = useRouter()
+  const { loggedIn } = useCustomerSession()
+  const { signInMode } = useStorefrontConfig()
+  const loginRequiredForCart = signInMode === 'DISABLE_GUEST_ADD_TO_CART' && !loggedIn
 
-  return (
+  return loginRequiredForCart ? (
+    <Button
+      color='primary'
+      variant='pill'
+      size='large'
+      {...rest}
+      {...action}
+      onClick={() => router.push('/account/signin')}
+    >
+      {children || <Trans id='Sign in' />}
+    </Button>
+  ) : (
     <Button type='submit' color='primary' variant='pill' size='large' {...rest} {...action}>
       {children || <Trans id='Add to Cart' />}
     </Button>
