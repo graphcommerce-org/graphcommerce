e62a0346fa178b98b102664144bf06736b2b87eb
Paul Hachmang <paul@reachdigital.nl>
2024-07-02 15:05:15 +0200

    Remove hideEmpty

--- packages/magento-product/components/ProductFiltersPro/ProductFiltersProAggregations.tsx
+++ packages/magento-product/components/ProductFiltersPro/ProductFiltersProAggregations.tsx
@@ -15,7 +15,6 @@ export type FilterRenderer = Record<string, React.FC<FilterProps>>
 
 export type ProductFiltersProAggregationsProps = {
   renderer?: FilterRenderer
-  hideEmpty?: boolean
 }
 
 export const productFiltersProSectionRenderer = {
@@ -29,30 +28,30 @@ export const productFiltersProChipRenderer = {
 }
 
 export function ProductFiltersProAggregations(props: ProductFiltersProAggregationsProps) {
-  const { renderer, hideEmpty } = props
+  const { renderer } = props
   const { params, aggregations, appliedAggregations, filterTypes } = useProductFiltersPro()
 
   return (
     <>
-      {excludeCategory(
-        applyAggregationCount(aggregations, appliedAggregations, params, hideEmpty),
-      ).map((aggregation) => {
-        const filterType = filterTypes[aggregation.attribute_code]
-        if (!filterType) return null
+      {excludeCategory(applyAggregationCount(aggregations, appliedAggregations, params)).map(
+        (aggregation) => {
+          const filterType = filterTypes[aggregation.attribute_code]
+          if (!filterType) return null
 
-        const Component = renderer?.[filterType]
-        if (!Component) {
-          if (process.env.NODE_ENV === 'development') {
-            // eslint-disable-next-line no-console
-            console.log(
-              `The renderer for filterType ${filterType} can not be found, please add it to the renderer prop: renderer={{ ${filterType}: (props) => <>MYRenderer</> }}}}`,
-            )
+          const Component = renderer?.[filterType]
+          if (!Component) {
+            if (process.env.NODE_ENV === 'development') {
+              // eslint-disable-next-line no-console
+              console.log(
+                `The renderer for filterType ${filterType} can not be found, please add it to the renderer prop: renderer={{ ${filterType}: (props) => <>MYRenderer</> }}}}`,
+              )
+            }
+            return null
           }
-          return null
-        }
 
-        return <Component key={aggregation.attribute_code} aggregation={aggregation} {...props} />
-      })}
+          return <Component key={aggregation.attribute_code} aggregation={aggregation} {...props} />
+        },
+      )}
     </>
   )
 }
