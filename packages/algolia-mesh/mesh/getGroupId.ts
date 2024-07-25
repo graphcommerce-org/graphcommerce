0d7db68368ea7c7493f203780c109ec1f10b590f
Renzo van Hootegem <renzo@reachdigital.nl>
2024-07-25 16:06:08 +0200

    Correct getGroupId by using the right header.

--- packages/algolia-mesh/mesh/getGroupId.ts
+++ packages/algolia-mesh/mesh/getGroupId.ts
@@ -3,5 +3,5 @@ import { MeshContext } from '@graphcommerce/graphql-mesh'
 export function getGroupId(context: MeshContext): number {
   const { headers } = context as MeshContext & { headers?: Record<string, string | undefined> }
   if (!headers?.authorization) return 0
-  return parseInt(headers?.['x-magento-cache-id'] || '0', 10)
+  return parseInt(headers?.['x-magento-group-id'] || '0', 10)
 }
