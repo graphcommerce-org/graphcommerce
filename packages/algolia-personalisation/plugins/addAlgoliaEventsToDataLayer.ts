48677515aa4bb17fbefb58b34da839d497362f03
Renzo van Hootegem <renzo@reachdigital.nl>
2024-08-01 12:18:10 +0200

    Added add to cart algolia event

--- packages/algolia-personalisation/plugins/addAlgoliaEventsToDataLayer.ts
+++ packages/algolia-personalisation/plugins/addAlgoliaEventsToDataLayer.ts
@@ -1,8 +1,21 @@
 import { sendEvent as sendEventBase } from '@graphcommerce/google-datalayer/api/sendEvent'
 import type { FunctionPlugin, PluginConfig } from '@graphcommerce/next-config'
+import prepareAddToCartEvent from '../utils/prepareAddToCartEvent'
 
 export const config: PluginConfig = {
   module: '@graphcommerce/google-datalayer/api/sendEvent',
   type: 'function',
 }
-export const sendEvent: FunctionPlugin<typeof sendEventBase> = (prev, eventName, eventData) => {}
+
+const googleToAlgoliaEventCall = {
+  add_to_cart: prepareAddToCartEvent,
+}
+export const sendEvent: FunctionPlugin<typeof sendEventBase> = (prev, eventName, eventData) => {
+  console.log('check', eventName, eventData)
+  if (googleToAlgoliaEventCall[eventName]) {
+    googleToAlgoliaEventCall[eventName](eventData)
+  } else {
+    console.log('Event function not found.. skipping')
+  }
+  return prev(eventName, eventData)
+}
