113ed91149e3499f460280b2754ccd4072a7c950
Renzo van Hootegem <renzo@reachdigital.nl>
2024-08-06 12:23:54 +0200

    Removed cart currency bug

--- packages/google-datalayer/mapping/datalayerItemsToCurrencyValue/datalayerItemsToCurrencyValue.ts
+++ packages/google-datalayer/mapping/datalayerItemsToCurrencyValue/datalayerItemsToCurrencyValue.ts
@@ -9,7 +9,7 @@ export function datalayerItemsToCurrencyValue(
   items: GoogleDatalayerItem[],
 ): DataLayerCurrencyValue {
   return {
-    currency: items[0].currency ?? '',
+    currency: items[0]?.currency ?? '',
     value: items.reduce((acc, item) => acc + (item.price ?? 0) * item.quantity, 0),
   }
 }
