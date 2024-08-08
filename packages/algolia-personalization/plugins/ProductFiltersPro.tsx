45db55b0dccec81c820ac04741b5d8962047df55
Renzo van Hootegem <renzvh@kpnplanet.nl>
2024-08-08 14:34:06 +0200

    Send event when filters are selected

--- packages/algolia-personalization/plugins/ProductFiltersPro.tsx
+++ packages/algolia-personalization/plugins/ProductFiltersPro.tsx
@@ -1,4 +1,4 @@
-import { sendEvent } from '@graphcommerce/google-datalayer'
+import { useSendEvent } from '@graphcommerce/google-datalayer/api/sendEvent'
 import { ProductFilterParams } from '@graphcommerce/magento-product'
 import type { FilterFormProviderProps } from '@graphcommerce/magento-product/components/ProductFiltersPro'
 import type { PluginProps, PluginConfig } from '@graphcommerce/next-config'
@@ -11,15 +11,19 @@ export const config: PluginConfig = {
 
 export function ProductFiltersPro(props: PluginProps<FilterFormProviderProps>) {
   const { Prev, handleSubmit: handleSubmitBase, ...rest } = props
-
+  console.log('check dat ik hier kom')
+  const sendEvent = useSendEvent()
   const handleSubmit = async (
     formValues: ProductFilterParams,
     next: (shallow?: boolean, replace?: boolean) => Promise<void>,
   ) => {
     await handleSubmitBase?.(formValues, next)
-    console.log('check dat ik hier kom')
-
-    sendEvent('select_content', { content_id: 'filters', data: formValues })
+    console.log('AAAAA^Åæ', formValues)
+    sendEvent('select_content', {
+      content_id: 'filters',
+      data: formValues,
+      content_type: 'filters',
+    })
   }
 
   return <Prev handleSubmit={handleSubmit} {...rest} />
