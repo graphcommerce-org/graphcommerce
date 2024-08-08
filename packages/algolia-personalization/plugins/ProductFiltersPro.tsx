35c575befffb79906bc7dcc64cb97990b72a3e12
Renzo van Hootegem <renzvh@kpnplanet.nl>
2024-08-08 14:40:00 +0200

    Corrected plugin so sendevent gets activated

--- packages/algolia-personalization/plugins/ProductFiltersPro.tsx
+++ packages/algolia-personalization/plugins/ProductFiltersPro.tsx
@@ -2,7 +2,6 @@ import { useSendEvent } from '@graphcommerce/google-datalayer/api/sendEvent'
 import { ProductFilterParams } from '@graphcommerce/magento-product'
 import type { FilterFormProviderProps } from '@graphcommerce/magento-product/components/ProductFiltersPro'
 import type { PluginProps, PluginConfig } from '@graphcommerce/next-config'
-import React from 'react'
 
 export const config: PluginConfig = {
   type: 'component',
@@ -11,14 +10,12 @@ export const config: PluginConfig = {
 
 export function ProductFiltersPro(props: PluginProps<FilterFormProviderProps>) {
   const { Prev, handleSubmit: handleSubmitBase, ...rest } = props
-  console.log('check dat ik hier kom')
   const sendEvent = useSendEvent()
   const handleSubmit = async (
     formValues: ProductFilterParams,
     next: (shallow?: boolean, replace?: boolean) => Promise<void>,
   ) => {
     await handleSubmitBase?.(formValues, next)
-    console.log('AAAAA^Åæ', formValues)
     sendEvent('select_content', {
       content_id: 'filters',
       data: formValues,
