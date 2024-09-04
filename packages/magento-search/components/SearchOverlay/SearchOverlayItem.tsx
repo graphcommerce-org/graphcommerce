f94c098b011919b9b5d579cdea4493d8ad921701
Paul Hachmang <paul@reachdigital.nl>
2024-09-04 16:07:53 +0200

    Reduced queries, wider screen, 4 products, close overlay

--- packages/magento-search/components/SearchOverlay/SearchOverlayItem.tsx
+++ packages/magento-search/components/SearchOverlay/SearchOverlayItem.tsx
@@ -1,5 +1,5 @@
 import { alpha, ListItemButton, ListItemButtonProps } from '@mui/material'
-import { forwardRef, ElementType } from 'react'
+import { forwardRef, ElementType, memo } from 'react'
 import { useSearchItem } from './SearchOverlayProvider'
 
 type SearchOverlayItemProps<C extends ElementType = 'li'> = ListItemButtonProps<
@@ -7,37 +7,39 @@ type SearchOverlayItemProps<C extends ElementType = 'li'> = ListItemButtonProps<
   { component?: C }
 >
 
-export const SearchOverlayItem = forwardRef(
-  <C extends ElementType = 'li'>(
-    props: SearchOverlayItemProps<C>,
-    rootRef: React.Ref<C extends 'li' ? HTMLLIElement : HTMLAnchorElement>,
-  ) => {
-    const { sx, component, ...rest } = props
+export const SearchOverlayItem = memo(
+  forwardRef(
+    <C extends ElementType = 'li'>(
+      props: SearchOverlayItemProps<C>,
+      rootRef: React.Ref<C extends 'li' ? HTMLLIElement : HTMLAnchorElement>,
+    ) => {
+      const { sx, component, ...rest } = props
 
-    const { getRootProps } = useSearchItem({ rootRef })
+      const { getRootProps } = useSearchItem({ rootRef })
 
-    return (
-      <ListItemButton
-        {...getRootProps()}
-        component={component}
-        sx={[
-          (theme) => ({
-            px: theme.page.horizontal,
-            mx: `calc(${theme.page.horizontal} * -1)`,
-            '&.Mui-selected': {
-              boxShadow: `inset 0 0 0 2px ${theme.palette.secondary.main}`,
-              backgroundColor: alpha(
-                theme.palette.background.paper,
-                theme.palette.action.selectedOpacity,
-              ),
-            },
-          }),
-          ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-        {...rest}
-      />
-    )
-  },
+      return (
+        <ListItemButton
+          {...getRootProps()}
+          component={component}
+          sx={[
+            (theme) => ({
+              px: theme.page.horizontal,
+              mx: `calc(${theme.page.horizontal} * -1)`,
+              '&.Mui-selected': {
+                boxShadow: `inset 0 0 0 2px ${theme.palette.secondary.main}`,
+                backgroundColor: alpha(
+                  theme.palette.background.paper,
+                  theme.palette.action.selectedOpacity,
+                ),
+              },
+            }),
+            ...(Array.isArray(sx) ? sx : [sx]),
+          ]}
+          {...rest}
+        />
+      )
+    },
+  ),
 ) as <C extends ElementType = 'li'>(
   props: SearchOverlayItemProps<C> & {
     ref?: React.Ref<C extends 'li' ? HTMLLIElement : HTMLAnchorElement>
