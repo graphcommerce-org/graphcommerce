9b18c5f09105df439544a8291895fbf8c82a9452
Paul Hachmang <paul@reachdigital.nl>
2024-09-11 11:42:44 +0200

    Created algolia_looking_similar, algolia_frequently_bought_together and algolia_related_products fields on products

--- packages/algolia-recommend/mesh/resolvers.ts
+++ packages/algolia-recommend/mesh/resolvers.ts
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/require-await */
 import fragments from '@graphcommerce/graphql/generated/fragments.json'
 import type {
+  AlgoliaLookingSimilarInput,
   MeshContext,
   ProductInterfaceResolvers,
+  RequireFields,
   ResolverFn,
   Resolvers,
   ResolversParentTypes,
   ResolversTypes,
+  SimpleProductalgolia_looking_similarArgs,
+  VirtualProductalgolia_looking_similarArgs,
 } from '@graphcommerce/graphql-mesh'
 import {
   GraphCommerceAlgoliaRecommendationLocation,
@@ -18,7 +22,7 @@ import { createFacetValueMapper } from './createValueFacetMapper'
 import { getRecommendations } from './getRecommendations'
 
 type ProductTypes = NonNullable<Awaited<ReturnType<ProductInterfaceResolvers['__resolveType']>>>
-const productTypes = fragments.possibleTypes.ProductInterface as ProductTypes[]
+const productInterfaceTypes = fragments.possibleTypes.ProductInterface as ProductTypes[]
 
 const resolvers: Resolvers = {
   Query: {
@@ -62,61 +66,106 @@ type ProductResolver = ResolverFn<
 >
 
 if (isEnabled(import.meta.graphCommerce.algolia.relatedProducts)) {
-  const resolver: ProductResolver = async (root, args, context, info) => {
-    if (!root.uid) return null
-    return getRecommendations(
+  const resolve: ProductResolver = async (root, args, context, info) =>
+    getRecommendations(
       'Related_products_Input',
       { objectID: atob(root.uid), threshold: 75 },
       context,
       info,
       await createProductMapper(context),
     )
-  }
 
-  productTypes.forEach((productType) => {
+  productInterfaceTypes.forEach((productType) => {
     if (!resolvers[productType]) resolvers[productType] = {}
-    resolvers[productType][enumToLocation(import.meta.graphCommerce.algolia.relatedProducts)] =
-      resolver
+    resolvers[productType][enumToLocation(import.meta.graphCommerce.algolia.relatedProducts)] = {
+      selectionSet: `{ uid }`,
+      resolve,
+    }
   })
 }
 
 if (isEnabled(import.meta.graphCommerce.algolia.lookingSimilar)) {
-  const resolver: ProductResolver = async (root, args, context, info) => {
-    if (!root.uid) return null
-    return getRecommendations(
+  const resolve: ProductResolver = async (root, args, context, info) =>
+    getRecommendations(
       'Looking_similar_Input',
       { objectID: atob(root.uid), threshold: 75 },
       context,
       info,
       await createProductMapper(context),
     )
-  }
 
-  productTypes.forEach((productType) => {
+  productInterfaceTypes.forEach((productType) => {
     if (!resolvers[productType]) resolvers[productType] = {}
-    resolvers[productType][enumToLocation(import.meta.graphCommerce.algolia.lookingSimilar)] =
-      resolver
+    resolvers[productType][enumToLocation(import.meta.graphCommerce.algolia.lookingSimilar)] = {
+      selectionSet: `{ uid }`,
+      resolve,
+    }
   })
 }
 
 if (isEnabled(import.meta.graphCommerce.algolia.frequentlyBoughtTogether)) {
-  const resolver: ProductResolver = async (root, args, context, info) => {
-    if (!root.uid) return null
-    return getRecommendations(
+  const resolver: ProductResolver = async (root, args, context, info) =>
+    getRecommendations(
       'Frequently_bought_together_Input',
       { objectID: atob(root.uid), threshold: 75 },
       context,
       info,
       await createProductMapper(context),
     )
-  }
 
-  productTypes.forEach((productType) => {
+  productInterfaceTypes.forEach((productType) => {
     if (!resolvers[productType]) resolvers[productType] = {}
     resolvers[productType][
       enumToLocation(import.meta.graphCommerce.algolia.frequentlyBoughtTogether)
     ] = resolver
   })
 }
+
+type AlgoliaRecommendationResolver = ResolverFn<
+  Maybe<Array<Maybe<ResolversTypes['ProductInterface']>>>,
+  ResolversParentTypes['ProductInterface'],
+  MeshContext,
+  { input?: AlgoliaLookingSimilarInput | null }
+>
+
+const similar: AlgoliaRecommendationResolver = async (root, args, context, info) =>
+  getRecommendations(
+    'Looking_similar_Input',
+    { objectID: atob(root.uid), threshold: 75, maxRecommendations: 8, ...args.input },
+    context,
+    info,
+    await createProductMapper(context),
+  )
+
+const related: AlgoliaRecommendationResolver = async (root, args, context, info) =>
+  getRecommendations(
+    'Related_products_Input',
+    { objectID: atob(root.uid), threshold: 75, maxRecommendations: 8, ...args.input },
+    context,
+    info,
+    await createProductMapper(context),
+  )
+
+const together: AlgoliaRecommendationResolver = async (root, args, context, info) =>
+  getRecommendations(
+    'Frequently_bought_together_Input',
+    { objectID: atob(root.uid), threshold: 75, maxRecommendations: 8, ...args.input },
+    context,
+    info,
+    await createProductMapper(context),
+  )
+
+productInterfaceTypes.forEach((productType) => {
+  if (!resolvers[productType]) resolvers[productType] = {}
+
+  resolvers[productType].algolia_looking_similar = { selectionSet: `{ uid }`, resolve: similar }
+
+  resolvers[productType].algolia_related_products = { selectionSet: `{ uid }`, resolve: related }
+
+  resolvers[productType].algolia_frequently_bought_together = {
+    selectionSet: `{ uid }`,
+    resolve: together,
+  }
+})
 
 export default resolvers
