d2c7829339859814abcb8226628f2f001e3fca66
Carlo Carels <carlo@reachdigital.nl>
2024-11-01 14:15:18 +0100

    chore(GCOM-1512): use correct ‘up’ data on the blogoverview page

--- examples/magento-graphcms/pages/blog/page/[page].tsx
+++ examples/magento-graphcms/pages/blog/page/[page].tsx
@@ -126,7 +126,7 @@ export const getStaticProps: GetPageStaticProps = async (context) => {
       ...(await blogPaths).data,
       ...(await layout).data,
       urlEntity: { relative_url: `blog` },
-      up: { href: '/blog', title: i18n._(/* i18n */ 'Blog') },
+      up: { href: '/', title: i18n._(/* i18n */ 'Back') },
       apolloState: await conf.then(() => client.cache.extract()),
     },
     revalidate: 60 * 20,
