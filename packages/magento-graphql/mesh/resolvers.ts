651eea0bdda1ed0f46f4c73d7edf52c8c1da5b54
Paul Hachmang <paul@reachdigital.nl>
2024-11-15 14:01:09 +0100

    Created a new field for products: `custom_attribute_option(attribute_code: "attribute_code")` to retrieve attribute option value labels. This field is only available in Magento 2.4.7 and up.

---
+++ packages/magento-graphql/mesh/resolvers.ts
@@ -0,0 +1,133 @@
+import fragments from '@graphcommerce/graphql/generated/fragments.json'
+import type {
+  AttributeInput,
+  CustomAttributeOptionOutput,
+  MeshContext,
+  ProductInterfaceResolvers,
+  Resolvers,
+  ResolversTypes,
+} from '@graphcommerce/graphql-mesh'
+import { filterNonNullableKeys } from '@graphcommerce/next-ui'
+import { print, Kind, SelectionSetNode, StringValueNode } from 'graphql'
+
+type CustomAttributeInput = { attribute_code: string; entity_type: 'catalog_product' }
+
+async function customAttributeMetadataV2(input: CustomAttributeInput, context: MeshContext) {
+  const cacheKey = `customAttributeMetadata-${input.entity_type}-${input.attribute_code}`
+  const cached = await context.cache.get(cacheKey)
+  if (cached) return cached
+
+  if (input.entity_type !== 'catalog_product')
+    throw Error('Only catalog_product is supported at this moment')
+
+  const response = context.m2.Query.customAttributeMetadataV2({
+    context,
+    key: input,
+    argsFromKeys: (attributes) => ({ attributes }),
+    selectionSet: /* GraphQL */ `
+      {
+        items {
+          __typename
+          code
+          label
+          default_value
+          entity_type
+          frontend_class
+          frontend_input
+          is_required
+          is_unique
+          label
+          ... on CatalogAttributeMetadata {
+            apply_to
+            is_comparable
+            is_filterable
+            is_filterable_in_search
+            is_html_allowed_on_front
+            is_searchable
+            is_used_for_price_rules
+            is_used_for_promo_rules
+            is_visible_in_advanced_search
+            is_visible_on_front
+            is_wysiwyg_enabled
+            used_in_product_listing
+          }
+          options {
+            label
+            is_default
+            value
+          }
+        }
+      }
+    `,
+    valuesFromResults: (values, attributes) => {
+      const all = filterNonNullableKeys(values.items)
+      return attributes.map((attribute) => all.find((v) => v.code === attribute.attribute_code))
+    },
+  })
+
+  // Cache for 1 hour
+  await context.cache.set(cacheKey, response, { ttl: 60 * 60 })
+  return response
+}
+
+type ProductResolver = Pick<ProductInterfaceResolvers<MeshContext>, 'custom_attribute_option'>
+
+const productResolver: ProductResolver = {
+  custom_attribute_option: {
+    selectionSet: (fieldNode) => {
+      return {
+        kind: Kind.SELECTION_SET,
+        selections: (fieldNode.arguments ?? [])
+          .map((arg) => arg.value)
+          .filter((value) => value.kind === Kind.STRING)
+          .map((value) => ({ kind: Kind.FIELD, name: { kind: Kind.NAME, value: value.value } })),
+      }
+    },
+    resolve: async (root, args, context) => {
+      if (import.meta.graphCommerce.magentoVersion <= 246) {
+        throw Error('This field is only available in Magento 2.4.7 and up')
+      }
+
+      type Result = ResolversTypes['CustomAttributeOptionOutput']
+      const result: Result = { raw: `${root[args.code]}`, options: [], attribute: null, errors: [] }
+
+      const values = result.raw.includes(',')
+        ? [result.raw, ...result.raw.split(',')]
+        : [result.raw]
+      const attribute = await customAttributeMetadataV2(
+        { attribute_code: args.code, entity_type: 'catalog_product' },
+        context,
+      )
+
+      result.attribute = attribute
+
+      if (!attribute) {
+        const message = `Attribute '${args.code}' found, but option ${values.join(', ')} not found`
+        result.errors.push({ message, type: 'ATTRIBUTE_NOT_FOUND' })
+        return result
+      }
+
+      values.forEach((v) => {
+        const found = attribute.options?.find((o) => o?.value === v)
+        if (found) {
+          result.options?.push(found)
+        } else {
+          const message = `Option '${v}' not found for attribute '${args.code}'`
+          result.errors.push({ message, type: 'ATTRIBUTE_NOT_FOUND' })
+        }
+      })
+      return result
+    },
+  },
+}
+
+const resolvers: Resolvers = {}
+
+type ProductTypes = NonNullable<Awaited<ReturnType<ProductInterfaceResolvers['__resolveType']>>>
+const productInterfaceTypes = fragments.possibleTypes.ProductInterface as ProductTypes[]
+
+productInterfaceTypes.forEach((productType) => {
+  if (!resolvers[productType]) resolvers[productType] = productResolver
+})
+
+export default resolvers
---
+++ packages/magento-graphql/plugins/meshConfigAttrValue.ts
@@ -0,0 +1,23 @@
+import type { meshConfig as meshConfigBase } from '@graphcommerce/graphql-mesh/meshConfig'
+import type { FunctionPlugin, PluginConfig } from '@graphcommerce/next-config'
+
+export const config: PluginConfig = {
+  module: '@graphcommerce/graphql-mesh/meshConfig',
+  type: 'function',
+}
+
+export const meshConfig: FunctionPlugin<typeof meshConfigBase> = (
+  prev,
+  baseConfig,
+  graphCommerceConfig,
+) =>
+  prev(
+    {
+      ...baseConfig,
+      additionalResolvers: [
+        ...(baseConfig.additionalResolvers ?? []),
+        '@graphcommerce/magento-graphql/mesh/resolvers.ts',
+      ],
+    },
+    graphCommerceConfig,
+  )
