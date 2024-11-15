863fe6d5124e267a0fd7c12698e4ea28057258a4
Renzo van Hootegem <renzvh@kpnplanet.nl>
2024-11-15 11:50:52 +0100

    Added all klarna payment methods

--- packages/magento-payment-klarna/plugins/AddKlarnaMethods.tsx
+++ packages/magento-payment-klarna/plugins/AddKlarnaMethods.tsx
@@ -1,21 +1,14 @@
 import { PaymentMethodContextProviderProps } from '@graphcommerce/magento-cart-payment-method'
 import type { PluginProps } from '@graphcommerce/next-config'
-import { KlarnaPaymentActionCard } from '../components/KlarnaPaymentActionCard/KlarnaPaymentActionCard'
-import { KlarnaPaymentOptions } from '../components/KlarnaPaymentOptions/KlarnaPaymentOptions'
-import { KlarnaPaymentPlaceOrder } from '../components/KlarnaPaymentPlaceOrder/KlarnaPaymentPlaceOrder'
+
+import { klarna } from '../methods'
 
 export const component = 'PaymentMethodContextProvider'
 export const exported = '@graphcommerce/magento-cart-payment-method'
 
-const klarna_pay_later = {
-  PaymentOptions: KlarnaPaymentOptions,
-  PaymentActionCard: KlarnaPaymentActionCard,
-  PaymentPlaceOrder: KlarnaPaymentPlaceOrder,
-}
-
 function AddKlarnaMethods(props: PluginProps<PaymentMethodContextProviderProps>) {
   const { modules, Prev, ...rest } = props
-  return <Prev {...rest} modules={{ ...modules, klarna_pay_later }} />
+  return <Prev {...rest} modules={{ ...modules, ...klarna }} />
 }
 
 export const Plugin = AddKlarnaMethods
