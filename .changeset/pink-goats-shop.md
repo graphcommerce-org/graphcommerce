66b418809ad4db2a465c4112ff9850ce0cfe611c
Carlo Carels <carlo@reachdigital.nl>
2024-11-20 14:13:29 +0100

    chore: Omit disableUnderline prop for ‘outlined’ variant, because its not supported.
    The `disableUnderline` prop is only available in the `Input` and `FilledInput` variants, not in `OutlinedInput`.
    
    See docs for reference:
    - [FilledInput (filled)](https://mui.com/material-ui/api/filled-input/#props)
    - [OutlinedInput (outlined)](https://mui.com/material-ui/api/outlined-input/#props)
    - [Input (standard)](https://mui.com/material-ui/api/input/#props)

---
+++ .changeset/pink-goats-shop.md
@@ -0,0 +1,5 @@
+---
+'@graphcommerce/ecommerce-ui': patch
+---
+
+Omit disableUnderline prop for ‘outlined’ variant, because its not supported.
--- packages/ecommerce-ui/components/FormComponents/NumberFieldElement.tsx
+++ packages/ecommerce-ui/components/FormComponents/NumberFieldElement.tsx
@@ -52,6 +52,13 @@ export function NumberFieldElement<T extends FieldValues>(props: NumberFieldElem
 
   const classes = withState({ size })
 
+  let InputPropsFiltered = InputProps
+
+  if (variant === 'outlined' && 'disableUnderline' in InputPropsFiltered) {
+    const { disableUnderline, ...filteredInputProps } = InputPropsFiltered
+    InputPropsFiltered = filteredInputProps
+  }
+
   if (required && !rules.required) {
     rules.required = i18n._(/* i18n */ 'This field is required')
   }
@@ -114,7 +121,7 @@ export function NumberFieldElement<T extends FieldValues>(props: NumberFieldElem
       ]}
       autoComplete='off'
       InputProps={{
-        ...InputProps,
+        ...InputPropsFiltered,
         startAdornment: (
           <Fab
             aria-label={i18n._(/* i18n */ 'Decrease')}
