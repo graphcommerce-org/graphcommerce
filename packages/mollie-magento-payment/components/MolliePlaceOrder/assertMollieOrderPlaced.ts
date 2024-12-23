dc70489c82aaf532bed7a6a500de8eae988e3773
Paul Hachmang <paul@reachdigital.nl>
2024-12-23 11:56:19 +0100

    Support Magento 2.4.7 placeOrder.errors field when placing a Mollie order

---
+++ packages/mollie-magento-payment/components/MolliePlaceOrder/assertMollieOrderPlaced.ts
@@ -0,0 +1,37 @@
+import { ApolloError, type FetchResult } from '@graphcommerce/graphql'
+import type { AssertedOrderPlaced, PlacedOrder } from '@graphcommerce/magento-cart-payment-method'
+import { assertOrderPlaced } from '@graphcommerce/magento-cart-payment-method'
+import { t } from '@lingui/macro'
+import type { MolliePlaceOrderMutation } from './MolliePlaceOrder.gql'
+
+/** Assert that the order was place successfully. */
+export function assertMollieOrderPlaced<T extends FetchResult<MolliePlaceOrderMutation>>(
+  result: T,
+): asserts result is AssertedOrderPlaced<T> & {
+  data: {
+    placeOrder: {
+      order: PlacedOrder<T> & {
+        mollie_redirect_url: NonNullable<PlacedOrder<T>['mollie_redirect_url']>
+        mollie_payment_token: NonNullable<PlacedOrder<T>['mollie_payment_token']>
+      }
+    }
+  }
+} {
+  assertOrderPlaced(result)
+
+  const { mollie_redirect_url, mollie_payment_token } = result.data.placeOrder.order
+
+  if (!mollie_redirect_url || !mollie_payment_token) {
+    console.error(
+      'Mollie: Order was placed, but no redirect url or payment token was returned, this is an issue on the Magento Mollie side.',
+      result,
+    )
+    throw new ApolloError({
+      graphQLErrors: [
+        {
+          message: t`An error occurred while processing your payment. Please contact the store owner`,
+        },
+      ],
+    })
+  }
+}
