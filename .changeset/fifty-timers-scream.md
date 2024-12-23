058b7bf64be29e1bc3d16551abcc1cd55b5413bd
Paul Hachmang <paul@reachdigital.nl>
2024-12-23 11:52:53 +0100

    Support Magento 2.4.7 placeOrder.errors field to handle possible errors while placing the order. An `assertOrderPlaced` method was created to assert a valid placed order.

---
+++ .changeset/fifty-timers-scream.md
@@ -0,0 +1,6 @@
+---
+'@graphcommerce/magento-cart-payment-method': patch
+'@graphcommerce/magento-graphql': patch
+---
+
+Support Magento 2.4.7 placeOrder.errors field to handle possible errors while placing the order. An `assertOrderPlaced` method was created to assert a valid placed order.
---
+++ packages/magento-cart-payment-method/PaymentMethodPlaceOrderNoop/assertOrderPlaced.ts
@@ -0,0 +1,40 @@
+import type { FetchResult } from '@graphcommerce/graphql'
+import { ApolloError } from '@graphcommerce/graphql'
+import { t } from '@lingui/macro'
+import type { PaymentMethodPlaceOrderNoopMutation } from './PaymentMethodPlaceOrderNoop.gql'
+
+export type PlacedOrder<T extends FetchResult<PaymentMethodPlaceOrderNoopMutation>> = NonNullable<
+  NonNullable<NonNullable<T['data']>['placeOrder']>['order']
+>
+
+export type AssertedOrderPlaced<T extends FetchResult<PaymentMethodPlaceOrderNoopMutation>> = T & {
+  data: {
+    placeOrder: { order: PlacedOrder<T> }
+  }
+}
+
+/** Assert that the order was place successfully. */
+export function assertOrderPlaced<T extends FetchResult<PaymentMethodPlaceOrderNoopMutation>>(
+  result: T,
+): asserts result is AssertedOrderPlaced<T> {
+  if (result.errors && result.errors.length > 0) {
+    const graphQLErrors = result.errors.filter((e) => e !== null)
+    throw new ApolloError({ graphQLErrors })
+  }
+
+  if (result.data?.placeOrder?.errors && result.data.placeOrder.errors.length > 0) {
+    const graphQLErrors = result.data.placeOrder.errors.filter((e) => e !== null)
+    throw new ApolloError({ graphQLErrors })
+  }
+
+  if (!result.data?.placeOrder?.order?.order_number) {
+    console.info('Error while placing order', result)
+    throw new ApolloError({
+      graphQLErrors: [
+        {
+          message: t`An error occurred while processing your payment. Please contact the store owner`,
+        },
+      ],
+    })
+  }
+}
