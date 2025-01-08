87df248a7154eed415da935d33f3cc6e48159ec9
Paul Hachmang <paul@reachdigital.nl>
2025-01-08 14:37:49 +0100

    Solve issue where plurals weren't properly defined

---
+++ .changeset/wicked-carpets-tie.md
@@ -0,0 +1,6 @@
+---
+'@graphcommerce/magento-open-source': patch
+'@graphcommerce/misc': patch
+---
+
+Solve issue where plurals weren't properly defined
--- packages/next-ui/utils/sxx.ts
+++ packages/next-ui/utils/sxx.ts
@@ -9,7 +9,9 @@ import type { SxProps, Theme } from '@mui/material'
  * sxx({ position: 'absolute', right: 0, top: 0 }, props.sx)
  * ```
  */
-export const sxx = (...sxPropsArray: (SxProps<Theme> | undefined | false)[]): SxProps<Theme> =>
+export const sxx = (
+  ...sxPropsArray: (SxProps<Theme> | undefined | null | false)[]
+): SxProps<Theme> =>
   sxPropsArray
     .filter((v) => !!v)
     .map((sx) => (Array.isArray(sx) ? sx : [sx]))
