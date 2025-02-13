798c12271e90cf0e02f021e7ad4aa088b4c7c2a3
Paul Hachmang <paul@reachdigital.nl>
2025-02-13 11:14:20 +0100

    Use the `storeConfig.head_shortcut_icon` when configured, if not configured it will use the favicon.ico and favicon.svg from the public folder.

--- packages/magento-store/components/GlobalHead/GlobalHead.tsx
+++ packages/magento-store/components/GlobalHead/GlobalHead.tsx
@@ -1,11 +1,38 @@
 import { useQuery } from '@graphcommerce/graphql'
 import type { GlobalHeadProps as GlobalHeadPropsBase } from '@graphcommerce/next-ui'
 import { GlobalHead as GlobalHeadBase } from '@graphcommerce/next-ui'
-import { StoreConfigDocument } from '../../queries/StoreConfig.gql'
+import { getImageProps } from 'next/image'
+import React from 'react'
+import { StoreConfigDocument } from '../../graphql'
 
-export type GlobalHeadProps = Omit<GlobalHeadPropsBase, 'name'>
+export type GlobalHeadProps = Omit<GlobalHeadPropsBase, 'name'> & { disableIcon?: boolean }
 
-export function GlobalHead(props: GlobalHeadProps) {
+export const GlobalHead = React.memo<GlobalHeadProps>((props) => {
+  const { children, disableIcon = false } = props
   const name = useQuery(StoreConfigDocument).data?.storeConfig?.website_name ?? ''
-  return <GlobalHeadBase name={name} {...props} />
-}
+
+  const { head_shortcut_icon, secure_base_media_url } =
+    useQuery(StoreConfigDocument).data?.storeConfig ?? {}
+
+  let icon: string | undefined
+  if (head_shortcut_icon && secure_base_media_url) {
+    icon = getImageProps({
+      src: `${secure_base_media_url}favicon/${head_shortcut_icon}`,
+      alt: 'favicon',
+      width: 16,
+      height: 16,
+    }).props.src
+  }
+
+  return (
+    <GlobalHeadBase name={name} {...props}>
+      {!disableIcon && (
+        <>
+          <link rel='icon' href={icon ?? '/favicon.ico'} sizes='any' key='icon' />
+          {!icon && <link rel='icon' href='/favicon.svg' type='image/svg+xml' key='icon-svg' />}
+        </>
+      )}
+      {children}
+    </GlobalHeadBase>
+  )
+})
