e5226a209c789f585868728354badc9a1ecce3bb
Paul Hachmang <paul@reachdigital.nl>
2025-02-20 14:36:17 +0100

    Cleaned up PageMeta to be easier to work with fragments

--- packages/magento-store/components/PageMeta/PageMeta.tsx
+++ packages/magento-store/components/PageMeta/PageMeta.tsx
@@ -3,12 +3,14 @@ import type { PageMetaProps as NextPageMetaProps } from '@graphcommerce/next-ui'
 import { PageMeta as NextPageMeta } from '@graphcommerce/next-ui'
 import { StoreConfigDocument } from '../../graphql'
 
-export type PageMetaProps = Omit<NextPageMetaProps, 'canonical'> & {
-  canonical?: string
+export type PageMetaProps = Omit<NextPageMetaProps, 'metaDescription' | 'title' | 'canonical'> & {
+  metaDescription?: string | null
+  title?: string | null
+  canonical?: string | null
 }
 
 export function PageMeta(props: PageMetaProps) {
-  const { children, title, metaDescription, metaKeywords, ...pageMetaProps } = props
+  const { children, title, metaDescription, metaKeywords, canonical, ...pageMetaProps } = props
   const config = useQuery(StoreConfigDocument)
 
   const prefix = config.data?.storeConfig?.title_prefix ?? ''
@@ -23,9 +25,10 @@ export function PageMeta(props: PageMetaProps) {
 
   return (
     <NextPageMeta
-      title={pageTitle ?? ''}
+      title={pageTitle}
       metaDescription={metaDescription ?? config.data?.storeConfig?.default_description}
       metaKeywords={metaKeywords ?? config.data?.storeConfig?.default_keywords}
+      canonical={canonical ?? undefined}
       {...pageMetaProps}
     >
       {children}
