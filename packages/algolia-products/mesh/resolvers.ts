854e87fdba12eb738f1e954d6323f31728f2f95b
Paul Hachmang <paul@reachdigital.nl>
2025-05-02 12:05:59 +0200

    Moved Customer group_id resolver to magento-graphql-rest package where it should belong.

--- packages/algolia-products/mesh/resolvers.ts
+++ packages/algolia-products/mesh/resolvers.ts
@@ -101,23 +101,6 @@ export const resolvers: Resolvers = {
       return items
     },
   },
-  Customer: {
-    group_id: {
-      resolve: async (root, args, context, info) => {
-        const { headers } = context as MeshContext & {
-          headers?: Record<string, string | undefined>
-        }
-        if (!headers?.authorization) return 0
-        const customer = await context.m2rest.Query.m2rest_GetV1CustomersMe({
-          root,
-          context,
-          info,
-          autoSelectionSetWithDepth: 10,
-        })
-        return customer?.group_id ?? null
-      },
-    },
-  },
   Query: {
     products: async (root, args, context, info) => {
       const isAgolia = (args.filter?.engine?.in ?? [args.filter?.engine?.eq])[0] === 'algolia'
