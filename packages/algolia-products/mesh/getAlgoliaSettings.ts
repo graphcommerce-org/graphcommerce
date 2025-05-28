a026714ac0373b666de2cee43c3c5dba58cd81e4
Paul Hachmang <paul@reachdigital.nl>
2025-05-28 15:57:24 +0200

    Solve issue where used configurations might not be scoped to the correct store. Move caching to the mesh cache and scope per store.
    This solves odd issues like sort options not being available on certain stores.

--- packages/algolia-products/mesh/getAlgoliaSettings.ts
+++ packages/algolia-products/mesh/getAlgoliaSettings.ts
@@ -1,22 +1,31 @@
 import type { AlgoliasettingsResponse, MeshContext } from '@graphcommerce/graphql-mesh'
+import type { GraphQLError } from 'graphql'
 import { getIndexName } from './getIndexName'
 
-let settingsCache: AlgoliasettingsResponse | null = null
+function isGraphQLError(err: unknown): err is GraphQLError {
+  return !!(err as GraphQLError)?.message
+}
 
 export async function getAlgoliaSettings(context: MeshContext): Promise<AlgoliasettingsResponse> {
-  if (!settingsCache) {
-    settingsCache = await context.algolia.Query.algolia_getSettings({
-      args: { indexName: getIndexName(context) },
-      selectionSet: /* GraphQL */ `
-        {
-          replicas
-          attributesForFaceting
-        }
-      `,
-      context,
-    })
-  }
+  const cacheKey = `algolia_getSettings_${getIndexName(context)}`
+  const settingsCached = context.cache.get(cacheKey)
+  if (settingsCached) return settingsCached
 
-  if (!settingsCache) throw Error('No settings found')
-  return settingsCache
+  const settings = await context.algolia.Query.algolia_getSettings({
+    args: { indexName: getIndexName(context) },
+    selectionSet: /* GraphQL */ `
+      {
+        replicas
+        attributesForFaceting
+      }
+    `,
+    context,
+  })
+
+  if (isGraphQLError(settings)) throw settings
+  if (!settings) throw Error('No settings found')
+
+  await context.cache.set(cacheKey, settings)
+
+  return settings
 }
