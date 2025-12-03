e8eaa3f0eba397df8179f9492a7403cf29018660
Paul Hachmang <paul@reachdigital.nl>
2025-12-03 22:42:48 +0100

    fix(mui): fix Variant type import - use TypographyVariant from @mui/material/styles

--- packages/magento-product/components/ProductPageDescription/ProductPageDescription.tsx
+++ packages/magento-product/components/ProductPageDescription/ProductPageDescription.tsx
@@ -7,14 +7,14 @@ import {
 } from '@graphcommerce/next-ui'
 import type { SxProps, Theme } from '@mui/material'
 import { Box, Typography } from '@mui/material'
-import type { Variant } from '@mui/material/styles'
+import type { TypographyVariant } from '@mui/material/styles'
 import type { ProductListItemRenderer } from '../ProductListItems/renderer'
 import { ProductPageName } from '../ProductPageName'
 import type { ProductPageDescriptionFragment } from './ProductPageDescription.gql'
 
 export type ProductPageDescriptionProps = Omit<ColumnTwoWithTopProps, 'top' | 'left'> & {
   sx?: SxProps<Theme>
-  fontSize?: 'responsive' | Variant
+  fontSize?: 'responsive' | TypographyVariant
   product: ProductPageDescriptionFragment
   productListRenderer: ProductListItemRenderer
 }
