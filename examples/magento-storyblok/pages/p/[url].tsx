2fb83759f2e08efc4fddb820dea7a80b7437d774
Bram van der Holst <bram@reachdigital.nl>
2026-04-17 12:18:27 +0200

    Fix product-global slug
    Slashes aren’t valid in Story slugs

--- examples/magento-storyblok/pages/p/[url].tsx
+++ examples/magento-storyblok/pages/p/[url].tsx
@@ -265,7 +265,7 @@ export const getStaticProps: GetPageStaticProps = async (context) => {
     fetchPolicy: cacheFirst(staticClient),
   })
 
-  const storyPage = fetchStory('product/global', context, staticClient)
+  const storyPage = fetchStory('product-global', context, staticClient)
   const globalConfig = fetchGlobalConfig(context)
 
   const product = productPage.then((pp) => pp.products?.items?.find((p) => p?.url_key === urlKey))
