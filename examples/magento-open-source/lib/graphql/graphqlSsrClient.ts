06082ad47a59217fbdfb24f1a60411e4d0eecd9b
Bram van der Holst <bram@reachdigital.nl>
2026-05-28 09:57:37 +0200

    Make GraphCommerce compatible with Apollo Client 4.2+.

--- examples/magento-open-source/lib/graphql/graphqlSsrClient.ts
+++ examples/magento-open-source/lib/graphql/graphqlSsrClient.ts
@@ -36,7 +36,7 @@ function client(context: GetStaticPropsContext, fetchPolicy: FetchPolicy = 'no-c
     defaultOptions: {
       preview: context as PreviewConfig,
       query: { errorPolicy: 'all', fetchPolicy },
-    } as ApolloClient.DefaultOptions,
+    },
   })
 }
 
